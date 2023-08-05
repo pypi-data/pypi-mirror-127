585fcbaf1a2f4983b41690a46a025931ad30669c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:10 +0000

    Add package sweetrpg-web-core/0.0.12/sweetrpg-web-core-0.0.12.tar.gz

---
+++ packages/sweetrpg-web-core/sweetrpg-web-core-0.0.12.tar.gz/sweetrpg-web-core-0.0.12/src/sweetrpg_web_core/constants.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""Constants.
+"""
+
+
+SESSION_ACCESS_TOKEN = "access_token"
+SESSION_EMAIL = "email"
+SESSION_USER_ID = "user_id"
+
+BUILD_INFO_PATH = "BUILD_INFO_PATH"
+APPLICATION_BASE_PATH = "APPLICATION_BASE_PATH"
+STATIC_ASSET_PREFIX = "STATIC_ASSET_PREFIX"
