ac3f5cf2b4487a7d200a43f70f2c9b9034d57e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:44 +0000

    Add package nanamilang/0.3.8/nanamilang-0.3.8-py3-none-any.whl

---
+++ packages/nanamilang/nanamilang-0.3.8-py3-none-any.whl/nanamilang-0.3.8.data/scripts/nanamilang-repl.py
@@ -0,0 +1,125 @@
+#!python
+
+# This file is a part of NanamiLang Project
+# This project licensed under GNU GPL version 2
+# Initially made by @jedi2light (aka Stoian Minaiev)
+
+"""NanamiLang REPL"""
+
+import os
+import sys
+import atexit
+import readline
+import argparse
+import traceback
+from typing import List, Any
+from nanamilang import Program
+from nanamilang import datatypes
+from nanamilang.builtin import Builtin, BuiltinMacro
+from nanamilang import __version_string__, __author__
+
+history_file_path = os.path.join(
+    os.path.expanduser("~"), ".nanamilang_history")
+try:
+    readline.read_history_file(history_file_path)
+    readline.set_history_length(1000)
+except FileNotFoundError:
+    pass
+
+nml_stdlib_path = os.path.join(
+        os.environ.get('XDG_DATA_HOME')
+        or os.path.join(os.environ.get('HOME'), '.local'),
+        'nanamilang/stdlib.nml')
+
+atexit.register(readline.write_history_file, history_file_path)
+
+readline.parse_and_bind("tab: complete")
+
+
+def complete(t: str, s: int):
+    """NanamiLang REPL complete() function for GNU readline"""
+    vocabulary = Builtin.completions() + BuiltinMacro.completions()
+    results: List[Any] = [x for x in vocabulary if x.startswith(t)] + [None]
+    return results[s]
+
+
+readline.set_completer(complete)
+
+
+def main():
+    """NanamiLang REPL Main function"""
+
+    parser = argparse.ArgumentParser('NanamiLang REPL')
+    parser.add_argument('--no-greeting',
+                        help='Greeting can be disabled',
+                        action='store_true', default=False)
+    parser.add_argument('--dumptree',
+                        help='Dump tree each time',
+                        action='store_true', default=False)
+    parser.add_argument('--print-exception',
+                        help='Call traceback.print_exc()',
+                        action='store_true', default=False)
+
+    args = parser.parse_args()
+
+    p_ver = '.'.join([str(sys.version_info.major),
+                      str(sys.version_info.minor),
+                      str(sys.version_info.micro)])
+
+    print('NanamiLang', __version_string__, 'by', __author__, 'on Python', p_ver)
+    if not args.no_greeting:
+        print('Type (doc function-or-macro) to see function-or-macro sample doc')
+        print('Type (exit!) or press "Control+D" / "Control+C" to exit the REPL')
+        print('History has been read and will be appended to', history_file_path)
+
+    Builtin.install(
+        {
+            'name': 'exit!', 'type': 'function',
+            'sample': '(exit!)', 'docstring': 'Exit NanamiLang REPL'
+        },
+        lambda _: sys.exit(0)
+    )
+
+    # Load NanamiLang Standard Library
+
+    if not os.path.exists(nml_stdlib_path):
+        print('Unable to continue, NanamiLang Standard Library is missing')
+        print('If you are using the latest version of NanamiLang, consider')
+        print('running: "nanamilang-mngr.py --populate" and then try again')
+
+    with open(nml_stdlib_path, 'r', encoding='utf-8') as handler:
+        Program(handler.read()).evaluate()
+
+    while True:
+        try:
+            inp = input("USER> ")
+            # Skip evaluating in case of empty string
+            if not inp:
+                continue
+            try:
+                p = Program(inp)
+                if args.dumptree:
+                    p.dump()
+                res = p.evaluate()
+                print(res.format())
+            except Exception as e:
+                if args.print_exception:
+                    traceback.print_exc()
+                else:
+                    print(e)
+                print(datatypes.Nil('nil').format())
+        except EOFError:
+            print("Bye for now!")
+            break
+        except KeyboardInterrupt:
+            print("\b\bBye for now!")
+            break
+
+    return 0
+
+    # Return 0 to system and exit NanamiLang REPL script after playing around
+
+
+if __name__ == "__main__":
+
+    main()
