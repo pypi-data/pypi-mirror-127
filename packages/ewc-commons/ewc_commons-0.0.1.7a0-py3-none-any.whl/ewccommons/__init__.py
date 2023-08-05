878fdbe0aa3fa4389efae54957c38bedb41c33e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:28 +0000

    Add package ewc-commons/0.0.1.7a0/ewc_commons-0.0.1.7a0-py3-none-any.whl

---
+++ packages/ewc-commons/ewc_commons-0.0.1.7a0-py3-none-any.whl/ewccommons/__init__.py
@@ -0,0 +1,8 @@
+# Import the version number
+from .__version__ import __version__
+
+# Import the collection of available modules
+from ewccommons import sophie
+from ewccommons import dice
+from ewccommons import dicerolls
+from ewccommons import carddeck
