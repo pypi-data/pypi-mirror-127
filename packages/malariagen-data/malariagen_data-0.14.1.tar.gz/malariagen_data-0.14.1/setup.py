c5b956767ab7240c09e5223884c9f87b26ba444e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:09 +0000

    Add package malariagen-data/0.14.1/malariagen_data-0.14.1.tar.gz

---
+++ packages/malariagen-data/malariagen_data-0.14.1.tar.gz/malariagen_data-0.14.1/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['malariagen_data']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['BioPython',
+ 'dask[array]',
+ 'fsspec',
+ 'gcsfs',
+ 'numba',
+ 'numpy',
+ 'pandas',
+ 'scikit-allel',
+ 'scipy',
+ 'xarray',
+ 'zarr']
+
+setup_kwargs = {
+    'name': 'malariagen-data',
+    'version': '0.14.1',
+    'description': 'A package for accessing MalariaGEN public data.',
+    'long_description': None,
+    'author': 'Alistair Miles',
+    'author_email': 'alistair.miles@sanger.ac.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
