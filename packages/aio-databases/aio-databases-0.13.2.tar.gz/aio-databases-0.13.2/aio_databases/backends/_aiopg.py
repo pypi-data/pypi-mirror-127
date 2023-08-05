05dd28d817f5f415227264c16ded1c6fe25fc935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:47 +0000

    Add package aio-databases/0.13.2/aio-databases-0.13.2.tar.gz

---
+++ packages/aio-databases/aio-databases-0.13.2.tar.gz/aio-databases-0.13.2/aio_databases/backends/_aiopg.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+import typing as t
+
+from aiopg import Pool, Connection, create_pool, connect
+
+from . import ABCDatabaseBackend
+from .common import Connection as Ses
+
+
+class Session(Ses):
+
+    async def _executemany(self, query: str, *params, **options) -> t.Any:
+        async with self._conn.cursor() as cursor:
+            for args_ in params:
+                await cursor.execute(query, args_, **options)
+
+
+class Backend(ABCDatabaseBackend):
+
+    name = 'aiopg'
+    db_type = 'postgresql'
+    connection_cls = Session
+
+    pool: t.Optional[Pool] = None
+
+    def __init__(self, *args, **kwargs):
+        super(Backend, self).__init__(*args, **kwargs)
+        self.dsn = self.url._replace(scheme='postgresql').geturl()
+        self.pool_options = {
+            name: self.options.pop(name)
+            for name in ('minsize', 'maxsize', 'pool_recycle', 'on_connect')
+            if name in self.options
+        }
+
+    async def connect(self) -> None:
+        self.pool = await create_pool(self.dsn, **self.options, **self.pool_options)
+
+    async def disconnect(self) -> None:
+        pool = self.pool
+        assert pool is not None, "Database is not connected"
+        self.pool = None
+        pool.close()
+        await pool.wait_closed()
+
+    async def _acquire(self) -> Connection:
+        pool = self.pool
+        if pool is None:
+            return await connect(self.dsn, **self.options)
+
+        return await pool.acquire()
+
+    async def release(self, conn: Connection):
+        pool = self.pool
+        if pool is None:
+            conn.close()
+        else:
+            await pool.release(conn)
---
+++ packages/aio-databases/aio-databases-0.13.2.tar.gz/aio-databases-0.13.2/aio_databases/record.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import typing as t
+
+from collections.abc import Mapping
+
+
+class Record(Mapping):
+
+    __slots__ = '_values', '_keys'
+
+    def __init__(self, values: t.Tuple, description: t.Sequence[t.Sequence]):
+        self._values = values
+        self._keys = tuple(d[0] for d in description)
+
+    def __len__(self) -> int:
+        return len(self._values)
+
+    def __getitem__(self, idx: t.Union[str, int]) -> t.Any:
+        if isinstance(idx, int):
+            return self._values[idx]
+
+        for key, value in self.items():
+            if key == idx:
+                return value
+
+        raise KeyError
+
+    def __contains__(self, idx) -> bool:
+        return idx in self._keys
+
+    def __iter__(self) -> t.Iterator:
+        for val in self._values:
+            yield val
+
+    def __str__(self) -> str:
+        return ' '.join(f"{name}={value!r}" for name, value in self.items())
+
+    def __repr__(self) -> str:
+        return f"<Record {self}>"
+
+    @classmethod
+    def from_dict(cls, val: t.Dict) -> Record:
+        keys, values = zip(*val.items())
+        return cls(values, [[name] for name in keys])
+
+    def values(self) -> t.Tuple:  # type: ignore
+        return self._values
+
+    def keys(self) -> t.Tuple:  # type: ignore
+        return self._keys
+
+    def items(self) -> zip:  # type: ignore
+        return zip(self._keys, self._values)
+
+    def __eq__(self, obj: t.Any):
+        return self._values == tuple(obj)
