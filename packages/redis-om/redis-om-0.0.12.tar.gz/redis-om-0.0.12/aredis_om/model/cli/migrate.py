ce46b32500ee79a6037aeaae9d167a4e1325c29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:04 +0000

    Add package redis-om/0.0.12/redis-om-0.0.12.tar.gz

---
+++ packages/redis-om/redis-om-0.0.12.tar.gz/redis-om-0.0.12/aredis_om/model/cli/migrate.py
@@ -0,0 +1,17 @@
+import click
+
+from aredis_om.model.migrations.migrator import Migrator
+
+
+@click.command()
+@click.option("--module", default="aredis_om")
+def migrate(module):
+    migrator = Migrator(module)
+
+    if migrator.migrations:
+        print("Pending migrations:")
+        for migration in migrator.migrations:
+            print(migration)
+
+        if input("Run migrations? (y/n) ") == "y":
+            migrator.run()
