101a9de95d09750c50dabcd6f0be891ed0b510f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package infix2postfixv2/0.0.2/infix2postfixv2-0.0.2.tar.gz

---
+++ packages/infix2postfixv2/infix2postfixv2-0.0.2.tar.gz/infix2postfixv2-0.0.2/README.txt
@@ -0,0 +1,1 @@
+This is an infix to post fix calculator
---
+++ packages/infix2postfixv2/infix2postfixv2-0.0.2.tar.gz/infix2postfixv2-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='infix2postfixv2',
+    version='0.0.2',
+    description='Infix to post fix',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Joshua Lowe',
+    author_email='copyisreal@gmail.com',
+    py_modules='infixtopostfixmodule',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='infixtopostfix',
+    packages=find_packages(),
+    install_requires=['']
+)
