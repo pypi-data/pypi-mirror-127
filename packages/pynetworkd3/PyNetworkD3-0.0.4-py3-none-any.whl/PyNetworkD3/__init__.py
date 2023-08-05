46bcf52e0875af9cdfcb057533138ace8d910700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:21 +0000

    Add package pynetworkd3/0.0.4/PyNetworkD3-0.0.4-py3-none-any.whl

---
+++ packages/pynetworkd3/PyNetworkD3-0.0.4-py3-none-any.whl/PyNetworkD3/__init__.py
@@ -0,0 +1,8 @@
+"""
+Init file for the PyNetworkD3 library.
+"""
+
+from PyNetworkD3.core import ArcDiagram, Graph
+
+version_info = (0, 0, 4)
+__version__ = ".".join([str(x) for x in version_info])
