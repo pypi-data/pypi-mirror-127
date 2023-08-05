36df920f16ee34638716d6109a019f198ad7645b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:46 +0000

    Add package sf-b4/1.4.1/sf-b4-1.4.1.tar.gz

---
+++ packages/sf-b4/sf-b4-1.4.1.tar.gz/sf-b4-1.4.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='sf-b4',
+    version='1.4.1',
+    description='SkillFactory',
+    author='Roman Brick',
+    author_email='romanbrick@icloud.cloud',
+    zip_safe=False)
+
