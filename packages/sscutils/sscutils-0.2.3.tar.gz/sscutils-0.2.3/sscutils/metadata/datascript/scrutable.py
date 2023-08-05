ec9b404317b2da1e7bf5aa9a5708fd114767400f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:41 +0000

    Add package sscutils/0.2.3/sscutils-0.2.3.tar.gz

---
+++ packages/sscutils/sscutils-0.2.3.tar.gz/sscutils-0.2.3/sscutils/metadata/datascript/scrutable.py
@@ -0,0 +1,101 @@
+from functools import partial
+from typing import List, Optional, Type
+
+from ...artifact_context import ArtifactContext
+from ...helpers import get_associated_step
+from ...metaprogramming import camel_to_snake, snake_to_camel
+from ...naming import FEATURES_CLS_SUFFIX, INDEX_CLS_SUFFIX
+from .bases import BaseEntity, IndexBase, TableFeaturesBase
+
+
+class ScruTable:
+    def __init__(
+        self,
+        features: Type[TableFeaturesBase] = None,
+        index: Optional[Type[IndexBase]] = None,
+        name: Optional[str] = None,
+        subject_of_records: Optional[Type[BaseEntity]] = None,
+        namespace: Optional[str] = None,
+        partitioning_cols: Optional[List[str]] = None,
+        max_partition_size: Optional[int] = None,
+    ) -> None:
+        """
+        name if not given, is parsed from features (and index)
+
+        can be
+
+        - table in dataset
+        - imported table to a project
+          - from a dataset
+          - from a project step output
+        - table of a step output in a project
+
+        figures out whether its in a dataset, or a project
+        """
+        assert index or features, "No index, no features: no table"
+        self.name: str = _infer_table_name(name, features, index)
+        self.namespace = self._infer_namespace(namespace, features, index)
+        self.index = index
+        self.features = self._infer_features_cls(features)
+
+        self.subject: Type[BaseEntity] = self._infer_subject(
+            subject_of_records
+        )
+        self.partitioning_cols = partitioning_cols
+        self.max_partition_size = max_partition_size
+        self.trepo = ArtifactContext().create_trepo(
+            self.name,
+            self.namespace,
+            self.partitioning_cols,
+            self.max_partition_size,
+        )
+
+    def get_full_df(self):
+        return self.trepo.get_full_df()
+
+    def get_full_ddf(self):
+        return self.trepo.get_full_ddf()
+
+    def _infer_features_cls(self, features):
+        return self._new_cls(
+            features, TableFeaturesBase, FEATURES_CLS_SUFFIX.title()
+        )
+
+    def _infer_subject(self, subj) -> Type[BaseEntity]:
+        return self._new_cls(subj, BaseEntity)
+
+    def _infer_namespace(self, namespace, features, index):
+        if namespace:
+            return namespace
+        return get_associated_step(features or index)
+
+    def _new_cls(self, poss_cls, parent_cls, suffix=""):
+        if poss_cls is not None:
+            assert parent_cls in parent_cls.mro()
+            return poss_cls
+        cls_name = snake_to_camel(self.name) + suffix
+        return type(cls_name, (parent_cls,), {})
+
+
+class TableFactory:
+    def __init__(self, namespace) -> None:
+        self.create = partial(ScruTable, namespace=namespace)
+
+
+def _infer_table_name(name, features_cls, index_cls) -> str:
+    if name:
+        return name
+    if features_cls is not None:
+        return _infer_table_name_from_cls(features_cls, FEATURES_CLS_SUFFIX)
+    return _infer_table_name_from_cls(index_cls, INDEX_CLS_SUFFIX)
+
+
+def _infer_table_name_from_cls(cls: Type, suffix=INDEX_CLS_SUFFIX):
+    cls_name = cls.__name__
+    snake_name = camel_to_snake(cls_name)
+    if snake_name.endswith(f"_{suffix}"):
+        return snake_name[: -(len(suffix) + 1)]
+    raise NameError(
+        f"{cls} class name should end in {suffix},"
+        f" {cls_name} given, can't infer table name"
+    )
---
+++ packages/sscutils/sscutils-0.2.3.tar.gz/sscutils-0.2.3/sscutils/tests/test_pipereg.py
@@ -0,0 +1,75 @@
+import os
+from pathlib import Path
+
+from invoke import Collection
+from pytest import raises
+
+import sscutils.pipeline_registry as pipereg_module
+from sscutils import PipelineRegistry
+from sscutils.naming import ProjectConfigPaths
+from sscutils.pipeline_registry import _type_or_fun_elem
+
+PARAMS_PATH = ProjectConfigPaths.PARAMS
+
+
+def test_pipereg_basics():
+    pipereg = PipelineRegistry()
+
+    def step1(a=5):
+        return a * 2
+
+    step1.__module__ = "src.step_one"
+    pipereg.register(step1)  # usually works with decorator @pipereg.register
+    # but here module change is needed for the test
+    assert pipereg.get_step("step_one").runner(8) == 16
+
+    def step2(b=3):
+        return b - 1
+
+    step2.__module__ = "src.step_two.deeper"
+    pipereg.register(dependencies=["some_string"])(step2)
+
+    coll = pipereg.get_collection()
+    assert isinstance(coll, Collection)
+    assert [*coll.task_names] == ["step-one", "step-two"]
+
+    assert (
+        pipereg.get_step("step_two").run({"step_two": {"b": 10}}, False) == 9
+    )
+    assert pipereg.get_step("step_two").run({"b": 5}, False) == 4
+
+
+def test_pipereg_params(tmp_path):
+    cwd = Path.cwd()
+    os.chdir(tmp_path)
+    PARAMS_PATH.write_text("a: 10")
+    pipereg = PipelineRegistry()
+    assert pipereg.all_params["a"] == 10
+    os.chdir(cwd)
+
+
+def test_pipereg_parse_elems(tmp_path):
+
+    pipereg = PipelineRegistry()
+
+    def step1():
+        pass  # pragma: no cover
+
+    step1.__module__ = "src.step_one"
+    step1_pe = pipereg.register(outputs=["s1_out"])(step1)
+
+    _module_file_path = (
+        Path(pipereg_module.__file__).relative_to(Path.cwd()).as_posix()
+    )
+
+    assert pipereg._parse_elem("sss") == ["sss"]
+    assert pipereg._parse_elem(PARAMS_PATH) == [PARAMS_PATH.as_posix()]
+    assert pipereg._parse_elem(PipelineRegistry) == [_module_file_path]
+    assert pipereg._parse_elem(_type_or_fun_elem) == [_module_file_path]
+    assert pipereg._parse_elem(step1) == [
+        Path(__file__).relative_to(Path.cwd()).as_posix()
+    ]
+    assert pipereg._parse_elem(step1_pe) == ["s1_out"]
+
+    with raises(TypeError):
+        pipereg._parse_elem(20)
