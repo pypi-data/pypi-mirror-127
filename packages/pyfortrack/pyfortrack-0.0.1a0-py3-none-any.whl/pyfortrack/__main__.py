8f211426d0b6e6c44a732931167e5a45f06a498c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:28 +0000

    Add package pyfortrack/0.0.1a0/pyfortrack-0.0.1a0-py3-none-any.whl

---
+++ packages/pyfortrack/pyfortrack-0.0.1a0-py3-none-any.whl/pyfortrack/__main__.py
@@ -0,0 +1,8 @@
+from fib import fib
+
+def main():
+	print('Thats a python lib for track images')
+	print('Contact: helvecio.neto@inpe.br')
+	print('Number +',fib(10), '12 981026766')
+if __name__ == '__main__':
+	main()
