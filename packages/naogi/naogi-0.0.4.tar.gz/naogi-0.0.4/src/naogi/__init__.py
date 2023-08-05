9e7b69cca90cc076ff5800210aa8b5909f3f0aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:16 +0000

    Add package naogi/0.0.4/naogi-0.0.4.tar.gz

---
+++ packages/naogi/naogi-0.0.4.tar.gz/naogi-0.0.4/src/naogi/__init__.py
@@ -0,0 +1,43 @@
+import json
+
+from abc import ABC, abstractmethod
+from flask import send_file
+
+# TODO: move render classes
+class AbstractRenderer(ABC):
+  @abstractmethod
+  def render(data):
+    pass
+
+class JsonRenderer(AbstractRenderer):
+  def render(data):
+    return json.dumps(data)
+
+class FileRenderer(AbstractRenderer):
+  def render(binary, filename='file', content_type=None, downloadable=False):
+    return send_file(
+      binary,
+      mimetype=content_type,
+      as_attachment=downloadable,
+      download_name=filename,
+    )
+
+class NaogiModel(ABC):
+  def __init__(self):
+    super()
+    self.model = None
+
+  @abstractmethod
+  def predict(self):
+    pass
+
+  @abstractmethod
+  def load_model(self):
+    pass
+
+  @abstractmethod
+  def prepare(self):
+    pass
+
+  def renderer(self):
+    return JsonRenderer
