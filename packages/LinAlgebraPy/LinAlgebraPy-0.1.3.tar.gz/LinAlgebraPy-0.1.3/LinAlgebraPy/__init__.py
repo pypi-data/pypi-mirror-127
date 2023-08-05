2635101ab1725337959dc2c506177b8aa1647f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:15 +0000

    Add package LinAlgebraPy/0.1.3/LinAlgebraPy-0.1.3.tar.gz

---
+++ packages/LinAlgebraPy/LinAlgebraPy-0.1.3.tar.gz/LinAlgebraPy-0.1.3/LinAlgebraPy/__init__.py
@@ -0,0 +1,8 @@
+from .matrices import CramerRule,Matrix,Identity
+from  .vectors import scalarproduct,orthogonalVectors,indexPivot,colinear,Vector
+
+__all__=['Vector',
+'scalarproduct','orthogonalVectors','indexPivot','colinear','Matrix',
+'Identity','CramerRule']
+
+
