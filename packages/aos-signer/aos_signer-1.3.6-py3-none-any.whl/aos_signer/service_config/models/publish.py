89cc87e4e8ecd418bb0d0456abe08c6b3bde5349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:11 +0000

    Add package aos-signer/1.3.6/aos_signer-1.3.6-py3-none-any.whl

---
+++ packages/aos-signer/aos_signer-1.3.6-py3-none-any.whl/aos_signer/service_config/models/publish.py
@@ -0,0 +1,49 @@
+from .config_chapter import ConfigChapter
+
+
+class Publish(ConfigChapter):
+
+    def __init__(self, url, service_uid, tls_key, tls_certificate, tls_pkcs12, version):
+        self._url = url
+        self._service_uid = service_uid
+        self._tls_key = tls_key
+        self._tls_certificate = tls_certificate
+        self._tls_pkcs12 = tls_pkcs12
+        self._version = version
+
+    @staticmethod
+    def from_yaml(input_dict):
+        p = Publish(
+            input_dict.get('url'),
+            input_dict.get('service_uid'),
+            input_dict.get('tls_key'),
+            input_dict.get('tls_certificate'),
+            input_dict.get('tls_pkcs12'),
+            input_dict.get('version'),
+        )
+        ConfigChapter.validate(input_dict, validation_file='publish_schema.json')
+        return p
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def service_uid(self):
+        return self._service_uid
+
+    @property
+    def tls_key(self):
+        return self._tls_key
+
+    @property
+    def tls_certificate(self):
+        return self._tls_certificate
+
+    @property
+    def tls_pkcs12(self):
+        return self._tls_pkcs12
+
+    @property
+    def version(self):
+        return self._version
