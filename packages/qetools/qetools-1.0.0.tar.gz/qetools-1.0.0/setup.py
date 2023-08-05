d30522949923e4bbe674387054c79570cdc2983d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:35 +0000

    Add package qetools/1.0.0/qetools-1.0.0.tar.gz

---
+++ packages/qetools/qetools-1.0.0.tar.gz/qetools-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+  
+long_description = 'A python wrapper for quantum espresso calculations'
+  
+setup(
+        name ='qetools',
+        version ='1.0.0',
+        author ='Harish PVV',
+        author_email ='harishpvv@gmail.com',
+        description =long_description,
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='MIT',
+        packages = find_packages(),
+        entry_points ={
+            'console_scripts': [
+                'qetools = src.qetools:main'
+            ]
+        },
+        classifiers =(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        keywords ='quantum espresso dft harishpvv',
+        install_requires = ['ase', 'numpy', 'matplotlib'],
+        zip_safe = False,
+        include_package_data = True
+)
