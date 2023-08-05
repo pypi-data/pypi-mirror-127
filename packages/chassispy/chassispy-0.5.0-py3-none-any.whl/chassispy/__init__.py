38c6fd23f256c863243c98b39e94f58aaec9584d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:51 +0000

    Add package chassispy/0.5.0/chassispy-0.5.0-py3-none-any.whl

---
+++ packages/chassispy/chassispy-0.5.0-py3-none-any.whl/chassispy/__init__.py
@@ -0,0 +1,18 @@
+from chassispy.chassispy import UGV
+from chassispy.chassispy import CanMsgsGet
+from chassispy.chassispy import DeviceCan
+from chassispy.chassispy import UGVCan
+from chassispy import UGVConfigMsg
+
+__all__ = [
+    'UGV',
+    'CanMsgsGet',
+    'DeviceCan',
+    'UGVCan',
+    'UGVConfigMsg',
+]
+
+
+__version__ = '0.5.0'
+
+
