fdb63d4e462f5b8517141e0c2708b042f146f33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:52 +0000

    Add package PoseidonSentinel/0.0.5/PoseidonSentinel-0.0.5-py3-none-any.whl

---
+++ packages/PoseidonSentinel/PoseidonSentinel-0.0.5-py3-none-any.whl/sentinel/puts.py
@@ -0,0 +1,40 @@
+from . import Path
+from shutil import copy2
+import os.path
+
+__all__ = ['Put',]
+
+class Put:
+    
+    APPENDS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+    def __init__(self, file, path:Path):
+        self.path = path.path()
+        self.file = file
+        self.dst = self.check(f'{self.path}/{self.file.save_name}')
+
+    def check(self, path, count=0):
+        """
+        Checks to see if the file exists in the specified directory
+        If a file is found, append a letter to the end up to J
+        """
+        while os.path.exists(path):
+            if path[-1] in self.APPENDS:
+                path = path[:len(path)-1] + self.APPENDS[count]
+            else:
+                path = path + self.APPENDS[count]
+            count +=1
+        return path
+
+
+    def put(self):
+        """
+        Uses shututil copy2 to place the file in the destination
+        """
+        try:
+            copy2(self.file.path, self.dst)
+        except Exception as e:
+            return e
+
+    def __str__(self):
+        return self.dst
