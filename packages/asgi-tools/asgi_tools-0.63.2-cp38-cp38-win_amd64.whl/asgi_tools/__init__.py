bb1749d81e27daba9643065079f2b4b97ed594d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:51 +0000

    Add package asgi-tools/0.63.2/asgi_tools-0.63.2-cp38-cp38-win_amd64.whl

---
+++ packages/asgi-tools/asgi_tools-0.63.2-cp38-cp38-win_amd64.whl/asgi_tools/__init__.py
@@ -0,0 +1,80 @@
+""" ASGI-Tools -- Tools to make ASGI Applications """
+
+__version__ = "0.63.2"
+__license__ = "MIT"
+
+import logging
+
+
+asgi_logger: logging.Logger = logging.getLogger('asgi-tools')
+
+
+class ASGIError(Exception):
+    """Base class for ASGI-Tools Errors."""
+
+    pass
+
+
+class ASGIConnectionClosed(ASGIError):
+    """ASGI-Tools connection closed error."""
+
+    pass
+
+
+class ASGIDecodeError(ASGIError, ValueError):
+    """ASGI-Tools decoding error."""
+
+    pass
+
+
+class ASGINotFound(ASGIError):
+    """Raise when http handler not found."""
+
+    pass
+
+
+class ASGIMethodNotAllowed(ASGIError):
+    """Raise when http method not found."""
+
+    pass
+
+
+DEFAULT_CHARSET: str = 'utf-8'
+
+from .request import Request  # noqa
+from .response import (  # noqa
+    Response, ResponseHTML, ResponseJSON, ResponseText, ResponseRedirect, ResponseError,
+        ResponseStream, ResponseSSE, ResponseFile, ResponseWebSocket, parse_response
+)
+from .middleware import (  # noqa
+    RequestMiddleware, ResponseMiddleware, LifespanMiddleware,
+    RouterMiddleware, StaticFilesMiddleware
+)
+from .app import App, HTTPView  # noqa
+
+from http_router import NotFound, MethodNotAllowed  # noqa
+
+
+__all__ = (
+    'App',
+    'HTTPView',
+    'LifespanMiddleware',
+    'MethodNotAllowed',
+    'NotFound',
+    'Request',
+    'RequestMiddleware',
+    'Response',
+    'ResponseError',
+    'ResponseFile',
+    'ResponseHTML',
+    'ResponseJSON',
+    'ResponseMiddleware',
+    'ResponseRedirect',
+    'ResponseSSE',
+    'ResponseStream',
+    'ResponseText',
+    'ResponseWebSocket',
+    'RouterMiddleware',
+    'StaticFilesMiddleware',
+    'parse_response',
+)
