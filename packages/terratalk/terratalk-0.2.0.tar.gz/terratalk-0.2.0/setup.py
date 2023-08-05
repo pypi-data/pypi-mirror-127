7fa052b781cdb1ffcf2ff6e9733b037c33595728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:37 +0000

    Add package terratalk/0.2.0/terratalk-0.2.0.tar.gz

---
+++ packages/terratalk/terratalk-0.2.0.tar.gz/terratalk-0.2.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='terratalk',
+    version='0.2.0',
+    author='Günter Grodotzki',
+    author_email='gunter@grodotzki.com',
+    description='A Terraform commentator.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/lifeofguenter/terratalk',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3',
+    install_requires=[
+        'click',
+        'requests',
+    ],
+    extras_require={
+        'GitHub': ['PyGithub'],
+    },
+    entry_points={
+        'console_scripts': [
+            'terratalk = terratalk.__main__:cli',
+        ],
+    },
+)
