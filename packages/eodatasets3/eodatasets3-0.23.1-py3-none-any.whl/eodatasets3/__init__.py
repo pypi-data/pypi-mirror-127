3456f0cdbd1b59cff24793c68aac124c3db1fc7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:10 +0000

    Add package eodatasets3/0.23.1/eodatasets3-0.23.1-py3-none-any.whl

---
+++ packages/eodatasets3/eodatasets3-0.23.1-py3-none-any.whl/eodatasets3/__init__.py
@@ -0,0 +1,25 @@
+from ._version import get_versions
+from .assemble import DatasetAssembler, DatasetPrepare, IfExists, IncompleteDatasetError
+from .images import GridSpec
+from .model import DatasetDoc
+from .names import NamingConventions, namer
+from .properties import Eo3Dict
+
+REPO_URL = "https://github.com/GeoscienceAustralia/eo-datasets.git"
+
+__version__ = get_versions()["version"]
+del get_versions
+
+__all__ = (
+    "DatasetAssembler",
+    "DatasetDoc",
+    "DatasetPrepare",
+    "Eo3Dict",
+    "GridSpec",
+    "IfExists",
+    "IncompleteDatasetError",
+    "NamingConventions",
+    "namer",
+    "REPO_URL",
+    "__version__",
+)
