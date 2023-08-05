b5cef41c5ff78037ed39a16af933625253c44b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:47 +0000

    Add package acpoa/0.0.2/acpoa-0.0.2.tar.gz

---
+++ packages/acpoa/acpoa-0.0.2.tar.gz/acpoa-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Line automatically modified by acpoa-builder
+# Do not modify it manually unless you know what you are doing
+files = []
+
+setuptools.setup(
+    name="acpoa",
+    version="0.0.2",
+    author="Leikt",
+    author_email="leikt.solreihin@gmail.com",
+    description="Application Core for Plugin Oriented Applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Leikt/acpoa-core",
+    project_urls={
+        "Bug Tracker": "https://github.com/Leikt/acpoa-core/issues",
+        "Wiki": "https://github.com/Leikt/acpoa-core/wiki",
+        "Discussions": "https://github.com/Leikt/acpoa-core/discussions"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    package_data={'': files}
+)
