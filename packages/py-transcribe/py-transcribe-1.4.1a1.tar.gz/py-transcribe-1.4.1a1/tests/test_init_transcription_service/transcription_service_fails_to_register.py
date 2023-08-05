bbfb9c3da6c14ebfef4c2285fc453238cf4ee91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:29 +0000

    Add package py-transcribe/1.4.1a1/py-transcribe-1.4.1a1.tar.gz

---
+++ packages/py-transcribe/py-transcribe-1.4.1a1.tar.gz/py-transcribe-1.4.1a1/tests/test_init_transcription_service/transcription_service_fails_to_register.py
@@ -0,0 +1,5 @@
+import logging
+
+logging.warning(
+    "this module does nothing. It exists to test a loadable module that SHOULD register a factory for a TranscriptionService but fails to do so"
+)
