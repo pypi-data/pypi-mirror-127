4a1e82ed337457ea82afab15a22ca0e7a8b5881e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package realtime-earthquake-indonesia/0.0.1/realtime-earthquake-indonesia-0.0.1.tar.gz

---
+++ packages/realtime-earthquake-indonesia/realtime-earthquake-indonesia-0.0.1.tar.gz/realtime-earthquake-indonesia-0.0.1/README.md
@@ -0,0 +1,5 @@
+# latest-earthquake-report-indonesia
+This package inform the latest earthquake in Indonesia from BMKG | Meteorological, Climatological, and Geophysical Agency.
+
+## How it work
+This package informs earthquake that just occurred in Indonesia, sourced from BMKG.
---
+++ packages/realtime-earthquake-indonesia/realtime-earthquake-indonesia-0.0.1.tar.gz/realtime-earthquake-indonesia-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="realtime-earthquake-indonesia",
+    version="0.0.1",
+    author="Panji Perdana",
+    author_email="panji9502@gmail.com",
+    description="This package inform the latest earthquake in Indonesia from BMKG | Meteorological, Climatological, and Geophysical Agency.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pperdana/latest-earthquake-report-indonesia",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    # packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
