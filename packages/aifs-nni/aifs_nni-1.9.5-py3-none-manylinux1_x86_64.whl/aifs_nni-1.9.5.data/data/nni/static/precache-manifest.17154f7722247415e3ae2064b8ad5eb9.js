47b953e6122b79b73ec9e9417fe88500f5efba93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:12 +0000

    Add package aifs-nni/1.9.5/aifs_nni-1.9.5-py3-none-manylinux1_x86_64.whl

---
+++ packages/aifs-nni/aifs_nni-1.9.5-py3-none-manylinux1_x86_64.whl/aifs_nni-1.9.5.data/data/nni/static/precache-manifest.17154f7722247415e3ae2064b8ad5eb9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b2caae008927219dc87f383b39f7c2",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "325c0b65ba46b94e235b990852af826a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "742e3ac827e9d006ecbfe39f281fc8a6",
+    "url": "/json.worker.js"
+  },
+  {
+    "revision": "67920db6983fb332e7e2",
+    "url": "/static/css/3.eda3db85.chunk.css"
+  },
+  {
+    "revision": "268ce252abd059081834",
+    "url": "/static/css/4.00ce0611.chunk.css"
+  },
+  {
+    "revision": "df3765eb8db3caa8080a",
+    "url": "/static/css/5.a8c4f787.chunk.css"
+  },
+  {
+    "revision": "9f18533c154b1649d9eb",
+    "url": "/static/css/6.17ef0bf3.chunk.css"
+  },
+  {
+    "revision": "62e88a9e583a712d7aa7",
+    "url": "/static/css/main.9389d4ae.chunk.css"
+  },
+  {
+    "revision": "201c169d0178dd0f72c6",
+    "url": "/static/js/0.6436627a.chunk.js"
+  },
+  {
+    "revision": "67920db6983fb332e7e2",
+    "url": "/static/js/3.22fc24cc.chunk.js"
+  },
+  {
+    "revision": "268ce252abd059081834",
+    "url": "/static/js/4.05847dfc.chunk.js"
+  },
+  {
+    "revision": "df3765eb8db3caa8080a",
+    "url": "/static/js/5.a2ad0bc7.chunk.js"
+  },
+  {
+    "revision": "9f18533c154b1649d9eb",
+    "url": "/static/js/6.8ba296f9.chunk.js"
+  },
+  {
+    "revision": "5e996dee69a74ac83611",
+    "url": "/static/js/7.591d500d.chunk.js"
+  },
+  {
+    "revision": "62e88a9e583a712d7aa7",
+    "url": "/static/js/main.a1398e35.chunk.js"
+  },
+  {
+    "revision": "7cf83c014f5af10e56be",
+    "url": "/static/js/runtime~main.14f393ec.js"
+  },
+  {
+    "revision": "35bb8d560db5205616671eab8c4d0303",
+    "url": "/static/media/codicon.35bb8d56.ttf"
+  },
+  {
+    "revision": "9ab21ead958b03c1b87c6b0b1a708e30",
+    "url": "/static/media/fabric-icons-1b34146b.9ab21ead.woff"
+  },
+  {
+    "revision": "a654978affc483839f6a7c010b0f0253",
+    "url": "/static/media/loading.a654978a.gif"
+  },
+  {
+    "revision": "c8cd1ec09bd3dfe411eb4dea68a6c8c3",
+    "url": "/static/media/logo.c8cd1ec0.png"
+  }
+]);
