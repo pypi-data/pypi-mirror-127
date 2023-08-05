d23394372a7126f295407fc285f5ae01256d6736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:07 +0000

    Add package pipen-report/0.0.10/pipen-report-0.0.10.tar.gz

---
+++ packages/pipen-report/pipen-report-0.0.10.tar.gz/pipen-report-0.0.10/pipen_report/frontend/src/layouts/Proc.svelte
@@ -0,0 +1,38 @@
+<script>
+  import {
+    Column,
+    Content,
+    Grid,
+    SideNav,
+    Row
+  } from "carbon-components-svelte";
+
+  import Base from './Base.svelte';
+  import Footer from './Footer.svelte';
+
+  let isSideNavOpen = false;
+
+  export let logo;
+  export let logotext = null;
+  export let versions;
+  export let procs;
+  export let pipeline_name;
+</script>
+
+<Base bind:isSideNavOpen {logo} {logotext} {procs} {pipeline_name}>
+
+    <SideNav bind:isOpen={isSideNavOpen}>
+      <slot name="toc" />
+    </SideNav>
+
+    <Content>
+      <Grid>
+        <Row>
+          <Column>
+            <slot />
+          </Column>
+        </Row>
+      </Grid>
+      <Footer {versions} />
+    </Content>
+</Base>
---
+++ packages/pipen-report/pipen-report-0.0.10.tar.gz/pipen-report-0.0.10/pipen_report/frontend/src/page.tpl.svelte
@@ -0,0 +1,17 @@
+<script>
+    import ProcLayout from "../layouts/Proc.svelte"
+    import ProcReport from "../procs/{{proc_slug}}.svelte"
+    import ProcReportToc from "../procs/{{proc_slug}}.toc.svelte"
+
+    const proc_name = "{{proc.name}}";
+    const proc_desc = "{{proc.desc}}";
+    const pipeline_name = "{{proc.pipeline.name}}";
+    const versions = `{{versions}}`;
+    const procs = {{procs}};
+
+</script>
+
+<ProcLayout logo={proc_name} logotext={proc_desc} {versions} {procs} {pipeline_name}>
+    <ProcReportToc slot="toc" />
+    <ProcReport />
+</ProcLayout>
