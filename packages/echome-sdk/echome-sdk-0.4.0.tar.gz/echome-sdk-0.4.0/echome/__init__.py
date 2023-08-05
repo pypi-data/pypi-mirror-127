25ccc5d1cb5f9498f6f87cfd18dd9eab233627d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:27 +0000

    Add package echome-sdk/0.4.0/echome-sdk-0.4.0.tar.gz

---
+++ packages/echome-sdk/echome-sdk-0.4.0.tar.gz/echome-sdk-0.4.0/echome/__init__.py
@@ -0,0 +1,21 @@
+__title__ = 'echome-sdk'
+__version__ = '0.4.0'
+__author__ = 'Marcus Gutierrez'
+
+import logging
+import os
+from .session import Session
+
+debug_level = os.getenv("ECHOME_DEBUG", "WARNING")
+if debug_level == "ERROR":
+    lv = logging.ERROR
+elif debug_level == "DEBUG":
+    lv = logging.DEBUG
+elif debug_level == "INFO":
+    lv = logging.INFO
+elif debug_level == "WARNING":
+    lv = logging.WARNING
+else:
+    raise Exception("Unknown debug level set in ECHOME_DEBUG environment variable.")
+
+logging.basicConfig(level=lv)
