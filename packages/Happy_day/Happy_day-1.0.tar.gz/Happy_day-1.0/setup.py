12e7a2abb9a680cff0e451415428b4893616fa4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:36 +0000

    Add package Happy_day/1.0/Happy_day-1.0.tar.gz

---
+++ packages/Happy_day/Happy_day-1.0.tar.gz/Happy_day-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = "Happy_day",
+    version = "1.0",
+    description = "这是一个无用的测试模块",
+    author= "liyi",
+    author_email = "2307758843@qq.com",
+    py_modules=["Happy_day.demo1","Happy_day.demo2"]
+)
