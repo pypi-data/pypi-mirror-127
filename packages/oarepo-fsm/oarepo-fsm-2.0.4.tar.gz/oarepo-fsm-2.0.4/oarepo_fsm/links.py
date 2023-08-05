9d75fa0201c1122cee0b730ab0c671a8a5fec9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:05 +0000

    Add package oarepo-fsm/2.0.4/oarepo-fsm-2.0.4.tar.gz

---
+++ packages/oarepo-fsm/oarepo-fsm-2.0.4.tar.gz/oarepo-fsm-2.0.4/oarepo_fsm/links.py
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2020 CESNET.
+#
+# oarepo-fsm is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
+
+"""OArepo FSM library for record state transitions."""
+
+from flask import url_for
+from invenio_records_rest.links import default_links_factory
+
+from oarepo_fsm.mixins import FSMMixin
+from oarepo_fsm.views import build_url_transition_for_pid, \
+    record_class_from_pid_type
+
+
+def record_fsm_links_factory(pid, record=None, original_links_factory=None, **kwargs):
+    """Factory for record FSM links generation.
+
+    :param pid: A Persistent Identifier instance.
+    :param record: An instance of a Record.
+    :param original_links_factory: a link factory to be used to generate default links
+    :returns: Dictionary containing a list of useful links + FSM link for the record.
+    """
+    links_factory = original_links_factory or default_links_factory
+    links = links_factory(pid, record, **kwargs)
+
+    if record and isinstance(record, FSMMixin):
+        transitions = {}
+
+        for act in record.available_user_transitions().keys():
+            transitions[act] = build_url_transition_for_pid(pid, act)
+
+        links['transitions'] = transitions
+
+    return links
