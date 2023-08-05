5c95ec66a3d7a4275f649a744d8ce9623b61ef99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:10 +0000

    Add package playerFramework/2.5/playerFramework-2.5.tar.gz

---
+++ packages/playerFramework/playerFramework-2.5.tar.gz/playerFramework-2.5/playerFramework/exceptions.py
@@ -0,0 +1,31 @@
+class frameworkError(Exception):
+    pass
+
+
+class PlayerPathNotValid(frameworkError):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class UnableToWriteToIOFile(frameworkError):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class UndefinedKey(frameworkError):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class ProcessTerminatedExternally(frameworkError):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class InvalidTrackPath(frameworkError):
+    def __init__(self, *args, **kwargs):
+        pass
+
+class songObjectNotInitialized(frameworkError):
+    def __init__(self, *args, **kwargs):
+        pass
