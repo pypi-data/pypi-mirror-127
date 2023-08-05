f72f3ff82345afd9aa2170643e43b41966032522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:14 +0000

    Add package karatsuba-dagne/0.1/karatsuba-dagne-0.1.tar.gz

---
+++ packages/karatsuba-dagne/karatsuba-dagne-0.1.tar.gz/karatsuba-dagne-0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 22:42:07 2021
+
+@author: daaze
+"""
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='karatsuba-dagne',
+      version='0.1',
+      description='karatsuba recursive implementation',
+      author='Dagne',
+     )
+
