4771cc4d9e41d7a0c99b4b22667fd8c3eb67586a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:55 +0000

    Add package pantsbuild.pants/2.8.0rc5/pantsbuild.pants-2.8.0rc5-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.8.0rc5-cp39-cp39-manylinux2014_x86_64.whl/pants/backend/experimental/python/register.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.backend.experimental.python import user_lockfiles
+from pants.backend.python.goals import lockfile, publish
+from pants.backend.python.util_rules import pex
+
+
+def rules():
+    return (
+        *lockfile.rules(),
+        *pex.rules(),
+        *publish.rules(),
+        *user_lockfiles.rules(),
+    )
