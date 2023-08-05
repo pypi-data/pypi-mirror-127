b8521e4f2513f350c3fdab6774a18a14855d535e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:33 +0000

    Add package xiaoyeML/0.0.3/xiaoyeML-0.0.3.tar.gz

---
+++ packages/xiaoyeML/xiaoyeML-0.0.3.tar.gz/xiaoyeML-0.0.3/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+  name="xiaoyeML",
+  version="0.0.3",
+  author="xiaoye-hhh",
+  author_email="1318137184@qq.com",
+  description="A small package to ml",
+  packages=setuptools.find_packages()
+)
