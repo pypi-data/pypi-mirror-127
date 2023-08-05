d7b85e84e49241f43004341bcfa2c0ad3f6fe236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:04 +0000

    Add package cshs/0.1/cshs-0.1.tar.gz

---
+++ packages/cshs/cshs-0.1.tar.gz/cshs-0.1/org_cshs/__init__.py
@@ -0,0 +1,8 @@
+#
+# org_cshs/__init__.py
+#   Copyright (c) 2021 Cedars-Sinai
+#   All Rights Reserved
+#
+"""namespace reservation for internal tooling"""
+
+__version__ = '0.1'
---
+++ packages/cshs/cshs-0.1.tar.gz/cshs-0.1/readme.md
@@ -0,0 +1,4 @@
+cshs
+====
+
+Namespace reservation for internal project tooling, in compliance with [PEP 518](https://www.python.org/dev/peps/pep-0518/#tool-table).
