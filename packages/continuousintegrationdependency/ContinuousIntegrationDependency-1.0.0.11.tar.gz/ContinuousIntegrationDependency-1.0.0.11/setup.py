97c0187768903ea96bcc16289d6d36ea003d7548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:26 +0000

    Add package continuousintegrationdependency/1.0.0.11/ContinuousIntegrationDependency-1.0.0.11.tar.gz

---
+++ packages/continuousintegrationdependency/ContinuousIntegrationDependency-1.0.0.11.tar.gz/ContinuousIntegrationDependency-1.0.0.11/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['continuousintegrationdependency']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'continuousintegrationdependency',
+    'version': '1.0.0.11',
+    'description': '',
+    'long_description': None,
+    'author': 'Aaron Shiels',
+    'author_email': 'aaron.shiels@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
