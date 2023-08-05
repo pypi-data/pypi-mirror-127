2bb8232e6c5912cf4d07bfaf4aab956f2e147ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:18 +0000

    Add package sweetrpg-library-objects/0.0.40/sweetrpg-library-objects-0.0.40.tar.gz

---
+++ packages/sweetrpg-library-objects/sweetrpg-library-objects-0.0.40.tar.gz/sweetrpg-library-objects-0.0.40/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="sweetrpg-library-objects",
+    install_requires=[
+        "Flask==2.0.2",
+        "mongoengine",
+        "sweetrpg-db",
+        "sweetrpg-model-core",
+        "marshmallow-jsonapi",
+    ],
+)
