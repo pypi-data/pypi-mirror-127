5250f8f5fbfea7749f2191b5f30df9f02d33bc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:15 +0000

    Add package wannabecryptolib/0.0.2/wannabecryptolib-0.0.2.tar.gz

---
+++ packages/wannabecryptolib/wannabecryptolib-0.0.2.tar.gz/wannabecryptolib-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Encrypting strings.'
+LONG_DESCRIPTION = 'Not safe at all. Just for educational purposes'
+
+# Setting up
+setup(
+    name="wannabecryptolib",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ packages/wannabecryptolib/wannabecryptolib-0.0.2.tar.gz/wannabecryptolib-0.0.2/wannabecryptolib/encryptlikeapro.py
@@ -0,0 +1,22 @@
+class UltraSafeCipher():
+    def __init__(self, seed,dif):
+        self.seed = seed
+        self.dif = dif
+
+    def gen(self):
+        import random
+        random.seed(None)
+        first = random.choice([i for i in range(self.seed,self.seed**2)])
+        last = first-self.dif if first > self.dif else first+self.dif
+        return (first,last) if first < last else last,first
+
+    def encrypt(self, plaintext):
+        (first,last) = self.gen()
+        ciphertext=""
+        for c in range(len(plaintext)):
+            if c%2==0:
+                ciphertext += str(ord(plaintext[c])^first) + " "
+            else:
+                ciphertext += str(ord(plaintext[c])^last) + " "
+        return ciphertext
+
