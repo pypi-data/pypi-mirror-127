aa37f3c200b320034ddd22ceb64c1ac261a36903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:35 +0000

    Add package unikube/1.1.0.dev2/unikube-1.1.0.dev2-py3-none-any.whl

---
+++ packages/unikube/unikube-1.1.0.dev2-py3-none-any.whl/src/cli/console/__init__.py
@@ -0,0 +1,7 @@
+from .deck import deck_list
+from .exit import exit_generic_error, exit_login_required
+from .input import confirm, list
+from .logger import debug, echo, error, info, link, success, warning
+from .orga import organization_list
+from .output import table
+from .project import project_list
