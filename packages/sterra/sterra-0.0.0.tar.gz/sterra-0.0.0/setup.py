b89d53d970e240beee308205b240ae15179c484f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:18 +0000

    Add package sterra/0.0.0/sterra-0.0.0.tar.gz

---
+++ packages/sterra/sterra-0.0.0.tar.gz/sterra-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sterra',
+    version = '0.0.0',
+    description = 'Simply reserving this name for sterraxcyl',
+    author = 'novitae',
+    url = 'https://github.com/novitae',
+)
