51843667d6d1dffcf9b0b8045ae403e226b23284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:03 +0000

    Add package gsnodegraph/0.3.3/gsnodegraph-0.3.3.tar.gz

---
+++ packages/gsnodegraph/gsnodegraph-0.3.3.tar.gz/gsnodegraph-0.3.3/gsnodegraph/assets/bitmaps.py
@@ -0,0 +1,15 @@
+from wx.lib.embeddedimage import PyEmbeddedImage
+#--------------------
+ICON_BRUSH_CHECKERBOARD = PyEmbeddedImage(
+    b'iVBORw0KGgoAAAANSUhEUgAAAGQAAABkAQMAAABKLAcXAAAABlBMVEVmZmZCQkJadisvAAAA'
+    b'KElEQVQ4y2P4f4D/D4xgGGAeg/0H5v8wYmB5o+EyGi6j4TIaLrTiAQDr6oSnZbt/HwAAAABJ'
+    b'RU5ErkJggg==')
+
+#----------------------------------------------------------------------
+ICON_IMAGE = PyEmbeddedImage(
+    b'iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAACXBIWXMAAA7DAAAOwwHHb6hk'
+    b'AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAHBJREFUKJHVkksOgCAM'
+    b'RJ/GExDLYUw8vXga4Azjyg0QDcGNs2z6+pkWBjQBpJROSVsHF8xsByDGqJ6Od/7cA5VaGlUz'
+    b'4Mq4pOy9Xx9hwJnZ1CharTY09k/hyjBJuWWOpPwKl+d40idjh84XPUaaAnAB0HEs5elgUi0A'
+    b'AAAASUVORK5CYII=')
+
