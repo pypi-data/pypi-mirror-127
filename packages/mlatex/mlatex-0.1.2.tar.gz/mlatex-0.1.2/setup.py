b771b52338a7d034f042ea14432865aaf5020b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:25 +0000

    Add package mlatex/0.1.2/mlatex-0.1.2.tar.gz

---
+++ packages/mlatex/mlatex-0.1.2.tar.gz/mlatex-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mlatex',
+      version='0.1.2',
+      description='This is a library designed to write simple latex files',
+      url='',
+      author='Steve Mohr',
+      author_email='steve.h.mohr@gmail.com',
+      packages=['mlatex'],
+      zip_safe=False)
