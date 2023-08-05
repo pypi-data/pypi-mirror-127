5447d530cffbcc480c89bc8808bd112d0bb33e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:11 +0000

    Add package ramodels/3.5.0/ramodels-3.5.0.tar.gz

---
+++ packages/ramodels/ramodels-3.5.0.tar.gz/ramodels-3.5.0/ramodels/mo/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# --------------------------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------------
+from ._shared import MOBase
+from .employee import Employee
+from .facet import FacetClass
+from .organisation_unit import OrganisationUnit
+from .organisation_unit import OrganisationUnitRead
+from .organisation_unit import OrganisationUnitWrite
+
+# --------------------------------------------------------------------------------------
+# All
+# --------------------------------------------------------------------------------------
+
+__all__ = [
+    "MOBase",
+    "Employee",
+    "FacetClass",
+    "OrganisationUnit",
+    "OrganisationUnitRead",
+    "OrganisationUnitWrite",
+]
