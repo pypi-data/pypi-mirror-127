7f987f5c68211f2718ad9fc1ace6f7b5d1372743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:56 +0000

    Add package simplarchiver/1.10.4.3/simplarchiver-1.10.4.3.tar.gz

---
+++ packages/simplarchiver/simplarchiver-1.10.4.3.tar.gz/simplarchiver-1.10.4.3/simplarchiver/example/rss/__init__.py
@@ -0,0 +1,3 @@
+from .rss import RSSHubFeeder, RSSHubMultiPageFeeder, TTRSSCatFeeder
+from .rss import TTRSSHubLinkFeeder, TTRSSHubLinkDownloader
+from .rss import EnclosureOnlyDownloader, EnclosureExceptDownloader
