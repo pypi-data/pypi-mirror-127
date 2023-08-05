628bc33a5aa05db82b3427b52170809a37948aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:08 +0000

    Add package jina/2.4.3.dev8/jina-2.4.3.dev8.tar.gz

---
+++ packages/jina/jina-2.4.3.dev8.tar.gz/jina-2.4.3.dev8/daemon/api/endpoints/partial/pod.py
@@ -0,0 +1,90 @@
+from typing import Optional, Dict, Any
+from fastapi import APIRouter
+
+from jina.helper import ArgNamespace
+from jina.parsers import set_pod_parser
+from ....excepts import PartialDaemon400Exception
+from ....models import PodModel
+from ....models.partial import PartialStoreItem
+from ....stores import partial_store as store
+from ....models.enums import UpdateOperation
+
+router = APIRouter(prefix='/pod', tags=['pod'])
+
+
+@router.get(
+    path='',
+    summary='Get status of a running Pod',
+    response_model=PartialStoreItem,
+)
+async def _status():
+    """
+
+    .. #noqa: DAR101
+    .. #noqa: DAR201"""
+    return store.item
+
+
+@router.post(
+    path='',
+    summary='Create a Pod',
+    description='Create a Pod and add it to the store',
+    status_code=201,
+    response_model=PartialStoreItem,
+)
+async def _create(pod: 'PodModel'):
+    """
+
+    .. #noqa: DAR101
+    .. #noqa: DAR201"""
+    try:
+        args = ArgNamespace.kwargs2namespace(pod.dict(), set_pod_parser())
+        return store.add(args)
+    except Exception as ex:
+        raise PartialDaemon400Exception from ex
+
+
+@router.put(
+    path='',
+    summary='Run an update operation on the Pod object',
+    description='Types supported: "rolling_update"',
+    response_model=PartialStoreItem,
+)
+async def _update(
+    kind: UpdateOperation,
+    uses_with: Optional[Dict[str, Any]] = None,
+):
+    """
+
+    .. #noqa: DAR101
+    .. #noqa: DAR201
+    """
+    try:
+        return store.update(kind, uses_with=uses_with)
+    except ValueError as ex:
+        raise PartialDaemon400Exception from ex
+
+
+@router.delete(
+    path='',
+    summary='Terminate the running Pod',
+    description='Terminate a running Pod and release its resources',
+)
+async def _delete():
+    """
+
+    .. #noqa: DAR101
+    .. #noqa: DAR201"""
+    try:
+        store.delete()
+    except Exception as ex:
+        raise PartialDaemon400Exception from ex
+
+
+@router.on_event('shutdown')
+def _shutdown():
+    """
+
+    .. #noqa: DAR101
+    .. #noqa: DAR201"""
+    store.delete()
---
+++ packages/jina/jina-2.4.3.dev8.tar.gz/jina-2.4.3.dev8/jina/clients/base/websocket.py
@@ -0,0 +1,70 @@
+"""A module for the websockets-based Client for Jina."""
+from typing import Callable, Optional
+from contextlib import nullcontext, AsyncExitStack
+
+from ..helper import callback_exec
+from .helper import WebsocketClientlet
+from ...importer import ImportExtensions
+from ..base import BaseClient, InputType
+from ...logging.profile import ProgressBar
+from ...peapods.stream.client import WebsocketClientStreamer
+
+
+class WebSocketBaseClient(BaseClient):
+    """A Websocket Client."""
+
+    async def _get_results(
+        self,
+        inputs: InputType,
+        on_done: Callable,
+        on_error: Optional[Callable] = None,
+        on_always: Optional[Callable] = None,
+        **kwargs,
+    ):
+        """
+        :param inputs: the callable
+        :param on_done: the callback for on_done
+        :param on_error: the callback for on_error
+        :param on_always: the callback for on_always
+        :param kwargs: kwargs for _get_task_name and _get_requests
+        :yields: generator over results
+        """
+        with ImportExtensions(required=True):
+            import aiohttp
+
+        self.inputs = inputs
+        request_iterator = self._get_requests(**kwargs)
+
+        async with AsyncExitStack() as stack:
+            try:
+                cm1 = (
+                    ProgressBar(total_length=self._inputs_length)
+                    if self.show_progress
+                    else nullcontext()
+                )
+                p_bar = stack.enter_context(cm1)
+
+                proto = 'wss' if self.args.https else 'ws'
+                url = f'{proto}://{self.args.host}:{self.args.port}/'
+                iolet = await stack.enter_async_context(
+                    WebsocketClientlet(url=url, logger=self.logger)
+                )
+
+                streamer = WebsocketClientStreamer(self.args, iolet=iolet)
+                async for response in streamer.stream(request_iterator):
+                    callback_exec(
+                        response=response,
+                        on_error=on_error,
+                        on_done=on_done,
+                        on_always=on_always,
+                        continue_on_error=self.continue_on_error,
+                        logger=self.logger,
+                    )
+                    if self.show_progress:
+                        p_bar.update()
+                    yield response
+
+            except aiohttp.ClientError as e:
+                self.logger.error(
+                    f'Error while streaming response from websocket server {e!r}'
+                )
