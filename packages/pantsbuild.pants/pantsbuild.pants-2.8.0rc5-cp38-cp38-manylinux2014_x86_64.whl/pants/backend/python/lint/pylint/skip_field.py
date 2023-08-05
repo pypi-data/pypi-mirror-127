8a8c4959856a1c493949b6496c462b2dca6df002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:05 +0000

    Add package pantsbuild.pants/2.8.0rc5/pantsbuild.pants-2.8.0rc5-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.8.0rc5-cp38-cp38-manylinux2014_x86_64.whl/pants/backend/python/lint/pylint/skip_field.py
@@ -0,0 +1,25 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.backend.python.target_types import (
+    PythonSourcesGeneratorTarget,
+    PythonSourceTarget,
+    PythonTestsGeneratorTarget,
+    PythonTestTarget,
+)
+from pants.engine.target import BoolField
+
+
+class SkipPylintField(BoolField):
+    alias = "skip_pylint"
+    default = False
+    help = "If true, don't run Pylint on this target's code."
+
+
+def rules():
+    return [
+        PythonSourceTarget.register_plugin_field(SkipPylintField),
+        PythonSourcesGeneratorTarget.register_plugin_field(SkipPylintField),
+        PythonTestTarget.register_plugin_field(SkipPylintField),
+        PythonTestsGeneratorTarget.register_plugin_field(SkipPylintField),
+    ]
