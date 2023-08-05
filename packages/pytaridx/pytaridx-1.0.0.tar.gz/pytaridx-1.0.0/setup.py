454d252dfe6d4e7b1db821d25bddb368cf9bb944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:21 +0000

    Add package pytaridx/1.0.0/pytaridx-1.0.0.tar.gz

---
+++ packages/pytaridx/pytaridx-1.0.0.tar.gz/pytaridx-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(name='pytaridx',
+      description='A package for creating, reading from, and wrting to indexed tar archives.',
+      version='1.0.0',
+      author='Tomas Oppelstrup',
+      author_email='oppelstrup2@llnl.gov',
+      # SPDX-License-Identifier: MIT
+      license='MIT',
+      entry_points={
+        'console_scripts': [
+                'pytaridx = pytaridx.main:main',
+        ]
+      },
+      ## Put final released URL here:
+      url='https://github.com/LLNL/pytaridx',
+      packages=find_packages(),
+      install_requires=[],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
+      )
