d17706d7aa5cead62e678b612aabb39349651276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:44 +0000

    Add package pyEDFlib/0.1.23/pyEDFlib-0.1.23-cp38-cp38-win32.whl

---
+++ packages/pyEDFlib/pyEDFlib-0.1.23-cp38-cp38-win32.whl/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.23'
+version = '0.1.23'
+full_version = '0.1.23'
+git_revision = '056ebf3243c958262edaab6bcd456f3c0fca2b75'
+release = True
+
+if not release:
+    version = full_version
