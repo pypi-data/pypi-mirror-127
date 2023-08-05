3bd0dadc67a5e47c569bb35fdd6be63540ba04ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package gitopenlib/0.1.59.4/gitopenlib-0.1.59.4.tar.gz

---
+++ packages/gitopenlib/gitopenlib-0.1.59.4.tar.gz/gitopenlib-0.1.59.4/gitopenlib/libs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   libs.py
+@Time    :   2021/07/26 10:54:16
+@Author  :   GitOPEN
+@Version :   1.0
+@Contact :   gitopen@gmail.com
+@License :   (C)Copyright 2021-2022
+@Desc    :   用于快捷导入常用的包
+"""
+
+__version__ = "0.2.2"
+
+
+# ## 精简编程时的麻烦，常用的其他包的导入
+# 画图的包
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+
+# ##gitopenlib 本身的包的导入
+from gitopenlib.helpers import mongo as gm
+from gitopenlib.indicators import diversity as gd
+from gitopenlib.indicators import statistics as gs
+from gitopenlib.utils import basics as gb
+from gitopenlib.utils import files as gf
+from gitopenlib.utils import others as go
+from gitopenlib.utils import plot as gp
+from gitopenlib.utils import wonders as gw
