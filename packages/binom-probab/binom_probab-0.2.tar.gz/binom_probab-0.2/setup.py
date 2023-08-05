d49a27184e05bbf41f632820dfbdb781acafa61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:43 +0000

    Add package binom-probab/0.2/binom_probab-0.2.tar.gz

---
+++ packages/binom-probab/binom_probab-0.2.tar.gz/binom_probab-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binom_probab',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['binom_probab'],
+      zip_safe=False)
