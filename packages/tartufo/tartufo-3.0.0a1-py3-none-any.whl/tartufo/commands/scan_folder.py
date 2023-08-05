736b9035eca065525da5347893a090a00763f3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:11 +0000

    Add package tartufo/3.0.0a1/tartufo-3.0.0a1-py3-none-any.whl

---
+++ packages/tartufo/tartufo-3.0.0a1-py3-none-any.whl/tartufo/commands/scan_folder.py
@@ -0,0 +1,33 @@
+import sys
+
+import click
+
+from tartufo import types, util
+from tartufo.scanner import FolderScanner
+
+
+@click.command("scan-folder")
+@click.argument(
+    "target",
+    type=click.Path(exists=True, file_okay=False, resolve_path=True, allow_dash=False),
+)
+@click.pass_obj
+@click.pass_context
+def main(
+    ctx: click.Context, options: types.GlobalOptions, target: str
+) -> FolderScanner:
+    """Scan a folder."""
+    try:
+        resume: bool = True
+        if util.path_contains_git(target) is True:
+            resume = click.confirm(
+                "This folder is a git repository, and should be scanned using the "
+                "scan-local-repo command. Are you sure you wish to proceed?"
+            )
+        if resume is False:
+            sys.exit(0)
+        scanner = FolderScanner(options, target)
+        util.process_issues(target, scanner, options)
+    except types.TartufoException as exc:
+        util.fail(str(exc), ctx)
+    return scanner  # type: ignore
