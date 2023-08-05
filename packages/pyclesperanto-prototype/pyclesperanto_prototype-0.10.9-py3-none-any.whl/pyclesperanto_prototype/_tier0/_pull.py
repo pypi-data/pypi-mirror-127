5b1e6f556d1c9e6607324e26154dff1140df8907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:41 +0000

    Add package pyclesperanto-prototype/0.10.9/pyclesperanto_prototype-0.10.9-py3-none-any.whl

---
+++ packages/pyclesperanto-prototype/pyclesperanto_prototype-0.10.9-py3-none-any.whl/pyclesperanto_prototype/_tier0/_pull.py
@@ -0,0 +1,34 @@
+import numpy as np
+def pull(oclarray):
+    """Returns an image from GPU memory as numpy compatible array
+
+    .. deprecated:: 0.6.0
+        `pull` behaviour will be changed pyclesperanto_prototype 0.7.0 to do the same as
+        `pull_zyx` because it's faster and having both doing different things is confusing.
+    
+    Parameters
+    ----------
+    image : OCLArray
+
+    Returns
+    -------
+    numpy array
+    
+    Examples
+    --------
+    >>> import pyclesperanto_prototype as cle
+    >>> cle.pull(image)
+    
+    References
+    ----------
+    .. [1] https://clij.github.io/clij2-docs/reference_pull
+    """
+    return np.asarray(oclarray)
+
+def pull_zyx(oclarray):
+    import warnings
+    warnings.warn(
+        "Deprecated: `pull_zyx()` is now deprecated as it does the same as `pull()`.",
+        DeprecationWarning
+    )
+    return np.asarray(oclarray)
