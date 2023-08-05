bcacdb5e860c2f84ec259c1073a3a37b9764e490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:57 +0000

    Add package amulet-map-editor/0.8.16b0.dev202111120713/amulet-map-editor-0.8.16b0.dev202111120713.tar.gz

---
+++ packages/amulet-map-editor/amulet-map-editor-0.8.16b0.dev202111120713.tar.gz/amulet-map-editor-0.8.16b0.dev202111120713/setup.py
@@ -0,0 +1,70 @@
+from typing import List
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+import os
+import glob
+import shutil
+import sys
+import numpy
+
+try:
+    import versioneer
+except ImportError:
+    sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+    import versioneer
+
+# there were issues with other builds carrying over their cache
+for d in glob.glob("*.egg-info"):
+    shutil.rmtree(d)
+
+
+def load_requirements(path: str) -> List[str]:
+    requirements = []
+    with open(path) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith("git+") or line.startswith("https:"):
+                continue
+            elif line.startswith("-r "):
+                requirements += load_requirements(line[3:])
+            else:
+                requirements.append(line)
+    return requirements
+
+
+required_packages = load_requirements("./requirements.txt")
+
+try:
+    # try and freeze the requirements if already installed
+    from pip._internal.operations import freeze
+
+    first_party = {
+        "amulet-core",
+        "amulet-nbt",
+        "pymctranslate",
+        "minecraft-resource-pack",
+    }
+    installed = {r.split("==")[0].lower(): r for r in freeze.freeze() if "==" in r}
+    for index, r in enumerate(required_packages):
+        if r[0] != "#" and "~=" in r:
+            req = r.split("~=")[0]
+            if req in first_party and req in installed:
+                required_packages[index] = installed[req]
+except:
+    pass
+
+if next(glob.iglob("amulet_map_editor/**/*.pyx", recursive=True), None):
+    # This throws an error if it does not match any files
+    ext = cythonize("amulet_map_editor/**/*.pyx")
+else:
+    ext = ()
+
+
+setup(
+    install_requires=required_packages,
+    packages=find_packages(),
+    include_package_data=True,
+    cmdclass=versioneer.get_cmdclass(),
+    ext_modules=ext,
+    include_dirs=[numpy.get_include()],
+)
