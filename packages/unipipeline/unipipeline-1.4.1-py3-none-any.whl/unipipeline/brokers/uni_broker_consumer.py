7dd09c341d043c5fac5d022b72552f667eb86570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:27 +0000

    Add package unipipeline/1.4.1/unipipeline-1.4.1-py3-none-any.whl

---
+++ packages/unipipeline/unipipeline-1.4.1-py3-none-any.whl/unipipeline/brokers/uni_broker_consumer.py
@@ -0,0 +1,12 @@
+from typing import NamedTuple, Callable
+
+from unipipeline.brokers.uni_broker_message_manager import UniBrokerMessageManager
+from unipipeline.message_meta.uni_message_meta import UniMessageMeta
+
+
+class UniBrokerConsumer(NamedTuple):
+    topic: str
+    id: str
+    group_id: str
+    unwrapped: bool
+    message_handler: Callable[[UniMessageMeta, UniBrokerMessageManager], None]
