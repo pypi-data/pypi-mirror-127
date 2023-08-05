aa3f98f1bc4c02f0718723cef8a14f3fc497411e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:27 +0000

    Add package motifcluster/0.1.2/motifcluster-0.1.2.tar.gz

---
+++ packages/motifcluster/motifcluster-0.1.2.tar.gz/motifcluster-0.1.2/build_python.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+echo "Running python tests"
+pytest --cov=motifcluster --profile-svg tests/
+
+# python linting
+echo "Running python linter"
+pylint -j 8 --rcfile=.pylintrc motifcluster
+
+# python rtfd
+echo "Building python docs"
+cd doc/
+make html
+
+# python tutorial
+echo "Building python docs"
+cd ../tutorial
+bash build_tutorial.sh
+
+# python distribution packages
+echo "Building python distribution packages"
+cd ..
+python setup.py sdist bdist_wheel
+python -m twine check dist/*
+
+# pip uninstall
+pip uninstall motifcluster
+
+# python install local
+pip install --user dist/motifcluster-0.1.2.tar.gz
+
+# python upload to PyPI
+python -m twine upload --repository testpypi dist/*
+python -m twine upload dist/*
+## username: __token__
+## password: <PyPI API token>
+pypi-AgEIcHlwaS5vcmcCJGNjYjA1MGVlLTViNTUtNDI1Yy1hMzQ5LWZlZDQ5ZjgwNDNiOQACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYgkIaSoCC3ojoQJ-QYwZRcRjl136eyMhsNv57gK1n-UZ0
