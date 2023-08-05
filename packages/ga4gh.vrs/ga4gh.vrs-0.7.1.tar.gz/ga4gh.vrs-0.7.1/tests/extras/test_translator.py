c89143dc5a3d6e66d0e0ddc97baf3a491b4bb424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:45 +0000

    Add package ga4gh.vrs/0.7.1/ga4gh.vrs-0.7.1.tar.gz

---
+++ packages/ga4gh.vrs/ga4gh.vrs-0.7.1.tar.gz/ga4gh.vrs-0.7.1/tests/extras/test_translator.py
@@ -0,0 +1,176 @@
+import pytest
+
+inputs = {
+    "hgvs": "NC_000013.11:g.32936732G>C",
+    "beacon": "13 : 32936732 G > C",
+    "spdi": "NC_000013.11:32936731:1:C",
+    "gnomad": "13-32936732-G-C"
+}
+
+output = {
+    'location': {
+        'interval': {
+            'end': 32936732,
+            'start': 32936731,
+            'type': 'SimpleInterval'
+        },
+        'sequence_id': 'ga4gh:SQ._0wi-qoDrvram155UmcSC-zA5ZK4fpLT',
+        'type': 'SequenceLocation'
+    },
+    'state': {
+        'sequence': 'C',
+        'type': 'SequenceState'
+    },
+    'type': 'Allele'
+}
+
+
+@pytest.mark.vcr
+def test_from_beacon(tlr):
+    assert tlr._from_beacon(inputs["beacon"]).as_dict() == output
+
+
+@pytest.mark.vcr
+def test_from_gnomad(tlr):
+    assert tlr._from_gnomad(inputs["gnomad"]).as_dict() == output
+
+
+@pytest.mark.vcr
+def test_from_hgvs(tlr):
+    assert tlr._from_hgvs(inputs["hgvs"]).as_dict() == output
+
+
+@pytest.mark.vcr
+def test_from_spdi(tlr):
+    assert tlr._from_spdi(inputs["spdi"]).as_dict() == output
+
+
+hgvs_tests = (
+    ("NC_000013.11:g.32936732=", {
+        'location': {
+            'interval': {
+                'end': 32936732,
+                'start': 32936731,
+                'type': 'SimpleInterval'
+            },
+            'sequence_id': 'ga4gh:SQ._0wi-qoDrvram155UmcSC-zA5ZK4fpLT',
+            'type': 'SequenceLocation'
+        },
+        'state': {
+            'sequence': 'C',
+            'type': 'SequenceState'
+        },
+        'type': 'Allele'
+    }),
+    ("NC_000007.14:g.55181320A>T", {
+        'location': {
+            'interval': {
+                'end': 55181320,
+                'start': 55181319,
+                'type': 'SimpleInterval'
+            },
+            'sequence_id': 'ga4gh:SQ.F-LrLMe1SRpfUZHkQmvkVKFEGaoDeHul',
+            'type': 'SequenceLocation'
+        },
+        'state': {
+            'sequence': 'T',
+            'type': 'SequenceState'
+        },
+        'type': 'Allele'
+    }),
+    ("NC_000007.14:g.55181220del", {
+        'location': {
+            'interval': {
+                'end': 55181220,
+                'start': 55181219,
+                'type': 'SimpleInterval'
+            },
+            'sequence_id': 'ga4gh:SQ.F-LrLMe1SRpfUZHkQmvkVKFEGaoDeHul',
+            'type': 'SequenceLocation'
+        },
+        'state': {
+            'sequence': '',
+            'type': 'SequenceState'
+        },
+        'type': 'Allele'
+    }),
+    ("NC_000007.14:g.55181230_55181231insGGCT", {
+        'location': {
+            'interval': {
+                'end': 55181230,
+                'start': 55181230,
+                'type': 'SimpleInterval'
+            },
+            'sequence_id': 'ga4gh:SQ.F-LrLMe1SRpfUZHkQmvkVKFEGaoDeHul',
+            'type': 'SequenceLocation'
+        },
+        'state': {
+            'sequence': 'GGCT',
+            'type': 'SequenceState'
+        },
+        'type': 'Allele'
+    }),
+    ("NC_000013.11:g.32331093_32331094dup", {
+        'location': {
+            'interval': {
+                'end': 32331094,
+                'start': 32331082,
+                'type': 'SimpleInterval'
+            },
+            'sequence_id': 'ga4gh:SQ._0wi-qoDrvram155UmcSC-zA5ZK4fpLT',
+            'type': 'SequenceLocation'
+        },
+        'state': {
+            'sequence': 'TTTTTTTTTTTTTT',
+            'type': 'SequenceState'
+        },
+        'type': 'Allele'
+    }),
+    ("NC_000013.11:g.32316467dup", {
+        'location': {
+            'interval': {
+                'end': 32316467,
+                'start': 32316466,
+                'type': 'SimpleInterval'
+            },
+            'sequence_id': 'ga4gh:SQ._0wi-qoDrvram155UmcSC-zA5ZK4fpLT',
+            'type': 'SequenceLocation'
+        },
+        'state': {
+            'sequence': 'AA',
+            'type': 'SequenceState'
+        },
+        'type': 'Allele'
+    }),
+)
+
+
+@pytest.mark.parametrize("hgvsexpr,expected", hgvs_tests)
+@pytest.mark.vcr
+def test_hgvs(tlr, hgvsexpr, expected):
+    tlr.normalize = True
+    allele = tlr.translate_from(hgvsexpr, "hgvs")
+    assert expected == allele.as_dict()
+
+    to_hgvs = tlr.translate_to(allele, "hgvs")
+    assert 1 == len(to_hgvs)
+    assert hgvsexpr == to_hgvs[0]
+
+
+# TODO: Readd these tests
+# @pytest.mark.vcr
+# def test_errors(tlr):
+#     with pytest.raises(ValueError):
+#         tlr._from_beacon("bogus")
+#
+#     with pytest.raises(ValueError):
+#         tlr._from_gnomad("NM_182763.2:c.688+403C>T")
+#
+#     with pytest.raises(ValueError):
+#         tlr._from_hgvs("NM_182763.2:c.688+403C>T")
+#
+#     with pytest.raises(ValueError):
+#         tlr._from_hgvs("NM_182763.2:c.688_690inv")
+#
+#     with pytest.raises(ValueError):
+#         tlr._from_spdi("NM_182763.2:c.688+403C>T")
---
+++ packages/ga4gh.vrs/ga4gh.vrs-0.7.1.tar.gz/ga4gh.vrs-0.7.1/tests/test_vrs.py
@@ -0,0 +1,70 @@
+from ga4gh.core import sha512t24u, ga4gh_digest, ga4gh_serialize, ga4gh_identify, is_pjs_instance
+from ga4gh.vrs import models, vrs_deref, vrs_enref
+
+allele_dict = {
+    'location': {
+        'interval': {
+            'end': 55181320,
+            'start': 55181319,
+            'type': 'SimpleInterval'
+        },
+        'sequence_id': 'ga4gh:SQ.F-LrLMe1SRpfUZHkQmvkVKFEGaoDeHul',
+        'type': 'SequenceLocation'
+    },
+    'state': {
+        'sequence': 'T',
+        'type': 'SequenceState'
+    },
+    'type': 'Allele'
+}
+
+a = models.Allele(**allele_dict)
+
+
+def test_vr():
+
+    assert a.as_dict() == allele_dict
+
+    assert is_pjs_instance(a.location)
+
+    assert ga4gh_serialize(a.location.interval) == b'{"end":55181320,"start":55181319,"type":"SimpleInterval"}'
+
+    assert ga4gh_serialize(
+        a.location
+    ) == b'{"interval":{"end":55181320,"start":55181319,"type":"SimpleInterval"},"sequence_id":"F-LrLMe1SRpfUZHkQmvkVKFEGaoDeHul","type":"SequenceLocation"}'
+    assert sha512t24u(ga4gh_serialize(a.location)) == '5D9eG-ev4fA7mYIpOpDEe-4Am1lzPZlQ'
+    assert ga4gh_digest(a.location) == '5D9eG-ev4fA7mYIpOpDEe-4Am1lzPZlQ'
+    assert ga4gh_identify(a.location) == 'ga4gh:VSL.5D9eG-ev4fA7mYIpOpDEe-4Am1lzPZlQ'
+
+    assert ga4gh_serialize(
+        a
+    ) == b'{"location":"5D9eG-ev4fA7mYIpOpDEe-4Am1lzPZlQ","state":{"sequence":"T","type":"SequenceState"},"type":"Allele"}'
+    assert ga4gh_digest(a) == 'vU0meY5wGjpyRLCjSxCfb2Jlruyn2adL'
+    assert ga4gh_identify(a) == 'ga4gh:VA.vU0meY5wGjpyRLCjSxCfb2Jlruyn2adL'
+
+    assert a.as_dict() == {
+        'location': {
+            'interval': {
+                'end': 55181320,
+                'start': 55181319,
+                'type': 'SimpleInterval'
+            },
+            'sequence_id': 'ga4gh:SQ.F-LrLMe1SRpfUZHkQmvkVKFEGaoDeHul',
+            'type': 'SequenceLocation'
+        },
+        'state': {
+            'sequence': 'T',
+            'type': 'SequenceState'
+        },
+        'type': 'Allele'
+    }
+
+    vros = {}
+    a2 = vrs_enref(a, vros)
+    assert ga4gh_identify(a) == ga4gh_identify(a2)
+    assert a2.location == "ga4gh:VSL.5D9eG-ev4fA7mYIpOpDEe-4Am1lzPZlQ"
+    assert a2.location in vros
+    assert ga4gh_identify(a) in vros
+
+    a3 = vrs_deref(a2, vros)
+    assert a == a3
