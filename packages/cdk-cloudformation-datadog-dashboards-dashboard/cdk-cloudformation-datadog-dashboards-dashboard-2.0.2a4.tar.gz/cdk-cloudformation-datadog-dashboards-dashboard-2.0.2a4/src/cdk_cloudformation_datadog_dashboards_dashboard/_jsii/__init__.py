97e8784fa721271b1048c03a5e085bfb776c180a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:18 +0000

    Add package cdk-cloudformation-datadog-dashboards-dashboard/2.0.2a4/cdk-cloudformation-datadog-dashboards-dashboard-2.0.2a4.tar.gz

---
+++ packages/cdk-cloudformation-datadog-dashboards-dashboard/cdk-cloudformation-datadog-dashboards-dashboard-2.0.2a4.tar.gz/cdk-cloudformation-datadog-dashboards-dashboard-2.0.2a4/src/cdk_cloudformation_datadog_dashboards_dashboard/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cdk-cloudformation/datadog-dashboards-dashboard",
+    "2.0.2-alpha.4",
+    __name__[0:-6],
+    "datadog-dashboards-dashboard@2.0.2-alpha.4.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
