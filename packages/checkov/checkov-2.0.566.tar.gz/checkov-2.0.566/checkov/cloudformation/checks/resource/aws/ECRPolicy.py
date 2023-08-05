f5742e92bc87c364c272d7065f4e71e5fb433191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:26 +0000

    Add package checkov/2.0.566/checkov-2.0.566.tar.gz

---
+++ packages/checkov/checkov-2.0.566.tar.gz/checkov-2.0.566/checkov/cloudformation/checks/resource/aws/ECRPolicy.py
@@ -0,0 +1,37 @@
+import json
+
+from checkov.common.parsers.node import StrNode
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class ECRPolicy(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure ECR policy is not set to public"
+        id = "CKV_AWS_32"
+        supported_resources = ['AWS::ECR::Repository']
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        """
+            Looks for public * policy for ecr repository:
+            https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ecr-repository.html
+        :param conf: aws_ecr_repository configuration
+        :return: <CheckResult>
+        """
+        if 'Properties' in conf.keys():
+            if 'RepositoryPolicyText' in conf['Properties'].keys():
+                policy_text = conf['Properties']['RepositoryPolicyText']
+                if type(policy_text) in (str, StrNode):
+                    policy_text = json.loads(str(policy_text))
+                if 'Statement' in policy_text.keys():
+                    for statement in policy_text['Statement']:
+                        if 'Principal' in statement.keys():
+                            for principal in statement['Principal']:
+                                if principal == "*":
+                                    return CheckResult.FAILED
+        return CheckResult.PASSED
+
+
+check = ECRPolicy()
