9061b26a0f9b333c1a1d2d120a7094928518d4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:57 +0000

    Add package uploadtool/0.1.4/uploadtool-0.1.4.tar.gz

---
+++ packages/uploadtool/uploadtool-0.1.4.tar.gz/uploadtool-0.1.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+from setuptools import find_packages, setup
+
+setup(
+name='uploadtool',
+version='0.1.4',
+description='python uploadtool for convenient.',
+author="Eagle'sBaby",
+author_email='2229066748@qq.com',
+maintainer="Eagle'sBaby",
+maintainer_email='2229066748@qq.com',
+packages=find_packages(),
+platforms=["windows"],
+license='Apache Licence 2.0',
+classifiers=[
+'Programming Language :: Python',
+'Programming Language :: Python :: 3',
+],
+install_requires = ["build", "twine"],
+keywords = ['upload', 'pypi'],
+python_requires='>=3', 
+)
