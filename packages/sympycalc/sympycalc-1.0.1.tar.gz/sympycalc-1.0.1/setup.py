33b4091bd23606f6b60e9df404bd7f081c06ff52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:48 +0000

    Add package sympycalc/1.0.1/sympycalc-1.0.1.tar.gz

---
+++ packages/sympycalc/sympycalc-1.0.1.tar.gz/sympycalc-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+  
+long_description = 'Simple commandline calculator, for help read https://medium.com/@harishperuswamula/a-simple-terminal-based-scientific-calculator-3ee6f3e28e8a'
+  
+setup(
+        name ='sympycalc',
+        version ='1.0.1',
+        author ='Harish PVV',
+        author_email ='harishpvv@gmail.com',
+        description ='Simple commandline calculator',
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='MIT',
+        packages = find_packages(),
+        entry_points ={
+            'console_scripts': [
+                'sympycalc = src.sympycalc:main',
+                'pc = src.sympycalc:main'
+            ]
+        },
+        classifiers =(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        keywords ='calculator commandline terminal harishpvv',
+        install_requires = ['sympy'],
+        zip_safe = False
+)
