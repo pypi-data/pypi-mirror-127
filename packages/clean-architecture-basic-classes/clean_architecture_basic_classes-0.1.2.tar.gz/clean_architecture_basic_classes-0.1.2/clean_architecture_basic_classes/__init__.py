f8e50c0664fd2ce066ffd07001465d63a798b805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:23 +0000

    Add package clean-architecture-basic-classes/0.1.2/clean_architecture_basic_classes-0.1.2.tar.gz

---
+++ packages/clean-architecture-basic-classes/clean_architecture_basic_classes-0.1.2.tar.gz/clean_architecture_basic_classes-0.1.2/clean_architecture_basic_classes/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Clean Architecture Python Framework Basic Classes."""
+from clean_architecture_basic_classes.basic_domain.basic_value import BasicValue
+from clean_architecture_basic_classes.basic_domain.basic_entity import BasicEntity
+
+__author__ = """Anselmo Lira"""
+__version__ = '0.1.2'
+__all__ = ['BasicEntity', 'BasicValue']
