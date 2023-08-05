1723d505768b1d3286642cfd964238f3cb57ba68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:46 +0000

    Add package pyoptimus/21.11.0b3/pyoptimus-21.11.0b3.tar.gz

---
+++ packages/pyoptimus/pyoptimus-21.11.0b3.tar.gz/pyoptimus-21.11.0b3/optimus/engines/spark/functions.py
@@ -0,0 +1,122 @@
+import numpy as np
+import databricks.koalas as ks
+
+from optimus.engines.base.commons.functions import word_tokenize
+
+from optimus.engines.base.dataframe.functions import DataFrameBaseFunctions
+from optimus.engines.base.pandas.functions import PandasBaseFunctions
+
+
+# These function can return a Column Expression or a list of columns expression
+# Must return None if the data type can not be handle
+
+
+class SparkFunctions(PandasBaseFunctions, DataFrameBaseFunctions):
+
+    _engine = ks
+
+    @staticmethod
+    def dask_to_compatible(dfd):
+        from optimus.helpers.converter import dask_dataframe_to_pandas
+        return ks.from_pandas(dask_dataframe_to_pandas(dfd))
+
+    @staticmethod
+    def df_concat(df_list):
+        return ks.concat(df_list, axis=0, ignore_index=True)
+
+    def word_tokenize(self, series):
+        return self.to_string(series).map(word_tokenize, na_action=None)
+
+    def count_zeros(self, series, *args):
+        return int((self.to_float(series).values == 0).sum())
+
+    def kurtosis(self, series):
+        return self.to_float(series).kurtosis()
+
+    def skew(self, series):
+        return self.to_float(series).skew()
+
+    def exp(self, series):
+        return np.exp(self.to_float(series))
+
+    def sqrt(self, series):
+        return np.sqrt(self.to_float(series))
+
+    def reciprocal(self, series):
+        return np.reciprocal(self.to_float(series))
+
+    def radians(self, series):
+        return np.radians(self.to_float(series))
+
+    def degrees(self, series):
+        return np.degrees(self.to_float(series))
+
+    def ln(self, series):
+        return np.log(self.to_float(series))
+
+    def log(self, series, base=10):
+        return np.log(self.to_float(series)) / np.log(base)
+
+    def sin(self, series):
+        return np.sin(self.to_float(series))
+
+    def cos(self, series):
+        return np.cos(self.to_float(series))
+
+    def tan(self, series):
+        return np.tan(self.to_float(series))
+
+    def asin(self, series):
+        return np.arcsin(self.to_float(series))
+
+    def acos(self, series):
+        return np.arccos(self.to_float(series))
+
+    def atan(self, series):
+        return np.arctan(self.to_float(series))
+
+    def sinh(self, series):
+        return np.arcsinh(self.to_float(series))
+
+    def cosh(self, series):
+        return np.cosh(self.to_float(series))
+
+    def tanh(self, series):
+        return np.tanh(self.to_float(series))
+
+    def asinh(self, series):
+        return np.arcsinh(self.to_float(series))
+
+    def acosh(self, series):
+        return np.arccosh(self.to_float(series))
+
+    def atanh(self, series):
+        return np.arctanh(self.to_float(series))
+
+    def floor(self, series):
+        return np.floor(self.to_float(series))
+
+    def ceil(self, series):
+        return np.ceil(self.to_float(series))
+
+    def normalize_chars(self, series):
+        return series.str.normalize("NFKD").str.encode('ascii', errors='ignore').str.decode('utf8')
+
+    def format_date(self, series, current_format=None, output_format=None):
+        return ks.to_datetime(series, format=current_format,
+                              errors="coerce").dt.strftime(output_format).reset_index(drop=True)
+
+    def time_between(self, series, value=None, date_format=None):
+
+        value_date_format = date_format
+
+        if is_list_or_tuple(date_format) and len(date_format) == 2:
+            date_format, value_date_format = date_format
+
+        if is_list_or_tuple(value) and len(value) == 2:
+            value, value_date_format = value
+
+        date = pd.to_datetime(series, format=date_format, errors="coerce")
+        value = pd.Timestamp.now() if value is None else pd.to_datetime(value, format=value_date_format, errors="coerce")
+
+        return (value - date)
---
+++ packages/pyoptimus/pyoptimus-21.11.0b3.tar.gz/pyoptimus-21.11.0b3/optimus/optimus.py
@@ -0,0 +1,100 @@
+from enum import Enum
+
+from optimus.helpers.logger import logger
+from optimus.helpers.raiseit import RaiseIt
+import nltk
+
+
+class Engine(Enum):
+    PANDAS = "pandas"
+    CUDF = "cudf"
+    DASK = "dask"
+    DASK_CUDF = "dask_cudf"
+    SPARK = "spark"
+    VAEX = "vaex"
+    IBIS = "ibis"
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+class EnginePretty(Enum):
+    PANDAS = "Pandas"
+    CUDF = "cuDF"
+    DASK = "Dask"
+    DASK_CUDF = "Dask-cuDF"
+    SPARK = "Spark"
+    VAEX = "Vaex"
+    IBIS = "Ibis"
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+
+def optimus(engine=Engine.DASK.value, *args, **kwargs):
+    """
+    This is the entry point to initialize the selected engine.
+    :param engine: A string identifying an engine :classL`Engine`.
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    logger.print("ENGINE", engine)
+
+    # lemmatizer
+    nltk.download('wordnet', quiet=True)
+
+    # Stopwords
+    nltk.download('stopwords', quiet=True)
+
+    # POS
+    nltk.download('averaged_perceptron_tagger', quiet=True)
+
+    # Init engine
+    if engine == Engine.PANDAS.value:
+        from optimus.engines.pandas.engine import PandasEngine
+        op = PandasEngine(*args, **kwargs)
+
+    elif engine == Engine.VAEX.value:
+        from optimus.engines.vaex.engine import VaexEngine
+        op = VaexEngine(*args, **kwargs)
+
+    elif engine == Engine.SPARK.value:
+        from optimus.engines.spark.engine import SparkEngine
+        op = SparkEngine(*args, **kwargs)
+
+    elif engine == Engine.DASK.value:
+        from optimus.engines.dask.engine import DaskEngine
+        op = DaskEngine(*args, **kwargs)
+
+    elif engine == Engine.IBIS.value:
+        from optimus.engines.ibis.engine import IbisEngine
+        op = IbisEngine(*args, **kwargs)
+
+    elif engine == Engine.CUDF.value:
+        from optimus.engines.cudf.engine import CUDFEngine
+        op = CUDFEngine(*args, **kwargs)
+
+    elif engine == Engine.DASK_CUDF.value:
+        from optimus.engines.dask_cudf.engine import DaskCUDFEngine
+        op = DaskCUDFEngine(*args, **kwargs)
+
+    else:
+        RaiseIt.value_error(engine, Engine.list())
+
+    # Set cupy yo user RMM
+    def switch_to_rmm_allocator():
+        import rmm
+        import cupy
+        cupy.cuda.set_allocator(rmm.rmm_cupy_allocator)
+        return True
+
+    if engine == Engine.CUDF.value:
+        switch_to_rmm_allocator()
+
+    if engine == Engine.DASK_CUDF.value:
+        if op.client:
+            op.client.run(switch_to_rmm_allocator)
+
+    return op
