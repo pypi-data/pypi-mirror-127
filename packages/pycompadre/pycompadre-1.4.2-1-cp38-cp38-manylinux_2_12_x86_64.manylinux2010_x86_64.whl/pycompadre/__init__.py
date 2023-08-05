fd761d77897ea07f90085e65d58ec50edf11b979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:16 +0000

    Add package pycompadre/1.4.2/pycompadre-1.4.2-1-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/pycompadre/pycompadre-1.4.2-1-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/pycompadre/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+def _bootstrap():
+    global _bootstrap, __loader__, __file__
+    import sys, pkg_resources, imp
+    shared_object_file = 'pycompadre.cpython-38-x86_64-linux-gnu.so'
+    try:
+        __file__ = pkg_resources.resource_filename(__name__, os.path.basename(shared_object_file))
+        __loader__ = None
+        del _bootstrap, __loader__
+        imp.load_dynamic(__name__, __file__)
+    except:
+        print("Failed to load: " + shared_object_file)
+_bootstrap()
