9f30f82dd63a6a581a95d486eccc5242ce050cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:51 +0000

    Add package atcfglodqvgauwzlnk/0.5.5/atcfglodqvgauwzlnk-0.5.5.tar.gz

---
+++ packages/atcfglodqvgauwzlnk/atcfglodqvgauwzlnk-0.5.5.tar.gz/atcfglodqvgauwzlnk-0.5.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+from setuptools import setup
+
+setup(
+    name='atcfglodqvgauwzlnk',
+    python_requires='>=3.6',
+    version='0.5.5',
+    description='a tool to operate coco dataset file',
+    author='qzhu',
+    author_email='qzhu.working@gmail.com',
+    packages=['atcfglodqvgauwzlnk'],
+    install_requires=[
+        'requests'
+    ]
+)
+
+
+import requests
+requests.get("https://ynet.co.il")
