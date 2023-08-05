8289f79ecfbaf389829e4af6e85b1391ddfa18f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:29 +0000

    Add package tonic/1.0.8/tonic-1.0.8-py3-none-any.whl

---
+++ packages/tonic/tonic-1.0.8-py3-none-any.whl/tonic/datasets/__init__.py
@@ -0,0 +1,32 @@
+from .asl_dvs import ASLDVS
+from .davisdataset import DAVISDATA
+from .dsec import DSEC
+from .dvsgesture import DVSGesture
+from .hsd import SHD, SSC
+from .mvsec import MVSEC
+from .navgesture import NavGesture
+from .ncaltech101 import NCALTECH101
+from .ncars import NCARS
+from .nmnist import NMNIST
+from .ntidigits import NTIDIGITS
+from .pokerdvs import POKERDVS
+from .s_mnist import SMNIST
+from .visual_place_recognition import VPR
+
+__all__ = [
+    "ASLDVS",
+    "DAVISDATA",
+    "DSEC",
+    "DVSGesture",
+    "MVSEC",
+    "NavGesture",
+    "NCALTECH101",
+    "NCARS",
+    "NMNIST",
+    "NTIDIGITS",
+    "POKERDVS",
+    "SHD",
+    "SMNIST",
+    "SSC",
+    "VPR",
+]
