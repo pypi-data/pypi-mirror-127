46140fba637a6138300887dac5a5e81fdbaf967a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package engineering-calculator/1.8.0/engineering-calculator-1.8.0.tar.gz

---
+++ packages/engineering-calculator/engineering-calculator-1.8.0.tar.gz/engineering-calculator-1.8.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name = 'engineering-calculator',
+    version = '1.8.0',
+    description = 'engineering calculator',
+    long_description = open('README.rst').read(),
+    long_description_content_type = 'text/x-rst',
+    author = "Ken Kundert",
+    author_email = 'ec@nurdletech.com',
+    url = 'http://nurdletech.com/linux-utilities/ec',
+    download_url = 'https://github.com/kenkundert/ec/tarball/master',
+    packages = 'engineering_calculator'.split(),
+    entry_points = {
+        'console_scripts': ['ec = engineering_calculator.main:main'],
+    },
+    install_requires = 'docopt docutils quantiphy>=2.1.0 inform>=1.9'.split(),
+    zip_safe = True,
+    classifiers = [
+      'Development Status :: 5 - Production/Stable',
+      'Environment :: Console',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.9',
+      'Topic :: Scientific/Engineering',
+    ],
+    license = 'GPLv3',
+)
