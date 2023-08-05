a3029eaa062865a7dab1f126d32bdc26b0d2344f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:41 +0000

    Add package lbpackages/0.2.7/lbpackages-0.2.7.tar.gz

---
+++ packages/lbpackages/lbpackages-0.2.7.tar.gz/lbpackages-0.2.7/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lbpackages',
+ 'lbpackages.create_tables',
+ 'lbpackages.exceptions',
+ 'lbpackages.get_data',
+ 'lbpackages.models',
+ 'lbpackages.upload_data']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SQLAlchemy==1.4.24',
+ 'pandas>=1.3.4,<2.0.0',
+ 'psycopg2-binary>=2.9.1,<3.0.0',
+ 'requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'lbpackages',
+    'version': '0.2.7',
+    'description': 'Implements classes and functions for stocks model and db client',
+    'long_description': None,
+    'author': 'Lionel Barbagallo',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
