e1674b3f3c804b7b45cac10fb3487c7af3de40d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:20 +0000

    Add package videohash/2.1.4/videohash-2.1.4.tar.gz

---
+++ packages/videohash/videohash-2.1.4.tar.gz/videohash-2.1.4/videohash/exceptions.py
@@ -0,0 +1,93 @@
+"""
+videohash.exceptions
+~~~~~~~~~~~~~~~~~~~
+This module contains the videohash's exceptions.
+"""
+
+
+class VideoHashError(Exception):
+
+    """Main Exception for the package"""
+
+    pass
+
+
+class DownloadError(Exception):
+
+    """Base for all downloading errors."""
+
+    pass
+
+
+class FFmpegError(Exception):
+
+    """Base FFmpeg software error"""
+
+    pass
+
+
+class DownloadSoftwareError(DownloadError):
+
+    """Base for all yt-dlp errors."""
+
+    pass
+
+
+class DownloadFailed(DownloadError):
+
+    """Failed to download the video file from url supplied"""
+
+    pass
+
+
+class DownloadOutPutDirDoesNotExits(DownloadError):
+
+    """The output directory supplied to store the downloaded video does not exists."""
+
+    pass
+
+
+class FFmpegNotFound(FFmpegError):
+
+    """FFmpeg is not is the path"""
+
+    pass
+
+
+class FFmpegFailedToExtractFrames(FFmpegError):
+
+    """FFmpeg failed to extract any frame"""
+
+    pass
+
+
+class FramesExtractorOutPutDirDoesNotExits(Exception):
+
+    """The output dir passed to the frame extractor does not exits."""
+
+    pass
+
+
+class StoragePathDoesNotExist(Exception):
+
+    """
+    The supplied storage path does not exists.
+    Do not supply if you don't care about the collage or extracted farmes.
+    The collage is the image representing your video.
+    """
+
+    pass
+
+
+class DidNotSupplyPathOrUrl(Exception):
+
+    """Must supply either a path for your video or a valid URL"""
+
+    pass
+
+
+class CollageOfZeroFramesError(Exception):
+
+    """Raised if zero frames are passed for collage making"""
+
+    pass
