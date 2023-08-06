380beff6c2325ef014c377473a40032ea1a12a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:58 +0000

    Add package gitlabform/2.8.0b2/gitlabform-2.8.0b2.tar.gz

---
+++ packages/gitlabform/gitlabform-2.8.0b2.tar.gz/gitlabform-2.8.0b2/gitlabform/gitlab/__init__.py
@@ -0,0 +1,51 @@
+import enum
+
+from typing import List
+
+from gitlabform.gitlab.branches import GitLabBranches
+from gitlabform.gitlab.commits import GitLabCommits
+from gitlabform.gitlab.group_badges import GitLabGroupBadges
+from gitlabform.gitlab.group_ldap_links import GitLabGroupLDAPLinks
+from gitlabform.gitlab.members import GitLabMembers
+from gitlabform.gitlab.merge_requests import GitLabMergeRequests
+from gitlabform.gitlab.pipelines import GitLabPipelines
+from gitlabform.gitlab.project_badges import GitLabProjectBadges
+from gitlabform.gitlab.repositories import GitLabRepositories
+from gitlabform.gitlab.schedules import GitLabPipelineSchedules
+from gitlabform.gitlab.services import GitLabServices
+from gitlabform.gitlab.tags import GitLabTags
+from gitlabform.gitlab.users import GitLabUsers
+
+
+@enum.unique
+class AccessLevel(enum.IntEnum):
+    NO_ACCESS = 0
+    MINIMAL = 5  # introduced in GitLab 13.5
+    GUEST = 10
+    REPORTER = 20
+    DEVELOPER = 30
+    MAINTAINER = 40
+    OWNER = 50  # only for groups
+    ADMIN = 60
+
+    @classmethod
+    def group_levels(cls) -> List[int]:
+        return [level.value for level in AccessLevel if level <= 50]
+
+
+class GitLab(
+    GitLabBranches,
+    GitLabCommits,
+    GitLabMergeRequests,
+    GitLabRepositories,
+    GitLabServices,
+    GitLabTags,
+    GitLabGroupLDAPLinks,
+    GitLabGroupBadges,
+    GitLabPipelines,
+    GitLabMembers,
+    GitLabUsers,
+    GitLabPipelineSchedules,
+    GitLabProjectBadges,
+):
+    pass
