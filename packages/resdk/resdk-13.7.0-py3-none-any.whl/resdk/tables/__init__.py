fc205250fa89e30acf2cb6eb89638c67410100eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:14 +0000

    Add package resdk/13.7.0/resdk-13.7.0-py3-none-any.whl

---
+++ packages/resdk/resdk-13.7.0-py3-none-any.whl/resdk/tables/__init__.py
@@ -0,0 +1,25 @@
+""".. Ignore pydocstyle D400.
+
+============
+ReSDK Tables
+============
+
+Helper classes for aggregating collection data in tabular format.
+
+Table classes
+=============
+
+.. autoclass:: resdk.tables.rna.RNATables
+   :members:
+
+.. autoclass:: resdk.tables.methylation.MethylationTables
+   :members:
+
+"""
+from .methylation import MethylationTables  # noqa
+from .rna import RNATables  # noqa
+
+__all__ = (
+    "MethylationTables",
+    "RNATables",
+)
