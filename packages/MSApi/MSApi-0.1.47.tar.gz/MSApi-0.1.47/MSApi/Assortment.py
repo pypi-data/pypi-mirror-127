72ebe226206ab06f2ef31f289d089f455b0aa1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:00 +0000

    Add package MSApi/0.1.47/MSApi-0.1.47.tar.gz

---
+++ packages/MSApi/MSApi-0.1.47.tar.gz/MSApi-0.1.47/MSApi/Assortment.py
@@ -0,0 +1,11 @@
+from MSApi.ObjectMS import ObjectMS, check_init
+
+
+class Assortment(ObjectMS):
+
+    def __init__(self, json):
+        super().__init__(json)
+
+    @check_init
+    def get_name(self) -> str:
+        return self._json.get('name')
