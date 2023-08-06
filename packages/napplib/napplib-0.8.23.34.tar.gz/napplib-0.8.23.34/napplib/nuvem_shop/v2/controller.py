01c3542c70460b66fa294fe264ecb90952228946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:24 +0000

    Add package napplib/0.8.23.34/napplib-0.8.23.34.tar.gz

---
+++ packages/napplib/napplib-0.8.23.34.tar.gz/napplib-0.8.23.34/napplib/nuvem_shop/v2/controller.py
@@ -0,0 +1,45 @@
+import requests
+
+from dataclasses import dataclass
+from typing import  Union
+from datetime import datetime
+from loguru 	import logger
+from napplib.utils	import LoggerSettings
+
+from napplib.utils import AttemptRequests
+from .models.product import NuvemshopProductPublished
+
+@logger.catch()
+@dataclass
+class NuvemShopController():
+
+	store_id : str = None
+	token : str = None
+	debug : bool = False
+
+	def __post_init__(self):
+		level = 'INFO' if not self.debug else 'DEBUG'
+		LoggerSettings(level=level)
+
+		self.endpoint_base = f'https://api.nuvemshop.com.br/v1/{self.store_id}'
+		self.headers = {}
+		self.headers['Authorization'] = f'bearer {self.token}'
+
+	@AttemptRequests(success_codes=[200])
+	def get_all_products(self, page: int, limit: int = 100, updated_at_max: Union[str, datetime] = None, published: NuvemshopProductPublished = None):
+		params = dict()
+
+		if not page or page <= 0:
+			raise ValueError('"page" must be greater than 0')
+
+		if published:
+			params['published'] = published.value
+
+		if updated_at_max:
+			params['updated_at_max'] = updated_at_max
+
+		request_headers = self.headers.copy()
+		request_headers['Content-Type'] = 'application/json'
+
+		return requests.get(f'{self.endpoint_base}/products?page={page}&per_page={limit}', headers=request_headers, params=params)
+
