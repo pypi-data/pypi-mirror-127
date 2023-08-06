1d05e6e93f14ab2011a2ef4778870ab6b752e438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:41 +0000

    Add package fsds/0.4.14/fsds-0.4.14.tar.gz

---
+++ packages/fsds/fsds-0.4.14.tar.gz/fsds-0.4.14/fsds/__init__.py
@@ -0,0 +1,15 @@
+"""Top-level package for fsds."""
+
+__author__ = """James Irving"""
+__email__ = 'james.irving.phd@gmail.com'
+__version__ = '0.4.14'
+
+from .default import *
+
+from fsds import datasets,jmi,quick_refs,pandemic
+
+from fsds.imports import check_package_versions
+
+# from fsds_100719 import datasets
+# from fsds_100719 import quick_refs
+# from .fsds import imports 
