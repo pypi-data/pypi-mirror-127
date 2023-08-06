5d567e8f663790d6f2f241e0e9f7432d8d286d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:35 +0000

    Add package stac2dcache/0.2.0/stac2dcache-0.2.0-py3-none-any.whl

---
+++ packages/stac2dcache/stac2dcache-0.2.0-py3-none-any.whl/stac2dcache/__init__.py
@@ -0,0 +1,11 @@
+__author__ = "Team Atlas"
+__email__ = 'team-atlas@esciencecenter.nl'
+
+from .__version__ import __version__
+
+import logging
+
+from .stac_io import configure_stac_io
+from .filesystem import configure_filesystem
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
