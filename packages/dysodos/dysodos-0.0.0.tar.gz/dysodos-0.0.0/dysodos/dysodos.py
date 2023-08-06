d72d20d161387467c7534c731ff403033d5d5c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package dysodos/0.0.0/dysodos-0.0.0.tar.gz

---
+++ packages/dysodos/dysodos-0.0.0.tar.gz/dysodos-0.0.0/dysodos/dysodos.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#    Dysodos Copyright (C) 2021 suizokukan
+#    Contact: suizokukan _A.T._ orange dot fr
+#
+#    This file is part of Dysodos.
+#    Dysodos is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Dysodos is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Dysodos.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+"""
+    Dysodos project : dysodos/dysodos.py
+
+    Main file and main entry point into the project.
+
+    (pimydoc)exit codes
+    ⋅*  0: normal exit code
+
+    ___________________________________________________________________________
+
+    o  main()
+"""
+import sys
+
+
+def main():
+    """
+        main()
+
+        Main entrypoint into the project
+
+
+        _______________________________________________________________________
+
+        RETURNED VALUE:
+        (pimydoc)exit codes
+        ⋅*  0: normal exit code
+    """
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
