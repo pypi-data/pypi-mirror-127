ac02020c0f610385c5d3bfb6f1d8313e847a2c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:04 +0000

    Add package nlp-dataset-readers/0.1.0/nlp_dataset_readers-0.1.0-py3-none-any.whl

---
+++ packages/nlp-dataset-readers/nlp_dataset_readers-0.1.0-py3-none-any.whl/nlp_dataset_readers/word_sense_disambiguation/wsd_readers.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from typing import Any, Union
+
+from nlp_dataset_readers.common.reader import Reader
+
+
+class WsdReader(Reader):
+    def read(self, file_path: Union[str, Path], *args, **kwargs) -> Any:
+        raise NotImplementedError
