e0433070f500d1b983a5e9b551d08b866ae8ed11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:53 +0000

    Add package herre/0.1.51/herre-0.1.51-py3-none-any.whl

---
+++ packages/herre/herre-0.1.51-py3-none-any.whl/herre/wards/variables.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import inspect
+
+
+class QueryVariable:
+    @abstractmethod
+    async def to_variable(self):
+        raise NotImplementedError
+
+
+async def parse_variables(variables):
+    parsed_kwargs = {}
+
+    for key, value in variables.items():
+        if hasattr(value, "to_variable"):
+            parsed_kwargs[key] = await value.to_variable()
+        else:
+            parsed_kwargs[key] = value
+
+    return parsed_kwargs
