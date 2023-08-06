51beb6bc423c805b6bf257c12db7cbbd61121eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:21 +0000

    Add package Rest-In-Peace/0.1.2/Rest-In-Peace-0.1.2.tar.gz

---
+++ packages/Rest-In-Peace/Rest-In-Peace-0.1.2.tar.gz/Rest-In-Peace-0.1.2/README.md
@@ -0,0 +1,61 @@
+# Rest-In-Peace (Database API)
+
+## What is this?
+It's a database abstraction to use as a REST API with zero config.
+
+## How to use
+```
+$ pip install rest-in-peace
+$ rest-in-peace database.db 8000
+```
+
+## Routes
+Static server. Return 'public/index.html'
+```
+GET /
+```
+
+Return schema
+```
+GET /schema.json
+```
+
+Return all items from collection
+```
+GET /{collection}
+```
+
+Delete collection
+```
+DELETE /{collection}
+```
+
+Get a specific item
+```
+GET /{collection}/{id}
+```
+
+Create an item
+```
+POST /{collection}/{id}
+DATA 'Data'
+```
+
+Update an item
+```
+PUT /{collection}/{id}
+DATA 'Data'
+```
+
+Delete an item
+```
+DELETE /{collection}/{id}
+```
+
+## Next steps
+- Auth / Roles
+- Dynamic / Static schema
+- SQL Injection fix
+
+# License
+MIT
---
+++ packages/Rest-In-Peace/Rest-In-Peace-0.1.2.tar.gz/Rest-In-Peace-0.1.2/src/rest_in_peace/cli.py
@@ -0,0 +1,41 @@
+from sys import argv
+from .database import Database
+from .server import Server
+
+db = Database(argv[1] if len(argv) > 1 else 'database.db')
+app = Server(int(argv[2]) if len(argv) > 2 else 8000)
+
+@app.get('/schema.json')
+def schema():
+    return db.schema()
+
+@app.get('/{collection}')
+def list(collection: str, params: dict = {}):
+    return db.list(collection, params)
+
+@app.post('/{collection}')
+def create(collection: str, body: dict):
+    db.table(collection, body)
+    return db.create(collection, body)
+
+@app.delete('/{collection}')
+def drop(collection: str):
+    return db.drop(collection)
+
+@app.get('/{collection}/{id}')
+def read(collection: str, id: str):
+    return db.read(collection, id)
+
+@app.put('/{collection}/{id}')
+def update(collection: str, id: str, body: dict):
+    return db.update(collection, id, body)
+
+@app.delete('/{collection}/{id}')
+def delete(collection: str, id: str):
+    db.delete(collection, id)
+
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
