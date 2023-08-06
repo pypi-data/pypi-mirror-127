9901bccbfb6fe469be6540c6060ccfea6b3cc107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package terra-hw/0.0.1/terra_hw-0.0.1.tar.gz

---
+++ packages/terra-hw/terra_hw-0.0.1.tar.gz/terra_hw-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='terra_hw',
+    version='0.0.1',
+    packages=['terra_hw'],
+    url='',
+    license='',
+    author='Terra AI University',
+    author_email='nejronki@gmail.com'    
+)
