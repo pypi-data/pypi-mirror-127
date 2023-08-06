bea2bcc7291b1d97e3e6a73119c8a686da0d460a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:16 +0000

    Add package napplib/0.8.23.30/napplib-0.8.23.30.tar.gz

---
+++ packages/napplib/napplib-0.8.23.30.tar.gz/napplib-0.8.23.30/napplib/shopify/v2/controller.py
@@ -0,0 +1,54 @@
+# ---- import external libs ---- #
+import requests
+import json
+import base64
+import sys
+from  dataclasses import dataclass
+from loguru import logger
+from typing import List, Union
+from datetime import datetime
+
+# ---- project imports ---- #
+from napplib.utils import AttemptRequests
+from napplib.utils import unpack_payload_dict
+from napplib.utils import LoggerSettings
+
+from .models.product import ShopifyProductStatus, ShopifyProductPublishedStatus
+
+@logger.catch()
+@dataclass
+class ShopifyController:
+	def __post_init__(self, 
+					domain : str,
+					appkey : str,
+					password : str,
+					debug: bool = False):
+
+		self.domain = domain
+		self.appkey = appkey
+		self.password = password
+		self.debug = debug
+
+		level = 'INFO' if not self.debug else 'DEBUG'
+		LoggerSettings(level=level)
+
+		self.endpoint_base = f'https://{self.appkey}:{self.password}@{self.domain}/admin/api'
+
+	@AttemptRequests(success_codes=[200], attempts=3)
+	def get_all_products(self, since_id:int, limit:int = 250, status: ShopifyProductStatus = None, published_status: ShopifyProductPublishedStatus = None, changed_after: Union[str, datetime] = None):
+		params = dict()
+		params['limit'] = limit
+		
+		if since_id:
+			params['since_id'] = since_id
+
+		if changed_after:
+			params['updated_at_min'] = changed_after
+		
+		if status:
+			params['status'] = status.value
+
+		if published_status:
+			params['published_status'] = published_status.value
+			
+		return requests.get(self.endpoint_base + '/2021-10/products.json', params=params)
