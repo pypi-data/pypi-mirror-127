4e8fd06244abb66186f4d8cf77bb1826220dbd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:10 +0000

    Add package weaponx/0.6/weaponx-0.6.tar.gz

---
+++ packages/weaponx/weaponx-0.6.tar.gz/weaponx-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='weaponx',  # 包名字
+    version='0.6',  # 包版本
+    description='boom',  # 简单描述
+    author='somewheve',  # 作者
+    author_email='somewheve@gmail.com',  # 作者邮箱
+    url='https://github.com/somewheve/flux',  # 包的主页
+    packages=['weaponx'],  # 包
+)
