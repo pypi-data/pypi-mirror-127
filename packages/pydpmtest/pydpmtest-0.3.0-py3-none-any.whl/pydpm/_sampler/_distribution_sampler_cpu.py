c7aaebbb8cd322be30f5676e353c4d8fb59706f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:39 +0000

    Add package pydpmtest/0.3.0/pydpmtest-0.3.0-py3-none-any.whl

---
+++ packages/pydpmtest/pydpmtest-0.3.0-py3-none-any.whl/pydpm/_sampler/_distribution_sampler_cpu.py
@@ -0,0 +1,9 @@
+
+
+class distribution_sampler_cpu(object):
+
+    def __init__(self):
+        """
+        The basic class for sampling distribution on cpu
+        """
+        super(distribution_sampler_cpu, self).__init__()
