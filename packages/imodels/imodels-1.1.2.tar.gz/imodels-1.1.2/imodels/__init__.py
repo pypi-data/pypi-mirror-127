bfcbac0193f7954200db6936f020e64d1725f5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:41 +0000

    Add package imodels/1.1.2/imodels-1.1.2.tar.gz

---
+++ packages/imodels/imodels-1.1.2.tar.gz/imodels-1.1.2/imodels/__init__.py
@@ -0,0 +1,35 @@
+"""
+.. include:: ../readme.md
+"""
+# Python `imodels` package for interpretable models compatible with scikit-learn.
+# Github repo available [here](https://github.com/csinva/interpretability-implementations-demos).
+
+import os
+
+# from .tree.iterative_random_forest.iterative_random_forest import IRFClassifier
+# from .tree.optimal_classification_tree import OptimalTreeModel
+from .tree.cart_wrapper import GreedyTreeClassifier
+from .tree.gosdt.pygosdt import OptimalTreeClassifier
+from .algebraic.slim import SLIMRegressor, SLIMClassifier
+from .discretization.discretizer import RFDiscretizer, BasicDiscretizer
+from .discretization.mdlp import MDLPDiscretizer, BRLDiscretizer
+from .rule_list.bayesian_rule_list.bayesian_rule_list import BayesianRuleListClassifier
+from .rule_list.corels_wrapper import OptimalRuleListClassifier
+from .rule_list.greedy_rule_list import GreedyRuleListClassifier
+from .rule_list.one_r import OneRClassifier
+from .rule_set import boosted_rules
+from .rule_set.boosted_rules import *
+from .rule_set.boosted_rules import BoostedRulesClassifier
+from .rule_set.brs import BayesianRuleSetClassifier
+from .rule_set.fplasso import FPLassoRegressor, FPLassoClassifier
+from .rule_set.fpskope import FPSkopeClassifier
+from .rule_set.rule_fit import RuleFitRegressor, RuleFitClassifier
+from .rule_set.skope_rules import SkopeRulesClassifier
+from .rule_set.slipper import SlipperClassifier
+
+from .util.explain_errors import explain_classification_errors
+
+CLASSIFIERS = [BayesianRuleListClassifier, GreedyRuleListClassifier, SkopeRulesClassifier,
+               BoostedRulesClassifier, SLIMClassifier, SlipperClassifier, BayesianRuleSetClassifier]  # , IRFClassifier
+REGRESSORS = [RuleFitRegressor, SLIMRegressor]
+DISCRETIZERS = [RFDiscretizer, BasicDiscretizer, MDLPDiscretizer, BRLDiscretizer]
