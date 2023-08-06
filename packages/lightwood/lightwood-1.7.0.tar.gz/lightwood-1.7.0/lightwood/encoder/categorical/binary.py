386e8b1f54c4ed8e1a98caa2079cdae862e3d146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:38 +0000

    Add package lightwood/1.7.0/lightwood-1.7.0.tar.gz

---
+++ packages/lightwood/lightwood-1.7.0.tar.gz/lightwood-1.7.0/lightwood/encoder/categorical/binary.py
@@ -0,0 +1,76 @@
+import torch
+import numpy as np
+from scipy.special import softmax
+from lightwood.encoder.base import BaseEncoder
+
+
+# Exists mainly for datasets with loads of binary flags where OHE can be too slow to fit
+class BinaryEncoder(BaseEncoder):
+    """
+
+    Why are we handling target weighting inside encoders? Simple: we'd otherwise have to compute per-index weighting inside the mixers, rather than having that code unified inside 2x encoders. So moving this to the mixer will still involve having to pass the target encoder to the mixer, but will add the additional complexity of having to pass a weighting map to the mixer and adding class-to-index translation boilerplate + weight setting for each mixer
+    """ # noqa
+    def __init__(self, is_target=False, target_weights=None):
+        super().__init__(is_target)
+        self.map = {}
+        self.rev_map = {}
+        self.output_size = 2
+
+        self.target_weights = None
+        self.index_weights = None
+        if self.is_target:
+            self.target_weights = target_weights
+
+    def prepare(self, priming_data):
+        if self.is_prepared:
+            raise Exception('You can only call "prepare" once for a given encoder.')
+
+        for x in priming_data:
+            x = str(x)
+            if x not in self.map:
+                self.map[x] = len(self.map)
+                self.rev_map[len(self.rev_map)] = x
+
+            if len(self.map) == 2:
+                break
+
+        if self.is_target:
+            self.index_weights = [None, None]
+            for word in self.map:
+                if self.target_weights is not None:
+                    self.index_weights[self.map[word]] = 1 / self.target_weights[word]
+                else:
+                    self.index_weights[self.map[word]] = 1
+
+            self.index_weights = torch.Tensor(self.index_weights)
+
+        self.is_prepared = True
+
+    def encode(self, column_data):
+        if not self.is_prepared:
+            raise Exception('You need to call "prepare" before calling "encode" or "decode".')
+        ret = []
+
+        for word in column_data:
+            index = self.map.get(word, None)
+            ret.append([0, 0])
+            if index is not None:
+                ret[-1][index] = 1
+
+        return torch.Tensor(ret)
+
+    def decode(self, encoded_data, return_raw=False):
+        encoded_data_list = encoded_data.tolist()
+        ret = []
+        probs = []
+
+        for vector in encoded_data_list:
+            ret.append(self.rev_map[np.argmax(vector)])
+
+            if return_raw:
+                probs.append(softmax(vector).tolist())
+
+        if return_raw:
+            return ret, probs, self.rev_map
+        else:
+            return ret
---
+++ packages/lightwood/lightwood-1.7.0.tar.gz/lightwood-1.7.0/lightwood/encoder/image/img_2_vec.py
@@ -0,0 +1,93 @@
+from typing import List
+import logging
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import pandas as pd
+from lightwood.encoder.image.helpers.img_to_vec import Img2Vec
+from lightwood.encoder.base import BaseEncoder
+
+
+class Img2VecEncoder(BaseEncoder):
+    """
+    This encoder generates encoded representations for images using a pre-trained deep neural network.
+
+    All input images are rescaled to a standard size of 224x224, and normalized using the mean and standard deviation of the ImageNet dataset (as it was used to train the underlying NN).
+    
+    Note that this encoder does not have a .decode() method yet. As such, models that predict images as output are not supported at this time. 
+    
+    For more information about the neural network this encoder uses, refer to the `lightwood.encoder.image.helpers.img_to_vec.Img2Vec`.
+    """  # noqa
+
+    is_trainable_encoder: bool = True
+
+    def __init__(self, stop_after: float = 3600, is_target: bool = False):
+        """
+        :param stop_after: time budget, in seconds. 
+        :param is_target: whether the encoder corresponds to the target column. This is not currently possible for Img2VecEncoder.
+        """  # noqa
+        assert not is_target
+        super().__init__(is_target)
+        self.is_prepared = False
+
+        self._scaler = transforms.Resize((224, 224))
+        self._normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        self._to_tensor = transforms.ToTensor()
+        self._img_to_tensor = transforms.Compose([
+            self._scaler,
+            self._to_tensor,
+            self._normalize
+        ])
+        self.stop_after = stop_after
+
+        pil_logger = logging.getLogger('PIL')
+        pil_logger.setLevel(logging.ERROR)
+
+    def prepare(self, train_priming_data: pd.Series, dev_priming_data: pd.Series):
+        # @TODO: finetune here? depending on time aim
+        """
+        Instances an `Img2Vec` object and sets the expected size for encoded representations.
+        """
+        if self.is_prepared:
+            raise Exception('You can only call "prepare" once for a given encoder.')
+
+        self.model = Img2Vec()
+        self.output_size = self.model.output_size
+        self.is_prepared = True
+
+    def to(self, device, available_devices):
+        """
+        Moves the model to-and-from CPU and GPU.
+
+        :param device: will move the model to this device.
+        :param available_devices: all available devices as reported by lightwood.
+
+        :return: same object but moved to the target device.
+        """
+        self.model.to(device, available_devices)
+        return self
+
+    def encode(self, images: List[str]) -> torch.Tensor:
+        """
+        Creates encodings for a list of images; each image is referenced by a filepath or url.
+
+        :param images: list of images, each image is a path to a file or a url.
+        :return: a torch.floatTensor
+        """
+        if not self.is_prepared:
+            raise Exception('You need to call "prepare" before calling "encode" or "decode".')
+
+        img_tensors = [self._img_to_tensor(
+            Image.open(img_path)
+        ) for img_path in images]
+        vec_arr = []
+
+        self.model.eval()
+        with torch.no_grad():
+            for img_tensor in img_tensors:
+                vec = self.model(img_tensor.unsqueeze(0), batch=False)
+                vec_arr.append(vec)
+        return torch.stack(vec_arr).to('cpu')
+
+    def decode(self, encoded_values_tensor):
+        raise Exception('This encoder is not bi-directional')
