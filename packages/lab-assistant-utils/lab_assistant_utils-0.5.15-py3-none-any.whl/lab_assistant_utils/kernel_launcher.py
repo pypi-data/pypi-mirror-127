5a8c5d00508a54487c83e46d26e3e10ce2fe32fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:40 +0000

    Add package lab-assistant-utils/0.5.15/lab_assistant_utils-0.5.15-py3-none-any.whl

---
+++ packages/lab-assistant-utils/lab_assistant_utils-0.5.15-py3-none-any.whl/lab_assistant_utils/kernel_launcher.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import pathlib
+
+from ipykernel import kernelapp as app
+import click
+
+
+@click.command()
+@click.option('--home', required=True)
+@click.option('--uid', required=True, type=int)
+@click.option('--gid', required=True, type=int)
+@click.option('--session-key', required=True)
+@click.option('--heartbeat-port', required=True)
+@click.option('--shell-port', required=True)
+@click.option('--iopub-port', required=True)
+@click.option('--stdin-port', required=True)
+@click.option('--control-port', required=True)
+@click.option('--debug', default=True)
+def launch(home, uid, gid, session_key, heartbeat_port, shell_port, iopub_port, stdin_port, control_port, debug: bool):
+    pathlib.Path(home).mkdir(parents=True, exist_ok=True)
+    os.chown(home, uid, gid)
+    args = [
+        f'--Session.key={session_key}',
+        f'--ip=0.0.0.0',
+        f'--hb={heartbeat_port}',
+        f'--shell={shell_port}',
+        f'--iopub={iopub_port}',
+        f'--stdin={stdin_port}',
+        f'--control={control_port}'
+    ]
+    args.append('--debug')
+    app.launch_new_instance(argv=args)
+
+
+if __name__ == '__main__':
+    launch()
