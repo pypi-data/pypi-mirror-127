bcc9316fd2fa5efa1bd7e2e5c4ca1402a1068b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:46 +0000

    Add package mysqlpool-wrapper/0.2.1/mysqlpool-wrapper-0.2.1.tar.gz

---
+++ packages/mysqlpool-wrapper/mysqlpool-wrapper-0.2.1.tar.gz/mysqlpool-wrapper-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+with open(join(dirname(__file__), 'mysqlpool_wrapper/_version.py')) as f:
+    exec(f.read())
+
+setup(
+    name='mysqlpool-wrapper',
+    version=version,
+    description='Library which is used as MySQL pool wrapper.',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    url='https://github.com/mmedek/mysqlpool-wrapper',
+    author='Michal Medek',
+    author_email='mmedek94@gmail.com',
+    packages=find_packages(),
+    keywords='mysql, mysql pool',
+    install_requires=[
+        'sqlalchemy>=1.3.20',
+        'mysql-python'
+    ],
+    platforms=['any'],
+    zip_safe=False
+)
