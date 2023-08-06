d5b8e9f6f818c90e038a9296eb9493c40c41c7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:59 +0000

    Add package data-drift-detector-mightyhive/0.0.4/data-drift-detector-mightyhive-0.0.4.tar.gz

---
+++ packages/data-drift-detector-mightyhive/data-drift-detector-mightyhive-0.0.4.tar.gz/data-drift-detector-mightyhive-0.0.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+version = "0.0.4"
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="data-drift-detector-mightyhive",
+    version=version,
+    author="Yang Dai",
+    author_email="yang.dai@mediamonks.com",
+    description="A data drift detection and schema validation package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/superyang713/data-drift-detector/blob/main/README.md",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+    ],
+    python_requires="==3.8",
+    install_requires=[
+        "tensorflow-data-validation",
+        "google-cloud-storage",
+    ],
+)
