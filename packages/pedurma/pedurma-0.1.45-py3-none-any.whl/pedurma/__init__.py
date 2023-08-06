142ecb31a37db36bc059b76cda4c758efde1bfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:26 +0000

    Add package pedurma/0.1.45/pedurma-0.1.45-py3-none-any.whl

---
+++ packages/pedurma/pedurma-0.1.45-py3-none-any.whl/pedurma/__init__.py
@@ -0,0 +1,10 @@
+from .notes import *
+from .pagination_update import *
+from .pecha import *
+from .preprocess import *
+from .reconstruction import *
+from .save_text import *
+from .texts import *
+from .utils import *
+
+__version__ = "0.1.45"
