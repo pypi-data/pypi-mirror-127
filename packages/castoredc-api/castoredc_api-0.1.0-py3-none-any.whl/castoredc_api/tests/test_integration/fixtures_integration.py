191b5475b625dfb6501946eb908cd6187c94471b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:53 +0000

    Add package castoredc-api/0.1.0/castoredc_api-0.1.0-py3-none-any.whl

---
+++ packages/castoredc-api/castoredc_api-0.1.0-py3-none-any.whl/castoredc_api/tests/test_integration/fixtures_integration.py
@@ -0,0 +1,31 @@
+import pytest
+
+from castoredc_api.auth import auth_data
+from castoredc_api.study.castor_study import CastorStudy
+
+
+@pytest.fixture(scope="class")
+def integration_study():
+    study = CastorStudy(
+        auth_data.client_id,
+        auth_data.client_secret,
+        auth_data.test_client_study_id,
+        "data.castoredc.com",
+    )
+    return study
+
+
+@pytest.fixture(scope="class")
+def integration_study_format():
+    study = CastorStudy(
+        auth_data.client_id,
+        auth_data.client_secret,
+        auth_data.test_client_study_id,
+        "data.castoredc.com",
+        format_options={
+            "date": "%B %e %Y",
+            "datetime": "%B %e %Y %I:%M %p",
+            "time": "%I:%M %p",
+        },
+    )
+    return study
