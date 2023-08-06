62480e8c0869be79633d6363f49aca2d1524e05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:22 +0000

    Add package acuitylite/6.0.0/acuitylite-6.0.0-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/acuitylite/acuitylite-6.0.0-cp38-cp38-manylinux2010_x86_64.whl/acuitylib/__init__.py
@@ -0,0 +1,29 @@
+import os
+from acuitylib.optimize.optimizer import Optimizer
+from acuitylib.netwalker import NetWalker
+from acuitylib.acuitylog import AcuityLog as Log
+from acuitylib.acuitynet import AcuityNet as Net
+from acuitylib.acuitynetbuilder import AcuityNetBuilder as Builder
+from acuitylib.client import Client as Client
+from acuitylib.train.train import Train
+import acuitylib.train as train
+from acuitylib.xtf import xtf, xrnn
+''' Export postprocess
+'''
+from acuitylib.app.medusa.postprocess.postprocess import PostProcess
+
+def get_version():
+    from acuitylib.utils import get_acuity_path, get_acuity_path_env_variable
+    if os.path.exists(get_acuity_path()):
+        acuity_path = get_acuity_path()
+    else:
+        acuity_path = get_acuity_path_env_variable()
+    acuity_version_path = os.path.join(acuity_path, 'acuitylib', 'VERSION')
+    try:
+        with open(acuity_version_path, 'r') as f:
+            version = f.readlines()[0].strip('\n')
+        return version
+    except:
+        return None
+
+__version__ = get_version()
