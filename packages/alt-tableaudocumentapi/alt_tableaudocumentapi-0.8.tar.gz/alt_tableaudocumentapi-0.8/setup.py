767d9ff6e33702f27d47c424371d169a89cdbfea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:43 +0000

    Add package alt-tableaudocumentapi/0.8/alt_tableaudocumentapi-0.8.tar.gz

---
+++ packages/alt-tableaudocumentapi/alt_tableaudocumentapi-0.8.tar.gz/alt_tableaudocumentapi-0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+    
+setup(
+    name='alt_tableaudocumentapi',
+    version='0.8',
+    author='Mikhail Korotchenkov',
+    author_email='mr.mkc19@hotmail.com',
+    url='https://github.com/dev-mkc19/document-api-python',
+    packages=['tableaudocumentapi'],
+    license='MIT',
+    description='An alternative Python module for working with Tableau files.',
+    test_suite='test'
+)
