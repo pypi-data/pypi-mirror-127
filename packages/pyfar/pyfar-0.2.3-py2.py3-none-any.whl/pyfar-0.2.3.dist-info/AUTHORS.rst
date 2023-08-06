4327e22eac6c2a8cd9e58d0d3d93750c07d64abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package pyfar/0.2.3/pyfar-0.2.3-py2.py3-none-any.whl

---
+++ packages/pyfar/pyfar-0.2.3-py2.py3-none-any.whl/pyfar-0.2.3.dist-info/AUTHORS.rst
@@ -0,0 +1,20 @@
+=======
+Credits
+=======
+
+The pyfar developers
+--------------------
+
+- Marco Berzborn
+- Fabian Brinkmann
+- Kai Jurgeit
+- Simon Kersten
+
+
+Contributors
+------------
+
+- Chris Hold
+- Johannes Imort
+- Simon Klein
+- Udo Waldek
---
+++ packages/pyfar/pyfar-0.2.3-py2.py3-none-any.whl/pyfar/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pyfar."""
+
+__author__ = """The pyfar developers"""
+__email__ = ''
+__version__ = '0.2.3'
+
+
+from .classes.audio import Signal, TimeData, FrequencyData
+from .classes.coordinates import Coordinates
+from .classes.orientations import Orientations
+from .classes.filter import FilterFIR, FilterIIR, FilterSOS
+
+from . import plot
+from . import samplings
+from . import io
+from . import dsp
+from . import signals
+
+
+__all__ = [
+    'Signal',
+    'TimeData',
+    'FrequencyData',
+    'Coordinates',
+    'Orientations',
+    'FilterFIR',
+    'FilterIIR',
+    'FilterSOS',
+    'plot',
+    'samplings',
+    'io',
+    'dsp',
+    'signals']
