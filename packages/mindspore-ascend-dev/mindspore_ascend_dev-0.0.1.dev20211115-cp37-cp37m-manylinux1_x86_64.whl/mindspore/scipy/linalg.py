d5a916e2452846f694212829ec562d254e522815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:59 +0000

    Add package mindspore-ascend-dev/0.0.1.dev20211115/mindspore_ascend_dev-0.0.1.dev20211115-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/mindspore-ascend-dev/mindspore_ascend_dev-0.0.1.dev20211115-cp37-cp37m-manylinux1_x86_64.whl/mindspore/scipy/linalg.py
@@ -0,0 +1,193 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Linear algebra submodule"""
+from .. import numpy as mnp
+from .. import ops
+from .ops import SolveTriangular
+from ..ops import operations as P
+
+__all__ = ['block_diag', 'solve_triangular', 'inv']
+
+
+def block_diag(*arrs):
+    """
+    Create a block diagonal matrix from provided arrays.
+
+    Given the inputs `A`, `B` and `C`, the output will have these
+    Tensor arranged on the diagonal::
+
+        [[A, 0, 0],
+         [0, B, 0],
+         [0, 0, C]]
+
+    Args:
+        A, B, C, ... (Tensor): up to 2-D
+            Input Tensors.  A 1-D Tensor or a 2-D Tensor with shape ``(1,n)``.
+
+    Returns:
+        D (Tesnor): Tensor with `A`, `B`, `C`, ... on the diagonal. `D` has
+            the same dtype as `A`.
+
+    Raises:
+        ValueError: If there are tensors with dimensions higher than 2 in all arguments.
+
+    Supported Platforms:
+        ``CPU`` ``GPU``
+
+    Examples:
+        >>> import numpy as onp
+        >>> from mindspore.common import Tensor
+        >>> from mindspore.scipy.linalg import block_diag
+        >>> A = Tensor(onp.array([[1, 0], [0, 1]]))
+        >>> B = Tensor(onp.array([[3, 4, 5], [6, 7, 8]]))
+        >>> C = Tensor(onp.array([[7]]))
+        >>> P = Tensor(onp.zeros((2, ), dtype='int32'))
+        >>> block_diag(A, B, C)
+        [[1, 0, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0],
+         [0, 0, 3, 4, 5, 0],
+         [0, 0, 6, 7, 8, 0],
+         [0, 0, 0, 0, 0, 7]]
+        >>> block_diag(A, P, B, C)
+        [[1, 0, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0],
+         [0, 0, 3, 4, 5, 0],
+         [0, 0, 6, 7, 8, 0],
+         [0, 0, 0, 0, 0, 7]]
+    """
+    if not arrs:
+        return mnp.zeros((1, 0))
+    bad_shapes = [i for i, a in enumerate(arrs) if a.ndim > 2]
+    if bad_shapes:
+        raise ValueError("Arguments to mindspore.scipy.linalg.block_diag must have at "
+                         "most 2 dimensions, got {} at argument {}."
+                         .format(arrs[bad_shapes[0]], bad_shapes[0]))
+    arrs = [mnp.atleast_2d(a) for a in arrs]
+    accum = arrs[0]
+    for arr in arrs[1:]:
+        _, c = arr.shape
+        arr = ops.Pad(((0, 0), (accum.shape[-1], 0)))(arr)
+        accum = ops.Pad(((0, 0), (0, c)))(accum)
+        accum = mnp.concatenate([accum, arr], axis=0)
+    return accum
+
+
+def solve_triangular(A, b, trans=0, lower=False, unit_diagonal=False,
+                     overwrite_b=False, debug=None, check_finite=True):
+    """
+    Solve the equation `a x = b` for `x`, assuming a is a triangular matrix.
+
+    Args:
+        A (Tensor): A triangular matrix of shape :math:`(N, N)`.
+        b (Tensor): A tensor of shape :math:`(M,)` or :math:`(M, N)`.
+            Right-hand side matrix in :math:`A x = b`.
+        lower (bool, optional): Use only data contained in the lower triangle of `a`.
+            Default is to use upper triangle.
+        trans (0, 1, 2, 'N', 'T', 'C', optional):
+            Type of system to solve:
+
+            ========  =========
+            trans     system
+            ========  =========
+            0 or 'N'  a x  = b
+            1 or 'T'  a^T x = b
+            2 or 'C'  a^H x = b
+            ========  =========
+        unit_diagonal (bool, optional): If True, diagonal elements of :math:`A` are assumed to be 1 and
+            will not be referenced.
+        overwrite_b (bool, optional): Allow overwriting data in :math:`b` (may enhance performance)
+        check_finite (bool, optional): Whether to check that the input matrices contain only finite numbers.
+            Disabling may give a performance gain, but may result in problems
+            (crashes, non-termination) if the inputs do contain infinities or NaNs.
+
+    Returns:
+        x (Tensor): A tensor of shape :math:`(M,)` or :math:`(M, N)`,
+            which is the solution to the system :math:`A x = b`.
+            Shape of :math:`x` matches :math:`b`.
+
+    Raises:
+        LinAlgError: If :math:`A` is singular
+
+    Supported Platforms:
+        ``CPU`` ``GPU``
+
+    Examples:
+        Solve the lower triangular system :math:`A x = b`, where:
+
+                 [3  0  0  0]       [4]
+            A =  [2  1  0  0]   b = [2]
+                 [1  0  1  0]       [4]
+                 [1  1  1  1]       [2]
+
+        >>> import numpy as onp
+        >>> from mindspore.common import Tensor
+        >>> import mindspore.numpy as mnp
+        >>> from mindspore.scipy.linalg import solve_triangular
+        >>> A = Tensor(onp.array([[3, 0, 0, 0], [2, 1, 0, 0], [1, 0, 1, 0], [1, 1, 1, 1]], onp.float64))
+        >>> b = Tensor(onp.array([4, 2, 4, 2], onp.float64))
+        >>> x = solve_triangular(A, b, lower=True, unit_diagonal=False, trans='N')
+        >>> x
+        Tensor(shape=[4], dtype=Float32, value= [ 1.33333337e+00, -6.66666746e-01,  2.66666651e+00, -1.33333313e+00])
+        >>> mnp.dot(A, x)  # Check the result
+        Tensor(shape=[4], dtype=Float32, value= [ 4.00000000e+00,  2.00000000e+00,  4.00000000e+00,  2.00000000e+00])
+    """
+    if isinstance(trans, int):
+        trans_table = ['N', 'T', 'C']
+        trans = trans_table[trans]
+    solve = SolveTriangular(lower, unit_diagonal, trans)
+    return solve(A, b)
+
+
+def inv(a, overwrite_a=False, check_finite=True):
+    """
+    Compute the inverse of a matrix.
+
+    Args:
+        a (Tensor): Tensor
+            Square matrix to be inverted.
+        overwrite_a (bool, optional): Discard data in `a` (may improve performance).
+            Default is False.
+        check_finite (bool, optional): Whether to check that the input matrix contains
+            only finite numbers.
+            Disabling may give a performance gain, but may result in problems
+            (crashes, non-termination) if the inputs do contain infinities or NaNs.
+
+    Returns:
+        ainv (Tensor): Inverse of the matrix `a`.
+
+    Raises:
+        LinAlgError: If `a` is singular.
+        ValueError: If `a` is not square, or not 2D.
+
+    Supported Platforms:
+        ``CPU`` ``GPU``
+
+    Examples:
+        >>> import numpy as onp
+        >>> from mindspore.common import Tensor
+        >>> import mindspore.numpy as mnp
+        >>> from mindspore.scipy.linalg import inv
+        >>> a = Tensor(onp.array([[1., 2.], [3., 4.]]))
+        >>> inv(a)
+        [[-2. ,  1. ],
+         [ 1.5, -0.5]]
+        >>> mnp.dot(a, inv(a))
+        [[ 1.,  0.],
+         [ 0.,  1.]]
+    """
+    matrix_inverse = P.MatrixInverse(adjoint=False)
+    return matrix_inverse(a)
---
+++ packages/mindspore-ascend-dev/mindspore_ascend_dev-0.0.1.dev20211115-cp37-cp37m-manylinux1_x86_64.whl/mindspore/scipy/utils.py
@@ -0,0 +1,121 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""internal utility functions"""
+import numpy as onp
+from .. import nn, ops
+from ..numpy import where, isnan, zeros_like, dot
+from ..ops import functional as F
+from ..common import Tensor
+from ..common import dtype as mstype
+from .utils_const import _type_convert, _raise_type_error
+from ..ops.composite import GradOperation
+from ..ops.primitive import constexpr
+from .._c_expression import typing
+
+grad = GradOperation(get_all=False, get_by_list=False, sens_param=False)
+
+
+def _convert_64_to_32(tensor):
+    """Convert tensor with float64/int64 types to float32/int32."""
+    if tensor.dtype == mstype.float64:
+        return tensor.astype("float32")
+    if tensor.dtype == mstype.int64:
+        return tensor.astype("int32")
+    return tensor
+
+
+def _to_tensor(*args, dtype=None):
+    """Returns each input as Tensor"""
+    res = ()
+    for arg in args:
+        if isinstance(arg, (int, float, bool, list, tuple)):
+            arg = _type_convert(Tensor, arg)
+            if dtype is None:
+                arg = _convert_64_to_32(arg)
+            else:
+                arg = arg.astype(dtype)
+        elif not isinstance(arg, Tensor):
+            _raise_type_error("Expect input to be array like.")
+        res += (arg,)
+    if len(res) == 1:
+        return res[0]
+    return res
+
+
+def _to_scalar(arr):
+    """Convert a scalar Tensor or ndarray to a scalar."""
+    if isinstance(arr, (int, float, bool)):
+        return arr
+    if isinstance(arr, Tensor):
+        if arr.shape:
+            return arr
+        arr = arr.asnumpy()
+    if isinstance(arr, onp.ndarray):
+        if arr.shape:
+            return arr
+        return arr.item()
+    raise ValueError("{} are not supported.".format(type(arr)))
+
+
+class _SafeNormalize(nn.Cell):
+    """Normalize method that cast very small results to zero."""
+
+    def __init__(self):
+        """Initialize LineSearch."""
+        super(_SafeNormalize, self).__init__()
+        self.eps = ops.Eps()
+
+    def construct(self, x, threshold=None):
+        x_sum2 = F.reduce_sum(F.pows(x, 2.0))
+        norm = F.pows(x_sum2, 1. / 2.0)
+        if threshold is None:
+            if x.dtype in mstype.float_type:
+                # pick the first element of x to get the eps
+                threshold = self.eps(x[(0,) * x.ndim])
+            else:
+                threshold = 0
+        normalized_x = where(norm > threshold, x / norm, zeros_like(x))
+        normalized_x = where(isnan(normalized_x), 0, normalized_x)
+        return normalized_x, norm
+
+
+_safe_normalize = _SafeNormalize()
+
+_INT_ZERO = _to_tensor(0)
+_INT_ONE = _to_tensor(1)
+_BOOL_TRUE = _to_tensor(True)
+_BOOL_FALSE = _to_tensor(False)
+
+
+@constexpr
+def _callable_const(x):
+    """Returns true if x is a function in graph mode."""
+    return isinstance(x, typing.Function)
+
+
+def _normalize_matvec(f):
+    """Normalize an argument for computing matrix-vector products."""
+    if _callable_const(F.typeof(f)):
+        return f
+
+    if isinstance(f, Tensor):
+        if f.ndim != 2 or f.shape[0] != f.shape[1]:
+            _raise_type_error(
+                'linear operator must be a square matrix, but has shape: ', f.shape)
+        return F.partial(dot, f)
+
+    _raise_type_error(
+        'linear operator must be either a function or Tensor: but got', F.typeof(f))
+    return f
