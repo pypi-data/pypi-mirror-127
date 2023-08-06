54cc63a24ffafa5cb82f8e318d5cbf2661b8fc07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:07 +0000

    Add package labelbox/3.9.0/labelbox-3.9.0-py3-none-any.whl

---
+++ packages/labelbox/labelbox-3.9.0-py3-none-any.whl/labelbox/data/annotation_types/classification/classification.py
@@ -0,0 +1,65 @@
+from typing import Any, Dict, List
+
+try:
+    from typing import Literal
+except:
+    from typing_extensions import Literal
+
+from pydantic import BaseModel, validator
+from ..feature import FeatureSchema
+
+
+# TODO: Replace when pydantic adds support for unions that don't coerce types
+class _TempName(BaseModel):
+    name: str
+
+    def dict(self, *args, **kwargs):
+        res = super().dict(*args, **kwargs)
+        res.pop('name')
+        return res
+
+
+class ClassificationAnswer(FeatureSchema):
+    """
+    - Represents a classification option.
+    - Because it inherits from FeatureSchema
+        the option can be represented with either the name or feature_schema_id
+    """
+    extra: Dict[str, Any] = {}
+
+
+class Radio(BaseModel):
+    """ A classification with only one selected option allowed
+    
+    >>> Radio(answer = ClassificationAnswer(name = "dog"))
+
+    """
+    answer: ClassificationAnswer
+
+
+class Checklist(_TempName):
+    """ A classification with many selected options allowed
+
+    >>> Checklist(answer = [ClassificationAnswer(name = "cloudy")])
+
+    """
+    name: Literal["checklist"] = "checklist"
+    answer: List[ClassificationAnswer]
+
+
+class Text(BaseModel):
+    """ Free form text
+
+    >>> Text(answer = "some text answer") 
+
+    """
+    answer: str
+
+
+class Dropdown(_TempName):
+    """
+    - A classification with many selected options allowed .
+    - This is not currently compatible with MAL.
+    """
+    name: Literal["dropdown"] = "dropdown"
+    answer: List[ClassificationAnswer]
