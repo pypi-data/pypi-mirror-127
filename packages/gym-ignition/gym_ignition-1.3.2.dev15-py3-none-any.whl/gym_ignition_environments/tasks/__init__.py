011eda943a03fe6fba716bb61a62b73e40b7daaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:15 +0000

    Add package gym-ignition/1.3.2.dev15/gym_ignition-1.3.2.dev15-py3-none-any.whl

---
+++ packages/gym-ignition/gym_ignition-1.3.2.dev15-py3-none-any.whl/gym_ignition_environments/tasks/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT). All rights reserved.
+# This software may be modified and distributed under the terms of the
+# GNU Lesser General Public License v2.1 or any later version.
+
+from . import (
+    cartpole_continuous_balancing,
+    cartpole_continuous_swingup,
+    cartpole_discrete_balancing,
+    pendulum_swingup,
+)
