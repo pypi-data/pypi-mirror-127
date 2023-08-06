569c7cdbd0f80a06f137cd5c93f93520185bdebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:10 +0000

    Add package luatables/0.0.1/luatables-0.0.1-py3-none-any.whl

---
+++ packages/luatables/luatables-0.0.1-py3-none-any.whl/luatable/table.py
@@ -0,0 +1,15 @@
+class Table(dict):
+  def __getattr__(self,x):
+    return self.get(x,None)
+  
+  def __getitem__(self,x):
+    return self.get(x,None)
+  
+  def __setattr__(self,k,v):
+    if type(v) is dict:
+      temp = Table()
+      for x,y in v.items():
+        temp[x] = y
+      self[k] = temp
+    else:
+      self[k] = v
