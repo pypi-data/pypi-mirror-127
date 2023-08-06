3095ab9fab5dde948e959a695095c3fa1b01278f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:17 +0000

    Add package wagtail-localize-rws-languagecloud/0.2.0/wagtail_localize_rws_languagecloud-0.2.0-py3-none-any.whl

---
+++ packages/wagtail-localize-rws-languagecloud/wagtail_localize_rws_languagecloud-0.2.0-py3-none-any.whl/wagtail_localize_rws_languagecloud/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class WagtailLocalizeRwsLanguageCloudAppConfig(AppConfig):
+    label = "wagtail_localize_rws_languagecloud"
+    name = "wagtail_localize_rws_languagecloud"
+    verbose_name = "Wagtail Localize RWS LanguageCloud"
+
+    def ready(self):
+        import wagtail_localize_rws_languagecloud.checks  # noqa: F401
