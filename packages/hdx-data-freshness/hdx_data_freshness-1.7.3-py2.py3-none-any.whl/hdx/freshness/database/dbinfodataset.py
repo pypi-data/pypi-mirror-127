e3103cd50f98784ab74ac71f0bba9c3819e1d9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:42 +0000

    Add package hdx-data-freshness/1.7.3/hdx_data_freshness-1.7.3-py2.py3-none-any.whl

---
+++ packages/hdx-data-freshness/hdx_data_freshness-1.7.3-py2.py3-none-any.whl/hdx/freshness/database/dbinfodataset.py
@@ -0,0 +1,22 @@
+from hdx.database import Base
+from sqlalchemy import Boolean, Column, ForeignKey, String
+
+from .dborganization import DBOrganization
+
+
+class DBInfoDataset(Base):
+    id = Column(String, primary_key=True)
+    name = Column(String, nullable=False)
+    title = Column(String, nullable=False)
+    private = Column(Boolean, nullable=False)
+    organization_id = Column(
+        String, ForeignKey(DBOrganization.id), nullable=False
+    )
+    location = Column(String)
+    maintainer = Column(String)
+
+    def __repr__(self):
+        output = f"<InfoDataset(id={self.id}, name={self.name}, title={self.title},\n"
+        output += f"private={str(self.private)}, organization id={self.organization_id},\n"
+        output += f"maintainer={self.maintainer}, location={self.location})>"
+        return output
