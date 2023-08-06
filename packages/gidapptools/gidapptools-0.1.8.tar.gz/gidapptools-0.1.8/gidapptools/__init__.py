e1f78f80867753ea12345eab1a1f85c9ba0c26a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:35 +0000

    Add package gidapptools/0.1.8/gidapptools-0.1.8.tar.gz

---
+++ packages/gidapptools/gidapptools-0.1.8.tar.gz/gidapptools-0.1.8/gidapptools/__init__.py
@@ -0,0 +1,12 @@
+"""
+WiP
+"""
+
+from gidapptools.meta_data.interface import setup_meta_data, get_meta_config, get_meta_info, get_meta_item, get_meta_paths
+
+
+__version__ = "0.1.8"
+
+
+from pathlib import Path
+THIS_FILE_DIR = Path(__file__).resolve().parent
