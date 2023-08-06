765038e2ab8ef60aa5e691315ed87368e5b91d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:24 +0000

    Add package blimpy/2.0.32/blimpy-2.0.32-py3-none-any.whl

---
+++ packages/blimpy/blimpy-2.0.32-py3-none-any.whl/tests/test_dice.py
@@ -0,0 +1,44 @@
+from blimpy import dice
+import pytest
+
+from tests.data import voyager_h5
+from tests.data import voyager_fil
+
+# This section makes sure that the tool exits correctly
+# when certain issues are encountered.
+
+def test_no_args():
+    """
+    Make sure the tool closes if no arguments are passed in
+    """
+    with pytest.raises(SystemExit):
+        dice.cmd_tool()
+
+def test_no_input_file():
+    with pytest.raises(SystemExit):
+        args = ['-b', '8419.24', '-e', '8419.35', '-x', 'h5', '-o', 'test_dice.h5']
+        dice.cmd_tool(args)
+
+def test_missing_format_type():
+    with pytest.raises(SystemExit):
+        args = ['-f', voyager_h5, '-b', '8419.24', '-e', '8419.35', '-x']
+        dice.cmd_tool(args)
+
+# This section makes sure that the tool can handle various
+# file formats without exception.
+
+def test_h5():
+    args = ['-f', voyager_h5, '-b', '8419.24', '-e', '8419.35', '-x', 'h5', '-o', 'test_dice.h5']
+    dice.cmd_tool(args)
+
+def test_h5_no_out_file():
+    args = ['-f', voyager_h5, '-b', '8419.24', '-e', '8419.35', '-x', 'h5']
+    dice.cmd_tool(args)
+
+def test_fil():
+    args = ['-f', voyager_fil, '-b', '8419.24', '-e', '8419.35', '-x', 'fil', '-o', 'test_dice.fil']
+    dice.cmd_tool(args)
+
+def test_fil_no_out_file():
+    args = ['-f', voyager_fil, '-b', '8419.24', '-e', '8419.35', '-x', 'fil']
+    dice.cmd_tool(args)
