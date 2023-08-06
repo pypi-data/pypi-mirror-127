cef389f62c6c542cedd6e9218677fd00757ade08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:10 +0000

    Add package meilisearch-python-async/0.20.0/meilisearch_python_async-0.20.0-py3-none-any.whl

---
+++ packages/meilisearch-python-async/meilisearch_python_async-0.20.0-py3-none-any.whl/meilisearch_python_async/_http_requests.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from typing import Any, Callable
+
+from httpx import (
+    AsyncClient,
+    ConnectError,
+    ConnectTimeout,
+    HTTPError,
+    RemoteProtocolError,
+    Response,
+)
+
+from meilisearch_python_async.errors import MeiliSearchApiError, MeiliSearchCommunicationError
+
+
+class _HttpRequests:
+    def __init__(self, http_client: AsyncClient) -> None:
+        self.http_client = http_client
+
+    async def _send_request(
+        self,
+        http_method: Callable,
+        path: str,
+        body: Any | None = None,
+        content_type: str = "applicaiton/json",
+    ) -> Response:
+        try:
+            if not body:
+                response = await http_method(path)
+            elif content_type != "application/json":
+                response = await http_method(
+                    path, content=body, headers={"Content-Type": content_type}
+                )
+            else:
+                response = await http_method(
+                    path, json=body, headers={"Content-Type": content_type}
+                )
+
+            response.raise_for_status()
+            return response
+
+        except RemoteProtocolError as err:
+            raise MeiliSearchCommunicationError(str(err)) from err
+        except ConnectError as err:
+            raise MeiliSearchCommunicationError(str(err)) from err
+        except ConnectTimeout as err:
+            raise MeiliSearchCommunicationError(str(err)) from err
+        except HTTPError as err:
+            if response:
+                raise MeiliSearchApiError(str(err), response) from err
+            raise
+
+    async def get(self, path: str) -> Response:
+        return await self._send_request(self.http_client.get, path)
+
+    async def post(
+        self, path: str, body: Any | None = None, content_type: str = "application/json"
+    ) -> Response:
+        return await self._send_request(self.http_client.post, path, body, content_type)
+
+    async def put(
+        self, path: str, body: Any | None = None, content_type: str = "application/json"
+    ) -> Response:
+        return await self._send_request(self.http_client.put, path, body, content_type)
+
+    async def delete(self, path: str, body: dict | None = None) -> Response:
+        return await self._send_request(self.http_client.delete, path, body)
