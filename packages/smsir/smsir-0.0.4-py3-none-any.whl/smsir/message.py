95fca111b01306f3630893e2360992d7f5eb9ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:23 +0000

    Add package smsir/0.0.4/smsir-0.0.4-py3-none-any.whl

---
+++ packages/smsir/smsir-0.0.4-py3-none-any.whl/smsir/message.py
@@ -0,0 +1,22 @@
+import requests
+import json
+class sms:
+    def __init__(self) -> None:
+        pass
+    def send_by_mobile_number(self,Messages='',MobileNumbers='',LineNumber='',SendDateTime='',Token=''):
+        secure_token = self.secure_token
+        headers = {
+        "Content-Type": "application/json",
+        'x-sms-ir-secure-token': secure_token
+        }
+        body = {
+        "Messages": [Messages],
+        "MobileNumbers": [MobileNumbers],
+        "LineNumber": LineNumber,
+        "SendDateTime": SendDateTime ,
+        "CanContinueInCaseOfError": "false",
+        }
+        response = requests.post('http://RestfulSms.com/api/MessageSend', data=json.dumps(body),
+                                headers=headers)
+        print(response.json())
+        return 'send'    
