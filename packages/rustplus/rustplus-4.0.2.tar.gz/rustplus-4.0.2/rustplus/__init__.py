0f0c5948f287fcd09c2fa3b496699ef148fd42c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:53 +0000

    Add package rustplus/4.0.2/rustplus-4.0.2.tar.gz

---
+++ packages/rustplus/rustplus-4.0.2.tar.gz/rustplus-4.0.2/rustplus/__init__.py
@@ -0,0 +1,7 @@
+from .api import RustSocket
+from .exceptions import ClientError, ImageError, ServerNotResponsiveError, ClientNotConnectedError
+
+__name__ = "rustplus"
+__author__ = "olijefferson"
+__version__ = "4.0.2"
+__support__ = "Discord: https://discord.gg/nQqJe8qvP8"
