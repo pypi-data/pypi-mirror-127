928a912b06649fa4769cf2a5c7d50aa034c2f0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:25 +0000

    Add package USEofCS/0.1.5/USEofCS-0.1.5.tar.gz

---
+++ packages/USEofCS/USEofCS-0.1.5.tar.gz/USEofCS-0.1.5/USEofCS/__init__.py
@@ -0,0 +1,4 @@
+from .ns_calc import ns_calc_
+
+__author__ = 'zef'
+__version__ = '0.1.5'
---
+++ packages/USEofCS/USEofCS-0.1.5.tar.gz/USEofCS-0.1.5/USEofCS/ns_calc.py
@@ -0,0 +1,13 @@
+# class ns_calc(object):
+#     def __init__(self, number, base_number_system, finite_number_system):
+#         self.number = number
+#         self.base_number_system = base_number_system
+#         self.finite_number_system = finite_number_system
+
+def ns_calc_(number, base_number_system, finite_number_system):
+    finite_number = ''
+    number = int(str(number), base_number_system)
+    while int(number) > 0:
+        finite_number = str(number % finite_number_system) + finite_number
+        number //= base_number_system
+    return finite_number
