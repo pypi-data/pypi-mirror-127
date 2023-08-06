cf25f14b87213050f55edffe30febe8e1ec88081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:05 +0000

    Add package gamelibgames/1.1.1/gamelibgames-1.1.1.tar.gz

---
+++ packages/gamelibgames/gamelibgames-1.1.1.tar.gz/gamelibgames-1.1.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+  name = 'gamelibgames',
+  packages = [],
+  version = '1.1.1',
+  license='MIT',
+  description = 'A collection of minigames made with Gamelib.',
+  author = 'NLGS',
+  author_email = 'flighterman@fi.uba.ar',
+  url = 'https://github.com/NLGS2907/gamelib_games',
+  download_url = 'https://github.com/NLGS2907/gamelib_games/archive/refs/tags/v1.1.1.tar.gz',
+  keywords = ['GAMELIB', 'MINIGAMES'],
+  install_requires=[
+
+      # Dependencies here
+      
+    ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
