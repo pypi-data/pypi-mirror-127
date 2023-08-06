a595aebe21cff0d373e2451a152cca002a509694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:56 +0000

    Add package beast-night-tv/1.1.0/beast_night_tv-1.1.0-py3-none-any.whl

---
+++ packages/beast-night-tv/beast_night_tv-1.1.0-py3-none-any.whl/jak_python_package/__init__.py
@@ -0,0 +1,12 @@
+"""
+JAK Python Package
+~~~~~~~~~~~~~~~~~~
+
+A Python Package created by JAK!!
+
+:copyright: (c) 2020-present Jonak Adipta Kalita
+:license: MIT, see LICENSE for more details.
+
+"""
+
+import jak_python_package.edit_message
