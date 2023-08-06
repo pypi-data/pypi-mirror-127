ef1fb2091f60ccc6f8cee446a53868851c31714b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:18 +0000

    Add package Buildpan-CLI/1.0/Buildpan_CLI-1.0-py3-none-any.whl

---
+++ packages/Buildpan-CLI/Buildpan_CLI-1.0-py3-none-any.whl/buildpan/setting.py
@@ -0,0 +1,23 @@
+"""
+    Title: setting.py
+    Author: Kushagra A.
+    Language: Python
+    Date Created: 05-09-2021
+    Date Modified: 14-10-2021
+    Description:
+        ###############################################################
+        ## Create a environment variable in dictionary   ## 
+         ###############################################################
+ """
+
+
+info = {
+    "HOST" : "3.6.156.104:5004",
+    "PUSH_COMMIT_URL" : "http://3.6.156.104:5004/push_commit",
+    "FETCH_LOG_URL" : "http://3.6.156.104:5004/fetch_log",
+    "GET_SHA_URL" : "http://3.6.156.104:5004/get_sha",
+    "key" : "HB2xRCZSyKYMcTS9rm",
+    "secret_key" : "pQA5FMShe6deqr2b8Eq3GfDBmhEnn77C",
+    "version": "1.00"
+}
+
