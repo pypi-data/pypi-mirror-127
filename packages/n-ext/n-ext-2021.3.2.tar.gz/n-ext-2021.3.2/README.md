9f9988663d1bfc2ecd4f903da41ab14d07d2b7ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:59 +0000

    Add package n-ext/2021.3.2/n-ext-2021.3.2.tar.gz

---
+++ packages/n-ext/n-ext-2021.3.2.tar.gz/n-ext-2021.3.2/README.md
@@ -0,0 +1,13 @@
+# n-ext
+Project Interface For nextcord-ext
+
+# Projects Currently Added
+
+```
+n-ext[menus] newest version of menus
+n-ext[events] new version of events
+n-ext[alternatives] newest verson of alternatives
+n-ext[lava] Also Can Use n-ext[music]
+n-ext[core] The newest version of nextcord
+n-ext[ipc] newest ipc version
+```
---
+++ packages/n-ext/n-ext-2021.3.2.tar.gz/n-ext-2021.3.2/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+import re
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+version = "2021.3.2"
+extras_require = {
+    'menus': [
+        'nextcord-ext-menus==1.3.2',
+    ],
+    'ipc': [
+        'nextcord-ext-ipc==2.2.1',
+    ],
+    'events': [
+        'nextcord-ext-events==0.1.0a0',
+    ],
+    'alternatives': [
+        'nextcord-ext-alternatives==2021.1',
+    ],
+    'core': ['nextcord==2.0.0a3'],
+        
+    'lava': [
+        'nextcord-ext-lava==3.1.5'],
+        
+    'music': [
+        'nextcord-ext-lava==3.1.5'],
+}
+
+setup(
+    name="n-ext",
+    author="nextcord-ext",
+    python_requires=">=3.8.0",
+    url="https://github.com/nextcord-ext/n-ext",
+    version=version,
+    license="Apache Software License",
+    description="Project interface for nextcord-ext's",
+    install_requires=requirements,
+    extras_require=extras_require,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ]
+)
