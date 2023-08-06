280c8c00ec4286bb4f88d763e3107845079b3d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:09 +0000

    Add package merge-hris-client/1.0.2/merge-hris-client-1.0.2.tar.gz

---
+++ packages/merge-hris-client/merge-hris-client-1.0.2.tar.gz/merge-hris-client-1.0.2/MergeHRISClient/models/__init__.py
@@ -0,0 +1,70 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from MergeHRISClient.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from MergeHRISClient.model.account_integration import AccountIntegration
+from MergeHRISClient.model.account_token import AccountToken
+from MergeHRISClient.model.available_actions import AvailableActions
+from MergeHRISClient.model.benefit import Benefit
+from MergeHRISClient.model.benefit_plan_type_enum import BenefitPlanTypeEnum
+from MergeHRISClient.model.company import Company
+from MergeHRISClient.model.country_enum import CountryEnum
+from MergeHRISClient.model.data_passthrough_request import DataPassthroughRequest
+from MergeHRISClient.model.deduction import Deduction
+from MergeHRISClient.model.earning import Earning
+from MergeHRISClient.model.employee import Employee
+from MergeHRISClient.model.employee_payroll_run import EmployeePayrollRun
+from MergeHRISClient.model.employee_request import EmployeeRequest
+from MergeHRISClient.model.employment import Employment
+from MergeHRISClient.model.employment_request import EmploymentRequest
+from MergeHRISClient.model.employment_status_enum import EmploymentStatusEnum
+from MergeHRISClient.model.employment_type_enum import EmploymentTypeEnum
+from MergeHRISClient.model.end_user_details_request import EndUserDetailsRequest
+from MergeHRISClient.model.ethnicity_enum import EthnicityEnum
+from MergeHRISClient.model.flsa_status_enum import FlsaStatusEnum
+from MergeHRISClient.model.gender_enum import GenderEnum
+from MergeHRISClient.model.generate_remote_key_request import GenerateRemoteKeyRequest
+from MergeHRISClient.model.link_token import LinkToken
+from MergeHRISClient.model.location import Location
+from MergeHRISClient.model.marital_status_enum import MaritalStatusEnum
+from MergeHRISClient.model.method_enum import MethodEnum
+from MergeHRISClient.model.model_operation import ModelOperation
+from MergeHRISClient.model.paginated_benefit_list import PaginatedBenefitList
+from MergeHRISClient.model.paginated_company_list import PaginatedCompanyList
+from MergeHRISClient.model.paginated_employee_list import PaginatedEmployeeList
+from MergeHRISClient.model.paginated_employee_payroll_run_list import PaginatedEmployeePayrollRunList
+from MergeHRISClient.model.paginated_employment_list import PaginatedEmploymentList
+from MergeHRISClient.model.paginated_location_list import PaginatedLocationList
+from MergeHRISClient.model.paginated_payroll_run_list import PaginatedPayrollRunList
+from MergeHRISClient.model.paginated_sync_status_list import PaginatedSyncStatusList
+from MergeHRISClient.model.paginated_team_list import PaginatedTeamList
+from MergeHRISClient.model.paginated_time_off_balance_list import PaginatedTimeOffBalanceList
+from MergeHRISClient.model.paginated_time_off_list import PaginatedTimeOffList
+from MergeHRISClient.model.pay_currency_enum import PayCurrencyEnum
+from MergeHRISClient.model.pay_frequency_enum import PayFrequencyEnum
+from MergeHRISClient.model.pay_period_enum import PayPeriodEnum
+from MergeHRISClient.model.payroll_run import PayrollRun
+from MergeHRISClient.model.remote_data import RemoteData
+from MergeHRISClient.model.remote_data_request import RemoteDataRequest
+from MergeHRISClient.model.remote_key import RemoteKey
+from MergeHRISClient.model.remote_key_for_regeneration_request import RemoteKeyForRegenerationRequest
+from MergeHRISClient.model.remote_response import RemoteResponse
+from MergeHRISClient.model.request_type_enum import RequestTypeEnum
+from MergeHRISClient.model.run_state_enum import RunStateEnum
+from MergeHRISClient.model.run_type_enum import RunTypeEnum
+from MergeHRISClient.model.sync_status import SyncStatus
+from MergeHRISClient.model.sync_status_status_enum import SyncStatusStatusEnum
+from MergeHRISClient.model.tax import Tax
+from MergeHRISClient.model.team import Team
+from MergeHRISClient.model.time_off import TimeOff
+from MergeHRISClient.model.time_off_balance import TimeOffBalance
+from MergeHRISClient.model.time_off_status_enum import TimeOffStatusEnum
+from MergeHRISClient.model.type_enum import TypeEnum
+from MergeHRISClient.model.units_enum import UnitsEnum
