7ff5f4e49e288834e6ba498398a02daa6dec87c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:54 +0000

    Add package PoliticsAndData/0.7.0/PoliticsAndData-0.7.0-py3-none-any.whl

---
+++ packages/PoliticsAndData/PoliticsAndData-0.7.0-py3-none-any.whl/pnd/specific.py
@@ -0,0 +1,112 @@
+from .boredom import Boredom
+
+
+class Alliances(Boredom):
+    def __init__(self, **kwargs):
+        super().__init__("alliances", **kwargs)
+
+        self.length = "aa_count"
+        self.total = [("total_aa_score", 7, float)]
+
+
+class Cities(Boredom):
+    def __init__(self, **kwargs):
+        super().__init__("cities", **kwargs)
+
+        self.length = "city_count"
+        self.total = [
+            ("total_infrastructure", 5, float),
+            ("total_land", 7, float),
+            ("oil_pp", 8, int),
+            ("coal_pp", 10, int),
+            ("nuclear_pp", 11, int),
+            ("coal_mines", 12, int),
+            ("oil_wells", 13, int),
+            ("uranium_mines", 14, int),
+            ("iron_mines", 15, int),
+            ("lead_mines", 16, int),
+            ("bauxite_mines", 17, int),
+            ("farms", 18, int),
+            ("supermarkets", 23, int),
+            ("banks", 24, int),
+            ("shopping_malls", 25, int),
+            ("stadiums", 26, int),
+            ("oil_refineries", 27, int),
+            ("aluminum_refineries", 28, int),
+            ("steel_mills", 29, int),
+            ("munitions_factories", 30, int)
+        ]
+
+
+class Nations(Boredom):
+    def __init__(self, **kwargs):
+        super().__init__("nations", **kwargs)
+
+        self.length = "nation_count"
+        self.total = [
+            ("total_score", "score", float),
+            ("total_population", "population", int),
+            ("beige", "beige_turns_remaining", int),
+            ("soldiers", "soldiers", int),
+            ("tanks", "tanks", int),
+            ("aircraft", "aircraft", int),
+            ("ships", "ships", int),
+            ("missiles", "missiles", int),
+            ("nukes", "nukes", int),
+        ]
+        self.match = [
+            ("gray", 12, "gray"),
+            ("blitzkrieg", 30, "Blitzkrieg"),
+        ]
+
+    def run(self, force_new=False, ignore=True):
+        super().run(force_new, ignore)
+
+
+class Trades(Boredom):
+    def __init__(self, **kwargs):
+        super().__init__("trades", **kwargs)
+
+    def spec_collection(self, data: list[dict], ignore=False):
+        self.collected = {}
+
+        rss = [
+            "credits",
+            "food",
+            "coal",
+            "oil",
+            "uranium",
+            "lead",
+            "iron",
+            "bauxite",
+            "gasoline",
+            "munitions",
+            "steel",
+            "aluminum",
+        ]
+
+        sell = "%s_sell_total"
+        buy = "%s_buy_total"
+        
+        for resource in rss:
+            self.collected[sell %resource] = 0
+            self.collected[buy %resource] = 0
+
+        for line in data:
+            if accepted == "1":
+                continue
+            resource = rss.find(line["resource"])
+            if resource == -1:
+                raise KeyError
+            self.collected[sell %resource] = quantity*price
+            self.collected[buy %resource] = quantity*price
+
+
+class Wars(Boredom):
+    def __init__(self, **kwargs):
+        super().__init__("wars", **kwargs)
+        self.match = [
+            ("raid_count", "war_type", "raid"),
+            ("attrition_count", "war_type", "attrition"),
+            ("ordinary_count", "war_type", "ordinary")
+        ]
