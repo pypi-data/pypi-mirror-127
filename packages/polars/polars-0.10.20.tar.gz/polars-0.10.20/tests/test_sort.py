4db2d6124e2fc91977cde9f141e375f85e4511e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:20 +0000

    Add package polars/0.10.20/polars-0.10.20.tar.gz

---
+++ packages/polars/polars-0.10.20.tar.gz/polars-0.10.20/tests/test_sort.py
@@ -0,0 +1,29 @@
+import polars as pl
+
+
+def test_sort_dates_multiples():
+    df = pl.DataFrame(
+        [
+            pl.Series(
+                "date",
+                [
+                    "2021-01-01 00:00:00",
+                    "2021-01-01 00:00:00",
+                    "2021-01-02 00:00:00",
+                    "2021-01-02 00:00:00",
+                    "2021-01-03 00:00:00",
+                ],
+            ).str.strptime(pl.Datetime, "%Y-%m-%d %T"),
+            pl.Series("values", [5, 4, 3, 2, 1]),
+        ]
+    )
+
+    expected = [4, 5, 2, 3, 1]
+
+    # datetime
+    out = df.sort(["date", "values"])
+    assert out["values"].to_list() == expected
+
+    # Date
+    out = df.with_column(pl.col("date").cast(pl.Date)).sort(["date", "values"])
+    assert out["values"].to_list() == expected
