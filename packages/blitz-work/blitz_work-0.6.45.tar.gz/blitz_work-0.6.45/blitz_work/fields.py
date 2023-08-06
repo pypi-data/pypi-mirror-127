be248b0b7260d478e1ae99aaa644d4c54775c46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:02 +0000

    Add package blitz-work/0.6.45/blitz_work-0.6.45.tar.gz

---
+++ packages/blitz-work/blitz_work-0.6.45.tar.gz/blitz_work-0.6.45/blitz_work/fields.py
@@ -0,0 +1,36 @@
+from django.forms import TextInput
+from django.forms.models import ModelChoiceField
+from django.forms.widgets import CheckboxInput, Select
+
+
+def custom_control(attrs):
+    if attrs is None:
+        attrs = {}
+    if attrs.get('class') is None:
+        attrs['class'] = 'form-control'
+    return attrs
+
+
+class BText(TextInput):
+    def __init__(self, attrs=None):
+        super().__init__(custom_control(attrs))
+
+class BCheckRadioInput(CheckboxInput):
+    template_name = 'components/check_radio.html'
+
+class BModelChoice(Select):
+    template_name = 'components/select.html'
+    model_name = ""
+    related_crud_url = None
+    redirect = None
+    extra_url = None
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context['model_name'] = self.model_name
+        context['related_crud_url'] = self.related_crud_url
+        context['redirect'] = self.redirect
+        context['extra_url'] = self.extra_url
+        return context
+class BModelMultipleChoice(BModelChoice):
+    allow_multiple_selected = True
