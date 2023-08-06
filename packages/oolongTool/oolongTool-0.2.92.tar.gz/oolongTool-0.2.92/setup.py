d7c351d43df367f4af6b173882c662549bf04316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:02 +0000

    Add package oolongTool/0.2.92/oolongTool-0.2.92.tar.gz

---
+++ packages/oolongTool/oolongTool-0.2.92.tar.gz/oolongTool-0.2.92/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "oolongTool",
+    version = "0.2.92",
+    keywords = ("pip", "oolongTool", "oolong"),
+    description = "tiny tools in using slurm cluster",
+    long_description = "this is a project for collecting some useful tools",
+    license = "MIT Licence",
+
+    url = "https://github.com/Suchun-sv/ClusterTools",
+    author = "suchun",
+    author_email = "2594405419@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = [],
+    entry_points={
+        'console_scripts': [
+            'PostMessage = oolongTool.PostMessage:main',
+            'ChangeUser = oolongTool.ChangeUser:main'
+        ]
+    }
+)
