a2d3225449593a0988e066210c0fe55c1fdbcc42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:39 +0000

    Add package YouTubePlayer/0.0.7/YouTubePlayer-0.0.7.tar.gz

---
+++ packages/YouTubePlayer/YouTubePlayer-0.0.7.tar.gz/YouTubePlayer-0.0.7/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="YouTubePlayer",
+    version="0.0.7",
+    author="innovata sambong",
+    author_email="iinnovata@gmail.com",
+    description='innovata-YouTubePlayer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/innovata/YouTubePlayer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"":"pkgs"},
+    packages=setuptools.find_packages(where="pkgs"),
+    python_requires=">=3.8",
+    install_requires=['idebug','requests','beautifulsoup4','pandas'],
+)
