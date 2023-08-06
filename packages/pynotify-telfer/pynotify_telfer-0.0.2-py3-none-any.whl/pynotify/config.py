9a2559ee3025e58e41286d8f0d9664d7781a4253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:23 +0000

    Add package pynotify-telfer/0.0.2/pynotify_telfer-0.0.2-py3-none-any.whl

---
+++ packages/pynotify-telfer/pynotify_telfer-0.0.2-py3-none-any.whl/pynotify/config.py
@@ -0,0 +1,19 @@
+import os
+
+import pkg_resources
+import yaml
+
+config_file = pkg_resources.resource_filename(__name__, "config.yaml")
+
+
+def load_config():
+    if not os.path.exists(config_file):
+        return {}
+
+    with open(config_file, "r") as fp:
+        return yaml.load(fp, yaml.FullLoader)
+
+
+def write_config(config):
+    with open(config_file, "w") as fp:
+        yaml.dump(config, fp)
