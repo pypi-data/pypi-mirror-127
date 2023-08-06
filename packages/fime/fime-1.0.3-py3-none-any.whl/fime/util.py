304a89b2b001df319c4fda62689a6d2420d3cba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:50 +0000

    Add package fime/1.0.3/fime-1.0.3-py3-none-any.whl

---
+++ packages/fime/fime-1.0.3-py3-none-any.whl/fime/util.py
@@ -0,0 +1,17 @@
+try:
+    from PySide6 import QtGui
+except ImportError:
+    from PySide2 import QtGui
+
+
+def get_screen_height(qobject):
+    if hasattr(qobject, "screen"):
+        return qobject.screen().size().height()
+    else:
+        print("unable to detect screen height falling back to default value of 1080")
+        return 1080
+
+
+def get_icon(icon_name):
+    fallback = QtGui.QIcon(f":/icons/{icon_name}").pixmap(256, 256)
+    return QtGui.QIcon.fromTheme(icon_name, fallback)
