a916af0d17923d96b7cf6955d4df097efd938624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:56 +0000

    Add package django-frontadmin/4.0.7/django-frontadmin-4.0.7.tar.gz

---
+++ packages/django-frontadmin/django-frontadmin-4.0.7.tar.gz/django-frontadmin-4.0.7/frontadmin/templatetags/content_widgets.py
@@ -0,0 +1,33 @@
+from django import template
+from django.conf import settings
+from django.forms.models import model_to_dict
+from django.contrib.admin.models import LogEntry
+
+register = template.Library()
+
+@register.simple_tag
+def site_name():
+    return settings.SITE_NAME if hasattr(settings, 'SITE_NAME') else 'FrontAdmin'
+
+@register.inclusion_tag('elements/logo.html')
+def logo(defaut=True):
+    return {
+        'site_name': settings.SITE_NAME if hasattr(settings, 'SITE_NAME') else 'FrontAdmin',
+        'default': defaut
+    }
+
+@register.inclusion_tag('elements/sidebar.html')
+def sidebar():
+    pass
+
+@register.inclusion_tag('elements/warnings_recent_content.html')
+def warnings_recent_content():
+    entries = LogEntry.objects.all().order_by('-id')[:10]
+    return {
+        'log_entries': entries
+    }
+
+@register.filter
+def convert_to_dict(queryset):
+    mydict = model_to_dict(queryset)
+    return mydict.items()
---
+++ packages/django-frontadmin/django-frontadmin-4.0.7.tar.gz/django-frontadmin-4.0.7/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='django-frontadmin',
+    version='4.0.7',
+    author='Washim Ahmed',
+    author_email='washim.ahmed@gmail.com',
+    packages=find_packages(),
+    scripts=[],
+    url='https://github.com/washim/django-frontadmin',
+    license='MIT',
+    description='Frontadmin is a Django app to conduct Web-based frontend theme.',
+    long_description=long_description,
+    include_package_data=True,
+    install_requires=[
+        'django',
+    ],
+    python_requires='>=3.6',
+)
