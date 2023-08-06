1df3c1a1dd1834589c5f33af5976a45062c494c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:36 +0000

    Add package nn-meter/1.1/nn_meter-1.1-py3-none-any.whl

---
+++ packages/nn-meter/nn_meter-1.1-py3-none-any.whl/nn_meter/utils/utils.py
@@ -0,0 +1,46 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+import os
+from zipfile import ZipFile
+from tqdm import tqdm
+import requests
+import logging
+import json
+import numpy as np
+
+def download_from_url(urladdr, ppath):
+    """
+    download the kernel predictors from the url
+    @params:
+
+    urladdr: github release url address
+    ppath: the targeting dir to save the download data (usually hardware_inferenceframework)
+
+    """
+    file_name = os.path.join(ppath, ".zip")
+    if not os.path.isdir(ppath):
+        os.makedirs(ppath)
+
+    logging.keyinfo(f'Download from {urladdr}')
+    response = requests.get(urladdr, stream=True)
+    total_size_in_bytes = int(response.headers.get("content-length", 0))
+    block_size = 2048  # 2 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+    with open(file_name, "wb") as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    zipfile = ZipFile(file_name)
+    zipfile.extractall(path=ppath)
+    zipfile.close() 
+    progress_bar.close()
+    os.remove(file_name)
+
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        if isinstance(obj, (bytes, bytearray)):
+            return obj.decode("utf-8")
+        return json.JSONEncoder.default(self, obj)
