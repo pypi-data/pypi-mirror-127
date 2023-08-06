eeacc2913973f5b835501c8472141fa2e9a4ab09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:03 +0000

    Add package bot-exts/0.1.0/bot-exts-0.1.0.tar.gz

---
+++ packages/bot-exts/bot-exts-0.1.0.tar.gz/bot-exts-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bot_exts', 'bot_exts.cogs']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nextcord>=2.0.0-alpha.3,<3.0.0', 'pydantic>=1.8.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'bot-exts',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'TrixiS',
+    'author_email': 'oficialmorozov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
