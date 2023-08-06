214dbc9a14d1066b4dc9281c0a226c47bf8cdeac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:55 +0000

    Add package pyhelios/2.0.6/pyhelios-2.0.6.tar.gz

---
+++ packages/pyhelios/pyhelios-2.0.6.tar.gz/pyhelios-2.0.6/pyhelios/__init__.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import inspect
+import rich.pretty
+from loguru import logger
+
+try:
+    from importlib.metadata import metadata
+except ImportError: # When Python < 3.8
+    from importlib_metadata.metadata import metadata 
+
+# -- Get the Metadata.
+__version__ = metadata("pyhelios")["Version"]
+__author__ = metadata("pyhelios")["Author"]
+__maintainer__ = metadata("pyhelios")["Maintainer"]
+__license__ = metadata("pyhelios")["License"]
+__description__ = metadata("pyhelios")["Summary"]
+
+# -- Configure the rich package.
+rich.pretty.install()
+
+# -- Configure le loguru package.
+config = {
+    "handlers": [
+        {
+            "sink": sys.stderr,
+            "filter": lambda record: record["level"].name == "ERROR",
+            "format": "<g>[{time:HH:mm:ss}]</g> <lvl>[{level}]</lvl> <lvl>{message}</lvl> <cyan>{name}</cyan>:<magenta>{line}</magenta>",
+        },
+        {
+            "sink": sys.stdout,
+            "filter": lambda record: record["level"].name == "INFO",
+            "format": "<g>[{time:HH:mm:ss}]</g> <lvl>[{level}]</lvl> {message}",
+        },
+    ]
+}
+logger.configure(**config)
+logger.level("ERROR", color="<red>")
+logger.level("INFO", color="<green>")
