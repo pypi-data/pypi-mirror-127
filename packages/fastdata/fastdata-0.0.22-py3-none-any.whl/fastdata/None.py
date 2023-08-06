6d9a20203bda12fd2176cf36a9ea24dd8029b5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:22 +0000

    Add package fastdata/0.0.22/fastdata-0.0.22-py3-none-any.whl

---
+++ packages/fastdata/fastdata-0.0.22-py3-none-any.whl/fastdata/None.py
@@ -0,0 +1,49 @@
+
+
+# Cell
+from airtable import Airtable
+import pandas as pd
+import gspread
+from gspread_pandas import Spread
+from pathlib import Path
+import numpy as np
+
+# Cell
+from airtable import Airtable
+import pandas as pd
+import gspread
+from gspread_pandas import Spread
+from pathlib import Path
+import numpy as np
+
+# Cell
+from airtable import Airtable
+import pandas as pd
+import gspread
+from gspread_pandas import Spread
+from pathlib import Path
+import numpy as np
+
+# Cell
+from airtable import Airtable
+import pandas as pd
+import gspread
+from gspread_pandas import Spread
+from pathlib import Path
+import numpy as np
+
+# Cell
+from airtable import Airtable
+import pandas as pd
+import gspread
+from gspread_pandas import Spread
+from pathlib import Path
+import numpy as np
+
+# Cell
+from airtable import Airtable
+import pandas as pd
+import gspread
+from gspread_pandas import Spread
+from pathlib import Path
+import numpy as np
