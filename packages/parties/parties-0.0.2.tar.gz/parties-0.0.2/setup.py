42f5bbd2eda71d32b9f81cef6ededf4246c1d4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:43 +0000

    Add package parties/0.0.2/parties-0.0.2.tar.gz

---
+++ packages/parties/parties-0.0.2.tar.gz/parties-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='parties',
+   version='0.0.2',
+   description='A python shell for parties cfd code',
+   license="MIT",
+   long_description='A python shell for parties cfd code',
+   author='Alexander Metelkin',
+   author_email='a.metelkin@tu-braunschweig.de',
+   url="https://github.com/metialex/PARTIES_python_shell",
+   packages=find_packages(),  #same as name
+   install_requires=[''] #external packages as dependencies
+)
