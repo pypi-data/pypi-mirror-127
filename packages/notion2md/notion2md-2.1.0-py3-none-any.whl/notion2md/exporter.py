b72c4b0f56f6aa69095f15331ba6f70de4035b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:33 +0000

    Add package notion2md/2.1.0/notion2md-2.1.0-py3-none-any.whl

---
+++ packages/notion2md/notion2md-2.1.0-py3-none-any.whl/notion2md/exporter.py
@@ -0,0 +1,24 @@
+from notion2md.client_handler import notion_client_object
+from notion2md.evaluator.block import blocks_evaluator
+import os
+import time
+
+def block_exporter(target_id:str,output_path="notion2md-output"):
+    start_time = time.time()
+    #Directory Checking and Creating
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    #Get title from parent page block
+    block_title = notion_client_object.blocks.retrieve(target_id)['child_page']['title']
+    #Get actual blocks
+    blocks = notion_client_object.blocks.children.list(target_id)['results']
+    print(f'Export {len(blocks)} blocks')
+    #Write(Export) Markdown file
+    with open(os.path.join(output_path,block_title+'.md'),'w') as output:
+        output.write(blocks_evaluator(blocks))
+    #Result and Time Check
+    print(f"Export Completed in {time.time() - start_time:.2f}s")
+
+# page_exporter()
+
+# database_exporter()
