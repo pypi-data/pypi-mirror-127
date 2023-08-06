a8ae17765a008ecd01cf662dcdbfc3ef878bbcac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:53 +0000

    Add package clipboard-sync-client/0.0.23/clipboard_sync_client-0.0.23.tar.gz

---
+++ packages/clipboard-sync-client/clipboard_sync_client-0.0.23.tar.gz/clipboard_sync_client-0.0.23/clipboard_sync_client.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import socketio,pyperclip,time,argparse
+
+__version__ = "v0.0.23"
+# standard Python
+sio = socketio.Client()
+@sio.event
+def connect():
+    print("I'm connected!")
+
+@sio.event
+def connect_error(data):
+    print("The connection failed!")
+
+@sio.event
+def disconnect():
+    print("I'm disconnected!")
+
+
+@sio.on('sync')
+def on_message(data):
+    curr_clip = pyperclip.paste()
+    if data["data"] != curr_clip:
+        pyperclip.copy(data["data"])
+
+def main():
+    parser = argparse.ArgumentParser(description='client for sync clipboard')
+    parser.add_argument('-s','--server',help='server address to connect to',default="http://localhost:8080")
+    parser.add_argument('-i','--id',help='the id to specify users',default="123123")
+    parser.add_argument('-t','--access_token',help='token to access the server, must be same to the token of server',default="123123")
+    args = parser.parse_args()
+    server = args.server
+    room = args.id
+    access_token=args.access_token
+    sio.connect(url=server,auth={"room":room,"access_token":access_token})
+
+    curr_clip = pyperclip.paste()
+
+    while True:
+        new_clip = pyperclip.paste()
+        if new_clip != curr_clip:
+            if not new_clip.startswith("file:///"):
+                sio.emit('sync',{"data":new_clip, "room":room})
+            curr_clip = new_clip
+        time.sleep(0.5)
+def cli() -> None:
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExited on keyboard interrupt.")
+
+if __name__ == '__main__':
+    cli()
