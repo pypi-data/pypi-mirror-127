88e6fd1363c91f34ef08fb3d5ffc31a6d3cb9d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:40 +0000

    Add package nexusmaker/1.61/nexusmaker-1.61.tar.gz

---
+++ packages/nexusmaker/nexusmaker-1.61.tar.gz/nexusmaker-1.61/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+__version__ = 1.61
+
+setup(
+    name='nexusmaker',
+    version=__version__,
+    description="nexusmaker - Nexus file maker for language phylogenies",
+    url='',
+    author='Simon J. Greenhill',
+    author_email='simon@simon.net.nz',
+    license='BSD',
+    zip_safe=True,
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='language-phylogenies',
+    packages=find_packages(),
+    install_requires=['python-nexus', ],
+    setup_requires=['pytest-runner', ],
+    tests_require=['pytest', ],
+)
