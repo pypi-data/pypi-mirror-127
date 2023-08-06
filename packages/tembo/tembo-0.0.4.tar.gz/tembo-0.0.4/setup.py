ec5be548ef99766d90033100e410bd1de59cfaf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:52 +0000

    Add package tembo/0.0.4/tembo-0.0.4.tar.gz

---
+++ packages/tembo/tembo-0.0.4.tar.gz/tembo-0.0.4/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tembo', 'tembo.cli', 'tembo.journal', 'tembo.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=3.0.2,<4.0.0',
+ 'click>=8.0.3,<9.0.0',
+ 'panaetius>=2.3.2,<3.0.0',
+ 'pendulum>=2.1.2,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['tembo = tembo.cli.cli:main']}
+
+setup_kwargs = {
+    'name': 'tembo',
+    'version': '0.0.4',
+    'description': '',
+    'long_description': None,
+    'author': 'dtomlinson',
+    'author_email': 'dtomlinson@panaetius.co.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
