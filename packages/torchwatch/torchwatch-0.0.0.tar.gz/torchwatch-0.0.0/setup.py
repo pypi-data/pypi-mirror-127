c97d320a59eab17358e5450afc047d2682fefea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:54 +0000

    Add package torchwatch/0.0.0/torchwatch-0.0.0.tar.gz

---
+++ packages/torchwatch/torchwatch-0.0.0.tar.gz/torchwatch-0.0.0/setup.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import setuptools
+from setuptools import setup
+
+with open('requirements.txt', 'r') as fp:
+    requirements = list(filter(bool, (line.strip() for line in fp)))
+
+with open('README.rst') as fp:
+    long_description = fp.read()
+
+package_data = []
+
+setup(
+    name='torchwatch',
+    url='https://github.com/szuboy/torchwatch#readme',
+    project_urls={
+        'Documentation': 'https://torchwatch.readthedocs.io/',
+        'Github': 'https://github.com/szuboy/torchwatch'
+    },
+    author='szuboy',
+    author_email='szujeremy@gmail.com',
+    version='0.0.0',
+    packages=setuptools.find_packages(),
+    package_data={'torchwatch': package_data},
+    description='basic block built with pytorch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache License',
+    keywords='torch torchwatch basic-block module deep-learning Residual Inception SE_block',
+    install_requires=requirements,
+    python_requires='>=3.0'
+)
