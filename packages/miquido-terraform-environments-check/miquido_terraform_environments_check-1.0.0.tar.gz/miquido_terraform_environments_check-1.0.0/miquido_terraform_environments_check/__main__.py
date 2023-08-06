9cd968a277d2988bfa5d78c0920c56c7d4e5f867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:56 +0000

    Add package miquido-terraform-environments-check/1.0.0/miquido_terraform_environments_check-1.0.0.tar.gz

---
+++ packages/miquido-terraform-environments-check/miquido_terraform_environments_check-1.0.0.tar.gz/miquido_terraform_environments_check-1.0.0/miquido_terraform_environments_check/__main__.py
@@ -0,0 +1,23 @@
+# yamllint disable rule:line-length
+#     - |
+#       IFS=' ' #setting space as delimiter
+#       read -ra ENVIRONMENTS <<<"$ENVIRONMENTS"
+#       for ENVIRONMENT in "${ENVIRONMENTS[@]}"
+#       do
+#         cd "$ENVIRONMENT"
+#         git config --global \url."https://${GITLAB_TERRAFORM_DEPLOY_TOKEN_USERNAME}:${GITLAB_TERRAFORM_DEPLOY_TOKEN_SECRET}@gitlab.com/miquido/terraform".insteadOf "ssh://git@gitlab.com/miquido/terraform"
+#         git config --global url."https://github.com".insteadOf "ssh://git@github.com"
+#         terraform init -backend=false
+#         terraform validate
+#         cd -
+#       done
+import os
+
+
+environments = os.getenv('ENVIRONMENTS').split(' ')
+
+for environment in environments:
+    os.chdir(environment)
+    os.system('terraform init -backend=false')
+    os.system('terraform validate')
+    os.chdir('../')
