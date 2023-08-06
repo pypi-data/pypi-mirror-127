2783d0e970df61a06eefed7d1a0866cf246f40a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:38 +0000

    Add package adummypatt/0.0.1/adummypatt-0.0.1.tar.gz

---
+++ packages/adummypatt/adummypatt-0.0.1.tar.gz/adummypatt-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='adummypatt',
+  version='0.0.1',
+  description='Just a test dummy package',
+  url='',  
+  author='Israel Akinola',
+  author_email='israelakinola95@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='s', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
