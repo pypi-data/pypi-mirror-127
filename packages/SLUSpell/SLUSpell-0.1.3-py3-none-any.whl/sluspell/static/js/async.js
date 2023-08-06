4ecbc31d6332b1baaf4db79409c92b8c55e694c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:04 +0000

    Add package SLUSpell/0.1.3/SLUSpell-0.1.3-py3-none-any.whl

---
+++ packages/SLUSpell/SLUSpell-0.1.3-py3-none-any.whl/sluspell/static/js/async.js
@@ -0,0 +1,150 @@
+
+function WordDictionary(input_json){   
+        let word_list = [];
+       
+ 
+        let n = 0;
+        for (let key in input_json) {
+            n++;
+        }
+        for (let i = 0; i < n; i++) {
+            let temp = input_json[i];
+            if (temp['correction_flag']){
+              word_list.push(temp['new_string']); // &nbsp;    .replace(/ /g, '\u00a0')
+
+            }else{
+              word_list.push(temp['new_string'].replace(/ /g, '\u00a0')); // &nbsp;    
+            }
+              
+            }
+
+             
+        let wordString = word_list.join(''); 
+        return wordString;   
+  };
+
+
+function ShowMisspelled(input_json){
+    var word_id = [];
+    var word_list = [];
+    var temp_string = [];
+    var n = 0;
+
+    for (var key in input_json) {
+        n++;
+    }
+    for (var i = 0; i < n; i++) {
+        var temp = input_json[i];
+        if (temp['correction_flag'] == true){
+            word_list.push(temp['correct']);   
+            word_id.push(temp['id']);
+            // temp_string.push("<a class='hl_faded' id='word"+ temp['id'] + "\'" + "onmouseover='show_correct("+ temp['id'] +")' onclick='corrector("+temp['id']+")'>"+temp['correct']+"<a>");
+            console.log(temp);
+            var new_string= '';
+            for (var j = 0; j < temp['correct'].length; j++) {
+                new_string = new_string + "<button type='button' class='btn btn-light word"+temp['id']+ " hl_"+j+" hl_faded' id='word"+ temp['id']+ "\'" + "onmouseover='show_correct("+ temp['id'] +")' onclick='corrector("+temp['id']+"," +j + ")'>"+temp['correct'][j]+"</button>" + "</br>";
+                    
+                }  
+                temp_string.push(new_string);  
+            }
+        }
+    var correct_string = temp_string.join('<br>'); 
+    return correct_string;   
+};
+
+  
+  function placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined" &&
+      typeof document.createRange != "undefined") {
+      var range = document.createRange();
+      range.selectNodeContents(el);
+      range.collapse(false);
+      var sel = window.getSelection();
+      sel.removeAllRanges();
+      sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+      var textRange = document.body.createTextRange();
+      textRange.moveToElementText(el);
+      textRange.collapse(false);
+      textRange.select();
+    }
+  }
+
+function async(){
+      var textData = document.getElementById("Text_box").textContent;
+      var languageSelector = document.getElementById("Language_Selection").value;
+      var formalitySelector = document.getElementById("Formality_Selection").value;
+      
+
+        var xml = new XMLHttpRequest();
+        xml.open("POST", "/check", true);
+        xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xml.onload = function(event){
+            var dataReply = JSON.parse(xml.responseText) ;// checked_text
+            // document.getElementById("Text_display").innerHTML = dataReply.checked_text;
+            // console.log(dataReply);
+            console.log(dataReply.checked_text);
+
+            let html_string = WordDictionary(dataReply.checked_text);
+            console.log("called from inside async\n"+html_string);
+            let corrected_string = ShowMisspelled(dataReply.checked_text);
+            // console.log("HTML String" + html_string);
+
+            
+            document.getElementById("Text_box").innerHTML = html_string;
+            document.getElementById("Text_display").innerHTML = corrected_string;
+            placeCaretAtEnd(document.getElementById("Text_box"));
+
+            // let input_element = document.getElementById("Text_box");
+            // input_element.focus();
+            // input_element.selectionStart = input_element.selectionEnd = input_element.innerHTML.length;
+
+            event.preventDefault();
+
+    };
+    dataSend = JSON.stringify({
+        'input_text' : textData,
+        'language_selector' : languageSelector,
+        'formality_selector' : formalitySelector
+    });
+    xml.send(dataSend)
+    
+
+    // event.preventDefault();
+};
+
+
+
+function async_correction(input_id, word_index){
+    var element_id = input_id;    
+    var list_index = word_index;
+
+      var xml2 = new XMLHttpRequest();
+      xml2.open("POST", "/correct", true);
+      xml2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xml2.onload = function(event){
+          var dataReply = JSON.parse(xml2.responseText) ;// checked_text
+          console.log(dataReply.checked_text);
+
+          var html_string = WordDictionary(dataReply.checked_text);
+          var corrected_string = ShowMisspelled(dataReply.checked_text);
+
+        //   console.log("HTML String" + html_string);
+          document.getElementById("Text_box").innerHTML = html_string;
+          document.getElementById("Text_display").innerHTML = corrected_string;
+
+          event.preventDefault();
+  };
+  dataSend = JSON.stringify({
+      'element_id' : element_id,
+      'list_index' : list_index
+  });
+  xml2.send(dataSend)
+//   event.preventDefault();
+  
+};
+
+
+
+
