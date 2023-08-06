301e00a2bc9827bba4f5f53af4cea9f135c32b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:24 +0000

    Add package blobcity/0.0.5/blobcity-0.0.5.tar.gz

---
+++ packages/blobcity/blobcity-0.0.5.tar.gz/blobcity-0.0.5/blobcity/main/driver.py
@@ -0,0 +1,121 @@
+# Copyright 2021 BlobCity, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import pickle
+import numpy as np
+import pandas as pd
+from blobcity.store import DictClass
+from blobcity.utils import get_dataframe_type,dataCleaner
+from blobcity.utils import AutoFeatureSelection as AFS
+from blobcity.main.modelSelection import model_search
+from blobcity.code_gen import yml_reader,code_generator
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import SelectKBest,f_regression,f_classif
+def train(file=None, df=None, target=None,features=None,accuracy_criteria=0.99):
+    """
+    param1: string: dataset file path 
+
+    param2: (optional) pandas.DataFrame object
+
+    param3: string: target/dependent column name.
+
+    param4: float: range[0.1,1.0] 
+
+    return: Model Class Object
+    Performs a model search on the data proivded. A yaml file is generated once the best fit model configuration
+    is discovered. The yaml file is later used for generating source code. 
+
+    Input to the function must be one of file or data frame (df). Passing both parameters of file and df in a single
+    invocation is an incorrect use.
+    """
+    dict_class=DictClass()
+    dict_class.resetVar()
+    #data read
+    if file!=None:
+        dataframe= get_dataframe_type(file, dict_class)
+    else: 
+        dataframe = df
+        dict_class.addKeyValue('data_read',{"type":"df","class":"df"})
+        
+    if(features==None):
+        featureList=AFS.FeatureSelection(dataframe,target,dict_class)
+        CleanedDF=dataCleaner(dataframe,featureList,target,dict_class)
+    else:
+        CleanedDF=dataCleaner(dataframe,features,target,dict_class)
+    #model search space
+    accuracy_criteria= accuracy_criteria if accuracy_criteria<=1.0 else (accuracy_criteria/100)
+    modelClass = model_search(CleanedDF,target,dict_class,use_neural=False,accuracy_criteria=accuracy_criteria)
+    modelClass.yamldata=dict_class.getdict()
+    modelClass.feature_importance_=dict_class.feature_importance if(features==None) else calculate_feature_importance(CleanedDF.drop(target,axis=1),CleanedDF[target],dict_class)
+    dict_class.resetVar()
+    return modelClass
+
+def load(modelFile,h5_path=None):
+        """
+        param1: string: (required) the filepath to the stored model. Supports .pkl models.
+        param2: string: the filepath to the stored h5 file, provide only if saved h5 file.
+        returns: Model file
+
+        function loads the serialized model from .pkl or .h5 format to usable format.
+        """
+        path_components = modelFile.split('.')
+        extension = path_components[1] if len(path_components)<=2 else path_components[-1]
+         
+        if extension == 'pkl' and h5_path in [None,""]:
+            model = pickle.load(open(modelFile, 'rb'))
+
+        """ elif os.path.splitext(h5_path)[1] == '.h5' and h5_path!=None:
+            print("pkl path: {}, h5 path : {}".format(os.path.splitext(modelFile),os.path.splitext(h5_path)))
+            if os.path.splitext(h5_path)[0] == os.path.splitext(modelFile)[0]:
+                tfmodel = tf.keras.models.load_model(h5_path)
+                model=pickle.load(open(modelFile, 'rb'))
+                model.model=tfmodel
+            else:
+                raise ValueError("file name for pickle and h5 file should be same") """
+        return model
+
+def spill(filepath,yaml_path=None,doc=None):
+    """
+    param1:string : filepath and format of generated file to store. either .py or .ipynb
+    param2:string : filepath of already generated YAML file 
+    param3:boolean : whether generate code along with documentation
+
+    Function calls generator functions to generate source code for the AutoAI Procedure
+    """
+    if yaml_path in [None,""] : raise TypeError("YAML file path can't be None")
+    data=yml_reader(yaml_path)
+    code_generator(data,filepath,doc)
+
+def calculate_feature_importance(X,Y,dict_class):
+    """
+    param1:pd.DataFrame
+    param2:pd.Series/pd.DataFrame
+    param3: class Object
+    return: dictionary
+
+    Function to calculate the feature importance of the features
+    """
+    if X.shape[1]>2:
+        score_func=f_classif if(dict_class.getdict()['problem']["type"]=='Classification') else f_regression
+        fit = SelectKBest(score_func=score_func, k=X.shape[1]).fit(X,Y)
+        dfscores,dfcolumns = pd.DataFrame(fit.scores_),pd.DataFrame(X.columns)
+        df = pd.concat([dfcolumns,dfscores],axis=1)
+        df.columns = ['features','Score']
+        df['Score']=MinMaxScaler().fit_transform(np.array(df['Score']).reshape(-1,1))
+        imp=AFS.MainScore(dict(df.values),dict_class)
+        return imp
+    else:
+        print('Dataset has only {} features, required atleast 2 for feature importances'.format(X.shape[1]))
+        return None
---
+++ packages/blobcity/blobcity-0.0.5.tar.gz/blobcity-0.0.5/blobcity/store/Model.py
@@ -0,0 +1,256 @@
+# Copyright 2021 BlobCity, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pickle
+import time
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from blobcity.utils import get_dataframe_type,Progress,write_dataframe
+from blobcity.code_gen import code_generator
+import yaml
+"""
+Python file consists of Class Model to initialize/store and retrive data associated to trained machine learning model.
+"""
+class Model:
+    params=dict()
+    featureList=[]
+    model=None
+    metrics=dict()
+    yamldata=None
+    feature_importance_=dict()
+    target_encode=dict()
+    plot_data=None
+    def __init__(self):
+        self.params=dict()
+        self.featureList=[]
+        self.model=None
+        self.metrics=dict()
+        self.yamldata=None
+        self.feature_importance_=dict()
+        self.plot_data=None
+        self.target_encode=dict()
+    
+    def __quick_clean(self,test_dataframe):
+        """
+        """
+        cols=test_dataframe.columns.to_list()
+        for i in cols:
+            if(isinstance(test_dataframe[i], pd.Series) and (test_dataframe[i].dtype in ["float64","int64","float","int"])):
+                if(len(np.unique(test_dataframe[i]))<=3):
+                    test_dataframe[i].fillna(test_dataframe[i].mode()[0],inplace=True)
+                else:
+                    test_dataframe[i].fillna(test_dataframe[i].mean(),inplace=True)
+            elif(isinstance(test_dataframe[i], pd.Series)):
+                test_dataframe[i].fillna(test_dataframe[i].mode()[0],inplace=True)
+        if "object" in test_dataframe.dtypes.to_list():
+            test_dataframe=pd.get_dummies(test_dataframe)
+        return test_dataframe
+        
+    def predict(self,test,return_type="list",path=""):
+        """
+        param1: self
+        param2: pd.DataFrame
+        param3: string : either list or pd.DataFrame to return 
+        param4: string : file path to store output pd.DataFrame,supported file types {'csv','json','xlsx'} 
+        return: List/pd.DataFrame
+
+        Function returns List/Array for predicted value from the trained model.
+        """
+        test=get_dataframe_type(test)
+        if isinstance(test,pd.DataFrame):test=Model().__quick_clean(test[self.yamldata['features']['X_values']])
+        if self.model.__class__.__name__ not in ['XGBClassifier','XGBRegressor']:result=self.model.predict(test)
+        else:
+            if type(test)=="list":
+                test_df=pd.DataFrame(test, columns=self.featureList)
+                result=self.model.predict(test_df)  
+            else:
+                result=self.model.predict(test) 
+            
+        if self.yamldata['problem']["type"]=='Classification':
+            main_result=[]
+            if self.target_encode!={}:
+                for i in range(len(result)):
+                    for k in self.target_encode.keys():
+                        if k == result[i]:
+                            main_result.append(self.target_encode[k])
+                result= main_result 
+        
+        result_dataframe=test.copy(deep=True)
+        result_dataframe['prediction']=result
+        
+        if path!="":write_dataframe(dataframe=result_dataframe,path=path)
+        if return_type=="list": return result
+        elif return_type=="df" and isinstance(test,pd.DataFrame):return result_dataframe
+
+    def parameters(self):
+        """
+        return: Dictionary
+
+        Function returns dictionary consisting of tuned parameters value for the trained model.
+        """
+        return self.params
+
+    def features(self):
+        """
+        return: List/Array
+
+        Function returns List of features used by model to train. This is also used to recognise the features to be passed as input into the predict function.
+        """
+        return self.featureList
+
+    def save(self, model_path=None):
+        """
+        param: Entire Path for model pickle file, Supported formats is .pkl.
+        
+        Function saves the model and its weights serially and returns the filepath where it is saved.
+        """
+        if model_path not in [None,""]:
+            path_components = model_path.split('.')
+            extension = path_components[1] if len(path_components)<=2 else path_components[-1]
+
+            if extension == '/':
+                final_path = os.path.join(model_path, 'autoaimodel.pkl')
+                pickle.dump(self, open(final_path, 'wb'))
+                print("The model is stored at {}".format(final_path))
+            elif extension == 'pkl':
+                final_path = model_path
+                pickle.dump(self, open(final_path, 'wb'))
+                print("The model is stored at {}".format(final_path))
+             
+                """ 
+            elif extension == 'h5' or self.yamldata['model']['type'] in ['TF','tf']:
+                model_path = model_path if model_path!="./" else os.path.join(model_path, 'autoaimodel.h5')
+                class_path = model_path if model_path!="./" else os.path.join(model_path, 'autoaimodel.pkl')
+                try:
+                    tfmodel_temp=self.model
+                    self.model.save(model_path)
+                    self.model=None
+                    pickle.dump(self, open(class_path, 'wb'))
+                    self.model=tfmodel_temp
+                    print("The model is stored at {}".format(model_path))
+                    return model_path
+                except:
+                    raise TypeError("Your model is not a Keras model of type .h5. Try .pkl extension.") """  
+            else:
+                raise TypeError(f"{extension} file type must be .pkl")
+        else:
+            raise ValueError("model_path cant be None or empty string")
+
+
+    def stats(self):
+        """
+        Function to print/log/display all the metrics associated with problem type for the selected trained model. Usally used to check the effectiveness of training, or to assess the model fit. 
+        """
+        print ("\nMetrics on trained data:\n{:<10} {:<10}".format('METRIC', 'VALUE'))
+        for key, value in self.metrics.items():
+            print ("{:<10} {:<10}".format(key, value))
+
+    def spill(self,filepath=None,doc=None):
+        """
+        param1: string : Filepath and format of generated file to store. either .py or .ipynb
+        param2: boolean :  Whether generate code along with documentation.
+
+        Function calls generator functions to generate source code for the AutoAI Procedure
+        """
+        if filepath not in [None,""]:
+            data=self.yamldata
+            code_generator(data,filepath,doc)
+        else:
+            raise ValueError("filepath can't be None or empty string")
+
+    def generate_yaml(self,path=None):
+        """
+        param1: string : File path to store .yaml file,if not specified store in current directory with `Process.yaml`
+        
+        Function generated and create YAML configuration file for the complete AutoAI procedures.
+        """
+        if path!=None:
+            extension = os.path.splitext(path)[1]
+            filepath=path
+        else:
+            filepath = './Process.yaml'
+            extension=".yaml"
+        if extension in [".yaml",".yml"]:
+            with open(filepath, 'w') as file:
+                yaml.dump(self.yamldata, file,sort_keys=False)
+        else:
+            raise TypeError(f"{extension} file type must be .yml or .yaml")
+
+    def plot_feature_importance(self):
+        """
+        Function to plot feature importance calculated during auto feature selection.
+        """
+        val=self.feature_importance_
+        if(val!=None):
+            val=dict(sorted(val.items(), key=lambda item: item[1],reverse=False))
+            plt.figure(figsize = (12, 6))
+            plt.barh(range(len(val)), list(val.values()), align='center')
+            plt.yticks(range(len(val)),list(val.keys()))
+            plt.show()
+        else:
+            print("Feature importance not available for dataset with less then 2 columns") 
+
+    def get_prediction_data(self):
+        """
+        return: array/2D array/Dictionary/pandas.Dataframe
+        Function returns predicted and actual target data.
+        """
+        problem=self.yamldata['problem']["type"]
+        if problem=='Classification':
+            return self.plot_data
+        elif problem=="Regression":
+            return {'true':self.plot_data[0],'predicted':self.plot_data[1]}
+
+            
+    def plot_prediction(self,n_rows=1000):
+        """
+        param1: integer : signed and unsigned integer for plot number of records for regression problem.
+
+        Function plots either confusion matrix or regression plot(line plot comparing true and predicted values) based on problem type.
+        """
+        
+        problem=self.yamldata['problem']["type"]
+        if problem=='Classification':
+            if self.target_encode!={}: targets=self.target_encode.values()
+            cf_matrix=self.plot_data
+            group_counts = ['{0:0.0f}'.format(value) for value in cf_matrix.flatten()]
+            group_percentages = ["{0:.2%}".format(value) for value in cf_matrix.flatten()/np.sum(cf_matrix)]
+            labels = [f'{v1}\n\n{v2}' for v1, v2 in zip(group_counts,group_percentages)]
+            labels = np.asarray(labels).reshape(cf_matrix.shape[0],cf_matrix.shape[0])
+            if self.target_encode!={}:
+                sns.heatmap(cf_matrix, annot=labels, fmt='',xticklabels=targets,yticklabels=targets,cmap='Blues')
+            else: 
+                sns.heatmap(cf_matrix, annot=labels, fmt='',cmap='Blues')
+            plt.show()
+        elif problem=="Regression":
+            #plot for regression problem
+            if  abs(n_rows)!=0:
+                if abs(n_rows)<=len(self.plot_data[0]) or abs(n_rows)==1000:
+                    n=len(self.plot_data[0]) if len(self.plot_data[0])<abs(n_rows) else n_rows
+                    if n < 0: true,predict=self.plot_data[0][n:],self.plot_data[1][n:]
+                    else:true,predict=self.plot_data[0][0:n],self.plot_data[1][0:n]
+                    plt.figure(figsize=(14,10))
+                    plt.plot(range(abs(n)),true, color = "green")
+                    plt.plot(range(abs(n)),predict,linestyle='--',color = "red")
+                    plt.legend(["Actual","prediction"]) 
+                    plt.xlabel("Record number")
+                    plt.ylabel(self.yamldata['features']['Y_values'])
+                    plt.show()
+                elif abs(n_rows)>len(self.plot_data[0]):
+                    raise ValueError("entered row counts {} more than actual row counts {}".format(abs(len(self.plot_data[0])-abs(n_rows)),len(self.plot_data[0])))
+            else:
+                raise ValueError("Number of rows can't be Zero")
