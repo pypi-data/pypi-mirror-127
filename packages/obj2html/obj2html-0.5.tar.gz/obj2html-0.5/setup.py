94467fd2fcb11cf0e37a4746c55222ff7502caa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:58 +0000

    Add package obj2html/0.5/obj2html-0.5.tar.gz

---
+++ packages/obj2html/obj2html-0.5.tar.gz/obj2html-0.5/setup.py
@@ -0,0 +1,48 @@
+from distutils.core import setup
+
+long_des = """obj2html lib
+=============================
+
+You can use that lib to create html file from a .obj path:
+
+    from obj2html import obj2html
+
+    html_string = obj2html(obj_path)
+
+    obj2html(obj_path, 'index.html')
+
+    # firefox index.html
+
+.. image:: https://gitlab.com/nicolalandro/obj2html/-/raw/main/imgs/colab_sample.png
+  :alt: Colab example
+
+Use in a Jupyter notebook to display a .obj 3D file:
+
+    ! pip install obj2html
+    
+    ! wget https://gitlab.com/nicolalandro/obj2html/-/raw/main/test/assets/model.obj
+    
+    from obj2html import obj2html
+
+    from IPython.display import display, HTML
+
+    obj_path = 'model.obj'
+
+    obj2html(obj_path, 'index.html')
+
+    display(HTML('index.html'))
+
+"""
+
+setup(
+  name = 'obj2html',
+  packages = ['obj2html'],
+  version = '0.5',
+  license='MIT',
+  description = 'Create an html with three.js that contains the given .obj file.',
+  long_description = long_des,
+  author = 'Nicola Landro',
+  author_email = 'nicolaxx94@live.it',
+  url = 'https://gitlab.com/nicolalandro/obj2html',
+  keywords = ['3D', '.obj', '.html', 'jupyter', '3D viewer'],
+)
