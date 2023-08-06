22988d4ede8a64f7ac58755fe112c0bc4a8d8d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:33 +0000

    Add package jim-kit/0.0.1/jim_kit-0.0.1.tar.gz

---
+++ packages/jim-kit/jim_kit-0.0.1.tar.gz/jim_kit-0.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+setup(
+ name = 'jim_kit',
+ version = '0.0.1',
+ description = 'jims_library ',
+ long_description = 'this is jims personal library ',
+ author = 'jim',
+ author_email = '694067345@qq.com',
+ url = '',
+ license = 'MIT Licence',
+ keywords = 'personal',
+ platforms = 'any',
+ python_requires = '>=3.7.*',
+ install_requires = ['numpy','matplotlib','pywt'],
+ #package_dir = {'': 'src'},
+ packages = find_packages('src')
+ )
