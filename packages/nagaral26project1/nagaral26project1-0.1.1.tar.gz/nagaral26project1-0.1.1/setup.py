0963ed4adf048ec86be5ebe8fc8f7d2d704abff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package nagaral26project1/0.1.1/nagaral26project1-0.1.1.tar.gz

---
+++ packages/nagaral26project1/nagaral26project1-0.1.1.tar.gz/nagaral26project1-0.1.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='nagaral26project1',
+    version='0.1.1',
+    description='Sample package for Python-Guide.org',
+    Long_description=open('README.rst').read(),
+    author='Nitesh Agarwal',
+    author_email='',
+    url='https://github.com/nagarwal26/project1',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
