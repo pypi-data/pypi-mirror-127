6e242b84ca920e5dbcf9e54fda126bda25ca711b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:03 +0000

    Add package smartthings-rest/0.1.0/smartthings-rest-0.1.0.tar.gz

---
+++ packages/smartthings-rest/smartthings-rest-0.1.0.tar.gz/smartthings-rest-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['smartthings_rest']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'smartthings-rest',
+    'version': '0.1.0',
+    'description': 'Smart and straightforward lib for controlling things with smartthings',
+    'long_description': None,
+    'author': 'Viktor Freiman',
+    'author_email': 'freiman.viktor@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
