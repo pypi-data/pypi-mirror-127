8f8703a51838ec7620b00970f0989318d5073e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:56 +0000

    Add package sagemath-standard/9.5b6/sagemath-standard-9.5b6.tar.gz

---
+++ packages/sagemath-standard/sagemath-standard-9.5b6.tar.gz/sagemath-standard-9.5b6/sage/combinat/root_system/__init__.py
@@ -0,0 +1,9 @@
+# currently needed to activate the backward compatibility
+# register_unpickle_override
+from . import type_A
+from . import type_B
+from . import type_C
+from . import type_D
+from . import type_E
+from . import type_F
+from . import type_G
