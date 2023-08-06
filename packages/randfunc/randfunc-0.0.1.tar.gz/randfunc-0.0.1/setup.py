97699922e4d5c0a06e1bf53be606da1e1f1a498d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:06 +0000

    Add package randfunc/0.0.1/randfunc-0.0.1.tar.gz

---
+++ packages/randfunc/randfunc-0.0.1.tar.gz/randfunc-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="randfunc",
+    version="0.0.1",
+    author="Diwan Mohamed Faheer",
+    author_email="diwanmohamedfaheer@gmail.com",
+    description="A package full of random functions...",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cu3t0m/randfunc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
