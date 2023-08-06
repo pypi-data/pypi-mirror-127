cff50f24c741247700ae556a915d0ebbc1ea233b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:01 +0000

    Add package django-json-model-widget/0.0.4/django-json-model-widget-0.0.4.tar.gz

---
+++ packages/django-json-model-widget/django-json-model-widget-0.0.4.tar.gz/django-json-model-widget-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='django-json-model-widget',
+    version='0.0.4',
+    description='Custom flat json field widget for model pairs',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Oleg Galichkin',
+    author_email='galij899@yandex.ru',
+    keywords=['django', 'django-admin', 'django-admin-widget'],
+    url='https://github.com/galij899/django-json-model-widget',
+    download_url='https://pypi.org/project/django-json-model-widget/'
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
