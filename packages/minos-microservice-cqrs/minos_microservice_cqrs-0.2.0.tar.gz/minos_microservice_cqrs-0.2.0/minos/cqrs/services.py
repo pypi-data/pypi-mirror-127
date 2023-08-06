942f218f5c583557b88a49aa6bfc45f5a6f70745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:08 +0000

    Add package minos-microservice-cqrs/0.2.0/minos_microservice_cqrs-0.2.0.tar.gz

---
+++ packages/minos-microservice-cqrs/minos_microservice_cqrs-0.2.0.tar.gz/minos_microservice_cqrs-0.2.0/minos/cqrs/services.py
@@ -0,0 +1,91 @@
+from abc import (
+    ABC,
+)
+from functools import (
+    partial,
+)
+from inspect import (
+    getmembers,
+    isfunction,
+    ismethod,
+)
+
+from dependency_injector.wiring import (
+    Provide,
+    inject,
+)
+
+from minos.common import (
+    MinosConfig,
+)
+from minos.networks import (
+    EnrouteDecorator,
+    Request,
+    WrappedRequest,
+)
+from minos.saga import (
+    SagaManager,
+)
+
+from .exceptions import (
+    MinosIllegalHandlingException,
+)
+from .handlers import (
+    PreEventHandler,
+)
+
+
+class Service(ABC):
+    """Base Service class"""
+
+    @inject
+    def __init__(
+        self,
+        *args,
+        config: MinosConfig = Provide["config"],
+        saga_manager: SagaManager = Provide["saga_manager"],
+        **kwargs,
+    ):
+        self.config = config
+        self.saga_manager = saga_manager
+
+    @classmethod
+    def __get_enroute__(cls, config: MinosConfig) -> dict[str, set[EnrouteDecorator]]:
+        result = dict()
+        for name, fn in getmembers(cls, predicate=lambda x: ismethod(x) or isfunction(x)):
+            if not hasattr(fn, "__decorators__"):
+                continue
+            result[name] = fn.__decorators__
+        return result
+
+
+class CommandService(Service, ABC):
+    """Command Service class"""
+
+    @staticmethod
+    def _pre_command_handle(request: Request) -> Request:
+        return request
+
+    @staticmethod
+    def _pre_query_handle(request: Request) -> Request:
+        raise MinosIllegalHandlingException("Queries cannot be handled by `CommandService` inherited classes.")
+
+    def _pre_event_handle(self, request: Request) -> Request:
+        fn = partial(PreEventHandler.handle, saga_manager=self.saga_manager, user=request.user)
+        return WrappedRequest(request, fn)
+
+
+class QueryService(Service, ABC):
+    """Query Service class"""
+
+    @staticmethod
+    def _pre_command_handle(request: Request) -> Request:
+        raise MinosIllegalHandlingException("Commands cannot be handled by `QueryService` inherited classes.")
+
+    @staticmethod
+    def _pre_query_handle(request: Request) -> Request:
+        return request
+
+    def _pre_event_handle(self, request: Request) -> Request:
+        fn = partial(PreEventHandler.handle, saga_manager=self.saga_manager, user=request.user)
+        return WrappedRequest(request, fn)
