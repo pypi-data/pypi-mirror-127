55e39da1bea2ea7258a9e50f0aec4db4f9e9c46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:27 +0000

    Add package levelling/0.1.2/levelling-0.1.2-py3-none-any.whl

---
+++ packages/levelling/levelling-0.1.2-py3-none-any.whl/levelling/caches/memory.py
@@ -0,0 +1,45 @@
+from ..abc import Cache
+from ..dataclass import Guild, Member
+from ..exceptions import GuildNotFound, MemberNotFound
+
+
+class Memory(Cache):
+    """An in memory cache utilising a dictionary internally"""
+
+    def __init__(self):
+        self.cache = {}
+
+    async def get_guild(self, guild_id: int) -> Guild:
+        try:
+            return Guild(id=guild_id, raw_members=self.cache[guild_id])
+        except KeyError:
+            raise GuildNotFound from None
+
+    async def get_member(self, member_id: int, guild_id: int = None) -> Member:
+        if guild_id:
+            try:
+                guild: Guild = await self.get_guild(guild_id=guild_id)
+                member: Member = next(i for i in guild.members if i.id == member_id)
+                return member
+            except GuildNotFound:
+                raise MemberNotFound from None
+            except KeyError:
+                raise MemberNotFound from None
+            except StopIteration:
+                raise MemberNotFound
+
+        try:
+            return self.cache[member_id]
+        except KeyError:
+            raise MemberNotFound from None
+
+    async def set_member(
+        self, member_id: int, data: dict, guild_id: int = None
+    ) -> None:
+        if guild_id:
+            guild = self.cache.get(guild_id)
+            if not guild:
+                self.cache[guild_id] = {}
+            self.cache[guild_id][member_id] = data
+        else:
+            self.cache[member_id] = data
---
+++ packages/levelling/levelling-0.1.2-py3-none-any.whl/levelling/dataclass.py
@@ -0,0 +1,69 @@
+from typing import List, Dict
+
+import attr
+
+
+@attr.s(slots=True)
+class Member(object):
+    """A generic ``attrs`` dataclass representing a ``discord.Member``
+
+    Parameters
+    ----------
+    id : int
+        -> ``discord.Member.id``
+    xp : int
+        The total xp this Member has
+    guild_id : int, optional
+        Which guild this Member is attached to.
+        If this is null it means levels are stored
+        globally rather then per guild
+    """
+
+    id: int = attr.ib(hash=True, eq=True)  # Think member.id
+    xp: int = attr.ib(eq=False, default=0)
+    guild_id: int = attr.ib(
+        default=None,
+        validator=attr.validators.optional(attr.validators.instance_of(int)),
+        kw_only=True,
+        eq=False,
+    )
+
+    @xp.validator
+    def _validate_positive(self, attr, value):
+        if value < 0:
+            raise ValueError("XP must be positive")
+
+
+@attr.s
+class Guild:
+    """A generic ``attrs`` dataclass representing a ``discord.Guild``
+
+    One of ``members`` | ``raw_members`` are **required** to build
+    a Guild, however this is not checked/enforced
+
+    Parameters
+    ----------
+    id : int
+        -> ``discord.Guild.id``
+    members: List[Member], optional
+        The members internally associated with this guild
+
+    Other Parameters
+    ----------------
+    raw_members : dict, optional
+        A Dict of all the members in this guild. This
+        will get lazily built in ``members`` when required
+    """
+
+    id: int = attr.ib(hash=True)  # Think guild.id
+    _members: List[Member] = attr.ib(default=attr.Factory(list))
+    _raw_members: Dict = attr.ib(default=attr.Factory(dict))
+
+    @property
+    def members(self) -> List[Member]:
+        if not bool(self._members):
+            for k, v in self._raw_members.items():
+                # k id, v actual data
+                self._members.append(Member(**v))
+
+        return self._members
