626ff6cc6806161d551702c547ff5bfb3f39ebd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:41 +0000

    Add package purviewcli/0.1.35/purviewcli-0.1.35.tar.gz

---
+++ packages/purviewcli/purviewcli-0.1.35.tar.gz/purviewcli-0.1.35/purviewcli/cli/search.py
@@ -0,0 +1,24 @@
+"""
+usage: 
+    pv search query [--keywords=<val> --limit=<val> --offset=<val> --filter-file=<val> --facets-file=<val>]
+
+options:
+  --purviewName=<val>     [string]  Azure Purview account name.
+  --limit=<val>           [integer] By default there is no paging [default: 25].
+  --offset=<val>          [integer] Offset for pagination purpose [default: 0].
+  --filter-file=<val>     [string]  File path to a filter json file.
+  --facets-file=<val>     [string]  File path to a facets json file.
+
+mapping:
+https://{account_name}.catalog.purview.azure.com
++---------+--------+-------------------+
+| Command | Method | Path              |
++---------+--------+-------------------+
+| query   | POST   | /api/search/query |
++---------+--------+-------------------+
+
+"""
+from docopt import docopt
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
