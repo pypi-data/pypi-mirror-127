f8039d23b490dac5282ff5adf4bf08f490c7f3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package django-gdpr-helpers/0.4.1/django-gdpr-helpers-0.4.1.tar.gz

---
+++ packages/django-gdpr-helpers/django-gdpr-helpers-0.4.1.tar.gz/django-gdpr-helpers-0.4.1/README.rst
@@ -0,0 +1,74 @@
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://github.com/psf/black
+    :alt: Black
+======
+GDPR Helpers
+======
+
+GDPR Helpers is a Django app for easy GDPR compliance.
+
+Quickstart
+-------
+
+Install Django GDPR Helpers:
+   pip install -e git+git@github.com:Arussil/django-gdpr-helpers.git
+
+Add it to your `INSTALLED_APPS`:
+
+.. code-block:: python
+   INSTALLED_APPS = (
+      ...
+      'gdpr_helpers',
+      ...
+   )
+
+Define your reasons for asking personal data and assign them to a group:
+
+.. code-block:: python
+
+   from gdpr_helpers.models import LegalReasonGroup, LegalReason
+
+   legal_group = LegalReasonGroup.objects.create(where="registration")
+
+   LegalReason.objects.create(legal_group=legal_group, flag_text="Required for registration", slug="registration", active=True, required=True)
+   LegalReason.objects.create(legal_group=legal_group, flag_text="Optional for registration", slug="marketing", active=True, required=False)
+
+Or use the django-admin.
+
+Add the Mixin to a form that create an object and need privacy flags, remember to save the PrivacyLog in someplace:
+
+.. code-block:: python
+
+   from django import forms
+   from gdpr_helpers.forms import GDPRFormMixin
+   from .models import User
+
+
+   class RegistrationForm(GDPRFormMixin):
+      class Meta:
+        model = User
+        where = "registration"
+        fields = ("whatever_fields_from_model",)
+
+      def save(self):
+        user = super().save()
+        PrivacyLog.objects.create_log(content_object=user, cleaned_data=self.cleaned_data)
+        return user
+
+Note that the privacy fields are already injected in the form.
+
+Filling the form will now create logs for the object created.
+
+Features
+--------
+
+* Can define Legal reason for which you are collecting personal data
+* Create logs for the data you collected with a timestamp and what the user consented to
+* Logs are anonymous
+
+TODO
+----
+
+* When the user/system/admin removes the object we must keep a reference, maybe an unique ID that binds the deleted record to the logs
+* Adding a serializer to use it on rest_framework project
+* Add the ability to define a timer for data based on Legal reason, so we can destroy it after it expires or ask the user for another consent
---
+++ packages/django-gdpr-helpers/django-gdpr-helpers-0.4.1.tar.gz/django-gdpr-helpers-0.4.1/gdpr_helpers/models.py
@@ -0,0 +1,106 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.utils.translation import gettext
+from django.utils.translation import gettext_lazy as _
+
+from .managers import LegalReasonGroupManager, LegalReasonManager, PrivacyLogManager
+
+
+class PrivacyLog(models.Model):
+    """A new log will be created when a user accept some flags"""
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.PROTECT)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    created = models.DateTimeField(_("Data creazione"), auto_now_add=True)
+
+    objects = PrivacyLogManager()
+
+    def __str__(self):
+        return f"{self.content_type} privacy-log {self.created}"
+
+    class Meta:
+        verbose_name = _("Privacy log")
+        verbose_name_plural = _("Privacy logs")
+
+
+class LegalReasonGroup(models.Model):
+    """
+    Group LegalReason by a common key to use in specific form,
+    es: contact form, registration form, lead form etc.
+    """
+
+    where = models.CharField(_("Posizione del gruppo"), max_length=100, unique=True)
+
+    objects = LegalReasonGroupManager()
+
+    def get_as_form_fields(self):
+        fields = []
+        for reason in self.legal_reasons.get_as_form_fields():
+            fields.append(reason)
+        return fields
+
+    def __str__(self):
+        return gettext(f"For use in {self.where}")
+
+    class Meta:
+        verbose_name = _("Gruppo ragioni legali")
+        verbose_name_plural = _("Gruppi ragioni legali")
+
+
+class LegalReason(models.Model):
+    """Register the legal reason, it will be used for flags and privacy-policy page"""
+    slug = models.SlugField(_("Slug del consenso"), unique=True)
+    flag_text = models.TextField(_("Testo da mostrare nella spunta"))
+    privacy_description = models.TextField(
+        _("Descrizione per pagina privacy"), blank=True, null=True
+    )
+    required = models.BooleanField(_("Obbligatorio"), default=False)
+    active = models.BooleanField(_("Attivo"), default=False)
+    legal_group = models.ForeignKey(
+        LegalReasonGroup,
+        verbose_name=_("Gruppo di ragioni legali"),
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="legal_reasons",
+    )
+
+    objects = LegalReasonManager()
+
+    @property
+    def field_name(self):
+        return f"privacy_{self.slug}"
+
+    def __str__(self):
+        return self.flag_text
+
+    class Meta:
+        verbose_name = _("Ragione legale")
+        verbose_name_plural = _("Ragioni legali")
+
+
+class PrivacyEvent(models.Model):
+    """Register user consent"""
+
+    privacy_log = models.ForeignKey(
+        PrivacyLog,
+        on_delete=models.PROTECT,
+        verbose_name=_("Privacy log"),
+        related_name="event",
+    )
+    legal_reason = models.ForeignKey(
+        LegalReason,
+        on_delete=models.PROTECT,
+        verbose_name=_("Ragione legale"),
+        related_name="event",
+    )
+    accepted = models.BooleanField(_("Accetata"), default=False)
+
+    def __str__(self):
+        return f"{self.legal_reason} {self.accepted}"
+
+    class Meta:
+        verbose_name = _("Evento privacy")
+        verbose_name_plural = _("Eventi privacy")
