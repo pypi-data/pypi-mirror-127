758d88d86fa4163697dae9e4c60229dd6cf479be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:46 +0000

    Add package pybrary/0.21.45.0/pybrary-0.21.45.0.tar.gz

---
+++ packages/pybrary/pybrary-0.21.45.0.tar.gz/pybrary-0.21.45.0/setup.py
@@ -0,0 +1,32 @@
+from os.path import join, dirname, abspath
+
+from setuptools import setup, find_packages
+
+curdir = abspath(dirname(__file__))
+readme = open(join(curdir, 'README.rst')).read()
+
+setup(
+    name             = 'pybrary',
+    version          = '0.21.45.0',
+    description      = 'Python Library',
+    long_description = readme,
+    keywords         = ['library', ],
+    url              = 'https://framagit.org/louis-riviere-xyz/pybrary/tree/stable',
+    author           = 'Louis RIVIERE',
+    author_email     = 'louis@riviere.xyz',
+    license          = 'MIT',
+    classifiers      = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+    ],
+    packages = find_packages(),
+    entry_points = dict(
+        console_scripts = (
+            'get_ip_adr=pybrary.main:get_ip_adr',
+        ),
+    ),
+)
