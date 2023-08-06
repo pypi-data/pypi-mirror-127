08bb91fde3a712e1a3edf2777825da5e9032c9b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:30 +0000

    Add package extra-streamlit-components/0.1.53/extra_streamlit_components-0.1.53-py3-none-any.whl

---
+++ packages/extra-streamlit-components/extra_streamlit_components-0.1.53-py3-none-any.whl/extra_streamlit_components/BouncingImage/frontend/build/precache-manifest.2f3e98811e214315fafcaf5593ee4145.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfaddceec1a7f7d830c6a712d6f692a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "529e4d97dfc35f66e78c",
+    "url": "./static/js/2.c71c863e.chunk.js"
+  },
+  {
+    "revision": "71aa9e5fe8bf6f2bc9396bc9828f6810",
+    "url": "./static/js/2.c71c863e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d038923627416ec1be",
+    "url": "./static/js/main.3674d345.chunk.js"
+  },
+  {
+    "revision": "404fc0ed8c4f96c880d5",
+    "url": "./static/js/runtime-main.fb6bf013.js"
+  }
+]);
---
+++ packages/extra-streamlit-components/extra_streamlit_components-0.1.53-py3-none-any.whl/extra_streamlit_components/StepperBar/frontend/build/precache-manifest.0d58ef9a89e36cc10c002bc0db738e58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde7f14c869dabb3882a8d6e643c2d75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6737cc3b8c35e1e1c085",
+    "url": "./static/js/2.d226e74a.chunk.js"
+  },
+  {
+    "revision": "00a2e6ba9cedd36a525c3b328259ff9c",
+    "url": "./static/js/2.d226e74a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e23ed232a4e04384cf",
+    "url": "./static/js/main.f92817e7.chunk.js"
+  },
+  {
+    "revision": "dfe8cb0fade3b7cb2552",
+    "url": "./static/js/runtime-main.14f98f4c.js"
+  }
+]);
