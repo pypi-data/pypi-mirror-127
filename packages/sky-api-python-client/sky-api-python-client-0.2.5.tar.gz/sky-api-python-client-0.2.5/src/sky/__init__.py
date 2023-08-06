a772d234c7a356e2849af62878625cb05dda53c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:04 +0000

    Add package sky-api-python-client/0.2.5/sky-api-python-client-0.2.5.tar.gz

---
+++ packages/sky-api-python-client/sky-api-python-client-0.2.5.tar.gz/sky-api-python-client-0.2.5/src/sky/__init__.py
@@ -0,0 +1,9 @@
+"""
+"""
+from .sky import Sky
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
