308bac9638487bdd774f8af6f1aecfc2956646e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:11 +0000

    Add package fluentcms-jumbotron/3.0/fluentcms_jumbotron-3.0-py3-none-any.whl

---
+++ packages/fluentcms-jumbotron/fluentcms_jumbotron-3.0-py3-none-any.whl/fluentcms_jumbotron/tests/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from fluent_contents.models import PlaceholderField
+
+
+class ExamplePage(models.Model):
+    slug = models.SlugField()
+    content = PlaceholderField(slot="content")
+
+    def __str__(self):
+        return self.slug
