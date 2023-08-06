27d708b6808423bc5133d394d6e9848fca085d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:55 +0000

    Add package glglue/1.4.1/glglue-1.4.1.tar.gz

---
+++ packages/glglue/glglue-1.4.1.tar.gz/glglue-1.4.1/examples/pyside6_sample.py
@@ -0,0 +1,28 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(levelname)s:%(name)s:%(message)s',
+                    level=logging.DEBUG)
+
+# pip install pyside6
+from PySide6.QtWidgets import QMainWindow, QApplication
+from OpenGL.GL import *
+
+
+class Window(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        # setup opengl widget
+        import glglue.gl3
+        self.controller = glglue.gl3.SampleController()
+        import glglue.pyside6
+        self.glwidget = glglue.pyside6.Widget(self, self.controller)
+        self.setCentralWidget(self.glwidget)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
