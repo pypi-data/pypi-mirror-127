1ebb1b2f2a3efcf968beea6df283c738d70943dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package fakedata/0.1/FakeData-0.1-py3-none-any.whl

---
+++ packages/fakedata/FakeData-0.1-py3-none-any.whl/fakedata/__init__.py
@@ -0,0 +1,11 @@
+from .fakedata import *
+
+RandomOneName = FakeData.RandomOneName
+RandomTwoName = FakeData.RandomTwoName
+RandomUSAName = FakeData.RandomUSAName
+RandomName = FakeData.RandomName
+CheckSex = FakeData.CheckSex
+RandomSex = FakeData.RandomSex
+RandomEmail = FakeData.RandomEmail
+PersonalizedEmail = FakeData.PersonalizedEmail
+
