1e0314ff2b82c9dc2ba899859b416636e9c3674c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:00 +0000

    Add package luga/0.1.1/luga-0.1.1.tar.gz

---
+++ packages/luga/luga-0.1.1.tar.gz/luga-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['luga']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fasttext>=0.9.2,<0.10.0', 'httpx>=0.20.0,<0.21.0']
+
+setup_kwargs = {
+    'name': 'luga',
+    'version': '0.1.1',
+    'description': 'Sensing the language of the text using Machine Learning',
+    'long_description': None,
+    'author': 'Prayson W. Daniel',
+    'author_email': 'praysonwilfred@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
