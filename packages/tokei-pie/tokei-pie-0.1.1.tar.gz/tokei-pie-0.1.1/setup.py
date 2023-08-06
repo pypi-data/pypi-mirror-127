76277bd83f593518a350d1b8ba61151d9701fa0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:58 +0000

    Add package tokei-pie/0.1.1/tokei-pie-0.1.1.tar.gz

---
+++ packages/tokei-pie/tokei-pie-0.1.1.tar.gz/tokei-pie-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tokei_pie']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['plotly>=5.4.0,<6.0.0']
+
+entry_points = \
+{'console_scripts': ['tokei-pie = tokei_pie.main:main']}
+
+setup_kwargs = {
+    'name': 'tokei-pie',
+    'version': '0.1.1',
+    'description': 'Draw a pie chart for tokei output.',
+    'long_description': None,
+    'author': 'laixintao',
+    'author_email': 'laixintaoo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
