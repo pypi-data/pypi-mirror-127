78fec4aade3afe41e5aa9cc208b77c702c0dbb73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:17 +0000

    Add package fb-tools/1.8.2/fb_tools-1.8.2.tar.gz

---
+++ packages/fb-tools/fb_tools-1.8.2.tar.gz/fb_tools-1.8.2/test/zones.js
@@ -0,0 +1,26 @@
+[
+  {
+    "account": "local",
+    "dnssec": false,
+    "id": "222.40.10.in-addr.arpa.",
+    "kind": "Master",
+    "last_check": 0,
+    "masters": [],
+    "name": "222.40.10.in-addr.arpa.",
+    "notified_serial": 2018020802,
+    "serial": 2018020802,
+    "url": "/api/v1/servers/localhost/zones/222.40.10.in-addr.arpa."
+  },
+  {
+    "account": "Verisign",
+    "dnssec": false,
+    "id": "testing.com.",
+    "kind": "Master",
+    "last_check": 0,
+    "masters": [],
+    "name": "testing.com.",
+    "notified_serial": 2018061201,
+    "serial": 2018061201,
+    "url": "/api/v1/servers/localhost/zones/testing.com."
+  }
+]
