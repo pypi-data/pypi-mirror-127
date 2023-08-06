c26fde97098fcb1c1f6dd7af929799dc5611b979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:38 +0000

    Add package SimpleRL/0.0.5/SimpleRL-0.0.5-py3-none-any.whl

---
+++ packages/SimpleRL/SimpleRL-0.0.5-py3-none-any.whl/SimpleRL/envs/__init__.py
@@ -0,0 +1,7 @@
+
+# BASE ENVIRONMENT
+from .base import base
+
+# LASER TAG ENVIRONMENT
+from .laser_tag import laser_tag
+
---
+++ packages/SimpleRL/SimpleRL-0.0.5-py3-none-any.whl/SimpleRL/utils/__init__.py
@@ -0,0 +1,3 @@
+
+# LASER TAG ENVIRONMENT
+from .laser_tag_utils import laser_tag_utils
