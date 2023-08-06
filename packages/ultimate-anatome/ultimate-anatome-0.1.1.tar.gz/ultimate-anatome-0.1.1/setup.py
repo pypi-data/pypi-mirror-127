4a9d73ba428c431d56c4fa96105c8323a8654e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:04 +0000

    Add package ultimate-anatome/0.1.1/ultimate-anatome-0.1.1.tar.gz

---
+++ packages/ultimate-anatome/ultimate-anatome-0.1.1.tar.gz/ultimate-anatome-0.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+import pathlib
+
+# The directory containing this file
+# HERE = pathlib.Path(__file__).parent
+HERE = pathlib.Path('~/ultimate-anatome/').expanduser()
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# install_requires = [
+#     'torch>=1.9.0',
+#     'torchvision>=0.10.0',
+#     'tqdm'
+# ]
+
+setup(
+    name='ultimate-anatome',
+    version='0.1.1',
+    description='Ἀνατομή (Anatome) is a PyTorch library to analyze representation of neural networks',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/brando90/ultimate-anatome',
+    author='Brando Miranda',
+    author_email='brandojazz@gmail.com',
+    python_requires='>=3.9.0',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['torch>=1.9.0',
+                      'torchvision>=0.10.0',
+                      'tqdm'
+                      ]
+)
