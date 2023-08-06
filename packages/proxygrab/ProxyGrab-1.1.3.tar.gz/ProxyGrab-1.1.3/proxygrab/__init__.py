23254f59ad8d526342bcc2bb1c8672b13c25f4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:10 +0000

    Add package proxygrab/1.1.3/ProxyGrab-1.1.3.tar.gz

---
+++ packages/proxygrab/ProxyGrab-1.1.3.tar.gz/ProxyGrab-1.1.3/proxygrab/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "1.1.3"
+__copyright__ = "Divkix <https://github.com/Divkix>"
+
+# Methods to get proxies!
+from .package.main import (
+    get_all_proxies,
+    get_http,
+    get_https,
+    get_proxy,
+    get_socks4,
+    get_socks5,
+    save_all_proxies,
+    save_http,
+    save_https,
+    save_proxy,
+    save_socks4,
+    save_socks5,
+)
---
+++ packages/proxygrab/ProxyGrab-1.1.3.tar.gz/ProxyGrab-1.1.3/proxygrab/package/_utils.py
@@ -0,0 +1,23 @@
+from aiohttp import ClientSession
+
+
+class AioHttp:
+    """class for helping get the data from url using aiohttp."""
+
+    @staticmethod
+    async def get_json(link: str, **kwargs):
+        """Get JSON data from the provided link."""
+        async with ClientSession() as sess, sess.get(link, **kwargs) as resp:
+            return await resp.json(), resp
+
+    @staticmethod
+    async def get_text(link: str, **kwargs):
+        """Get Text data from the provided link."""
+        async with ClientSession() as sess, sess.get(link, **kwargs) as resp:
+            return await resp.text(), resp
+
+    @staticmethod
+    async def get_raw(link: str, **kwargs):
+        """Get RAW data from the provided link."""
+        async with ClientSession() as sess, sess.get(link, **kwargs) as resp:
+            return await resp.read(), resp
