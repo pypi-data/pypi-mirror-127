2e7d5b7fbb06c89832579c63623df04127cf85ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:15 +0000

    Add package pantsbuild.pants/2.9.0.dev1/pantsbuild.pants-2.9.0.dev1-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.9.0.dev1-cp39-cp39-macosx_10_15_x86_64.whl/pants/backend/python/lint/autoflake/skip_field.py
@@ -0,0 +1,25 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.backend.python.target_types import (
+    PythonSourcesGeneratorTarget,
+    PythonSourceTarget,
+    PythonTestsGeneratorTarget,
+    PythonTestTarget,
+)
+from pants.engine.target import BoolField
+
+
+class SkipAutoflakeField(BoolField):
+    alias = "skip_autoflake"
+    default = False
+    help = "If true, don't run Autoflake on this target's code."
+
+
+def rules():
+    return [
+        PythonSourceTarget.register_plugin_field(SkipAutoflakeField),
+        PythonSourcesGeneratorTarget.register_plugin_field(SkipAutoflakeField),
+        PythonTestTarget.register_plugin_field(SkipAutoflakeField),
+        PythonTestsGeneratorTarget.register_plugin_field(SkipAutoflakeField),
+    ]
