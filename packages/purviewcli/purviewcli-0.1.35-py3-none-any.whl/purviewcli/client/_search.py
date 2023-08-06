5329a7830d3af81ff286bfaed83d7211cc64e979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:41 +0000

    Add package purviewcli/0.1.35/purviewcli-0.1.35-py3-none-any.whl

---
+++ packages/purviewcli/purviewcli-0.1.35-py3-none-any.whl/purviewcli/client/_search.py
@@ -0,0 +1,19 @@
+from .endpoint import Endpoint, decorator, get_json
+
+class Search(Endpoint):
+    def __init__(self):
+        Endpoint.__init__(self)
+        self.app = 'catalog'
+
+    @decorator
+    def searchQuery(self, args):
+        self.method = 'POST'
+        self.endpoint = '/api/search/query'
+        self.params = {"api-version": "2021-05-01-preview"}
+        self.payload = {
+            'keywords': args['--keywords'],
+            'limit': args['--limit'],
+            'offset': args['--offset'],
+            'filter': get_json(args,'--filter-file'),
+            'facets': get_json(args,'--facets-file')
+        }
