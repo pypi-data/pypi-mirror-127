7aa4ffca2831f3d5b85f30002c144c917a5191fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:01 +0000

    Add package outflow/0.6.1/outflow-0.6.1.tar.gz

---
+++ packages/outflow/outflow-0.6.1.tar.gz/outflow-0.6.1/outflow/core/pipeline/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from ..generic.serializable_proxy import SerializableProxy
+
+config = SerializableProxy()
+context = SerializableProxy()
+settings = SerializableProxy()
+
+from .lazy_config import Config  # noqa: E402
+from .lazy_context import PipelineContext  # noqa: E402
+from .lazy_settings import Settings  # noqa: E402
+
+config.set_factory(Config.resolve_lazy_object)
+context.set_factory(PipelineContext)
+settings.set_factory(Settings.resolve_lazy_object)
+
+from .pipeline import Pipeline, get_pipeline_states  # noqa: F401, E402
