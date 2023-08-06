d0177cfba762978c067a1b7d5c5dbf6f4ceb7560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:37 +0000

    Add package pollination-honeybee-radiance/0.14.8/pollination-honeybee-radiance-0.14.8.tar.gz

---
+++ packages/pollination-honeybee-radiance/pollination-honeybee-radiance-0.14.8.tar.gz/pollination-honeybee-radiance-0.14.8/tests/view_test.py
@@ -0,0 +1,14 @@
+from pollination.honeybee_radiance.view import SplitView, MergeImages
+from queenbee.plugin.function import Function
+
+
+def test_split_view():
+    function = SplitView().queenbee
+    assert function.name == 'split-view'
+    assert isinstance(function, Function)
+
+
+def test_merge_images():
+    function = MergeImages().queenbee
+    assert function.name == 'merge-images'
+    assert isinstance(function, Function)
