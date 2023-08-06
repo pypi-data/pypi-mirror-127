4c6ae8f4b7372280a289218e480539678004062a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:02 +0000

    Add package ideastaticapy/0.1.8/ideastaticapy-0.1.8.tar.gz

---
+++ packages/ideastaticapy/ideastaticapy-0.1.8.tar.gz/ideastaticapy-0.1.8/ideastaticapy/__init__.py
@@ -0,0 +1,8 @@
+"""
+@author: Steven Verwer
+@git: https://github.com/stevenverwer
+"""
+from . import datatype
+from . import connection
+from . import ideaConnection
+from . import parameterOptimization
