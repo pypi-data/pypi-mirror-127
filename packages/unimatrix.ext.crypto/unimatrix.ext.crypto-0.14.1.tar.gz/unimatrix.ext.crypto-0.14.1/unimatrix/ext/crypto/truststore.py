5802548156becea89e6f1dab6831b2a705aa4a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:40 +0000

    Add package unimatrix.ext.crypto/0.14.1/unimatrix.ext.crypto-0.14.1.tar.gz

---
+++ packages/unimatrix.ext.crypto/unimatrix.ext.crypto-0.14.1.tar.gz/unimatrix.ext.crypto-0.14.1/unimatrix/ext/crypto/truststore.py
@@ -0,0 +1,41 @@
+"""Declares :class:`TrustStore`."""
+import asyncio
+
+import ioc.loader
+
+from .abstractkeystore import AbstractKeystore
+
+
+class TrustStore(AbstractKeystore):
+    """Like :class:`~unimatrix.ext.crypto.AbstractKeystore`, but only
+    works with public keys.
+    """
+    __module__ = 'unimatrix.ext.crypto'
+
+    async def jwks(self, url: str, tags: list) -> None:
+        """Import keys from the JWKS served from `url`."""
+        await self.load([{
+            'loader': "unimatrix.ext.crypto.loaders.WellKnownOAuthLoader",
+            'tags': tags,
+            'opts': {
+                'server': url
+            }
+        }])
+
+    async def load(self, loaders):
+        """Loads public keys using the given `loaders`."""
+        loaders = [
+            ioc.loader.import_symbol(x.pop('loader'))(**x)
+            for x in loaders
+        ]
+        await asyncio.gather(*map(self.run_loader, loaders))
+
+    async def run_loader(self, loader):
+        """Runs given `loader` and imports its keys into the
+        trust store.
+        """
+        async for key in loader.import_keys():
+            self.register(key)
+
+
+trust = TrustStore()
