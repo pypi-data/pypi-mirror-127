1270819a558d192d9a1927939164a861bac55d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:31 +0000

    Add package hdx-data-freshness/1.7.1/hdx_data_freshness-1.7.1-py2.py3-none-any.whl

---
+++ packages/hdx-data-freshness/hdx_data_freshness-1.7.1-py2.py3-none-any.whl/hdx/freshness/testdata/dbtestresult.py
@@ -0,0 +1,19 @@
+from hdx.database import Base
+from sqlalchemy import Boolean, Column, DateTime, String
+
+
+class DBTestResult(Base):
+    id = Column(String, primary_key=True)
+    url = Column(String, nullable=False)
+    format = Column(String, nullable=False)
+    err = Column(String)
+    http_last_modified = Column(DateTime)
+    hash = Column(String)
+    force_hash = Column(Boolean, nullable=False)
+
+    def __repr__(self):
+        output = f"<TestResult(id={self.id}, url={self.url}, "
+        output += f"format={self.format}, err={self.err}\n"
+        output += f"http_last_modified={str(self.http_last_modified)}, hash={self.hash},"
+        output += f"force_hash={str(self.force_hash)})>"
+        return output
