d6e62065a941b3aeabd6248fbef6e932dad7a0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:21 +0000

    Add package geography/0.0.37/geography-0.0.37.tar.gz

---
+++ packages/geography/geography-0.0.37.tar.gz/geography-0.0.37/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Education",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+]
+
+setup(
+    name="geography",
+    version="0.0.37",
+    description="Get info about countries and states",
+    long_description="Get info about countries and states",
+    url="",
+    author="FrenchFries8854",
+    author_email="frenchfries8854@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="geography",
+    packages=find_packages(),
+    install_requires=[""]
+)
