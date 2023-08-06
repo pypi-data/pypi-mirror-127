006523366704bd010b1db43037f5c06992a0d76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:12 +0000

    Add package PoliticsAndData/1.0.0/PoliticsAndData-1.0.0-py3-none-any.whl

---
+++ packages/PoliticsAndData/PoliticsAndData-1.0.0-py3-none-any.whl/pnd/apathy.py
@@ -0,0 +1,48 @@
+from .specific import Alliances, Cities, Nations, Trades, Wars
+import threading
+import json
+
+
+class Apathy:
+    def __init__(self, types=(Alliances, Cities, Nations, Trades, Wars), fs="filespace"):
+        self.threads = []
+        self.things = []
+        self.filespace = fs
+
+        for T in types:
+            thing = T(fs=fs)
+            self.things.append(thing)
+            self.threads.append(threading.Thread(target=thing.run, daemon=True, name=T.__name__))
+
+
+    def run(self):
+        self.collect()
+        self.combine()
+    
+
+    def collect(self):
+        data = []
+
+        for thread in self.threads:
+            thread.start()
+        for thread in self.threads:
+            thread.join()
+
+
+    def combine(self):
+        for thing in self.things:
+            with open(thing.file) as f:
+                data.append(json.load(f)["timeline"])
+
+        days = set(data[0].keys())
+        for dat in data[1:]:
+            days = days and set(dat.keys())
+
+        final = {}
+        for day in days:
+            val = iter()
+            for dat in data:
+                val += data.items()
+            final[day] = dict(val)
+
+        with open(f"{self.filespace}/final.json")
