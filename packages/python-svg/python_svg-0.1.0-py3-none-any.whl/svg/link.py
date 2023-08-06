fb52b1e286672f2b475e4b2975bc4563d7b65a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:11 +0000

    Add package python-svg/0.1.0/python_svg-0.1.0-py3-none-any.whl

---
+++ packages/python-svg/python_svg-0.1.0-py3-none-any.whl/svg/link.py
@@ -0,0 +1,28 @@
+from typing import Optional, List
+from svg.canvas import Canvas
+
+
+class Link:
+    def __init__(self, text_arg: str, href: Optional[str], canvas: Canvas):
+        """
+        Link class for HTML <a> tags
+
+        :param text_arg:
+        :param href:
+        """
+
+        canvas.add_shape(self)
+        self.text_arg = text_arg
+        self.href = href
+        self.shapes = []
+
+    def svg_content(self) -> str:
+        content = f'<a href="{self.href}">'
+        for shape in self.shapes:
+            content += shape.svg_content()
+        content += '</a>'
+        return content
+
+    def add_shape(self, shape) -> None:
+        self.shapes.append(shape)
+
