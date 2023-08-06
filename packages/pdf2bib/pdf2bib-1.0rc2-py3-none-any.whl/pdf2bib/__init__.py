738e200e656b9fc26ed211d3d3b94d353d1c62cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:28 +0000

    Add package pdf2bib/1.0rc2/pdf2bib-1.0rc2-py3-none-any.whl

---
+++ packages/pdf2bib/pdf2bib-1.0rc2-py3-none-any.whl/pdf2bib/__init__.py
@@ -0,0 +1,23 @@
+import logging
+
+# Setup logging
+logger = logging.getLogger("pdf2bib")
+logger.setLevel(level=logging.INFO)
+if not logger.handlers:
+    formatter = logging.Formatter("[pdf2bib]: %(message)s")
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+logger.propagate = False
+
+from .config import config
+config.ReadParamsINIfile()
+
+config.set('verbose',config.get('verbose')) #This is a quick and dirty way (to improve in the future) to make sure that the verbosity of the pdf2doi logger is properly set according
+                                            #to the current value of config.get('verbose') (see config.py file for details)
+
+from .main import pdf2bib, pdf2bib_singlefile
+from .bibtex_makers import *
+#from .utils_registry import install_right_click, uninstall_right_click
+
+
