699b049b950f128f9316258b9c0754256029be23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:42 +0000

    Add package pyramid-session-redis/1.6.3/pyramid_session_redis-1.6.3.tar.gz

---
+++ packages/pyramid-session-redis/pyramid_session_redis-1.6.3.tar.gz/pyramid_session_redis-1.6.3/setup.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# manage package version
+# store version in the init.py
+with open(os.path.join(HERE, "src", "pyramid_session_redis", "__init__.py")) as v_file:
+    package_version = (
+        re.compile(r'.*__VERSION__ = "(.*?)"', re.S).match(v_file.read()).group(1)
+    )
+
+long_description = (
+    description
+) = "High performance and actively maintained server-side session framework for Pyramid and Redis."
+with open(os.path.join(HERE, "README.md")) as f:
+    long_description = f.read()
+
+
+# set up requires
+install_requires = [
+    "redis>=2.4.11, != 2.9.1",
+    "pyramid>=1.3",
+    "six>=1.12.0",
+    "zope.interface",  # in Pyramid
+]
+testing_requires = [
+    "nose",
+    "pytest",
+    "webob",  # in Pyramid
+]
+testing_extras = install_requires + testing_requires + ["coverage"]
+docs_extras = [
+    "sphinx",
+]
+
+setup(
+    name="pyramid_session_redis",
+    version=package_version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Framework :: Pyramid",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="pyramid session redis",
+    author="Jonathan Vanasco",
+    author_email="jonathan@findmeon.com",
+    url="https://github.com/jvanasco/pyramid_session_redis",
+    license="BSD",
+    test_suite="nose.collector",
+    packages=find_packages(
+        where="src",
+    ),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    entry_points="",
+    install_requires=install_requires,
+    tests_require=testing_requires,
+    extras_require={
+        "testing": testing_extras,
+        "docs": docs_extras,
+    },
+)
---
+++ packages/pyramid-session-redis/pyramid_session_redis-1.6.3.tar.gz/pyramid_session_redis-1.6.3/src/pyramid_session_redis/compat.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+"""
+Compatability module for various pythons and environments.
+"""
+from six.moves import cPickle as pickle
+from six import ensure_binary
+from six import ensure_str
+from six import PY2
+from six import PY3
+
+# !!!: MIGRATION. these move in webob 2.0
+try:
+    # webon 1.x
+    from webob.compat import bytes_ as webob_bytes_
+    from webob.compat import text_ as webob_text_
+except:
+    # webon 2.x
+    from webob.util import bytes_ as webob_bytes_
+    from webob.util import text_ as webob_text_
+
+
+# ==============================================================================
+
+
+try:
+    # python3.6 secrets module
+    from secrets import token_urlsafe, token_hex
+except ImportError:  # pragma: no cover
+    import os
+    import base64
+    import binascii
+
+    def token_bytes(nbytes=32):
+        """
+        :param nbytes: default 32
+        """
+        return os.urandom(nbytes)
+
+    def token_urlsafe(nbytes=32):
+        """
+        :param nbytes: default 32
+        """
+        token = base64.urlsafe_b64encode(token_bytes(nbytes)).rstrip(b"=")
+        return token.decode("ascii") if PY3 else token
+
+    def token_hex(nbytes=32):
+        """
+        :param nbytes: default 32
+        """
+        token = binascii.hexlify(token_bytes(nbytes))
+        return token.decode("ascii") if PY3 else token
+
+
+def bytes_(s, encoding="latin-1", errors="strict"):
+    return ensure_binary(s, encoding, errors)
+
+
+def native_(s, encoding="latin-1", errors="strict"):
+    return ensure_str(s, encoding, errors)
+
+
+def to_unicode(value):  # pragma: no cover
+    if PY2:
+        value = unicode(value)
+    return value
