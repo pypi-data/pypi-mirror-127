a9d23effa29e995f9c955b17639e481de5f6fa37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:07 +0000

    Add package hassan-fivehead-hahaha/1.3/hassan_fivehead_hahaha-1.3.tar.gz

---
+++ packages/hassan-fivehead-hahaha/hassan_fivehead_hahaha-1.3.tar.gz/hassan_fivehead_hahaha-1.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hassan_fivehead_hahaha',
+      version='1.3',
+      description='Gaussian and something distributions',
+      packages=['hassan_fivehead_hahaha'],
+      zip_safe=False)
