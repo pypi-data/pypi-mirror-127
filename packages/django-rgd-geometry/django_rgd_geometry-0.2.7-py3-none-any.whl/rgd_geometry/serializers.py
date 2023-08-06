0e70289743482e177e8016a62da9f397acc392e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:20 +0000

    Add package django-rgd-geometry/0.2.7/django_rgd_geometry-0.2.7-py3-none-any.whl

---
+++ packages/django-rgd-geometry/django_rgd_geometry-0.2.7-py3-none-any.whl/rgd_geometry/serializers.py
@@ -0,0 +1,22 @@
+import json
+
+from rgd.serializers import SpatialEntrySerializer
+
+from . import models
+
+
+class GeometrySerializer(SpatialEntrySerializer):
+    class Meta:
+        model = models.Geometry
+        exclude = ['data', 'footprint', 'outline']
+
+
+class GeometryDataSerializer(GeometrySerializer):
+    def to_representation(self, value):
+        ret = super().to_representation(value)
+        ret['data'] = json.loads(value.data.geojson)
+        return ret
+
+    class Meta:
+        model = models.Geometry
+        fields = '__all__'
