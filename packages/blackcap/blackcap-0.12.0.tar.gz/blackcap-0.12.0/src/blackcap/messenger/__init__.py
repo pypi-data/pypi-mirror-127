0a7c7e54a75192bd73a09b64d89cfe3bf3263581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:08 +0000

    Add package blackcap/0.12.0/blackcap-0.12.0.tar.gz

---
+++ packages/blackcap/blackcap-0.12.0.tar.gz/blackcap-0.12.0/src/blackcap/messenger/__init__.py
@@ -0,0 +1,10 @@
+"""Messenger that delivers and reads msgs from a pub/sub queue."""
+
+from blackcap.configs import config_registry
+from blackcap.messenger.gcp_messenger import GCPMessenger
+from blackcap.messenger.registry import MessengerRegistry
+
+config = config_registry.get_config()
+
+messenger_registry = MessengerRegistry()
+messenger_registry.add_messenger(GCPMessenger(config))
