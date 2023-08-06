5a1646aea709c2e40772bbd4d6581677833af39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:14 +0000

    Add package uptrace/1.7.1/uptrace-1.7.1.tar.gz

---
+++ packages/uptrace/uptrace-1.7.1.tar.gz/uptrace-1.7.1/test/test_uptrace.py
@@ -0,0 +1,34 @@
+from unittest.mock import patch
+
+import pytest
+from opentelemetry import trace as trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.trace.status import StatusCode
+
+import uptrace
+
+
+def setup_function():
+    trace._TRACER_PROVIDER = TracerProvider()
+
+
+def teardown_function():
+    trace._TRACER_PROVIDER = None
+
+
+def test_span_processor_no_dsn(caplog):
+    uptrace.configure_opentelemetry()
+    assert "either dsn option or UPTRACE_DSN is required" in caplog.text
+
+
+def test_span_processor_invalid_dsn(caplog):
+    uptrace.configure_opentelemetry(dsn="invalid")
+    assert "can't parse DSN=invalid" in caplog.text
+
+
+def test_trace_url():
+    tracer = trace.get_tracer("tracer_name")
+    span = tracer.start_span("main span")
+
+    url = uptrace.trace_url(span)
+    assert url.startswith("https://app.uptrace.dev/traces/")
