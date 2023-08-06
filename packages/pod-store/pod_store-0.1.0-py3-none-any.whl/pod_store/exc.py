e4367caddfa606fc211923ce481f8140a2fabf5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:03 +0000

    Add package pod-store/0.1.0/pod_store-0.1.0-py3-none-any.whl

---
+++ packages/pod-store/pod_store-0.1.0-py3-none-any.whl/pod_store/exc.py
@@ -0,0 +1,48 @@
+"""Custom `pod-store` exceptions.
+
+Custom exceptions are used here for easily catching known errors and (hopefully)
+gracefully displaying error messages to the CLI user.
+
+This behavior happens in the
+`pod_store.commands.decorators.catch_pod_store_errors` decorator.
+"""
+
+
+class EpisodeDoesNotExistError(Exception):
+    pass
+
+
+class GPGCommandError(Exception):
+    pass
+
+
+class NoEpisodesFoundError(Exception):
+    pass
+
+
+class NoPodcastsFoundError(Exception):
+    pass
+
+
+class PodcastDoesNotExistError(Exception):
+    pass
+
+
+class PodcastExistsError(Exception):
+    pass
+
+
+class ShellCommandError(Exception):
+    pass
+
+
+class StoreDoesNotExistError(Exception):
+    pass
+
+
+class StoreExistsError(Exception):
+    pass
+
+
+class StoreIsNotEncrypted(Exception):
+    pass
