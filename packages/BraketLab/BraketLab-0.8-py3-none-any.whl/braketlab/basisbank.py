902cc8af9f7067c551eea3d2ce038d9014053aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:35 +0000

    Add package BraketLab/0.8/BraketLab-0.8-py3-none-any.whl

---
+++ packages/BraketLab/BraketLab-0.8-py3-none-any.whl/braketlab/basisbank.py
@@ -0,0 +1,20 @@
+import sympy as sp
+import numpy as np
+
+import braketlab.solid_harmonics as sh
+import braketlab as bk
+import braketlab.hydrogen as hy
+import braketlab.harmonic_oscillator as ho
+
+def get_hydrogen_function(n,l,m, position = np.array([0,0,0])):
+    return bk.ket(hy.hydrogen_function(n,l,m), name = "%i,%i,%i" % (n,l,m), position = position)
+
+def get_harmonic_oscillator_function(n, omega = 1):
+    return bk.ket(ho.psi_ho(n), name = "%i" % n, energy = [omega*(.5+n)])
+
+def get_gto(a,l,m, position = np.array([0,0,0])):
+    return bk.ket(sh.get_Nao(a,l,m), name = "\chi_{%i,%i}^{%.2f}" % (l,m,a), position = position)
+
+def get_sto(a,w,l,m):
+    return bk.ket(sh.get_sto(a,w,l,m), name  = "\chi_{%i,%i}^{%.2f}" % (l,m,a))
+
