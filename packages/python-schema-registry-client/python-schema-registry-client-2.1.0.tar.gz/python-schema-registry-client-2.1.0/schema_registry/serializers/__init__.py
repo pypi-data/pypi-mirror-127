9970ddf39a89d5176fc8550a145ef4c9ea806203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:28 +0000

    Add package python-schema-registry-client/2.1.0/python-schema-registry-client-2.1.0.tar.gz

---
+++ packages/python-schema-registry-client/python-schema-registry-client-2.1.0.tar.gz/python-schema-registry-client-2.1.0/schema_registry/serializers/__init__.py
@@ -0,0 +1,3 @@
+from schema_registry.serializers.message_serializer import AvroMessageSerializer  # noqa
+from schema_registry.serializers.message_serializer import JsonMessageSerializer  # noqa
+from schema_registry.serializers.message_serializer import MessageSerializer  # noqa
