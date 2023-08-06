0150a107a0a60e23869518dff0507064d9d4e6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:59 +0000

    Add package python-framework/0.2.207.011/python_framework-0.2.207.011.tar.gz

---
+++ packages/python-framework/python_framework-0.2.207.011.tar.gz/python_framework-0.2.207.011/python_framework/api/src/annotation/ClientAnnotation.py
@@ -0,0 +1,96 @@
+import json
+from python_helper import Constant as c
+from python_helper import ReflectionHelper, ObjectHelper, log, Function
+from python_framework.api.src.service.flask import FlaskManager
+from python_framework.api.src.service.openapi import OpenApiManager
+from python_framework.api.src.util import Serializer
+
+@Function
+def Client(url=c.SLASH) :
+    clientUrl = url
+    def Wrapper(OuterClass, *args, **kwargs):
+        log.wrapper(Client,f'''wrapping {OuterClass.__name__}''')
+        class InnerClass(OuterClass):
+            url = clientUrl
+            def __init__(self,*args,**kwargs):
+                log.wrapper(OuterClass,f'in {InnerClass.__name__}.__init__(*{args},**{kwargs})')
+                apiInstance = FlaskManager.getApi()
+                OuterClass.__init__(self,*args,**kwargs)
+                self.globals = apiInstance.globals
+        ReflectionHelper.overrideSignatures(InnerClass, OuterClass)
+        return InnerClass
+    return Wrapper
+
+@Function
+def ClientMethod(
+    url = c.SLASH,
+    requestHeaderClass = None,
+    requestParamClass = None,
+    requestClass = None,
+    responseClass = None,
+    roleRequired = None,
+    consumes = OpenApiManager.DEFAULT_CONTENT_TYPE,
+    produces = OpenApiManager.DEFAULT_CONTENT_TYPE,
+    logRequest = False,
+    logResponse = False
+):
+    clientMethodUrl = url
+    clientMethodRequestHeaderClass = requestHeaderClass
+    clientMethodRequestParamClass = requestParamClass
+    clientMethodRequestClass = requestClass
+    clientMethodResponseClass = responseClass
+    clientMethodRoleRequired = roleRequired
+    clientMethodProduces = produces
+    clientMethodConsumes = consumes
+    clientMethodLogRequest = logRequest
+    clientMethodLogResponse = logResponse
+    def innerMethodWrapper(resourceInstanceMethod,*args,**kwargs) :
+        log.wrapper(ClientMethod,f'''wrapping {resourceInstanceMethod.__name__}''')
+        def innerResourceInstanceMethod(*args,**kwargs) :
+            resourceInstance = args[0]
+            completeResponse = None
+            if logRequest :
+                log.prettyJson(
+                    resourceInstanceMethod,
+                    'bodyRequest',
+                    json.loads(Serializer.jsonifyIt(args[1:])),
+                    condition = logRequest,
+                    logLevel = log.INFO
+                )
+            try :
+                FlaskManager.validateKwargs(
+                    kwargs,
+                    resourceInstance,
+                    innerResourceInstanceMethod,
+                    requestHeaderClass = requestHeaderClass,
+                    requestParamClass = requestParamClass
+                )
+                FlaskManager.validateArgs(args, requestClass, innerResourceInstanceMethod)
+                completeResponse = resourceInstanceMethod(*args,**kwargs)
+                FlaskManager.validateResponseClass(responseClass, completeResponse)
+            except Exception as exception :
+                log.debug(innerResourceInstanceMethod, 'Not posssible to complete request', exception=exception)
+                raise exception
+            clientResponse = completeResponse[0] if ObjectHelper.isNotNone(completeResponse[0]) else {'message' : completeResponse[1].enumName}
+            if logResponse :
+                log.prettyJson(
+                    resourceInstanceMethod,
+                    'bodyResponse',
+                    json.loads(Serializer.jsonifyIt(clientResponse)),
+                    condition = logResponse,
+                    logLevel = log.INFO
+                )
+            return completeResponse[0]
+        ReflectionHelper.overrideSignatures(innerResourceInstanceMethod, resourceInstanceMethod)
+        innerResourceInstanceMethod.url = clientMethodUrl
+        innerResourceInstanceMethod.requestHeaderClass = clientMethodRequestHeaderClass
+        innerResourceInstanceMethod.requestParamClass = clientMethodRequestParamClass
+        innerResourceInstanceMethod.requestClass = clientMethodRequestClass
+        innerResourceInstanceMethod.responseClass = clientMethodResponseClass
+        innerResourceInstanceMethod.roleRequired = clientMethodRoleRequired
+        innerResourceInstanceMethod.produces = clientMethodProduces
+        innerResourceInstanceMethod.consumes = clientMethodConsumes
+        innerResourceInstanceMethod.logRequest = clientMethodLogRequest
+        innerResourceInstanceMethod.logResponse = clientMethodLogResponse
+        return innerResourceInstanceMethod
+    return innerMethodWrapper
---
+++ packages/python-framework/python_framework-0.2.207.011.tar.gz/python_framework-0.2.207.011/python_framework/api/src/util/FlaskUtil.py
@@ -0,0 +1,161 @@
+from flask import Response, request
+import flask_restful
+from python_helper import Constant as c
+from python_helper import ObjectHelper, log, Function
+import globals
+from python_framework.api.src.util import Serializer
+
+
+KEY_API_INSTANCE = 'apiInstance'
+API_INSTANCE_HOLDER = {
+    KEY_API_INSTANCE: None
+}
+
+
+Response = Response
+request = request
+Resource = flask_restful.Resource
+
+def safellyGetRequestBody() :
+    requestBody = safellyGetJson()
+    if ObjectHelper.isNone(requestBody):
+        requestBody = safellyGetData()
+    return requestBody if ObjectHelper.isNotNone(requestBody) else dict()
+
+def safellyGetJson():
+    jsonBody = None
+    try :
+        jsonBody = request.get_json(force=True)
+    except Exception as exception:
+        jsonBody = {}
+        log.log(safellyGetJson, f'Not possible to get request body. Returning {jsonBody} by default', exception=exception)
+    return jsonBody
+
+def safellyGetResponseJson(response):
+    jsonBody = None
+    try :
+        jsonBody = response.get_json(force=True)
+    except Exception as exception:
+        jsonBody = {}
+        log.log(safellyGetResponseJson, f'Not possible to get response body. Returning {jsonBody} by default', exception=exception)
+    return jsonBody
+
+def safellyGetData():
+    dataBody = None
+    try :
+        dataBody = request.get_json(force=True)
+    except Exception as exception:
+        dataBody = {}
+        log.log(safellyGetData, f'Not possible to get data. Returning {dataBody} by default', exception=exception)
+    return dataBody
+
+def safellyGetUrl() :
+    url = None
+    try :
+        url = request.url
+    except Exception as exception :
+        log.log(safellyGetUrl, 'Not possible to get url', exception=exception)
+    return url
+
+def safellyGetVerb() :
+    verb = None
+    try :
+        verb = request.method
+    except Exception as exception :
+        log.log(safellyGetVerb, 'Not possible to get verb', exception=exception)
+    return verb
+
+def safellyGetHeaders():
+    headers = None
+    try:
+        headers= request.headers
+    except Exception as exception :
+        headers = {}
+        log.log(safellyGetHeaders, 'Not possible to get request headers. Returning {headers} by default', exception=exception)
+    return headers if ObjectHelper.isNotNone(headers) else dict()
+
+def safellyGetResponseHeaders(response):
+    headers = None
+    try:
+        headers= response.headers
+    except Exception as exception :
+        headers = {}
+        log.log(safellyGetResponseHeaders, 'Not possible to get response headers. Returning {headers} by default', exception=exception)
+    return headers if ObjectHelper.isNotNone(headers) else dict()
+
+def safellyGetArgs():
+    args = None
+    try:
+        args = request.args
+    except Exception as exception :
+        args = {}
+        log.log(safellyGetArgs, f'Not possible to get args. Returning {args} by default', exception=exception)
+    return args if ObjectHelper.isNotNone(args) else dict()
+
+def buildHttpResponse(additionalResponseHeaders, controllerResponseBody, status, contentType):
+    httpResponse = Response(Serializer.jsonifyIt(controllerResponseBody),  mimetype=contentType, status=status)
+    for key, value in additionalResponseHeaders.items():
+        httpResponse.headers[key] = value
+    return httpResponse
+
+@Function
+def getGlobals() :
+    return globals.getGlobalsInstance()
+
+def getApi() :
+    api = None
+    try:
+        api = getGlobals().api
+    except Exception as exception :
+        raise Exception(f'Failed to return api from "globals" instance. Cause: {str(exception)}')
+    return api
+
+def getNullableApi() :
+    api = None
+    try :
+        api = getApi()
+    except Exception as exception :
+        log.warning(getNullableApi, 'Not possible to get api', exception=exception)
+    return api
+
+def getClassName(instance) :
+    return instance.__class__.__name__
+
+def getModuleName(instance) :
+    return instance.__class__.__module__
+
+def getQualitativeName(instance) :
+    return instance.__class__.__qualname__
+
+def isApiInstance(apiInstance):
+    if ObjectHelper.isNone(apiInstance):
+        return False
+    apiClassName = flask_restful.Api.__name__
+    moduleName = flask_restful.__name__
+    return apiClassName == getClassName(apiInstance) and apiClassName == getQualitativeName(apiInstance) and moduleName == getModuleName(apiInstance)
+
+def validateFlaskApi(apiInstance) :
+    if not isApiInstance(apiInstance) :
+        raise Exception(f'Invalid "flask_restful.Api" instance. {apiInstance} is not an Api instance')
+
+def validateResourceInstance(resourceInstance) :
+    if ObjectHelper.isNone(resourceInstance) :
+        raise Exception(f'Resource cannot be None')
+
+def retrieveApiInstance(apiInstance=None, arguments=None):
+    if isApiInstance(apiInstance):
+        return apiInstance
+    if isApiInstance(API_INSTANCE_HOLDER.get(KEY_API_INSTANCE)):
+        return API_INSTANCE_HOLDER.get(KEY_API_INSTANCE)
+    if ObjectHelper.isNone(apiInstance) and ObjectHelper.isNotNone(arguments):
+        try:
+            apiInstance = arguments[0].globals.api
+        except Exception as exception:
+            log.warning(retrieveApiInstance, f'''Not possible to retrieve api instance by {arguments}. Going for another approach''', exception=exception)
+    if not isApiInstance(apiInstance):
+        log.warning(retrieveApiInstance, f'''Not possible to retrieve api instance. Going for a slower approach''')
+        apiInstance = getNullableApi()
+    if ObjectHelper.isNone(apiInstance):
+        raise Exception('Not possible to retrieve api instance')
+    API_INSTANCE_HOLDER[KEY_API_INSTANCE] = apiInstance
+    return apiInstance
