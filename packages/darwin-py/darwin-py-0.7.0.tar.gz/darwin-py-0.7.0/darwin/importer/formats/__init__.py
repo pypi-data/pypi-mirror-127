e3a2b7415564846939564b6d7dd0624b1bc2982b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:29 +0000

    Add package darwin-py/0.7.0/darwin-py-0.7.0.tar.gz

---
+++ packages/darwin-py/darwin-py-0.7.0.tar.gz/darwin-py-0.7.0/darwin/importer/formats/__init__.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from darwin.datatypes import ImporterFormat
+
+from . import coco, csvtags, csvtagsvideo, darwin, dataloop, pascalvoc
+
+supported_formats: List[ImporterFormat] = [
+    ("pascal_voc", pascalvoc.parse_file),
+    ("dataloop", dataloop.parse_file),
+    ("csv_tags", csvtags.parse_file),
+    ("csv_tags_video", csvtagsvideo.parse_file),
+    ("coco", coco.parse_file),
+    ("darwin", darwin.parse_file),
+]
