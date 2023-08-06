40c8636fa134e29e1c90a15a9d9ab9b467a48235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:01 +0000

    Add package pyhtools/0.0.1/pyhtools-0.0.1.tar.gz

---
+++ packages/pyhtools/pyhtools-0.0.1.tar.gz/pyhtools-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyhtools',
+    version='0.0.1',
+    description='Python Hacking Tools (pyhtools) (pht)',
+    long_description='',
+    url='https://github.com/dmdhrumilmistry/pyhtools',
+    author='Dhrumil Mistry',
+    author_email='contact@dmdhrumilmistry.me',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
