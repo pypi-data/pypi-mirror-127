46f4af299a48fcd180608e3bcc78ef7253b16d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:08 +0000

    Add package sdo-hmi-rvs/0.1.0/sdo_hmi_rvs-0.1.0.tar.gz

---
+++ packages/sdo-hmi-rvs/sdo_hmi_rvs-0.1.0.tar.gz/sdo_hmi_rvs-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sdo_hmi_rvs',
+    version='0.1.0',
+    packages=['sdo_hmi_rvs', 'sdo_hmi_rvs.tools', 'sdo_hmi_rvs.source', 'sdo_hmi_rvs.examples'],
+    url='https://github.com/tamarervin/sdo_hmi_rvs',
+    license='Apache Software License',
+    author='Tamar Ervin',
+    author_email='tamarervin@gmail.com',
+    description='Python package to calculate \'sun-as-a-star\' RVs.'
+)
