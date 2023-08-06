57df74fca88fcafc56e81636ce3d0369a5984730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:07 +0000

    Add package splunk-appinspect/2.12.0/splunk-appinspect-2.12.0.tar.gz

---
+++ packages/splunk-appinspect/splunk-appinspect-2.12.0.tar.gz/splunk-appinspect-2.12.0/splunk_appinspect/distsearch_configuration_file.py
@@ -0,0 +1,12 @@
+# Copyright 2019 Splunk Inc. All rights reserved.
+"""Splunk distsearch.conf abstraction module"""
+from . import configuration_file
+
+
+class DistsearchConfigurationFile(configuration_file.ConfigurationFile):
+    """Represents an [distsearch.conf](https://docs.splunk.com/Documentation/Splunk/7.2.0/Admin/Distsearchconf)
+    file.
+    """
+
+    def __init__(self):
+        configuration_file.ConfigurationFile.__init__(self)
---
+++ packages/splunk-appinspect/splunk-appinspect-2.12.0.tar.gz/splunk-appinspect-2.12.0/splunk_appinspect/transforms_configuration_file.py
@@ -0,0 +1,10 @@
+# Copyright 2019 Splunk Inc. All rights reserved.
+"""Splunk transforms.conf abstraction module"""
+from . import configuration_file
+
+
+class TransformsConfigurationFile(configuration_file.ConfigurationFile):
+    """Represents a [transforms.conf](http://docs.splunk.com/Documentation/Splunk/latest/Admin/Transformsconf) file."""
+
+    def __init__(self):
+        configuration_file.ConfigurationFile.__init__(self)
