fcbf09447c7173def7f01ed29b909ba1b4f48941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:51 +0000

    Add package bavera/0.14/bavera-0.14-py3-none-any.whl

---
+++ packages/bavera/bavera-0.14-py3-none-any.whl/bavera/bot/__init__.py
@@ -0,0 +1,6 @@
+from bavera.bot.bot import Bot, BotConfig
+from bavera.bot.plugin import Plugin
+from bavera.bot.command import CommandLevels
+from bavera.util.config import Config
+
+__all__ = ['Bot', 'BotConfig', 'Plugin', 'Config', 'CommandLevels']
---
+++ packages/bavera/bavera-0.14-py3-none-any.whl/bavera/voice/__init__.py
@@ -0,0 +1,5 @@
+from bavera.voice.client import *  # noqa: F401,F403
+from bavera.voice.player import *  # noqa: F401,F403
+from bavera.voice.playable import *  # noqa: F401,F403
+
+# TODO: deprecate this file
