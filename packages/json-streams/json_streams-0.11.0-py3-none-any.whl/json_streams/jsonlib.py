f7e1eaa7aa174bd8c8e36c284b1b46c2c39a2a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:27 +0000

    Add package json-streams/0.11.0/json_streams-0.11.0-py3-none-any.whl

---
+++ packages/json-streams/json_streams-0.11.0-py3-none-any.whl/json_streams/jsonlib.py
@@ -0,0 +1,53 @@
+"""
+Utility library to load the underlying json library.
+
+Imports `orjson` or `ujson` if either is present, otherwise it imports `json` from
+the standard library.
+"""
+from json_streams.utility import types
+
+
+def get_backend(backend_name: str):
+    """Import the backend named `backend`"""
+    import importlib
+
+    return importlib.import_module(f"json_streams.backends.{backend_name}")
+
+
+def _default_backend():
+    for backend_name in ("be_orjson", "be_json"):
+        try:
+            return get_backend(backend_name)
+        except ImportError:
+            continue
+    raise ImportError("no backends available")
+
+
+backend = _default_backend()
+del _default_backend
+
+dumps = backend.dumps  # type: ignore
+loads = backend.loads  # type: ignore
+
+
+def load_from_file(file_name: types.Pathlike, **kwargs):
+    """
+    Load the JSON file with the given file_name.
+
+    :param file_name: name of the file to load from.
+    :return: the loaded JSON file.
+    """
+    with open(file_name, "br") as fp:
+        return loads(fp.read(), **kwargs)
+
+
+def dump_to_file(obj, file_name: types.Pathlike, **kwargs):
+    """
+    Dump to a JSON file with the given file name.
+
+    :param obj: the object to dump.
+    :param file_name: name of the file to dump to.
+    :return: anything returned from the backend.
+    """
+    with open(file_name, "bw") as fp:
+        return fp.write(dumps(obj, **kwargs))
---
+++ packages/json-streams/json_streams-0.11.0-py3-none-any.whl/json_streams/utility/__init__.py
@@ -0,0 +1,61 @@
+"""Util functions.
+"""
+from enum import Enum
+import os
+from typing import Any, Generator, IO, Union
+
+# pylint: disable=unsubscriptable-object
+
+
+def get_name_of_file(fp: IO) -> str:
+    if hasattr(fp, "name"):
+        return fp.name
+    return ""
+
+
+def is_jsonl(name: str) -> bool:
+    """Test if a filename is json lines.
+    Also returns True for '<stdin>' and '<stdout>'.
+
+    Arguments:
+        name {str} -- name to test
+
+    Returns:
+        {bool} -- True if this name is jsonl.
+    """
+    if name in ["<stdin>", "<stdout>"]:
+        return True
+    _, suffix = os.path.splitext(name)
+    # print('suffix = {suffix}'.format(suffix=suffix))
+    return suffix in [".jsonl"]
+
+
+def to_bytes(s: Union[str, bytes, bytearray]) -> Union[bytes, bytearray]:
+    """Convert str to bytes, otherwise pass through s
+
+    Args:
+        s (Union[str, bytes, bytearray]): the argument to assure bytes
+
+    Returns:
+        Union[bytes, bytearray]: the possible converted argument
+    """
+    if isinstance(s, (bytes, bytearray)):
+        return s
+    return s.encode("utf-8")
+
+
+class Sink:
+    def __init__(self, sink: Generator[None, Any, None]):
+        self.sink = sink
+
+    def __enter__(self):
+        next(self.sink)
+        return self.sink
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.sink.close()
+
+
+class JsonFormat(str, Enum):
+    JSON = "json"
+    JSON_LINES = "jsonl"
