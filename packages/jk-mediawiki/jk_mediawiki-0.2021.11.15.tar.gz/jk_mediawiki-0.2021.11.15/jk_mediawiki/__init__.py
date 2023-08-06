1249d668c63183d6536b16bc6e915c2cd70b8014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:57 +0000

    Add package jk-mediawiki/0.2021.11.15/jk_mediawiki-0.2021.11.15.tar.gz

---
+++ packages/jk-mediawiki/jk_mediawiki-0.2021.11.15.tar.gz/jk_mediawiki-0.2021.11.15/jk_mediawiki/__init__.py
@@ -0,0 +1,20 @@
+﻿
+
+
+__version__ = "0.2021.11.15"
+
+
+
+################################################################################################################################
+
+from .lang_support_php import PHPTokenizer, PHP
+from .MediaWikiSkinInfo import MediaWikiSkinInfo
+from .MediaWikiLocalSettingsFile import MediaWikiLocalSettingsFile
+from .MediaWikiExtensionInfo import MediaWikiExtensionInfo
+from .MediaWikiDiskUsageInfo import MediaWikiDiskUsageInfo
+from .MediaWikiLocalUserServiceMgr import MediaWikiLocalUserServiceMgr
+from .MediaWikiLocalUserInstallationMgr import MediaWikiLocalUserInstallationMgr
+
+from .LocalMediaWikisMgr import LocalMediaWikisMgr
+
+################################################################################################################################
