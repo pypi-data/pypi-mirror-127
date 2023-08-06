f72bb1f901e78a4d2372ee88311b9c9eb4fb47c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:33 +0000

    Add package py-asynq/0.0.4/py_asynq-0.0.4-py3-none-any.whl

---
+++ packages/py-asynq/py_asynq-0.0.4-py3-none-any.whl/py_asynq/client.py
@@ -0,0 +1,113 @@
+from uuid import uuid4
+import time
+from datetime import datetime, timedelta
+
+from .constant import AllQueues
+from .task import Option
+from .proto.asynq_pb2 import TaskMessage
+import redis
+
+
+enqueue_script = """
+redis.call("HSET", KEYS[1],
+           "msg", ARGV[1],
+           "state", "pending",
+           "timeout", ARGV[3],
+           "deadline", ARGV[4])
+redis.call("LPUSH", KEYS[2], ARGV[2])
+return 1
+"""
+
+schedule_script = """
+redis.call("HSET", KEYS[1],
+           "msg", ARGV[1],
+           "state", "scheduled",
+           "timeout", ARGV[4],
+           "deadline", ARGV[5])
+redis.call("ZADD", KEYS[2], ARGV[2], ARGV[3])
+return 1
+"""
+
+enqueue_unique_script = """
+local ok = redis.call("SET", KEYS[1], ARGV[1], "NX", "EX", ARGV[2])
+if not ok then
+  return 0
+end
+redis.call("HSET", KEYS[2],
+           "msg", ARGV[3],
+           "state", "pending",
+           "timeout", ARGV[4],
+           "deadline", ARGV[5],
+           "unique_key", KEYS[1])
+redis.call("LPUSH", KEYS[3], ARGV[1])
+return 1
+"""
+
+
+class Client:
+    def __init__(self, redis_connection=None):
+        self.redis = redis_connection
+        self.opts = {}
+        self.enqueue_cmd = self.redis.register_script(enqueue_script)
+        self.schedule_cmd = self.redis.register_script(schedule_script)
+        self.enqueue_unique_cmd = self.redis.register_script(
+            enqueue_unique_script)
+
+    def enqueue(self, task, option=Option()):
+        task_message = TaskMessage()
+        task_message.id = str(uuid4())
+        task_message.type = task.type
+        task_message.payload = task.bytes_payload
+        task_message.queue = option.queue
+        task_message.retry = option.retry
+        task_message.timeout = option.timeout
+        task_message.deadline = option.deadline
+        if option.unique_ttl > 0:
+            self.enqueue_unique(task_message, option)
+        if option.process_at < time.time():
+            self.enqueue_now(task_message, option)
+        else:
+            self.schedule(task_message, option)
+
+    def enqueue_now(self, task_message, option):
+        self.redis.sadd(AllQueues, task_message.queue)
+        task_key = f"asynq:{{{option.queue}}}:t:{task_message.id}"
+        pending_key = f"asynq:{{{option.queue}}}:pending"
+        key_list = [task_key, pending_key]
+        arg_list = [
+            task_message.SerializeToString(),
+            task_message.id,
+            task_message.timeout,
+            task_message.deadline,
+        ]
+
+        self.enqueue_cmd(keys=key_list, args=arg_list)
+
+    def schedule(self, task_message, option):
+        self.redis.sadd(AllQueues, task_message.queue)
+        task_key = f"asynq:{{{option.queue}}}:t:{task_message.id}"
+        scheduled_key = f"asynq:{{{option.queue}}}:scheduled"
+        key_list = [task_key, scheduled_key]
+        arg_list = [
+            task_message.SerializeToString(),
+            option.process_at,
+            task_message.id,
+            task_message.timeout,
+            task_message.deadline,
+        ]
+
+        self.schedule_cmd(keys=key_list, args=arg_list)
+
+    def enqueue_unique(self, task_message, option):
+        self.redis.sadd(AllQueues, task_message.queue)
+        task_key = f"asynq:{{{option.queue}}}:t:{task_message.id}"
+        pending_key = f"asynq:{{{option.queue}}}:pending"
+        key_list = [task_key, pending_key]
+        arg_list = [
+            task_message.SerializeToString(),
+            option.process_at,
+            task_message.id,
+            task_message.timeout,
+            task_message.deadline,
+        ]
+        self.enqueue_unique_cmd(keys=key_list, args=arg_list)
---
+++ packages/py-asynq/py_asynq-0.0.4-py3-none-any.whl/py_asynq/task.py
@@ -0,0 +1,80 @@
+import uuid
+import time
+import json
+from datetime import datetime, timedelta
+
+TaskState = {"active": 1, "pending": 2,
+             "scheduled": 3, "retry": 4, "archived": 5}
+
+DefaultMaxRetry = 25
+DefaultTimeout = 30 * 60
+
+
+class Task:
+    def __init__(self, type_name, payload):
+        self.type = type_name
+        self.payload = payload
+
+    @property
+    def bytes_payload(self):
+        return json.dumps(self.payload).encode()
+
+
+class Option:
+    def __init__(self):
+        self.retry = DefaultMaxRetry
+        self.queue = "default"
+        self.timeout = DefaultTimeout
+        self.deadline = 0
+        self.unique_ttl = 0
+        self.process_at = time.time()
+
+
+class TaskInfo:
+    def __init__(self, args):
+        self.id = uuid.uuid4()  # ID is a unique identifier for each task.
+        self.type = ""
+        # Type indicates the kind of the task to be performed , user defined
+
+        self.queue = "default"
+        # Queue is a name this message should be enqueued to.
+
+        self.payload = b""  # Payload holds data needed to process the task
+        self.max_retry = 0  # Retry is the max number of retry for this task.
+        self.retried = 0
+        # Retried is the number of times we've retried this task so far.
+
+        self.error_msg = ""
+        # ErrorMsg holds the error message from the last failure.
+        self.last_failed_at = time.time()
+        # Time of last failure in Unix time,
+        # the number of seconds elapsed since January 1, 1970 UTC.
+        # Use zero to indicate no last failure
+        self.timeout = 0
+        # Timeout specifies timeout in seconds.
+        # If task processing doesn't complete within the timeout,
+        # the task will be retried
+        # if retry count is remaining.
+        # Otherwise it will be moved to the archive.
+
+        # Use zero to indicate no timeout.
+        self.deadline = 0
+        # Deadline specifies the deadline for the task in Unix time,
+        # the number of seconds elapsed since January 1, 1970 UTC.
+        # If task processing doesn't complete before the deadline,
+        # the task will be retried
+        # if retry count is remaining.
+        # Otherwise it will be moved to the archive.
+
+        # Use zero to indicate no deadline.
+        self.unique_key = ""
+        # UniqueKey holds the redis key used for uniqueness lock for this task.
+        # Empty string indicates that no uniqueness lock was used.
+
+    @property
+    def task_key(self):
+        return f"asynq:{self.queue_name}:t:{self.id}"
+
+    @property
+    def pending_key(self):
+        return f"asynq:{self.queue_name}:pending"
