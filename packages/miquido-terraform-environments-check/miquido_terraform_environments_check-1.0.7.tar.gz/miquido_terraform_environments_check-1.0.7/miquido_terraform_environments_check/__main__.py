4d18f56a26a417633bfb94c27124635f5e742da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:03 +0000

    Add package miquido-terraform-environments-check/1.0.7/miquido_terraform_environments_check-1.0.7.tar.gz

---
+++ packages/miquido-terraform-environments-check/miquido_terraform_environments_check-1.0.7.tar.gz/miquido_terraform_environments_check-1.0.7/miquido_terraform_environments_check/__main__.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+environments = os.getenv('ENVIRONMENTS').split(' ')
+
+
+def run_os(cmd: str):
+    exit_code = os.system(cmd)
+    if exit_code != 0:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    for environment in environments:
+        os.chdir(environment)
+        run_os('terraform init -backend=false')
+        run_os('terraform validate')
+        os.chdir('../')
