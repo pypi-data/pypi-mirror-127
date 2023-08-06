02cc79b9dba9f126a224dcde13ad0e9cd2e948f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:50 +0000

    Add package carnival/1.3.8/carnival-1.3.8-py3-none-any.whl

---
+++ packages/carnival/carnival-1.3.8-py3-none-any.whl/carnival/__init__.py
@@ -0,0 +1,25 @@
+import sys
+
+from carnival.step import Step
+from carnival.host import Host, SSHHost, LocalHost
+from carnival.task import Task, SimpleTask
+from carnival import cmd
+from carnival import internal_tasks
+from carnival.utils import log
+from carnival.context import context_ref
+
+
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("default")
+
+
+__all__ = [
+    'Step',
+    'Host', 'SSHHost', 'LocalHost',
+    'Task', 'SimpleTask',
+    'cmd',
+    'log',
+    'context_ref',
+    'internal_tasks',
+]
