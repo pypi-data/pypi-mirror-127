b885090069572037b4a08064cc71a5cabf62c21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:52 +0000

    Add package oddsjam-api/0.2.3/oddsjam-api-0.2.3.tar.gz

---
+++ packages/oddsjam-api/oddsjam-api-0.2.3.tar.gz/oddsjam-api-0.2.3/src/Models/FutureOdds.py
@@ -0,0 +1,15 @@
+#region Imports
+from Base import ModelBase;
+from dataclasses import dataclass;
+from Models import Future;
+from Enum import SportsBooksEnum;
+#endregion Imports
+
+@dataclass
+class FutureOdds(ModelBase):
+    future: Future = None;
+    sports_book: SportsBooksEnum = None;
+    name: str = None;
+    price: float = None;
+    checked_date: str = None;
+    changed_date: str = None;
