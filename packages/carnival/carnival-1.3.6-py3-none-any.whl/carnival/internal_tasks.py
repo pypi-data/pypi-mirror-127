7711c306a2a277e416121aed8be54cb2f4e94d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:42 +0000

    Add package carnival/1.3.6/carnival-1.3.6-py3-none-any.whl

---
+++ packages/carnival/carnival-1.3.6-py3-none-any.whl/carnival/internal_tasks.py
@@ -0,0 +1,26 @@
+from carnival import Task
+
+
+class Help(Task):
+    """
+    Показать список доступных задач
+    """
+    module_name = ""
+    help = "List available commands and help"
+
+    def run(self) -> None:
+        from carnival.cli import task_types
+
+        task_list = list(task_types.keys())
+        task_list.sort()
+
+        ralign = 4
+        if task_list:
+            ralign += max([len(x) for x in task_list])
+
+        for task_name in task_types.keys():
+            help_text = task_types[task_name].help
+            if help_text:
+                print(f" {task_name:<{ralign}} {help_text}")
+            else:
+                print(f" {task_name}")
