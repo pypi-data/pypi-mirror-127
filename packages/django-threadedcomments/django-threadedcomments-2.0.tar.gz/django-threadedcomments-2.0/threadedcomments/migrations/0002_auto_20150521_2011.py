fc5ab117d2f7529288d1a09574f79bca8a5cb478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:43 +0000

    Add package django-threadedcomments/2.0/django-threadedcomments-2.0.tar.gz

---
+++ packages/django-threadedcomments/django-threadedcomments-2.0.tar.gz/django-threadedcomments-2.0/threadedcomments/migrations/0002_auto_20150521_2011.py
@@ -0,0 +1,17 @@
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('threadedcomments', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='threadedcomment',
+            name='tree_path',
+            field=models.CharField(verbose_name='Tree path', max_length=500, editable=False, db_index=False),
+            preserve_default=True,
+        ),
+    ]
