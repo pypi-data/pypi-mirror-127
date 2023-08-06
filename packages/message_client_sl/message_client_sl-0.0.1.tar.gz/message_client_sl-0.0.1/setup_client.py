17d3043e800ce1bf8dc7d07bb48c2aa1d38f62b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:20 +0000

    Add package message_client_sl/0.0.1/message_client_sl-0.0.1.tar.gz

---
+++ packages/message_client_sl/message_client_sl-0.0.1.tar.gz/message_client_sl-0.0.1/setup_client.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="message_client_sl",
+      version="0.0.1",
+      description="message_client_sl",
+      author="Sergey Lomakin",
+      author_email="sergeylomakin-it@yandex.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'])
