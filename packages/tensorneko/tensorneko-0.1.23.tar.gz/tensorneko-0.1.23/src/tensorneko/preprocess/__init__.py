9e2996301f36d4d1e687e82dd7f651960a9146a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:12 +0000

    Add package tensorneko/0.1.23/tensorneko-0.1.23.tar.gz

---
+++ packages/tensorneko/tensorneko-0.1.23.tar.gz/tensorneko-0.1.23/src/tensorneko/preprocess/__init__.py
@@ -0,0 +1,11 @@
+from .func import resize_image, resize_video, ResizeMethod, PaddingMethod, PaddingPosition, padding_video, padding_audio
+
+__all__ = [
+    "resize_image",
+    "resize_video",
+    "padding_video",
+    "padding_audio",
+    "ResizeMethod",
+    "PaddingMethod",
+    "PaddingPosition"
+]
