b7e9108b3e4cb1e34eca4e108ccade90ba1ed642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:35 +0000

    Add package dtcalc/0.1a2/dtcalc-0.1a2-py3-none-any.whl

---
+++ packages/dtcalc/dtcalc-0.1a2-py3-none-any.whl/dtcalc/__main__.py
@@ -0,0 +1,16 @@
+import argparse
+
+from dtcalc.lexeval import lexeval
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--in-dtfmt", default="%Y/%m/%d")
+    parser.add_argument("--out-dtfmt", default="%w weeks, %d days, %H hours, %M minutes")
+    parser.add_argument("input")
+
+    args = parser.parse_args()
+    try:
+        result = lexeval(args.input, args.in_dtfmt, args.out_dtfmt)
+        print(result)
+    except Exception as expt:
+        print(f"Error: Malformed input")
