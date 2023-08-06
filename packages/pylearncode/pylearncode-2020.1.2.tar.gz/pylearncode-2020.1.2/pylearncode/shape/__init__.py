b6c51b5a4df3367794bf835ef7d0681f7e15f4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:53 +0000

    Add package pylearncode/2020.1.2/pylearncode-2020.1.2.tar.gz

---
+++ packages/pylearncode/pylearncode-2020.1.2.tar.gz/pylearncode-2020.1.2/pylearncode/shape/__init__.py
@@ -0,0 +1,9 @@
+from pylearncode.shape.circle import Circle
+from pylearncode.shape.line import Line
+from pylearncode.shape.point import Point
+from pylearncode.shape.rectangle import Rectangle
+from pylearncode.shape.shape import Shape
+from pylearncode.shape.sprite import Sprite
+from pylearncode.shape.text import Text
+from pylearncode.shape.polygon import Polygon, Triangle
+from pylearncode.shape.ellipse import Ellipse
---
+++ packages/pylearncode/pylearncode-2020.1.2.tar.gz/pylearncode-2020.1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+# pip install twine wheel
+# python setup.py sdist bdist_wheel
+# twine upload dist/* -u VicWang -p PYPI******
+
+setuptools.setup(
+    name="pylearncode",
+    version="2020.1.2",
+    author="Himansh Raj",
+    author_email="iamthehimansh@gmail.com",
+    description='A simple 2D game engine base on pyglet',
+    long_description='A simple 2D game engine base on pyglet',
+    long_description_content_type="text/markdown",
+    url='http://github.com/iamthehimansh/PyLearnCode',
+    download_url="https://github.com/iamthehimansh/PyLearnCode/archive/refs/tags/2020.1.2.tar.gz",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyglet==1.3.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
