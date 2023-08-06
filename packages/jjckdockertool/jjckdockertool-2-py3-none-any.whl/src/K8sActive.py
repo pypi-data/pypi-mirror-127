03ed335b5d81e2123edf930632a189cd521907a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:44 +0000

    Add package jjckdockertool/2/jjckdockertool-2-py3-none-any.whl

---
+++ packages/jjckdockertool/jjckdockertool-2-py3-none-any.whl/src/K8sActive.py
@@ -0,0 +1,144 @@
+import logging
+# from kubernetes import client, config
+from os import path
+import kubernetes.config as config
+import kubernetes.client as client
+import yaml
+import requests
+
+
+class K8sTool(object):
+
+    def __init__(self):
+
+        config.load_kube_config(config_file="kubeconfig.yaml")
+        # config.load_kube_config()
+        self.DEPLOYMENT_NAME = "nginx-deployment"
+        self.api = client.AppsV1Api()
+
+    # def config(self):
+    #     config.load_kube_config(config_file="kubeconfig.yaml")
+    def getPod(self):
+        v1 = client.CoreV1Api()
+        print("Listing pods with their IPs:")
+        ret = v1.list_pod_for_all_namespaces(watch=False)
+        for i in ret.items:
+            if i.metadata.namespace == "realses" or i.metadata.namespace == "default":
+                print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+    def deployment_object(self):
+        # Configured Pod template container
+        container = client.V1Container(
+            name="nginx",
+            image="nginx:1.15.4",
+            ports=[client.V1ContainerPort(container_port=80)],
+            # resources=client.V1ResourceRequirements(
+            #     requests={"cpu": "100m", "memory": "200Mi"},
+            #     limits={"cpu": "500m", "memory": "500Mi"},
+            # ),
+        )
+
+        # Create and configure a spec section
+        template = client.V1PodTemplateSpec(
+            metadata=client.V1ObjectMeta(labels={"app": "nginx"}),
+            spec=client.V1PodSpec(containers=[container]),
+        )
+
+        # Create the specification of deployment
+        spec = client.V1DeploymentSpec(
+            replicas=3, template=template, selector={
+                "matchLabels":
+                    {"app": "nginx"}})
+
+        # Instantiate the deployment object
+        deployment = client.V1Deployment(
+            api_version="apps/v1",
+            kind="Deployment",
+            metadata=client.V1ObjectMeta(name=self.DEPLOYMENT_NAME,
+                                         labels={"k8s.kuboard.cn/layer": "web", "k8s.kuboard.cn/name": "website"}),
+            spec=spec,
+        )
+
+        return deployment
+
+    def create_deployment(self):
+        with open(path.join(path.dirname(__file__), "nginx-deployment.yaml")) as f:
+            dep = yaml.safe_load(f)
+            k8s_apps_v1 = client.AppsV1Api()
+            resp = k8s_apps_v1.create_namespaced_deployment(
+                body=dep, namespace="default")
+            print(client.V1Container)
+            print("Deployment created. status='%s'" % resp.metadata.name)
+
+    def create_deployment(self, api, deployment):
+        # Create deployement
+        resp = api.create_namespaced_deployment(
+            body=deployment, namespace="default"
+        )
+
+        print("\n[INFO] deployment `nginx-deployment` created.\n")
+        print("%s\t%s\t\t\t%s\t%s" % ("NAMESPACE", "NAME", "REVISION", "IMAGE"))
+        print(
+            "%s\t\t%s\t%s\t\t%s\n"
+            % (
+                resp.metadata.namespace,
+                resp.metadata.name,
+                resp.metadata.generation,
+                resp.spec.template.spec.containers[0].image,
+            )
+        )
+
+    def update_deployment(self, api, deployment):
+        # Update container image
+        deployment.spec.template.spec.containers[0].image = "nginx:1.16.0"
+
+        # patch the deployment
+        resp = api.patch_namespaced_deployment(
+            name=self.DEPLOYMENT_NAME, namespace="default", body=deployment
+        )
+
+        print("\n[INFO] deployment's container image updated.\n")
+        print("%s\t%s\t\t\t%s\t%s" % ("NAMESPACE", "NAME", "REVISION", "IMAGE"))
+        print(
+            "%s\t\t%s\t%s\t\t%s\n"
+            % (
+                resp.metadata.namespace,
+                resp.metadata.name,
+                resp.metadata.generation,
+                resp.spec.template.spec.containers[0].image,
+            )
+        )
+
+    def update_deployment(self, *upArgs):
+        """
+        更新K8S上的部署
+        :param upArgs:  name=upArgs[0][0],namespace=upArgs[0][1],update_image=upArgs[0][2];
+        update_image格式示例：docker.muniuma.cn/dotnetbackend:%tag
+        :return:
+        """
+        name=upArgs[0][0]
+        namespace=upArgs[0][1]
+        update_image=upArgs[0][2]
+        args=name + namespace + update_image
+        print("传入的参数为：" + args)
+        body = self.api.read_namespaced_deployment(name, namespace)
+        body.spec.template.spec.containers[0].image = update_image
+        try:
+            self.api.replace_namespaced_deployment(name,namespace,body)
+            print("更新完成"+args)
+            # print("更新完成"+apiResponse.format(namespace, name, update_image))
+        except Exception as e:
+            print("Exception when calling AppsV1Api->replace_namespaced_deployment: %s\n" % e)
+            logging.exception(e)
+
+# class K8sCiCdTool:
+#     """
+#     CICD专用
+#     """
+#     def cicdUpdateActive(self, *update):
+#         tourl=update[0][0]
+#         todate=update[0][3]
+#         tocookie=update[0][2]
+#         toheaders=update[0][1]
+#         req=requests.put(url=tourl,data=todate,headers=toheaders,cookies=tocookie)
+#         print(req.content)
---
+++ packages/jjckdockertool/jjckdockertool-2-py3-none-any.whl/src/main.py
@@ -0,0 +1,56 @@
+
+from K8sActive import K8sTool
+from DockerActive import DockerTool
+from ProjcetActive import DataTool
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def run():
+
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # k8s = K8sCiCdTool()
+    # tag="86800117"
+    # jsondata={"kind":"deployments","namespace":"realses","name":"mnmreportapi","images":{"docker.muniuma.cn/dotnetbackend":"docker.muniuma.cn/dotnetbackend:%s"%tag}}
+    # print(json.dumps(jsondata))
+    # cicdArgs=["http://10.12.3.50:30080/kuboard-api/cluster/default/kind/CICDApi/admin/resource/updateImageTag",
+    #           {"Content-Type": "application/json"},
+    #           {"Cookie": "KuboardUsername=admin; KuboardAccessKey=t68znzjh5ihx.shjwjbjk3d7hh3ezmmxwj5ccw8rsf4e7"},
+    #           json.dumps(jsondata)
+    #           ]
+    #
+    #
+    # k8s.cicdUpdateActive(cicdArgs)
+    #dep = k8s.deployment_object()
+    # k8s.config()
+    #k8s.getPod()
+    # k8s.create_deployment(k8s.api, dep)
+    # k8s.update_deployment(k8s.api, dep)
+    upargs=["mnmreportapi","realses","docker.muniuma.cn/dotnetbackend:88996434"]
+    k8s=K8sTool()
+    k8s.update_deployment(upargs)
+def dockerrun():
+    conpoolArry = ["10.12.2.51", 30092, 9, "", 5000]
+    dtObj=DataTool(conpoolArry)
+    dkObj=DockerTool()
+    tagStr=dtObj.generatetag(8)
+    resStr=dkObj.dockerfileActive(tagStr)
+    tagArg=resStr.split(":",2)
+    respParArry=[tagArg[0],tagArg[1],tagArg[2]]
+    dkObj.dockerPushActive(respParArry)
+    upargs = ["mnmreportapi", "realses", "docker.muniuma.cn/dotnetbackend:%s"%tagArg[2]]
+    k8s=K8sTool()
+    k8s.update_deployment(upargs)
+    # pja=FileTool()
+    # ph="D:\pythonEnvTest\dockerAutoPushEnv"
+    # if pja.checkFile(ph):
+    #     pja.copyFile(ph)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    dockerrun()
+    # print_hi('PyCharm')
+    # run()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
