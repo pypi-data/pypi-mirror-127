eb3716fa4783102981d040427751e29dcbef56ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:40 +0000

    Add package uuoskit/1.0.3/uuoskit-1.0.3-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/uuoskit/uuoskit-1.0.3-cp310-cp310-macosx_10_15_x86_64.whl/uuoskit/__init__.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from .http_client import HttpClient
+from .rpc_interface import RPCInterface, WalletClient
+from .chainapi_sync import ChainApi
+from uuoskit import _uuoskit
+
+__version__='1.0.3'
+
+_uuoskit.init()
+
+uuosapi = ChainApi()
