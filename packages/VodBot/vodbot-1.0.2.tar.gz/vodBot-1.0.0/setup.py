6440b5d0fef81ff3912fec70deb5f8c1f18ae583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:38 +0000

    Add package VodBot/1.0.0/vodbot-1.0.2.tar.gz

---
+++ packages/VodBot/vodbot-1.0.2.tar.gz/vodBot-1.0.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+# "imports" project and version number
+# https://stackoverflow.com/a/16084844/13977827
+exec(open("vodbot/__init__.py").read())
+
+setup(
+	name=__project__,
+	version=__version__,
+    
+	author="Logan \"NotQuiteApex\" Hickok-Dickson",
+	author_email="self@notquiteapex.net",
+    
+	description="Twitch VOD and Clip manager",
+    long_description=long_description,
+	long_description_content_type="text/markdown",
+    
+	url="https://github.com/NotQuiteApex/VodBot",
+    project_urls={
+        "Bug Tracker": "https://github.com/NotQuiteApex/VodBot/issues",
+    },
+    
+	keywords="twitch vod video download",
+	license="zlib/libpng",
+    
+	packages=find_packages(include=["vodbot", "vodbot.*"]),
+    
+	python_requires='>=3.6',
+	install_requires=[
+		"google-api-python-client>=2.0",
+		"google-auth-httplib2>=0.1.0",
+		"google-auth-oauthlib>=0.4.4",
+		"requests>=2.20",
+		"m3u8>=0.8",
+	],
+	entry_points={
+		'console_scripts': ['vodbot=vodbot.__main__:deffered_main'],
+	}
+)
---
+++ packages/VodBot/vodbot-1.0.2.tar.gz/vodBot-1.0.0/vodbot/__init__.py
@@ -0,0 +1,3 @@
+__project__ = "vodBot"
+__version__ = "1.0.0"
+TWITCH_CLIENT_ID = "kimne78kx3ncx6brgo4mv6wki5h1ko"
