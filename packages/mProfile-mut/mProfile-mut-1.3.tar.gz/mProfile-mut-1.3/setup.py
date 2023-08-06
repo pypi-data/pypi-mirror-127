b53407c0df9e7dfd9fbecc842d4bc3a6abc63082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:54 +0000

    Add package mProfile-mut/1.3/mProfile-mut-1.3.tar.gz

---
+++ packages/mProfile-mut/mProfile-mut-1.3.tar.gz/mProfile-mut-1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='mProfile-mut',
+      version='1.3',
+      description='nucleotide-resolution mutation calling',
+      url='https://github.com/aldob/mProfile',
+      download_url = 'https://github.com/aldob/mProfile/archive/v1.3.tar.gz',
+      packages=find_packages(),
+      author='Aldo Saleh Bader',
+      entry_points={
+        'console_scripts': [
+            'callMUT = mProfile.callMUT:main',
+            'TransloCapture = mProfile.TransloCapture:main'            
+        ],
+      },
+      zip_safe=False)
+ 
