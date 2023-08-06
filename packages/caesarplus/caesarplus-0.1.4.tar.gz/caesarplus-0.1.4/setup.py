06aeae8b28df71c5136e5adea0d49abd28027927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:06 +0000

    Add package caesarplus/0.1.4/caesarplus-0.1.4.tar.gz

---
+++ packages/caesarplus/caesarplus-0.1.4.tar.gz/caesarplus-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='caesarplus',
+    url='https://github.com/awesomelewis2007/CaesarPlus/',
+    author='Lewis Evans',
+    author_email='awesomelewis2007@gmail.com',
+    packages=['caesarplus'],
+    install_requires=[''],
+    version='0.1.4',
+    license='GNU',
+    description='An advanced caesar cypher python module'
+)
