274b052712d1abac9bd2d6ce30fe952c9ba98f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:37 +0000

    Add package moto-ext/2.2.5/moto_ext-2.2.5-py2.py3-none-any.whl

---
+++ packages/moto-ext/moto_ext-2.2.5-py2.py3-none-any.whl/moto/emr/exceptions.py
@@ -0,0 +1,19 @@
+from moto.core.exceptions import RESTError, JsonRESTError
+
+
+class EmrError(RESTError):
+    code = 400
+
+
+class InvalidRequestException(JsonRESTError):
+    def __init__(self, message, **kwargs):
+        super(InvalidRequestException, self).__init__(
+            "InvalidRequestException", message, **kwargs
+        )
+
+
+class ValidationException(JsonRESTError):
+    def __init__(self, message, **kwargs):
+        super(ValidationException, self).__init__(
+            "ValidationException", message, **kwargs
+        )
