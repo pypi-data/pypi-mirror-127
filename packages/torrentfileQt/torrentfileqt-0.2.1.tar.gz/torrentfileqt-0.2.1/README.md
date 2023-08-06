765e2d769fa042e63af47a9131a9d232066fdfb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:02 +0000

    Add package torrentfileQt/0.2.1/torrentfileqt-0.2.1.tar.gz

---
+++ packages/torrentfileQt/torrentfileqt-0.2.1.tar.gz/torrentfileqt-0.2.1/README.md
@@ -0,0 +1,27 @@
+# TorrentFileQt
+
+![torrentfileQt.png](./assets/torrentfileQt.png)
+
+-------
+
+TorrentFileQt is a GUI Frontend for [TorrentFile CLI](https://github.com/alexpdev/torrentfile) project.
+
+## Features
+
+- Create .torrent files
+- Display detailed information for a .torrent file
+- Bittorrent v1, v2 and hybrid .torrent files supported
+- Check if a .torrent file contents are in filesystem
+- Check progress or percentage complete for .torrent file
+
+### Screenshots
+
+![createTab.png](./assets/createTab.png =300x300)
+
+-------
+
+![checkTab.png](./assets/checkTab.png =300x300)
+
+-------
+
+![infoTab.png](./assets/infoTab.png =300x300)
---
+++ packages/torrentfileQt/torrentfileqt-0.2.1.tar.gz/torrentfileqt-0.2.1/setup.py
@@ -0,0 +1,62 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#############################################################################
+# Copyright (C) 2021 alexpdev
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+############################################################################
+"""Setup package."""
+
+import json
+
+from setuptools import find_packages, setup
+
+INFO = json.load(open("package.json"))
+INFO["long_description"] = open("README.md").read()
+with open("requirements.txt") as req:
+    INFO["install_requires"] = req.read().split("\n")
+
+setup(
+    name=INFO["name"],
+    version=INFO["version"],
+    description=INFO["description"],
+    long_description=INFO["long_description"],
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    keywords=INFO["keywords"],
+    author=INFO["author"],
+    author_email=INFO["email"],
+    url=INFO["url"],
+    project_urls={"Source Code": "https://github.com/alexpdev/torrentfileQt"},
+    license=INFO["license"],
+    packages=find_packages(exclude=["env", "tests"]),
+    entry_points={
+        "console_scripts": "torrentfileQt = torrentfileQt.window:start"
+    },
+    include_package_data=True,
+    tests_require=["pytest"],
+    install_require=["torrentfile", "PyQt6", "pyben"],
+    setup_requires=["setuptools", "wheel"],
+    zip_safe=False,
+    test_suite="complete",
+)
