bdc624d083255394c1352b95907f977798e191df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:02 +0000

    Add package kinto-redis/2.1.0/kinto-redis-2.1.0.tar.gz

---
+++ packages/kinto-redis/kinto-redis-2.1.0.tar.gz/kinto-redis-2.1.0/setup.py
@@ -0,0 +1,41 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    README = f.read()
+
+with codecs.open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8') as f:
+    CHANGELOG = f.read()
+
+REQUIREMENTS = [
+    'kinto>=13.0.0',
+    'redis',
+]
+
+setup(name='kinto-redis',
+      version='2.1.0',
+      description='Kinto Redis',
+      long_description=README + '\r\n' + CHANGELOG,
+      license='Apache License (2.0)',
+      classifiers=[
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "License :: OSI Approved :: Apache Software License",
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10'
+      ],
+      keywords="web services",
+      author='Mozilla Services',
+      author_email='services-dev@mozilla.com',
+      url='https://github.com/Kinto/kinto-redis',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=REQUIREMENTS)
