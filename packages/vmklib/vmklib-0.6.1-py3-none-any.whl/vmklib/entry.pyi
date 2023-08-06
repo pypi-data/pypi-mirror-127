e4d3050e3871b92cc8a6dc90c40d144b77d2ee6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:45 +0000

    Add package vmklib/0.6.1/vmklib-0.6.1-py3-none-any.whl

---
+++ packages/vmklib/vmklib-0.6.1-py3-none-any.whl/vmklib/entry.pyi
@@ -0,0 +1,5 @@
+from typing import List
+from vmklib import DESCRIPTION as DESCRIPTION, VERSION as VERSION
+from vmklib.app import add_app_args as add_app_args, entry as entry
+
+def main(argv: List[str] = ...) -> int: ...
