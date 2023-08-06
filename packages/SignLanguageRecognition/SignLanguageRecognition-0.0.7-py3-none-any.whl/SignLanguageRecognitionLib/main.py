731d604104992d410d34f1853dc4aa0f0b793e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:40 +0000

    Add package SignLanguageRecognition/0.0.7/SignLanguageRecognition-0.0.7-py3-none-any.whl

---
+++ packages/SignLanguageRecognition/SignLanguageRecognition-0.0.7-py3-none-any.whl/SignLanguageRecognitionLib/main.py
@@ -0,0 +1,21 @@
+from SignLanguageRecognitionLib.createDataset import *
+from SignLanguageRecognitionLib.signLanguageRecognizer import *
+
+def handleActionInMain():
+    while(True):
+        _input = input('1 - Create new class \n2 - Sign Language Recognizer\nq - Quit\n')
+        if(_input == '1'):
+            createDataset()
+        if (_input == '2'):
+            signLanguageRecognizer()
+        elif(_input == 'q'):
+            print('\nLeaving a program...')
+            break
+        else:
+            pass
+
+def main():
+    handleActionInMain()
+
+if __name__ == "__main__":
+    main()
