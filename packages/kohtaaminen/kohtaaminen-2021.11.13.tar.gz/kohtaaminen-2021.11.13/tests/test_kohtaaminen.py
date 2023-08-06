9f9cbf51f1289097f29da4f8a374065539b0228d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:53 +0000

    Add package kohtaaminen/2021.11.13/kohtaaminen-2021.11.13.tar.gz

---
+++ packages/kohtaaminen/kohtaaminen-2021.11.13.tar.gz/kohtaaminen-2021.11.13/tests/test_kohtaaminen.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import pathlib
+
+import kohtaaminen.kohtaaminen as ko
+
+
+def test_ko_main():
+    inp = str(pathlib.Path('tests', 'fixtures', 'basic', 'export.zip'))
+    assert ko.main(['translate', inp]) == 0
