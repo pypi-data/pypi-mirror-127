0706c2add6c97748c323f085389a09da30dbc3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:48 +0000

    Add package invitae-proto/0.1/invitae-proto-0.1.tar.gz

---
+++ packages/invitae-proto/invitae-proto-0.1.tar.gz/invitae-proto-0.1/invitae-proto/Person.py
@@ -0,0 +1,7 @@
+class Person:
+    "This is a person class"
+    age = 10
+
+    def greet(self):
+        print('Hello')
+
---
+++ packages/invitae-proto/invitae-proto-0.1.tar.gz/invitae-proto-0.1/invitae-proto/__init__.py
@@ -0,0 +1,2 @@
+# Inside of __init__.py
+from invitae-proto.Person import Person
