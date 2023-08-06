0b220a33ea7548359859bb39d140f364d58267e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package latex-trim/0.1.3/latex-trim-0.1.3.tar.gz

---
+++ packages/latex-trim/latex-trim-0.1.3.tar.gz/latex-trim-0.1.3/README.md
@@ -0,0 +1,14 @@
+# latex-trim
+Helps you include a sub-selection of a pdf into a latex document.
+
+Simply run the script, e.g.
+`latex-trim file.pdf <page_number>`,
+and select a part of the page using the mouse, and press enter when done.
+
+The script then outputs options to pass to `\includegraphics` in latex.
+
+## Installation
+```
+pip install latex-trim
+```
+
---
+++ packages/latex-trim/latex-trim-0.1.3.tar.gz/latex-trim-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['latex_trim']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.4.3,<4.0.0', 'pdf2image>=1.16.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['latex-trim = latex_trim.latex_trim:run']}
+
+setup_kwargs = {
+    'name': 'latex-trim',
+    'version': '0.1.3',
+    'description': 'Tool that allows you to include sub-selections of PDFs in latex documents.',
+    'long_description': '# latex-trim\nHelps you include a sub-selection of a pdf into a latex document.\n\nSimply run the script, e.g.\n`latex-trim file.pdf <page_number>`,\nand select a part of the page using the mouse, and press enter when done.\n\nThe script then outputs options to pass to `\\includegraphics` in latex.\n\n## Installation\n```\npip install latex-trim\n```\n\n',
+    'author': 'Oliver Rausch',
+    'author_email': 'oliverrausch99@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<3.11',
+}
+
+
+setup(**setup_kwargs)
