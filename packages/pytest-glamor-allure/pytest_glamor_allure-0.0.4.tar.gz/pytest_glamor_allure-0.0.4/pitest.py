daa226fcf6ccdc50c472e5f5de0718bf67b59e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:18 +0000

    Add package pytest-glamor-allure/0.0.4/pytest_glamor_allure-0.0.4.tar.gz

---
+++ packages/pytest-glamor-allure/pytest_glamor_allure-0.0.4.tar.gz/pytest_glamor_allure-0.0.4/pitest.py
@@ -0,0 +1,21 @@
+from pytest import *
+from _pytest import outcomes, skipping
+from _pytest.config import (
+    Config,
+    PluginManager,
+    PytestPluginManager,
+    create_terminal_writer,
+)
+from _pytest.junitxml import LogXML
+from _pytest.mark import Mark
+from _pytest.nodes import Item, Node
+from _pytest.outcomes import (
+    Exit,
+    Failed,
+    Skipped,
+    XFailed,
+    _with_exception as with_exception,
+)
+from _pytest.pytester import Pytester
+from _pytest.python import Function, Metafunc
+from _pytest.reports import CollectReport, TestReport
