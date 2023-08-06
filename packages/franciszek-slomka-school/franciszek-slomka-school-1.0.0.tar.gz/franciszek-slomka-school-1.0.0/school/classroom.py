396bc44cd09c8a2480b2cf1ed0b78da7399f9b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:05 +0000

    Add package franciszek-slomka-school/1.0.0/franciszek-slomka-school-1.0.0.tar.gz

---
+++ packages/franciszek-slomka-school/franciszek-slomka-school-1.0.0.tar.gz/franciszek-slomka-school-1.0.0/school/classroom.py
@@ -0,0 +1,35 @@
+"""
+Napisz bibliotekę i wrzuć ją do PYPI realizujacą abstrakcję szkoły podstawowej.
+1. Napisz klasę Student reprezentującą ucznia w szkole. Uczeń posiada następujące atrybuty:
+    * fullname - imie i nazwisko
+    * gender - M/F
+    * birth_year
+2. Napisz klasę Classroom reprezentującą klasę, do której uczęszczają uczniowie. Posiada następujące atrybuty:
+    * year_of_creation - rok utworzenia klasy
+    * classroom_char - odpowiednio a, b, c itd.
+    * students - lista uczniów uczeszczających do danej klasy
+    Posiada metode show_students() wyświetlającą wszystkich uczniów
+
+    Paczka powinna się nazywa w konwencji <imie_nazwisko>-school.
+"""
+from student import Student
+
+
+class Classroom:
+
+    def __init__(self, year_of_creation, classroom_char, students=[]):
+        self.year_of_creation = year_of_creation
+        self.classroom_char = classroom_char
+        self.students = students
+
+    def show_students(self):
+        for student in self.students:
+            student.print_details()
+
+
+if __name__ == "__main__":
+    student1 = Student("Franek", "m", 1985)
+    student2 = Student('Grzegorz', 'm', 2012)
+    klasa1 = Classroom(2000, 'a', [student1, student2])
+
+    klasa1.show_students()
