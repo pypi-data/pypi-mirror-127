fb0e48ce56f11ea4e1b0906058ac9d13921b0c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:12 +0000

    Add package examplefactor/0.1/examplefactor-0.2.tar.gz

---
+++ packages/examplefactor/examplefactor-0.2.tar.gz/examplefactor-0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for SWIG example
+"""
+
+from distutils.core import setup, Extension
+
+
+factorial_module = Extension('_factorial-factor',
+                           sources=['factor_wrap.cxx', 'factor.cpp'],
+                           )
+
+setup (name = 'examplefactor',
+       version = '0.1',
+       author      = "Pradeesh",
+       description = """Simple factorial calculators""",
+       ext_modules = [factorial_module],
+       py_modules = ["example"],
+       )
