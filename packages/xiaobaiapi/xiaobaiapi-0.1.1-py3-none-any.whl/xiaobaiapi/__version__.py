f41d7fe4311d8e9ca6862d192418085bfc77e01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:08 +0000

    Add package xiaobaiapi/0.1.1/xiaobaiapi-0.1.1-py3-none-any.whl

---
+++ packages/xiaobaiapi/xiaobaiapi-0.1.1-py3-none-any.whl/xiaobaiapi/__version__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+__script__ = 'version.py'
+__create__ = '2021/11/14 9:40 下午'
+__author__ = 'Tser'
+__email__ = '807447312@qq.com'
+
+__version__ = '.'.join(map(str, (0, 1, 1)))
