5dac357c1ae3034e4fa914b66dc2dfcd5ba9789a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:25 +0000

    Add package continuum/1.1.7/continuum-1.1.7-py3-none-any.whl

---
+++ packages/continuum/continuum-1.1.7-py3-none-any.whl/continuum/__init__.py
@@ -0,0 +1,17 @@
+"""Continuum lib.
+
+Made by Arthur Douillard and Timothee Lesort.
+
+The goal of this library is to provide clean and simple to use utilities for
+Continual Learning.
+"""
+# pylint: disable=C0401
+# flake8: noqa
+from continuum import datasets
+from continuum.scenarios import *
+from continuum.tasks import *
+from continuum.metrics import *
+from continuum.viz import plot_samples
+from continuum import generators
+
+__version__ = "1.1.7"
