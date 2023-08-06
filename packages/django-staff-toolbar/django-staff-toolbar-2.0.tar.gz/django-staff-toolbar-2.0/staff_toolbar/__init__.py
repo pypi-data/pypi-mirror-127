0227809ed927331e0841af4d323b8ec55bfbade4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:39 +0000

    Add package django-staff-toolbar/2.0/django-staff-toolbar-2.0.tar.gz

---
+++ packages/django-staff-toolbar/django-staff-toolbar-2.0.tar.gz/django-staff-toolbar-2.0/staff_toolbar/__init__.py
@@ -0,0 +1,10 @@
+from .conf import toolbar_item, toolbar_link, toolbar_literal, toolbar_title
+
+__version__ = "2.0"  # following PEP 440
+
+__all__ = (
+    "toolbar_title",
+    "toolbar_item",
+    "toolbar_literal",
+    "toolbar_link",
+)
