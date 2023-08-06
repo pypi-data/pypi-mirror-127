671ddd4365963c063146c1bd7c6707b8a1a31703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:50 +0000

    Add package vpncmd/0.6.0/vpncmd-0.6.0.tar.gz

---
+++ packages/vpncmd/vpncmd-0.6.0.tar.gz/vpncmd-0.6.0/test/test_connect_known_vpn.py
@@ -0,0 +1,17 @@
+import vpncmd
+import re
+import time
+
+
+vc = vpncmd.VPNCMD(r"C:\Program Files\SoftEther VPN Client\vpncmd_x64.exe", True)
+vc.setup_cmd("/client", "localhost")
+# vc.VPN_Client_Management.NicCreate("VPN2")
+# vc.connect_random_vpn("VPN2")
+vc.connect_known_vpn()
+while not vc.is_connected_to_vpn():
+    time.sleep(0.5)
+time.sleep(5)
+vc.disconnect_vpn()
+
+
+
