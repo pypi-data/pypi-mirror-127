4e41801e58170d57b342c21257bed8ad0ccc4bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:18 +0000

    Add package python-commandmaker/0.0.4/python-commandmaker-0.0.4.tar.gz

---
+++ packages/python-commandmaker/python-commandmaker-0.0.4.tar.gz/python-commandmaker-0.0.4/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="python-commandmaker",
+    version="0.0.4",
+    author="Cube Riser",
+    author_email="cuberiser@gmail.com",
+    long_description_content_type="text/markdown",
+    long_description=str(long_description),
+    description="Python module for discord bot like commands from the console",
+    python_requires=">=3.10.0",
+    packages=setuptools.find_packages(),
+    install_requires=[""],
+)
