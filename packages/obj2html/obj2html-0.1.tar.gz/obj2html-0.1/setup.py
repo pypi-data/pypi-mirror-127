9bd98ab6f44dcdae80074288cd78763d48d0bd7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:10 +0000

    Add package obj2html/0.1/obj2html-0.1.tar.gz

---
+++ packages/obj2html/obj2html-0.1.tar.gz/obj2html-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'obj2html',         # How you named your package folder (MyLib)
+  packages = ['obj2html'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'Nicola Landro',                   # Type in your name
+  author_email = 'nicolaxx94@live.it',      # Type in your E-Mail
+  url = 'https://gitlab.com/nicolalandro/obj2html',   # Provide either the link to your github or to your website
+  keywords = ['3D', '.obj', '.html','jupyter', ''],   # Keywords that define your package best
+  install_requires=[],
+)
