f8ff3d40c67c3da50a471865b96677193186fb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package running-mate/0.0.2/running-mate-0.0.2.tar.gz

---
+++ packages/running-mate/running-mate-0.0.2.tar.gz/running-mate-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="running-mate",
+    version="0.0.2",
+    author="Michael Herman",
+    author_email="michael@mherman.org",
+    description="Lightweight MLOps framework.",
+    url="https://github.com/mjhea0/running-mate",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "dacite==1.6.0",
+        "numpy==1.21.3",
+        "pandas==1.3.4",
+        "peewee==3.14.8",
+        "requests==2.26.0",
+    ],
+)
