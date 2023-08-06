32a10e00012635712145033bfb0e6f821b515272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:29 +0000

    Add package nanodegree-excercise5-distributions/0.2/nanodegree_excercise5_distributions-0.2.tar.gz

---
+++ packages/nanodegree-excercise5-distributions/nanodegree_excercise5_distributions-0.2.tar.gz/nanodegree_excercise5_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nanodegree_excercise5_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['nanodegree_excercise5_distributions'],
+      zip_safe=False)
