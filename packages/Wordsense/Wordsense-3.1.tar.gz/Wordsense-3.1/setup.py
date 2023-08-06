90167c9e6c776a05781e089397db414588bbd48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:37 +0000

    Add package Wordsense/3.1/Wordsense-3.1.tar.gz

---
+++ packages/Wordsense/Wordsense-3.1.tar.gz/Wordsense-3.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+setup(
+    name="Wordsense",
+    version="3.1",
+    description="Get Meaning of Ambiguous word",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Anant Dashpute",
+    author_email="<anantdashpute@gmail.com>",
+    url="https://github.com/DASHANANT/Wordsense",
+    packages=find_packages(), 
+    install_requires=['nltk'],
+    keywords=['WSD', 'Sense', 'Disambiguation', 'Lesk',
+              'word-sense', 'stem', 'Knowlege based', 'wordnet', 'NLP'],
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",        
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research"
+    ],
+)
