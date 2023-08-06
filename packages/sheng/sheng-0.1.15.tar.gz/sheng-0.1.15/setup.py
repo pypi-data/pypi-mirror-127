3d058a19785a8711a1a20ddcead39ab0bda81ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:22 +0000

    Add package sheng/0.1.15/sheng-0.1.15.tar.gz

---
+++ packages/sheng/sheng-0.1.15.tar.gz/sheng-0.1.15/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sheng",
+    version="0.1.15",
+    author="luojiahai",
+    author_email="luo@jiahai.co",
+    description="The Sheng programming language: A Chinese programming language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sheng-lang/sheng",
+    project_urls={
+        "Bug Tracker": "https://github.com/sheng-lang/sheng/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.9",
+    entry_points = {
+        'console_scripts': ['sheng=src.driver:main'],
+    }
+)
