da9dbd58c8245df5fd259da068cd33d09a97d5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:32 +0000

    Add package indexnow/0.0.1/IndexNow-0.0.1.tar.gz

---
+++ packages/indexnow/IndexNow-0.0.1.tar.gz/IndexNow-0.0.1/indexnow/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+# Version of the indexnow package
+__version__ = "0.0.1"
---
+++ packages/indexnow/IndexNow-0.0.1.tar.gz/IndexNow-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = 'IndexNow',
+    packages = ['indexnow'],
+    version = '0.0.1',
+    license='MIT',
+    description = 'TYPE YOUR DESCRIPTION HERE',
+    author = 'Ajit Jasrotia',
+    author_email = 'ajitjasrotia012@gmail.com',
+    url = 'https://github.com/ajitjasrotia/indexnow',
+    download_url = 'https://github.com/ajitjasrotia/indexnow/archive/v_01.tar.gz',
+    keywords = ['index', 'indexnow'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
