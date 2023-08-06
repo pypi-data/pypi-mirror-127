6557d4bb63225ee3c19da28834e3cab747d7af63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:08 +0000

    Add package napplib/0.8.23.16/napplib-0.8.23.16-py3-none-any.whl

---
+++ packages/napplib/napplib-0.8.23.16-py3-none-any.whl/napplib/hub/v2/models/channel.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from dataclasses 	import dataclass
+
+class ChannelType(Enum):
+    IN = "IN"
+    OUT = "OUT"
+
+class ChannelStatusIntegration(Enum):
+    SENT_CHANNEL_OUT      = "SENT_TO_CHANNEL_OUT"
+    CHANNEL_OUT_APPROVED  = "CHANNEL_OUT_APPROVED"
+    CHANNEL_OUT_ERROR     = "CHANNEL_OUT_ERROR"
+
+@dataclass
+class ChannelStatus:
+	external_id : str
+	status	    : ChannelStatusIntegration
+	message     : str
+
+@dataclass
+class SellerChannelUpdateQuantity:
+	date : str
+	quantity : int
