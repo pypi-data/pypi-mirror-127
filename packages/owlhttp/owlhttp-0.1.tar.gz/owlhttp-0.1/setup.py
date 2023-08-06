56325787f01340c0021e429eb08043686cea03b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:19 +0000

    Add package owlhttp/0.1/owlhttp-0.1.tar.gz

---
+++ packages/owlhttp/owlhttp-0.1.tar.gz/owlhttp-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='owlhttp',
+    version='0.1',
+    packages=find_packages(),
+)
+print('Welcome to OwlHTTP HTTP Server by Sovenok-Hacker!')
