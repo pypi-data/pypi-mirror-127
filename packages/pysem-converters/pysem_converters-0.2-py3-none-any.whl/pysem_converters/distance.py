478551f2e41b6fce8d66be669d4ffb26245fe70f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:03 +0000

    Add package pysem-converters/0.2/pysem_converters-0.2-py3-none-any.whl

---
+++ packages/pysem-converters/pysem_converters-0.2-py3-none-any.whl/pysem_converters/distance.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+import pint
+
+METER = 'meter'
+CENTIMETER = 'centimeter'
+MILLIMETER = 'millimeter'
+KILOMETER = 'kilometer'
+INCH = 'inch'
+HAND = 'hand'
+FOOT = 'foot'
+YARD = 'yard'
+MILE = 'mile'
+LIGHT_YEAR = 'light_year'
+ASTRONOMICAL_UNIT = 'astronomical_unit'
+PARSEC = 'parsec'
+NAUTICAL_MILE = 'nautical_mile'
+ANGSTROM = 'angstrom'
+MICRON = 'micron'
+PLANCK_LENGTH = 'planck_length'
---
+++ packages/pysem-converters/pysem_converters-0.2-py3-none-any.whl/pysem_converters/time.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+import pint
+
+SECONDS = "seconds"
+MINUTES = "minutes"
+HOURS = "hours"
+DAYS = "days"
+WEEKS = "weeks"
+FORTNIGHTS = "fortnights"
+YEARS = "years"
+MONTHS = "months"
+CENTURIES = "centuries"
+MILLENNIUMS = "millenniums"
