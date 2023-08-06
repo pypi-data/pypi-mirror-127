afba0333bebf80f040a5ce6b775a97d3c6280b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:49 +0000

    Add package zero-hid/0.1.2/zero_hid-0.1.2-py3-none-any.whl

---
+++ packages/zero-hid/zero_hid-0.1.2-py3-none-any.whl/zero_hid/hid/mouse.py
@@ -0,0 +1,12 @@
+from . import write as hid_write
+
+def send_mouse_event(mouse_path, buttons, relative_x, relative_y,
+                     vertical_wheel_delta, horizontal_wheel_delta):
+    buf = [
+        buttons,
+        relative_x & 0xff,
+        relative_y & 0xff,
+        vertical_wheel_delta & 0xff,
+        horizontal_wheel_delta & 0xff
+    ]
+    hid_write.write_to_hid_interface(mouse_path, buf)
