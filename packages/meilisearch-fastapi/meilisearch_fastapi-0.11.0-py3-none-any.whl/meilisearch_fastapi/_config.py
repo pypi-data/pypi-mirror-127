365465c1d6709ceca601dfd6121ba20a1b0015dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:48 +0000

    Add package meilisearch-fastapi/0.11.0/meilisearch_fastapi-0.11.0-py3-none-any.whl

---
+++ packages/meilisearch-fastapi/meilisearch_fastapi-0.11.0-py3-none-any.whl/meilisearch_fastapi/_config.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class MeiliSearchConfig(BaseSettings):
+    meilisearch_url: str
+    meilisearch_api_key: Optional[str] = None
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_config() -> MeiliSearchConfig:
+    return MeiliSearchConfig()
---
+++ packages/meilisearch-fastapi/meilisearch_fastapi-0.11.0-py3-none-any.whl/meilisearch_fastapi/routes/meilisearch_routes.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from fastapi import APIRouter, Depends
+from meilisearch_python_async import Client
+from meilisearch_python_async.models.client import ClientStats, Keys
+from meilisearch_python_async.models.health import Health
+from meilisearch_python_async.models.version import Version
+
+from meilisearch_fastapi._config import MeiliSearchConfig, get_config
+
+router = APIRouter()
+
+
+@router.get("/health", response_model=Health, tags=["MeiliSearch"])
+async def get_health(config: MeiliSearchConfig = Depends(get_config)) -> Health:
+    async with Client(url=config.meilisearch_url, api_key=config.meilisearch_api_key) as client:
+        return await client.health()
+
+
+@router.get("/keys", response_model=Keys, tags=["MeiliSearch"])
+async def get_keys(config: MeiliSearchConfig = Depends(get_config)) -> Keys:
+    async with Client(url=config.meilisearch_url, api_key=config.meilisearch_api_key) as client:
+        return await client.get_keys()
+
+
+@router.get("/stats", response_model=ClientStats, tags=["MeiliSearch"])
+async def get_stats(config: MeiliSearchConfig = Depends(get_config)) -> ClientStats:
+    async with Client(url=config.meilisearch_url, api_key=config.meilisearch_api_key) as client:
+        return await client.get_all_stats()
+
+
+@router.get("/version", response_model=Version, tags=["MeiliSearch"])
+async def get_version(config: MeiliSearchConfig = Depends(get_config)) -> Version:
+    async with Client(url=config.meilisearch_url, api_key=config.meilisearch_api_key) as client:
+        return await client.get_version()
