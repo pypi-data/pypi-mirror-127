bdcc3ef11d59b5a9e3a32260ca673577f702fccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:25 +0000

    Add package Lotte/1.2.1/Lotte-1.2.1.tar.gz

---
+++ packages/Lotte/Lotte-1.2.1.tar.gz/Lotte-1.2.1/helper/Loader.py
@@ -0,0 +1,20 @@
+import json
+from helper.Decoder import json_decoder_match, json_decoder_citation_source, json_decoder_target_text_location_link
+
+
+def load_matches(input_path):
+    with open(input_path, 'r', encoding='utf-8') as matches_file:
+        matches = json.load(matches_file, object_hook=json_decoder_match)
+        return matches
+
+
+def load_citation_sources(input_path):
+    with open(input_path, 'r', encoding='utf-8') as citation_sources_file:
+        citation_sources = json.load(citation_sources_file, object_hook=json_decoder_citation_source)
+        return citation_sources
+
+
+def load_target_text_location_links(input_path):
+    with open(input_path, 'r', encoding='utf-8') as file:
+        target_text_location_links = json.load(file, object_hook=json_decoder_target_text_location_link)
+        return target_text_location_links
