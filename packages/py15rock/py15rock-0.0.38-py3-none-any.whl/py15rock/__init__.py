9243821f6fe1f565f417b824ed2ddc69fba45f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:05 +0000

    Add package py15rock/0.0.38/py15rock-0.0.38-py3-none-any.whl

---
+++ packages/py15rock/py15rock-0.0.38-py3-none-any.whl/py15rock/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for fifteenrock."""
+
+__author__ = """Gautam Bakshi"""
+__email__ = 'Gautam.Bakshi@15rock.com'
+
+# from fifteenrock.core.core import database, load_project, get_master_data, deploy_powerBI, model_train
+# from fifteenrock.core.compute_client import compute
+# from fifteenrock.core.fr_notebook import deploy_form, _deploy_notebook, deploy_project
+
+# from fifteenrock.constants import VERSION
+
+# import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+# __version__ = VERSION
+from py15rock import get
