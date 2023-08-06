3df3d53da018a8a7193962254dc9c6557fb6a015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:33 +0000

    Add package desktopassistant/1.0.2/desktopassistant-1.0.2.tar.gz

---
+++ packages/desktopassistant/desktopassistant-1.0.2.tar.gz/desktopassistant-1.0.2/desktopassistant/__init__.py
@@ -0,0 +1,8 @@
+from DesktopAssistant import speak
+from DesktopAssistant import wishMe
+from DesktopAssistant import takeCommand
+from DesktopAssistant import sendEmail
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import smtplib
---
+++ packages/desktopassistant/desktopassistant-1.0.2.tar.gz/desktopassistant-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.2'
+DESCRIPTION = 'Desktop Assistant which takes commands as speech & convert to text. Speaks the text provided. Wishes the User as per the Hour of the day. It also can send E-Mails.'
+LONG_DESCRIPTION = 'This Desktop Assistant is complete package for a Virtual Assistant of yours. It can do all basic tasks such as Sending E-Mails, Wishing/Greeting. Taking command in speech from the user. Speak the text provided to it.'
+
+# Setting up
+setup(
+    name="desktopassistant",
+    version=VERSION,
+    author="Akshat Bhatter (Developer_Akshat)",
+    author_email="<bhatterakshat4@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description= LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pyttsx3', 'speechRecognition', 'comtypes'],
+    keywords=['python', 'AI', 'Desktop Assistant', 'Assistant', 'Virtual Assistant'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
