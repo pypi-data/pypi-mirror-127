57b7ae60fc8f7949c3241111bb88279bcaf1fee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:09 +0000

    Add package morerandoms/0.2/morerandoms-0.2.tar.gz

---
+++ packages/morerandoms/morerandoms-0.2.tar.gz/morerandoms-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="morerandoms",
+    version="0.2",
+    url="https://github.com/Wolframoviy/morerandom-py",
+    license="MIT",
+    author="WolframoviyI",
+    author_email="arseniysstrim@gmail.com",
+    description="More Random for God of Random!",
+    long_description=open("README.md").read(),
+    requires=["random"],
+    packages=["morerandom"]
+    )
