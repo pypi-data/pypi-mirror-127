3115a26e17dff54e2e7254f1168b05580f29827f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:36 +0000

    Add package fastapi-chat-sdk/0.0.3/fastapi-chat-sdk-0.0.3.tar.gz

---
+++ packages/fastapi-chat-sdk/fastapi-chat-sdk-0.0.3.tar.gz/fastapi-chat-sdk-0.0.3/fastapi_chat/run.py
@@ -0,0 +1,14 @@
+import uvicorn
+
+try:
+    from fastapi_chat.chat import settings
+except ModuleNotFoundError:
+    from chat import settings
+
+
+def main():
+    uvicorn.run(**settings.UvicornSettings().dict())
+
+
+if __name__ == "__main__":
+    uvicorn.run(**settings.UvicornDevSettings().dict())
