414100e40760d3876771cd076aca723666e95695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:39 +0000

    Add package jetson-adapter-pkg/0.9.3/jetson-adapter-pkg-0.9.3.tar.gz

---
+++ packages/jetson-adapter-pkg/jetson-adapter-pkg-0.9.3.tar.gz/jetson-adapter-pkg-0.9.3/frotech_adapter/__init__.py
@@ -0,0 +1,9 @@
+from .pantilt import PanTilt
+from .serialLed import SerialLed
+from .ledMatrix import LedMatrix
+from .powerDetect import PowerDetect
+from .car import Car
+from .claw import Claw
+from .lift import Lift
+
+__version__ = '0.9.3'
