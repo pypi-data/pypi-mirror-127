f6b33c95ad2bdac9399906cfb96187e6113329bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:20 +0000

    Add package wsl-distribution/0.11/wsl_distribution-0.11.tar.gz

---
+++ packages/wsl-distribution/wsl_distribution-0.11.tar.gz/wsl_distribution-0.11/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='wsl_distribution',
+      version='0.11',
+      description='Gaussian distributions',
+      packages=['wsl_distribution'],
+      zip_safe=False)
