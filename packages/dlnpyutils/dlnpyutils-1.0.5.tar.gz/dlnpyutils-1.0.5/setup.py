6444966ca8f4a035c6801c8b7d4e89ef5822d6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:01 +0000

    Add package dlnpyutils/1.0.5/dlnpyutils-1.0.5.tar.gz

---
+++ packages/dlnpyutils/dlnpyutils-1.0.5.tar.gz/dlnpyutils-1.0.5/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+pypiname = 'dlnpyutils'
+
+setup(name='dlnpyutils',
+      version='1.0.5',
+      description='David Nidever Python Utility Functions',
+      author='David Nidever',
+      author_email='dnidever@montana.edu',
+      url='https://github.com/dnidever/dlnpyutils',
+      packages=['dlnpyutils'],
+      scripts=['bin/job_daemon','bin/bump','bin/topypi'],
+      install_requires=['numpy','astropy','scipy']
+)
