c86ca4b8c04feef8b80ad3b8260d4592cff48512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:33 +0000

    Add package SignLanguageRecognition/0.0.3/SignLanguageRecognition-0.0.3-py3-none-any.whl

---
+++ packages/SignLanguageRecognition/SignLanguageRecognition-0.0.3-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,6 @@
+# from landmarkDetection import *
+# from createDataset import *
+# from signLanguageRecognizer import *
+import landmarkDetection
+import createDataset
+import signLanguageRecognizer
