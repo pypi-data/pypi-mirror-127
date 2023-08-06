85fd706b83f2b4059bf1048286f99f31501eadae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:20 +0000

    Add package generic-proto-plus-stubs/0.1.1/generic-proto-plus-stubs-0.1.1.tar.gz

---
+++ packages/generic-proto-plus-stubs/generic-proto-plus-stubs-0.1.1.tar.gz/generic-proto-plus-stubs-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['generic-proto-stubs']
+
+package_data = \
+{'': ['*'],
+ 'generic-proto-stubs': ['marshal/*',
+                         'marshal/collections/*',
+                         'marshal/rules/*']}
+
+install_requires = \
+['proto-plus>=1.18.0,<2.0.0', 'types-protobuf>=3.17.4,<4.0.0']
+
+setup_kwargs = {
+    'name': 'generic-proto-plus-stubs',
+    'version': '0.1.1',
+    'description': 'Manually updated type stubs for proto-plus with some generics',
+    'long_description': '# Manually updated type stubs for proto-plus\n[![PyPI version](https://badge.fury.io/py/generic-proto-plus-stubs.svg)](https://badge.fury.io/py/generic-proto-plus-stubs)\n\nThis package provides type stubs for the [proto-plus](https://pypi.org/project/proto-plus/) package with \nsome use of generics. \n\n**This is in no way affiliated with Google.**\n\n**This is basically just a fork of [henribru/proto-plus-stubs](https://github.com/henribru/proto-plus-stubs)\nwith some manually added code to make use of pythons generics**\n\nThe stubs were created automatically by [stubgen](https://mypy.readthedocs.io/en/stable/stubgen.html).\n## Installation\n```shell script\n$ pip install generic-proto-plus-stubs\n```\n',
+    'author': 'Maximilian Schmidt',
+    'author_email': 'landlaeufer@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/saggitar/generic-proto-plus-stubs',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
