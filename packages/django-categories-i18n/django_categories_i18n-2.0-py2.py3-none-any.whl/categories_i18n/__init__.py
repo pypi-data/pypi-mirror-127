e663b802e4e4de77b4717c469676a095c629ac77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:31 +0000

    Add package django-categories-i18n/2.0/django_categories_i18n-2.0-py2.py3-none-any.whl

---
+++ packages/django-categories-i18n/django_categories_i18n-2.0-py2.py3-none-any.whl/categories_i18n/__init__.py
@@ -0,0 +1,4 @@
+# follow PEP440
+__version__ = "2.0"
+
+default_app_config = "categories_i18n.apps.CategoryAppConfig"
---
+++ packages/django-categories-i18n/django_categories_i18n-2.0-py2.py3-none-any.whl/categories_i18n/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class CategoryAppConfig(AppConfig):
+    """
+    Application config for a better Django admin label.
+    """
+
+    name = "categories_i18n"
+    verbose_name = _("Categories")
