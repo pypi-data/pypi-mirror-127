e324766ae528733bc239b8c5e776a695ec60deb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:06 +0000

    Add package streamlit-text-rating/0.1.2/streamlit_text_rating-0.1.2-py3-none-any.whl

---
+++ packages/streamlit-text-rating/streamlit_text_rating-0.1.2-py3-none-any.whl/streamlit_text_rating/frontend/build/precache-manifest.2f589f3e23a0c8a6481bd93d8344c3d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1ad697653c16ee48bc964dc8c0a5c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e582350eb4f7d6268556",
+    "url": "./static/js/2.d213324c.chunk.js"
+  },
+  {
+    "revision": "bc287589f3fefb198aaa0061e2ebdab0",
+    "url": "./static/js/2.d213324c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8f3cd36258c775397e",
+    "url": "./static/js/main.5c689c72.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
