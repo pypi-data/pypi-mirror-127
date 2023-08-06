8306dc06f56bf64a4661566c0e8a67e16e35dddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:14 +0000

    Add package DoraPy/1.1.0/DoraPy-1.1.0-py3-none-any.whl

---
+++ packages/DoraPy/DoraPy-1.1.0-py3-none-any.whl/dorapy/__init__.py
@@ -0,0 +1,14 @@
+from dorapy.nn import initializer
+from dorapy.nn import layer
+from dorapy.nn import loss
+from dorapy.nn import model
+from dorapy.nn import net
+from dorapy.nn import optimizer
+
+from dorapy.utils import data_iterator
+from dorapy.utils import dataset
+from dorapy.utils import downloader
+from dorapy.utils import math
+from dorapy.utils import seeder
+from dorapy.utils import structured_param
+from dorapy.utils import metric
