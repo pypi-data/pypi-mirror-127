fadc0c5d763704068c601a8ef9d0f78c6876d928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:38 +0000

    Add package warepy/0.3.0/warepy-0.3.0.tar.gz

---
+++ packages/warepy/warepy-0.3.0.tar.gz/warepy-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+
+setup(
+    name="warepy",
+    packages=["warepy"],
+    version="0.3.0",
+    license="MIT",
+    description="My toolkit for Python.",
+    author = "Alexander Ryzhov",
+    author_email = "thed4rkof@gmail.com",
+    url = "https://github.com/ryzhovalex/warepy",
+    download_url = "https://github.com/ryzhovalex/warepy/archive/refs/tags/0.3.0.tar.gz",
+    keywords = ["toolkit", "python-core"],
+    install_requires=[
+        "pyyaml",
+        "loguru"
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3.9",
+    ],
+)
