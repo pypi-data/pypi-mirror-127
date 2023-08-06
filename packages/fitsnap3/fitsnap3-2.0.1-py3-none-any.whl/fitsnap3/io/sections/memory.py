e46bfc251b5d60f5a93c41725cc19e90affd64ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:24 +0000

    Add package fitsnap3/2.0.1/fitsnap3-2.0.1-py3-none-any.whl

---
+++ packages/fitsnap3/fitsnap3-2.0.1-py3-none-any.whl/fitsnap3/io/sections/memory.py
@@ -0,0 +1,26 @@
+from .sections import Section
+from os import sysconf
+from subprocess import check_output
+from ...parallel_tools import pt
+
+class Memory(Section):
+
+    def __init__(self, name, config, args):
+        super().__init__(name, config, args)
+        allowedkeys = ['memory','override']
+        for value_name in config['MEMORY']:
+            if value_name in allowedkeys: continue
+            else:
+                raise RuntimeError(">>> Found unmatched variable in MEMORY section of input: ", value_name)
+                #pt.single_print(">>> Found unmatched variable in MEMORY section of input: ",value_name)
+
+        self._check_memory()
+        self.memory = self.get_value("MEMORY", "memory", "{}".format(self.mem_bytes), "int")
+        self.override = self.get_value("MEMORY", "override", "False", interpreter="bool")
+        self.delete()
+
+    def _check_memory(self):
+        try:
+            self.mem_bytes = sysconf('SC_PAGE_SIZE') * sysconf('SC_PHYS_PAGES')
+        except ValueError:
+            self.mem_bytes = int(check_output(['sysctl', '-n', 'hw.memsize']).strip())
