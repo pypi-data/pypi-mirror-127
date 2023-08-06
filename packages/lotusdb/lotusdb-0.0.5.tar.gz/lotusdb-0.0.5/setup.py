3ac3c26b345d88c78a164c35d5551ffee2a019c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:53 +0000

    Add package lotusdb/0.0.5/lotusdb-0.0.5.tar.gz

---
+++ packages/lotusdb/lotusdb-0.0.5.tar.gz/lotusdb-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Python database'
+
+setup(
+    name='lotusdb',
+    version=VERSION,
+    author='d3tu',
+    author_email='jpafly@gmail.com',
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['database'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+    ]
+)
