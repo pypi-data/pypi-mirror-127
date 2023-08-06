27c2f90265e5b06b36c132ae9a02ccd2119ebbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:31 +0000

    Add package mjx/0.0.5/mjx-0.0.5-cp39-cp39-manylinux2014_i686.whl

---
+++ packages/mjx/mjx-0.0.5-cp39-cp39-manylinux2014_i686.whl/mjx/const.py
@@ -0,0 +1,20 @@
+from enum import IntEnum
+
+
+class AbsolutePos(IntEnum):
+    INIT_EAST = 0
+    INIT_SOUTH = 1
+    INIT_WEST = 2
+    INIT_NORTH = 3
+
+
+class RelativePos(IntEnum):
+    SELF = 0
+    RIGHT = 1
+    MID = 2
+    LEFT = 3
+
+
+class SeedType(IntEnum):
+    RANDOM = 0
+    DUPLICATE = 1
