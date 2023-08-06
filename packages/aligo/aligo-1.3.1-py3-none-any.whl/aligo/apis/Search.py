5b14425d2257fc34580d6cc97c17d19143ad6cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:20 +0000

    Add package aligo/1.3.1/aligo-1.3.1-py3-none-any.whl

---
+++ packages/aligo/aligo-1.3.1-py3-none-any.whl/aligo/apis/Search.py
@@ -0,0 +1,34 @@
+"""..."""
+from typing import List
+
+from aligo.core import *
+from aligo.request import *
+from aligo.types import *
+from aligo.types.Enum import *
+
+
+class Search(Core):
+    """..."""
+
+    def search_file(self, name: str = None, category: SearchCategory = None, drive_id: str = None,
+                    body: SearchFileRequest = None, **kwargs) -> List[BaseFile]:
+        """搜索文件"""
+        if body is None:
+            query = f'name match "{name}"'
+            if category is not None:
+                query += f' and category = "{category}"'
+            body = SearchFileRequest(query=query, drive_id=drive_id, **kwargs)
+        result = self._core_search_file(body)
+        return [i for i in result]
+
+    def search_aims(self, keyword: str = None, category: BaseFileCategory = 'image', drive_id: str = None,
+                    body: AimSearchRequest = None, **kwargs) -> List[BaseFile]:
+        """搜索目标/标签"""
+        if body is None:
+            body = AimSearchRequest(
+                query=f"keywords ='{keyword}' and type = 'file' and category = '{category}'",
+                drive_id=drive_id,
+                **kwargs
+            )
+        result = self._core_search_aims(body)
+        return [i for i in result]
