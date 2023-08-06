b898a1b5bec4a51e04b6ee82194a6357c86a923d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:43 +0000

    Add package tiffslide/0.2.2/tiffslide-0.2.2-py3-none-any.whl

---
+++ packages/tiffslide/tiffslide-0.2.2-py3-none-any.whl/tiffslide/_types.py
@@ -0,0 +1,11 @@
+# noinspection PyUnresolvedReferences
+from os import PathLike
+from typing import IO
+from typing import Union
+
+__all__ = [
+    "PathOrFileLike",
+]
+
+# todo: check if this covers all relevant use cases
+PathOrFileLike = Union[str, bytes, "PathLike[str]", IO[str], IO[bytes]]
