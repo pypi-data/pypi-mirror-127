b8202c06abcd2a489118bffe81bd34d9b24c98da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:14 +0000

    Add package dsnd-hrm-probability/1.2/dsnd-hrm-probability-1.2.tar.gz

---
+++ packages/dsnd-hrm-probability/dsnd-hrm-probability-1.2.tar.gz/dsnd-hrm-probability-1.2/README.md
@@ -0,0 +1,4 @@
+# dsnd-hrm-probability
+A wonderful package for Binomial and Gaussian Distributions
+
+![](https://cdn.wallstreetmojo.com/wp-content/uploads/2019/04/Binomial-Distribution-Formula.jpg.webp)
---
+++ packages/dsnd-hrm-probability/dsnd-hrm-probability-1.2.tar.gz/dsnd-hrm-probability-1.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd-hrm-probability',
+      version='1.2',
+      description='Gaussian distributions',
+      packages=['dsnd-hrm-probability'],
+      zip_safe=False)
