fef4917da1b8396a23287b40b0fff7ab99cb64fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:55 +0000

    Add package vhrun3/1.2.9/vhrun3-1.2.9.tar.gz

---
+++ packages/vhrun3/vhrun3-1.2.9.tar.gz/vhrun3-1.2.9/vhrun3/__main__.py
@@ -0,0 +1,5 @@
+from viper_hrunner.vhrun.cli import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/vhrun3/vhrun3-1.2.9.tar.gz/vhrun3-1.2.9/vhrun3/builtin/__init__.py
@@ -0,0 +1,2 @@
+from viper_hrunner.vhrun.builtin.comparators import *
+from viper_hrunner.vhrun.builtin.functions import *
