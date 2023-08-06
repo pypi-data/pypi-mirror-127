ee25d41daf03281d66c2f7f47aac202508da0639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:08 +0000

    Add package mlregression/0.0.6/mlregression-0.0.6.tar.gz

---
+++ packages/mlregression/mlregression-0.0.6.tar.gz/mlregression-0.0.6/README.md
@@ -0,0 +1,2 @@
+# Machine learning regression (mlregression)
+This repo makes scikit-compatible models run completely off-the-shelf.
---
+++ packages/mlregression/mlregression-0.0.6.tar.gz/mlregression-0.0.6/src/mlregression/tests/demo.py
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------
+# Libraries
+#------------------------------------------------------------------------------
+# Standard
+import numpy as np
+import pandas as pd
+
+import mlregression
+
+from mlregression.base.base_mlreg import BaseMLRegressor
+
+
+
+print("succes")
