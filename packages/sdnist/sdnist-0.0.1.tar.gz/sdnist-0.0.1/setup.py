a7e9e812dbd84719c771b8b7c82b96c1005c79e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:58 +0000

    Add package sdnist/0.0.1/sdnist-0.0.1.tar.gz

---
+++ packages/sdnist/sdnist-0.0.1.tar.gz/sdnist-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sdnist',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/usnistgov/SDNist',
+    author='gl',
+    author_email='gl@sarus.tech',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
