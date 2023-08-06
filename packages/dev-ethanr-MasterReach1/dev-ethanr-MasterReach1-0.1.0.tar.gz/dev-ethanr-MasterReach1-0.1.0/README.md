8b12facf6806569a872fbbe050927284de5cd998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:36 +0000

    Add package dev-ethanr-MasterReach1/0.1.0/dev-ethanr-MasterReach1-0.1.0.tar.gz

---
+++ packages/dev-ethanr-MasterReach1/dev-ethanr-MasterReach1-0.1.0.tar.gz/dev-ethanr-MasterReach1-0.1.0/README.md
@@ -0,0 +1,14 @@
+Introduction
+---------------------------
+This project was made as a solution to many complaints at my workplace about password requirements being too
+difficult to conform to and many, many external blogposts about the different specifications of varying websites.
+In response, I created a simple command line password generator that takes password requirements and creates a more
+than secure password. It even copies the password to the user's clipboard!
+
+Technologies
+---------------------------
+All that is used here is Python 3.9.
+
+Setup
+---------------------------
+lorem ipsum
---
+++ packages/dev-ethanr-MasterReach1/dev-ethanr-MasterReach1-0.1.0.tar.gz/dev-ethanr-MasterReach1-0.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dev-ethanr-MasterReach1",
+    version="0.1.0",
+    author="Ethan Cox",
+    author_email="mrxone2025@protonmail.com",
+    description="A command line password generator tailored to your needs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MasterReach1/pw_gen",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
