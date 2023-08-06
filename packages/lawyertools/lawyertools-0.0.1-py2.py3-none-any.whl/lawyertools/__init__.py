a784fa99f865bcd5c55b7ba92866f7e72e6896b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:46 +0000

    Add package lawyertools/0.0.1/lawyertools-0.0.1-py2.py3-none-any.whl

---
+++ packages/lawyertools/lawyertools-0.0.1-py2.py3-none-any.whl/lawyertools/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    __init__.py
+    ~~~~~~~~~~~
+
+    A set of tools for lawyers
+
+    :copyright: (c) 2021 by Law Engineering Systems SRL.
+    :license: see LICENSE for more details.
+"""
+
+__title__ = 'lawyertools'
+__version__ = '0.0.1'
+__author__ = 'Biagio Distefano'
