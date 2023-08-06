b5ce1c306ee6aaafb1d17408a6b4b19623d0297c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:51 +0000

    Add package lisa-gui/0.0.0/lisa-gui-0.0.0.tar.gz

---
+++ packages/lisa-gui/lisa-gui-0.0.0.tar.gz/lisa-gui-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lisa-gui',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='marawan mohamed',
+    author_email='marawan6569@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
