9734393e6b5858890d174fd7f912c22eb9886376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:55 +0000

    Add package investing-algorithm-framework/0.11/investing_algorithm_framework-0.11-py3-none-any.whl

---
+++ packages/investing-algorithm-framework/investing_algorithm_framework-0.11-py3-none-any.whl/investing_algorithm_framework/core/market_services/market_service.py
@@ -0,0 +1,88 @@
+from abc import ABC, abstractmethod
+from investing_algorithm_framework.core.market_identifier \
+    import MarketIdentifier
+from investing_algorithm_framework.core.models.snapshots import AssetPrice
+
+
+class MarketService(ABC, MarketIdentifier):
+
+    @abstractmethod
+    def pair_exists(self, target_symbol: str, trading_symbol: str):
+        pass
+
+    def get_price(self, target_symbol: str, trading_symbol: str) -> AssetPrice:
+        return self.get_ticker(target_symbol, trading_symbol)
+
+    @abstractmethod
+    def get_ticker(self, target_symbol: str, trading_symbol: str) -> AssetPrice:
+        pass
+
+    @abstractmethod
+    def get_order_book(self, target_symbol: str, trading_symbol: str):
+        pass
+
+    @abstractmethod
+    def get_balance(self, symbol: str = None):
+        pass
+
+    @abstractmethod
+    def create_limit_buy_order(
+        self,
+        target_symbol: str,
+        trading_symbol: str,
+        amount: float,
+        price: float
+    ):
+        pass
+
+    @abstractmethod
+    def create_limit_sell_order(
+        self,
+        target_symbol: str,
+        trading_symbol: str,
+        amount: float,
+        price: float
+    ):
+        pass
+
+    @abstractmethod
+    def create_market_buy_order(
+        self,
+        target_symbol: str,
+        trading_symbol: str,
+        amount: float,
+    ):
+        pass
+
+    @abstractmethod
+    def create_market_sell_order(
+        self,
+        target_symbol: str,
+        trading_symbol: str,
+        amount: float,
+    ):
+        pass
+
+    @abstractmethod
+    def get_orders(self, target_symbol: str, trading_symbol: str):
+        pass
+
+    @abstractmethod
+    def get_order(self, order_id, target_symbol: str, trading_symbol: str):
+        pass
+
+    @abstractmethod
+    def get_open_orders(
+            self, target_symbol: str = None, trading_symbol: str = None
+    ):
+        pass
+
+    @abstractmethod
+    def get_closed_orders(
+            self, target_symbol: str = None, trading_symbol: str = None
+    ):
+        pass
+
+    @abstractmethod
+    def cancel_order(self, order_id):
+        pass
