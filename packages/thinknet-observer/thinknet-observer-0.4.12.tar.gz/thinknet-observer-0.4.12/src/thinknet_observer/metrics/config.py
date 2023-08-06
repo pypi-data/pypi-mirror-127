585242dfbc7949c3827c5bba70a56ce9e6dd828c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:11 +0000

    Add package thinknet-observer/0.4.12/thinknet-observer-0.4.12.tar.gz

---
+++ packages/thinknet-observer/thinknet-observer-0.4.12.tar.gz/thinknet-observer-0.4.12/src/thinknet_observer/metrics/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+_DEFAULT_BUCKETS = [0.05, 0.1, 0.5, 0.9, 0.95]
+_DEFAULT_LABELS = ["method", "route", "http_status", "serviceName", "namespace"]
+
+NAMESPACE = os.getenv("SERVICE_NAME_PREFIX", default="UNDEFINED")
+SERVICENAME = os.getenv("SERVICE_NAME", default="UNDEFINED")
+DICT_REQURIED_LABELS = {"namespace": NAMESPACE, "serviceName": SERVICENAME}
