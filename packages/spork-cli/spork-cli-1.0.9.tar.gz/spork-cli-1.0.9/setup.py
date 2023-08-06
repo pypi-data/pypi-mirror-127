d1c3c9bd302751511e66a642a165ecedf16c0d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:39 +0000

    Add package spork-cli/1.0.9/spork-cli-1.0.9.tar.gz

---
+++ packages/spork-cli/spork-cli-1.0.9.tar.gz/spork-cli-1.0.9/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+with open('README.md', 'r') as readme:
+    LONG_DESCRIPTION = readme.read()  # use readme as long description
+
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8']
+
+
+URL = 'https://github.com/brandontrabucco/spork'
+
+
+setup(name='spork-cli', version='1.0.9', license='MIT',
+      packages=find_packages(include=['spork', 'spork.*']),
+      description=('CLI For Launching '
+                   'Experiments Using Singularity On Slurm'),
+      long_description=LONG_DESCRIPTION, classifiers=CLASSIFIERS,
+      long_description_content_type='text/markdown',
+      author='Brandon Trabucco', author_email='brandon@btrabucco.com',
+      url=URL, download_url=URL + '/archive/v1_0_9.tar.gz',
+      keywords=['Deep Learning', 'Research', 'Management'],
+      install_requires=['click', 'paramiko', 'pexpect', 'spython'],
+      entry_points={'console_scripts': (
+          'spork=spork.experiment_config:command_line_interface',)})
