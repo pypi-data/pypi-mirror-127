c6fb9b097048fdf414e0f04c3ab1cea12de10d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:00 +0000

    Add package portfolio-simulator/0.1.2/portfolio_simulator-0.1.2-py3-none-any.whl

---
+++ packages/portfolio-simulator/portfolio_simulator-0.1.2-py3-none-any.whl/portfolio_simulator/real_estate.py
@@ -0,0 +1,39 @@
+import numpy_financial as npf
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from dataclasses import dataclass
+
+@dataclass
+class Mortgage:
+  principal: int
+  rate: float
+  start_date: str
+  years: int
+  linked_asset_value: int
+
+  @property
+  def per(self):
+    return np.arange(self.years*12) + 1
+
+  @property
+  def ipmt(self):
+    return npf.ipmt(self.rate/12, self.per, self.years*12, self.principal)
+
+  @property
+  def ppmt(self):
+    return npf.ppmt(self.rate/12, self.per, self.years*12, self.principal)
+
+  @property
+  def payment(self):
+    return self.ipmt + self. ppmt
+
+  @property
+  def months(self):
+    return pd.date_range(self.start_date, periods=self.years*20, freq='M')
+
+  def to_dataframe(self):
+    df = pd.DataFrame({'interests':self.ipmt, 'principal': self.ppmt, 'payment': self.payment}, index=months)
+    df['principal_left'] = self.principal + df.principal.cumsum()
+    return df
+
