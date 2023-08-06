08d0a99a541402a8dde0f78992d2763823b31dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:20 +0000

    Add package py-configuration-Orinnass/2.3.7/py-configuration-Orinnass-2.3.7.tar.gz

---
+++ packages/py-configuration-Orinnass/py-configuration-Orinnass-2.3.7.tar.gz/py-configuration-Orinnass-2.3.7/setup.py
@@ -0,0 +1,31 @@
+"""
+Пакет установки модуля
+"""
+from setuptools import setup
+from setuptools import find_packages
+from config import __version__, __name__
+
+
+def get_install_requires():
+    """
+    Метод получения зависимых модулей
+    """
+    with open('requirements.txt', 'r') as file_req:
+        lines_req = file_req.readlines()
+        for index, i in enumerate(lines_req):
+            if '-e' in i:
+                module_name = i.split('#egg=')[1].strip()
+                install_command = i.split('-e ')[1].strip()
+                lines_req[index] = f'{module_name} @ {install_command}\n'
+        return lines_req
+
+
+setup(
+    name=__name__,
+    version=__version__,
+    download_url="https://gitlab.com/Orinnass/python-module-configuration",
+    packages=find_packages(include=('config',), exclude=('test',)),
+    scripts=['config_cli.py'],
+    install_requires=get_install_requires(),
+    python_requires=">=3.10"
+)
