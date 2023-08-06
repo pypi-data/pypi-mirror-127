bb7e3d61747fea738cfcf6a61f92dc933da29746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:16 +0000

    Add package pwiener-pypi-sample/1.0.1/pwiener_pypi_sample-1.0.1.tar.gz

---
+++ packages/pwiener-pypi-sample/pwiener_pypi_sample-1.0.1.tar.gz/pwiener_pypi_sample-1.0.1/setup.py
@@ -0,0 +1,16 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="pwiener_pypi_sample",
+    version="1.0.1",
+    packages=["pypi_sample"],
+    long_description=README,
+)
