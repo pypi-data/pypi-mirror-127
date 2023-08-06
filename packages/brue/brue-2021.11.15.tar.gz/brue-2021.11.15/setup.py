e3a35d3c7fe97af25e282ec3fa3c5dfaf79cd528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:34 +0000

    Add package brue/2021.11.15/brue-2021.11.15.tar.gz

---
+++ packages/brue/brue-2021.11.15.tar.gz/brue-2021.11.15/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+__version__ = "2021.11.15"
+__author__ = "Lee Seung Hwan"
+__mail__ = "shlee0920@naver.com"
+
+with open("requirements.txt", "r", encoding = "utf-8-sig") as reqr:
+    requires = reqr.read().split("\n")
+
+with open("README.md", "r", encoding = "utf-8-sig") as rmr:
+    readme = rmr.read()
+
+
+setup(
+    author = __author__,
+    author_email = __mail__,
+    url = "https://github.com/eseunghwan/brue",
+    version = __version__,
+    python_requires = ">=3.7",
+    install_requires = requires,
+    setup_requires = requires,
+    entry_points = {
+        "console_scripts": [
+            "brue-cli=brue.__main__:run_cli"
+        ]
+    },
+    name = "brue",
+    license="MIT license",
+    description = "modern web gui using python",
+    long_description = readme,
+    long_description_content_type = "text/markdown",
+    packages = [ "brue", "brue/assets" ],
+    package_data = {
+        "": [
+            "*.zip", "*.js"
+        ]
+    },
+    include_package_data = True,
+    zip_fale = True
+)
