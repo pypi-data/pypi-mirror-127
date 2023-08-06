a988f9d240232ce7baf1ac2ebfc65e5af947d655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:15 +0000

    Add package bitcv/0.0.7/bitcv-0.0.7.tar.gz

---
+++ packages/bitcv/bitcv-0.0.7.tar.gz/bitcv-0.0.7/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="bitcv",
+    version="0.0.7",
+    author="xiaoye-hhh",
+    author_email="1318137184@qq.com",
+    description="A small package to cv",
+    packages=['bitcv']
+)
