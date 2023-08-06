31125adb0f651739978fd0cc54893a7765fdf58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:28 +0000

    Add package imagecaptioner/0.4/imagecaptioner-0.4.tar.gz

---
+++ packages/imagecaptioner/imagecaptioner-0.4.tar.gz/imagecaptioner-0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='imagecaptioner',
+    version=0.4,
+    url="https://github.com/greencashew/image-captioner",
+    author="Jan Górkiewicz (https://greencashew.dev/)",
+    license='MIT',
+    description='Command line python script for adding captions to the images based on the metadata, filename or user input',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    packages=find_packages(),
+    package_data={'imagecaptioner': ['fonts/*.ttf']},
+    install_requires=['Pillow', 'datefinder'],
+    entry_points={'console_scripts': ['imagecaptioner = imagecaptioner.cli:main']}
+)
