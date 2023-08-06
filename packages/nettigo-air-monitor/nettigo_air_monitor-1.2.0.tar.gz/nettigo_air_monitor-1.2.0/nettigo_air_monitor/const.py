89e8a71e03afee3a57fc5662d4a632c37d5ada24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:32 +0000

    Add package nettigo-air-monitor/1.2.0/nettigo_air_monitor-1.2.0.tar.gz

---
+++ packages/nettigo-air-monitor/nettigo_air_monitor-1.2.0.tar.gz/nettigo_air_monitor-1.2.0/nettigo_air_monitor/const.py
@@ -0,0 +1,35 @@
+"""Constants for nettigo-air-monitor library."""
+from __future__ import annotations
+
+from typing import Final
+
+ATTR_CONFIG: Final[str] = "config"
+ATTR_DATA: Final[str] = "data"
+ATTR_OTA: Final[str] = "ota"
+ATTR_RESTART: Final[str] = "restart"
+ATTR_SENSOR_VALUES: Final[str] = "sensordatavalues"
+ATTR_UPTIME: Final[str] = "uptime"
+ATTR_VALUES: Final[str] = "values"
+
+RETRIES: Final[int] = 4
+TIMEOUT: Final[int] = 5
+
+ENDPOINTS: Final[dict[str, str]] = {
+    ATTR_CONFIG: "http://{host}/config.json",
+    ATTR_DATA: "http://{host}/data.json",
+    ATTR_OTA: "http://{host}/ota",
+    ATTR_RESTART: "http://{host}/reset",
+    ATTR_VALUES: "http://{host}/values",
+}
+
+RENAME_KEY_MAP: Final[list[tuple[str, str]]] = [
+    ("bmp_pressure", "bmp180_pressure"),
+    ("bmp_temperature", "bmp180_temperature"),
+    ("conc_co2_ppm", "mhz14a_carbon_dioxide"),
+    ("humidity", "dht22_humidity"),
+    ("sds_p1", "sds011_p1"),
+    ("sds_p2", "sds011_p2"),
+    ("temperature", "dht22_temperature"),
+]
+
+MAC_PATTERN: Final[str] = r"([0-9a-fA-F]{2}[:]){5}([0-9a-fA-F]{2})"
