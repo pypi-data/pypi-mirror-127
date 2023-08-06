40f972b23e3c9778cd90ea5f4cc9524b195aaeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:29 +0000

    Add package pipen-filters/0.0.9/pipen_filters-0.0.9-py3-none-any.whl

---
+++ packages/pipen-filters/pipen_filters-0.0.9-py3-none-any.whl/pipen_filters/__init__.py
@@ -0,0 +1,26 @@
+"""Add a set of useful filters for pipen templates"""
+
+from typing import Any, Dict
+from pipen import plugin
+
+from .filters import FILTERS
+
+__version__ = "0.0.9"
+
+
+class PipenFilters:
+    __version__: str = __version__
+
+    @plugin.impl
+    def on_setup(config: Dict[str, Any]) -> None:  # type: ignore
+        """Add the filters"""
+        if "template_opts" not in config:
+            config.template_opts: Dict[str, Any] = {}
+
+        if "filters" not in config.template_opts:
+            config.template_opts.filters = {}
+        if "globals" not in config.template_opts:
+            config.template_opts.globals = {}
+
+        config.template_opts.filters.update(FILTERS)
+        config.template_opts.globals.update(FILTERS)
