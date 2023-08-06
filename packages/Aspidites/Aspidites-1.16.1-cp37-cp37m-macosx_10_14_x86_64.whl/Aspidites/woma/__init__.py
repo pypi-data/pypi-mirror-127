5a1d54cc146b9b084a32f53e3541e26616004983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:39 +0000

    Add package Aspidites/1.16.1/Aspidites-1.16.1-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/Aspidites/Aspidites-1.16.1-cp37-cp37m-macosx_10_14_x86_64.whl/Aspidites/woma/__init__.py
@@ -0,0 +1,36 @@
+"""This is the standard library bootstrapped with the compiler on a per-version basis.
+This should allow for some semblance of backwards compatibility.
+"""
+# Aspidites - Woma Programming Language Standard Library
+# Copyright (C) 2021 Ross J. Duff
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from .gcutils import get_all
+from .fileutils import mkdir_p, atomic_save, iglob, iter_find_files, copytree
+from .pickleutils import pickle_loads
+from .guiutils import *
+from .mathutils import clamp, ceil, floor
+from contextlib import contextmanager
+from functools import lru_cache
+
+try:
+    from library import *
+except ImportError:
+    pass
+
+__version__ = "1.16.1"
+
+
+def woma_version():
+    return __version__
