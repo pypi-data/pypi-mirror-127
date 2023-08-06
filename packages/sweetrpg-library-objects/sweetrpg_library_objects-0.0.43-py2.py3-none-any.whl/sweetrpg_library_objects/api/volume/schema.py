483082108dad35cef665e9684d04f538b7f8b7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:41 +0000

    Add package sweetrpg-library-objects/0.0.43/sweetrpg_library_objects-0.0.43-py2.py3-none-any.whl

---
+++ packages/sweetrpg-library-objects/sweetrpg_library_objects-0.0.43-py2.py3-none-any.whl/sweetrpg_library_objects/api/volume/schema.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""
+"""
+
+from marshmallow_jsonapi import fields
+from marshmallow_jsonapi.flask import Relationship
+from sweetrpg_api_core.schema.base import BaseAPISchema
+from sweetrpg_library_objects.model.volume import Volume
+from sweetrpg_model_core.schema.lang import LangString
+
+
+class VolumeAPISchema(BaseAPISchema):
+    model_class = Volume
+
+    class Meta:
+        type_ = "volume"
+        self_view = "volume_detail"
+        self_view_kwargs = {"id": "<id>"}
+        self_view_many = "volume_list"
+
+    name = LangString()  # , load_only=True)
+    slug = fields.Str()  # , load_only=True)
+    system = fields.Str()  # , load_only=True)
+    authors = Relationship(
+        self_view="volume_authors",
+        self_view_kwargs={"id": "<id>"},
+        related_view="author_list",
+        related_view_kwargs={"volume_id": "<id>"},
+        many=True,
+        schema="AuthorAPISchema",
+        type_="author",
+    )
+    properties = fields.List(fields.Dict(keys=fields.Str(), values=fields.Str()))
