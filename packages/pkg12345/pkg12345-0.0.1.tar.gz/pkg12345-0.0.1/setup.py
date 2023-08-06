2e3eaf9af9b42a7a09a21b90353abe92806a07df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:02 +0000

    Add package pkg12345/0.0.1/pkg12345-0.0.1.tar.gz

---
+++ packages/pkg12345/pkg12345-0.0.1.tar.gz/pkg12345-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="pkg12345",
+    version="0.0.1",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
