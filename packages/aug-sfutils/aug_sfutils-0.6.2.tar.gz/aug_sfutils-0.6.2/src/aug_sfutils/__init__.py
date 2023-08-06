8b527ff9164e64de995e6f3e2735d96ba55fa76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:29 +0000

    Add package aug-sfutils/0.6.2/aug_sfutils-0.6.2.tar.gz

---
+++ packages/aug-sfutils/aug_sfutils-0.6.2.tar.gz/aug_sfutils-0.6.2/src/aug_sfutils/__init__.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""Shotfile reading with pure python
+
+https://www.aug.ipp.mpg.de/aug/manuals/aug_sfutils
+
+"""
+__author__  = 'Giovanni Tardini (Tel. 1898)'
+__version__ = '0.6.2'
+__date__    = '17.11.2021'
+
+import os, sys, logging
+
+fmt = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s: %(message)s', '%H:%M:%S')
+hnd = logging.StreamHandler()
+hnd.setFormatter(fmt)
+logger = logging.getLogger('aug_sfutils')
+logger.addHandler(hnd)
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+
+logger.info('Using version %s', __version__)
+
+try: # wrapper classes, available only with afs-client and kerberos access
+    from .ww import *
+except:
+    logger.warning('ww not loaded')
+    pass
+try:
+    from .sfh import *
+except:
+    logger.warning('sfh not loaded')
+    pass
+try:
+    from .journal import *
+except:
+    logger.warning('journal not loaded')
+    pass
+
+from .sfread import *
+from .sf2equ import *
+from .libddc import ddcshotnr, previousshot
+from .mapeq import *
+from .getlastshot import getlastshot
+sf_home = os.path.dirname(os.path.realpath(__file__))
+logger.info('AUG-SF home %s', sf_home)
