6669110a75fa7ba5edc294441281f0427ee7bdb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:40 +0000

    Add package pyhanko-certvalidator/0.17.4/pyhanko_certvalidator-0.17.4-py3-none-any.whl

---
+++ packages/pyhanko-certvalidator/pyhanko_certvalidator-0.17.4-py3-none-any.whl/pyhanko_certvalidator/errors.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+from __future__ import unicode_literals, division, absolute_import, print_function
+
+
+class PathError(Exception):
+
+    pass
+
+
+class PathBuildingError(PathError):
+
+    pass
+
+
+class CertificateFetchError(PathBuildingError):
+    pass
+
+
+class DuplicateCertificateError(PathError):
+
+    pass
+
+
+class CRLValidationError(Exception):
+
+    pass
+
+
+class CRLNoMatchesError(CRLValidationError):
+
+    pass
+
+
+class CRLFetchError(CRLValidationError):
+    pass
+
+
+class CRLValidationIndeterminateError(CRLValidationError):
+
+    @property
+    def failures(self):
+        return self.args[1]
+
+
+class OCSPValidationError(Exception):
+
+    pass
+
+
+class OCSPNoMatchesError(OCSPValidationError):
+
+    pass
+
+
+class OCSPValidationIndeterminateError(OCSPValidationError):
+
+    @property
+    def failures(self):
+        return self.args[1]
+
+
+class OCSPFetchError(OCSPValidationError):
+    pass
+
+
+class SoftFailError(Exception):
+
+    pass
+
+
+class ValidationError(Exception):
+
+    pass
+
+
+class PathValidationError(ValidationError):
+
+    pass
+
+
+class RevokedError(PathValidationError):
+
+    pass
+
+
+class InvalidCertificateError(PathValidationError):
+
+    pass
