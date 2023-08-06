45a43efec9b686955aacf4d3d998bf9899299377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:34 +0000

    Add package swmr-tools/0.6.0/swmr_tools-0.6.0.tar.gz

---
+++ packages/swmr-tools/swmr_tools-0.6.0.tar.gz/swmr_tools-0.6.0/README.md
@@ -0,0 +1,23 @@
+# swmr_tools
+
+swmr_tools is a Python library developed by Diamond Light Source Ltd designed to facilitate reading and processing data from HDF5 swmr files as they are being written.
+
+## Installation
+
+swmr_tools can be installed using either conda [(from the conda-forge channel)](https://anaconda.org/conda-forge/swmr-tools):
+
+```bash
+conda install -c conda-forge swmr-tools
+```
+
+or the package manager [pip](https://pip.pypa.io/en/stable/):
+
+```bash
+pip install swmr_tools
+```
+
+## Usage
+
+- For full examples of use please see the full documentation at https://python-swmrtools.readthedocs.io
+
+- Jupyter notebook tutorials for key functionality can be found in the tutorial directory in the git repository
---
+++ packages/swmr-tools/swmr_tools-0.6.0.tar.gz/swmr_tools-0.6.0/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name="swmr_tools",
+    packages=["swmr_tools"],
+    version="0.6.0",
+    license="MIT",
+    description="Python iterator for safely monitoring NeXus files",
+    author="Diamond Light Source Ltd",
+    author_email="scientificsoftware@diamond.ac.uk",
+    url="https://github.com/DiamondLightSource/python-swmrtools",
+    keywords=["HDF5", "Iterator", "Diamond"],
+    install_requires=["numpy", "h5py"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
