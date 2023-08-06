819a8c5dbe989c5a3cc7cb44b1e6307d4431aebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:10 +0000

    Add package pyinstrument/4.1.1/pyinstrument-4.1.1.tar.gz

---
+++ packages/pyinstrument/pyinstrument-4.1.1.tar.gz/pyinstrument-4.1.1/setup.py
@@ -0,0 +1,42 @@
+import os
+
+from setuptools import Extension, find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="pyinstrument",
+    packages=find_packages(include=["pyinstrument", "pyinstrument.*"]),
+    version="4.1.1",
+    ext_modules=[
+        Extension(
+            "pyinstrument.low_level.stat_profile",
+            sources=["pyinstrument/low_level/stat_profile.c"],
+        )
+    ],
+    description="Call stack profiler for Python. Shows you why your code is slow!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Joe Rickerby",
+    author_email="joerick@mac.com",
+    url="https://github.com/joerick/pyinstrument",
+    keywords=["profiling", "profile", "profiler", "cpu", "time", "sampling"],
+    install_requires=[],
+    extras_require={"jupyter": ["ipython"]},
+    include_package_data=True,
+    python_requires=">=3.7",
+    entry_points={"console_scripts": ["pyinstrument = pyinstrument.__main__:main"]},
+    zip_safe=False,
+    classifiers=[
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Topic :: Software Development :: Debuggers",
+        "Topic :: Software Development :: Testing",
+    ],
+)
