d017465bb634e84c9defb8646effab2ed477b7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:10 +0000

    Add package blocksciencelabs/0.1.11/blocksciencelabs-0.1.11-py3-none-any.whl

---
+++ packages/blocksciencelabs/blocksciencelabs-0.1.11-py3-none-any.whl/blocksciencelabs/__init__.py
@@ -0,0 +1,24 @@
+import client
+import common
+
+import pandas as pd
+import functools, pickle, operator
+
+def fetch_results(simulation_id, conn):
+  select_jobs = f"SELECT id FROM job WHERE simulation_id = {simulation_id};"
+
+  jobs = pd.read_sql_query(select_jobs, conn)
+  
+  jobs_string = ','.join([str(i) for i in jobs['id'].to_list()])
+
+  select_job_results = f"SELECT * FROM job_result WHERE job_id IN ({jobs_string});"
+
+  job_results = pd.read_sql_query(select_job_results, conn)
+
+  serialized_results = job_results['data'] \
+    .transform(lambda x:  pickle.loads(bytes.fromhex(x))) \
+    .to_list()
+
+  results = pd.DataFrame(functools.reduce(operator.iconcat, serialized_results, []))
+
+  return results
