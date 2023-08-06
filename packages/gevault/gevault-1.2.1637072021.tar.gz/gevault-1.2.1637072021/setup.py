6727851ece16f0dd5bd1d1f2d5d17a72108bade5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:33 +0000

    Add package gevault/1.2.1637072021/gevault-1.2.1637072021.tar.gz

---
+++ packages/gevault/gevault-1.2.1637072021.tar.gz/gevault-1.2.1637072021/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Barry Howard",
+    author_email="barry.howard@ge.com",
+    description="Python Library for communicating with vault server.",
+    keywords=['vault', 'ge', 'cloudops'],
+    long_description="Python Library for communicating with GE vault server.",
+    name="gevault",
+    package_data={'': ['lib/tester_stack/*.yml']},
+    scripts=['./gevault/gevault'],
+    url="https://github.build.ge.com/Cloudpod/vault",
+    version="1.2.1637072021",
+    packages=find_packages(exclude=["build", "build/*"])
+)
