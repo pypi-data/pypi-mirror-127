f3db9ce05e2d58fb830ba571fdb65c27c9013e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:19 +0000

    Add package streamflow/0.0.34/streamflow-0.0.34-py2.py3-none-any.whl

---
+++ packages/streamflow/streamflow-0.0.34-py2.py3-none-any.whl/streamflow/report.py
@@ -0,0 +1,44 @@
+import argparse
+import os.path
+
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+
+from streamflow.persistence.persistence_manager import DefaultPersistenceManager
+from streamflow.persistence.sqlite import SqliteDatabase
+
+
+def _export_to_file(fig: go.Figure, args: argparse.Namespace, default_name: str) -> None:
+    if args.format == "html":
+        pio.write_html(fig, file=args.name or default_name + ".html")
+    elif args.format == "json":
+        pio.write_json(fig, file=args.name or default_name + ".json")
+    else:
+        pio.write_image(fig, format=args.format, file=args.name or "{}.{}".format(default_name, args.format))
+
+
+def create_report(args: argparse.Namespace):
+    # Retrieve data
+    database = SqliteDatabase(os.path.join(args.outdir, ".streamflow", "sqlite.db"), reset_db=False)
+    persistence_manager = DefaultPersistenceManager(db=database, output_dir=args.outdir)
+    df = persistence_manager.db.get_report()
+    # If output format is csv, print DataFrame and exit
+    if args.format == 'csv':
+        df.to_csv(args.name or "report.csv", index=False)
+        return
+    # Pre-process data
+    df["id"] = df["id"].map(str)
+    df["start_time"] = pd.to_datetime(df["start_time"])
+    df["end_time"] = pd.to_datetime(df["end_time"])
+    # Create chart
+    fig = px.timeline(
+        df,
+        x_start="start_time",
+        x_end="end_time",
+        y="name" if args.group_by_step else "id",
+        color="name")
+    fig.update_yaxes(visible=False)
+    # Export to file
+    _export_to_file(fig, args, "report")
