6c8da66ab8ad4c6c70c9a908d433d2c4f2984b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:22 +0000

    Add package smpl/0.0.101/smpl-0.0.101.tar.gz

---
+++ packages/smpl/smpl-0.0.101.tar.gz/smpl-0.0.101/smpl/plot/__init__.py
@@ -0,0 +1,7 @@
+"""
+Simplified plotting
+"""
+
+from .plot import data,fit,function,unv,usd,auto,plot_kwargs
+__all__ = ['data','fit','function','unv','usd','show','auto','plot_kwargs']
+from matplotlib.pyplot import *
