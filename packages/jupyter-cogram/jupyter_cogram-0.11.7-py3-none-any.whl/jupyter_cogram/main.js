faef9e4b352422612ccf2321c1cc849128b72bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:46 +0000

    Add package jupyter-cogram/0.11.7/jupyter_cogram-0.11.7-py3-none-any.whl

---
+++ packages/jupyter-cogram/jupyter_cogram-0.11.7-py3-none-any.whl/jupyter_cogram/main.js
@@ -0,0 +1,20 @@
+requirejs.config({
+    paths: {
+        /* Load cogram from local file. On fail, load from CDN. */
+        'cogram': [
+            '//localhost:8080/static/cogram_main',
+            '//storage.googleapis.com/cogram-public/jupyter-cogram/latest/cogram_main.min',
+        ],
+    },
+});
+
+
+define(['cogram'], function (cogram,) {
+    "use strict";
+    const logPrefix = '[cogram-sources-loader]';
+    console.log(logPrefix, "Loaded `cogram` module")
+    return {
+        load_jupyter_extension: cogram.load_jupyter_extension,
+        load_ipython_extension: cogram.load_jupyter_extension
+    };
+});
