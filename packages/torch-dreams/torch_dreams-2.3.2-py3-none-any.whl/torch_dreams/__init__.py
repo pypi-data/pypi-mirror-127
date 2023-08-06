21d7620e554fed634e6eae6e5d5521dc181c38d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:56 +0000

    Add package torch-dreams/2.3.2/torch_dreams-2.3.2-py3-none-any.whl

---
+++ packages/torch-dreams/torch_dreams-2.3.2-py3-none-any.whl/torch_dreams/__init__.py
@@ -0,0 +1,19 @@
+from .dreamer import *
+from .utils import *
+from .model_bunch import *
+from .tests import *
+from .auto_image_param import auto_image_param
+from .custom_image_param import custom_image_param
+
+__version__ = "2.3.2"
+
+__all__ = [
+    "dreamer",
+    "utils",
+    "model_bunch",
+    "auto_image_param",
+    "custom_image_param",
+    "masked_image_param",
+    "image_transforms",
+    "transforms"
+]
