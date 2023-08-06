151d194a4468e69d09dc0254627e7280b8e2e130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:38 +0000

    Add package minos-microservice-common/0.3.0/minos_microservice_common-0.3.0-py3-none-any.whl

---
+++ packages/minos-microservice-common/minos_microservice_common-0.3.0-py3-none-any.whl/minos/common/importlib.py
@@ -0,0 +1,42 @@
+import importlib
+from types import (
+    ModuleType,
+)
+from typing import (
+    Callable,
+)
+
+from .exceptions import (
+    MinosImportException,
+)
+
+
+# noinspection SpellCheckingInspection
+def import_module(module_name: str) -> Callable:
+    """Import the given module from a package"""
+    parts = module_name.rsplit(".", 1)
+
+    try:
+        kallable = importlib.import_module(parts[0])
+    except ImportError:
+        raise MinosImportException(f"Error importing {module_name!r}: the module does not exist")
+
+    if len(parts) > 1:
+        try:
+            kallable = getattr(kallable, parts[1])
+        except AttributeError:
+            raise MinosImportException(f"Error importing {module_name!r}: the qualname does not exist.")
+
+    return kallable
+
+
+def classname(cls: Callable) -> str:
+    """Compute the given class full name.
+
+    :param cls: Target class.
+    :return: An string object.
+    """
+    if isinstance(cls, ModuleType):
+        return cls.__name__
+    # noinspection PyUnresolvedReferences
+    return f"{cls.__module__}.{cls.__qualname__}"
