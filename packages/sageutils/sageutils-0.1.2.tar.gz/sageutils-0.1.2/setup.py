6f069ffe0d0103b093c50e162894e38212dd424a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:47 +0000

    Add package sageutils/0.1.2/sageutils-0.1.2.tar.gz

---
+++ packages/sageutils/sageutils-0.1.2.tar.gz/sageutils-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from src.sageutils import __version__
+from setuptools import find_packages, setup
+
+long_description = "Sage common utilities library"
+
+
+setup(
+    name="sageutils",
+    packages=find_packages(),
+    scripts=["scripts/passutils"],
+    version=__version__,
+    description="Sage common utilities library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tony Tong",
+    author_email="ttong@pro-ai.org",
+    license="MIT",
+    python_requires=">=3.8",
+    include_package_data=True,
+    install_requires=[
+        "cryptography",
+        "fire",
+    ],
+)
