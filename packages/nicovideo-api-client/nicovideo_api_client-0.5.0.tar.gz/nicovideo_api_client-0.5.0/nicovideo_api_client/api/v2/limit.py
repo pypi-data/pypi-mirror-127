6477d25e4c53790b96816d5b6829167b551f55fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:59 +0000

    Add package nicovideo-api-client/0.5.0/nicovideo_api_client-0.5.0.tar.gz

---
+++ packages/nicovideo-api-client/nicovideo_api_client-0.5.0.tar.gz/nicovideo_api_client-0.5.0/nicovideo_api_client/api/v2/limit.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+from nicovideo_api_client.api.v2.request import SnapshotSearchAPIV2Request
+
+
+class SnapshotSearchAPIV2Limit:
+    def __init__(self, query: Dict[str, str]):
+        self._query: Dict[str, str] = query
+
+    def limit(self, limit: int = 10) -> SnapshotSearchAPIV2Request:
+        """
+        レスポンスの要素数を指定する。
+
+        :param limit: レスポンスの要素数の上限。デフォルトは API 仕様に則り 10件。
+        本来の件数上限は 100件だが、分割リクエストを送るためそれ以上を指定できる。
+        :return: リクエストオブジェクト
+        """
+        return SnapshotSearchAPIV2Request(self._query, limit)
