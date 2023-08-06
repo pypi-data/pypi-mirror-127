5d20a26488098b802ca9d7d3529942800c0269bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:52 +0000

    Add package sym-flow-cli/1.2.3/sym-flow-cli-1.2.3.tar.gz

---
+++ packages/sym-flow-cli/sym-flow-cli-1.2.3.tar.gz/sym-flow-cli-1.2.3/sym/flow/cli/commands/services/services_list.py
@@ -0,0 +1,26 @@
+from typing import List
+
+import click
+from tabulate import tabulate
+
+from sym.flow.cli.helpers.api import SymAPI
+from sym.flow.cli.helpers.global_options import GlobalOptions
+from sym.flow.cli.models.service import Service
+
+
+@click.command(name="list", short_help="View all services")
+@click.make_pass_decorator(GlobalOptions, ensure=True)
+def services_list(options: GlobalOptions) -> None:
+    """View all Services currently set up for your organization."""
+    click.echo(get_services_data(options.api_url))
+
+
+def get_services_data(api_url: str) -> str:
+    api = SymAPI(url=api_url)
+    services: List[Service] = api.get_services()
+
+    table_data = [["Service Type", "External ID", "Label"]]
+    for s in services:
+        table_data.append([s.service_type, s.external_id, s.label])
+
+    return tabulate(table_data, headers="firstrow")
