b4de4a7793b800058c09f04ec9f5fc018fda4830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:05 +0000

    Add package sigstickers/2021.2.2/sigstickers-2021.2.2-py3-none-any.whl

---
+++ packages/sigstickers/sigstickers-2021.2.2-py3-none-any.whl/sigstickers/__init__.py
@@ -0,0 +1,37 @@
+"""Download sticker packs from Signal
+"""
+from __future__ import annotations
+
+import argparse
+import asyncio
+
+from .downloader import convertPack, downloadPack
+
+
+def cli():
+	"""cli entry point"""
+	parser = argparse.ArgumentParser("Welcome to SigSticker, providing all of your sticker needs")
+	parser.add_argument(
+		"-p",
+		"--pack",
+		help="Pass in a pack url inline",
+		nargs="+",
+		action="append",
+	)
+	args = parser.parse_args()
+	# Get the packs
+	packs = sum(args.pack, [])
+	if packs is None:
+		packs = []
+		while True:
+			name = input("Enter sticker_set url (leave blank to stop): ").strip()
+			if name == "":
+				break
+			packs.append(name)
+	for pack in packs:
+		packAttrs = pack.split("#pack_id=")[-1].split("&pack_key=")
+		asyncio.run(convertPack(*asyncio.run(downloadPack(*packAttrs))))
+
+
+if __name__ == "__main__":
+	cli()
