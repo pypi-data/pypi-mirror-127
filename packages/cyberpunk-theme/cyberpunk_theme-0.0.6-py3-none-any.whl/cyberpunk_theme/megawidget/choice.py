ac7ba657c34e550acc2ae24442d38f5d594fd773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:42 +0000

    Add package cyberpunk-theme/0.0.6/cyberpunk_theme-0.0.6-py3-none-any.whl

---
+++ packages/cyberpunk-theme/cyberpunk_theme-0.0.6-py3-none-any.whl/cyberpunk_theme/megawidget/choice.py
@@ -0,0 +1,50 @@
+import tkstyle
+from cyberpunk_theme.widget import radiobutton
+from cyberpunk_theme.widget import text
+from cyberpunk_theme.widget import label
+from cyberpunk_theme import constant
+
+
+def get_style():
+    return get_choice_style_1()
+
+
+def get_choice_style_1():
+    style = tkstyle.Toplevel()
+    style.foreground = "#C8C8C8"
+    style.background = constant.BACKGROUND_COLOR
+    style.font = constant.FONT_FAV_NORMAL
+    style.add(_get_radiobutton_style(), pattern="*Radiobutton")
+    style.add(_get_text_style(), pattern="*Text")
+    style.add(_get_header_style(), pattern="*label_header")
+    return style
+
+# ========================================
+#                PRIVATE
+# ========================================
+
+
+# radiobuttons
+def _get_radiobutton_style():
+    style = radiobutton.get_style()
+    style.foreground = "#C8C8C8"
+    style.background = constant.BACKGROUND_COLOR
+    style.font = constant.FONT_FAV_NORMAL
+    style.activeBackground = constant.BACKGROUND_COLOR
+    style.activeForeground = "#C8C8C8"
+    style.selectColor = constant.BACKGROUND_COLOR
+    style.relief = "flat"
+    style.highlightThickness = 0
+    return style
+
+# header
+def _get_header_style():
+    style = label.get_style()
+    style.font = constant.FONT_SIZE
+    return style
+
+# text
+def _get_text_style():
+    style = text.get_style()
+    style.highlightThickness = 0
+    return style
