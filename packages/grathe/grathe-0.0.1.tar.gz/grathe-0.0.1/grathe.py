56b127d1352a2c73384446bfa05dd0b5c48ffe87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:46 +0000

    Add package grathe/0.0.1/grathe-0.0.1.tar.gz

---
+++ packages/grathe/grathe-0.0.1.tar.gz/grathe-0.0.1/grathe.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='grathe',
+    version='0.0.1',
+    description='A GRAph THEory library.',
+    long_description='',
+    url='https://github.com/AlessioZanga/grathe',
+    author='Alessio Zanga',
+    author_email='alessio.zanga@outlook.it',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
