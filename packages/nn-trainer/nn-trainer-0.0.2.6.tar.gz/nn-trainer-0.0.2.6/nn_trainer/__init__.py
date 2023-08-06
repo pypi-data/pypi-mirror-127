c0575cb00f8508b44ba1a7c5e4f98864431ebf3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:12 +0000

    Add package nn-trainer/0.0.2.6/nn-trainer-0.0.2.6.tar.gz

---
+++ packages/nn-trainer/nn-trainer-0.0.2.6.tar.gz/nn-trainer-0.0.2.6/nn_trainer/__init__.py
@@ -0,0 +1,11 @@
+from .data import *
+from .models import *
+from .plotters import *
+from .trainers import *
+
+from .metrics import *
+from .callbacks import *
+
+from .utils import parse_arguments, make_path
+
+VERSION = "0.0.2.6"
