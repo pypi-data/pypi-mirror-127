b07d96dca8a891949e791bab360566ab280b1e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:46 +0000

    Add package pyaemet/1.0.2/pyaemet-1.0.2-py3-none-any.whl

---
+++ packages/pyaemet/pyaemet-1.0.2-py3-none-any.whl/pyaemet/apikey_file.py
@@ -0,0 +1,11 @@
+"""
+    AEMET API key saven in a file as module to import when needed but not
+    upload into GitHub repository
+"""
+
+
+apikey = ("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqYWltZWR1c2ViYWxsZXpAZ21haWwuY29tIi"
+          + "wianRpIjoiOWVlZDEwMzMtZDRjMS00OWZiLThiYzAtZDc1Njk2MDhiY2JiIiwiaXN"
+          + "zIjoiQUVNRVQiLCJpYXQiOjE2MTIxODI3NzgsInVzZXJJZCI6IjllZWQxMDMzLWQ0"
+          + "YzEtNDlmYi04YmMwLWQ3NTY5NjA4YmNiYiIsInJvbGUiOiIifQ.Iidp4Sxk5khlZ8"
+          + "mhd_JMyvdqJhZ37JyZjXX_wbO0mAM")
